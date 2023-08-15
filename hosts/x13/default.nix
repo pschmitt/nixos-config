800c49f650174259c9f59dc7b8378eeafa568407
Philipp Schmitt <philipp@schmitt.co>
2023-08-15 22:42:04 +0200

    Enable a few hardware modules

--- hosts/x13/default.nix
+++ hosts/x13/default.nix
@@ -1,8 +1,9 @@
 { pkgs, inputs, ... }: {
   imports = [
-    # inputs.hardware.nixosModules.common-cpu-amd
+    inputs.hardware.nixosModules.common-cpu-amd
     # inputs.hardware.nixosModules.common-gpu-amd
-    # inputs.hardware.nixosModules.common-pc-ssd
+    inputs.hardware.nixosModules.common-pc-ssd
+    inputs.hardware.nixosModules.common-pc-laptop-acpi_call
 
     ./hardware-configuration.nix
 
