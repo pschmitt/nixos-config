b45f7767957a4a19131e52d2b9c268b77194496c
Philipp Schmitt <philipp@schmitt.co>
2023-08-15 22:53:16 +0200

    Make laptop config more generic

--- common/laptop/default.nix
+++ common/laptop/default.nix
@@ -117,30 +117,17 @@ in
     tmp = { useTmpfs = true; };
   };
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
   # Enable networking
   networking = {
-    hostName = "x13"; # Define your hostname.
-    # wireless.enable = true;  # Enables wireless support via wpa_supplicant.
     networkmanager = {
       enable = true;
       dns = "systemd-resolved";
-    };
-
-    # Disable the firewall altogether.
-    firewall = {
-      enable = false;
-      # allowedTCPPorts = [ ... ];
-      # allowedUDPPorts = [ ... ];
     };
   };
 
   # FIXME Disable wait-online services, this somehow results in NM not being started at all.
   # systemd.network.wait-online.enable = false;
-  systemd.services.NetworkManager-wait-online.enable = false;
+  # systemd.services.NetworkManager-wait-online.enable = false;
 
   services.resolved = {
     enable = true;
