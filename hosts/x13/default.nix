a9b3582a257d723e4cfbd798a98affa68414c9f8
Philipp Schmitt <philipp@schmitt.co>
2023-08-15 22:53:16 +0200

    Make laptop config more generic

--- hosts/x13/default.nix
+++ hosts/x13/default.nix
@@ -10,4 +10,21 @@
     # ../../common/global
     ../../common/laptop
   ];
+
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking = {
+    hostName = "x13"; # Define your hostname.
+    # wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+    # Disable the firewall altogether.
+    firewall = {
+      enable = false;
+      # allowedTCPPorts = [ ... ];
+      # allowedUDPPorts = [ ... ];
+    };
+  };
 }
