7d78ec5dbd01d3ad32c76dd06730916b1d04c210
Philipp Schmitt <philipp@schmitt.co>
2023-08-21 22:21:20 +0200

    Fix typo

--- common/laptop/default.nix
+++ common/laptop/default.nix
@@ -50,8 +50,7 @@ in
 
     # This will additionally add your inputs to the system's legacy channels
     # Making legacy nix commands consistent as well, awesome!
-    nixPath = lib.mapAttrsToList (key: value: "${key}=${value.to.path}") config
-.nix.registry;
+    nixPath = lib.mapAttrsToList (key: value: "${key}=${value.to.path}") config.nix.registry;
 
     gc = {
       automatic = true;
