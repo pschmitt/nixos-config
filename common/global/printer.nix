f00bfee567aea02383a92e656f4da269ac8bd47b
Philipp Schmitt <philipp@schmitt.co>
2023-10-03 23:17:10 +0200

    Declarative printer definition

--- common/global/printer.nix
+++ common/global/printer.nix
@@ -1,6 +1,27 @@
 { inputs, outputs, lib, config, pkgs, ... }:
 
 {
-  services.printing.enable = true;
-  services.printing.drivers = with pkgs; [ cups-brother-hll2340dw ];
+  services.printing = {
+    enable = true;
+    drivers = with pkgs; [ cups-brother-hll2340dw ];
+  };
+
+  hardware.printers = {
+    ensureDefaultPrinter = "bro";
+    ensurePrinters = [{
+      name = "bro";
+      description = "Brother HL-L2340DW";
+      deviceUri = "ipp://printer.lan:631";
+      location = "home";
+      model = "brother-HLL2340D-cups-en.ppd";
+      ppdOptions = {
+        PageSize = "A4";
+        MediaType = "PLAIN";
+        InputSlot = "TRAY1";
+        Duplex = "None";
+        Resolution = "600dpi";
+        TonerSaveMode = "OFF";
+      };
+    }];
+  };
 }
