42cc4791f9e7f89534f668be035238c72545fd93
Philipp Schmitt <philipp@schmitt.co>
2023-10-31 18:38:10 +0100

    Use custom hacompanion package

--- common/laptop/hacompanion.nix
+++ common/laptop/hacompanion.nix
@@ -1,15 +1,24 @@
 { inputs, lib, config, pkgs, ... }: {
+  environment.systemPackages = with pkgs; [ hacompanion ];
+
   systemd.services.hacompanion = {
     enable = true;
     description = "Home Assistant Companion application";
     documentation = [ "https://github.com/tobias-kuendig/hacompanion" ];
     after = [ "NetworkManager-wait-online.service" ];
-    path = with pkgs; [ zsh inputs.hyprland.packages.${pkgs.system}.hyprland ];
+    path = with pkgs; [
+      "/home/pschmitt"
+      "/run/current-system/sw"
+      # alsa-utils
+      # inputs.hyprland.packages.${pkgs.system}.hyprland
+      # lm_sensors
+      # zsh
+    ];
 
     serviceConfig = {
       User = "pschmitt";
       EnvironmentFile = "/home/pschmitt/.config/hacompanion/secrets";
-      ExecStart = "/home/pschmitt/.local/share/zinit/polaris/bin/hacompanion -config ~/.config/hacompanion/hacompanion.toml";
+      ExecStart = "${pkgs.hacompanion}/bin/hacompanion -config ~/.config/hacompanion/hacompanion.toml";
       Restart = "always";
       RestartSec = 5;
     };
