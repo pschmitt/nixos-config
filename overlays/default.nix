cce2c43d95f161041ac989203ef0dbb569f5eab0
Philipp Schmitt <philipp@schmitt.co>
2023-11-06 23:08:08 +0100

    WIP: Add python-designate (to openstack-cli)

--- overlays/default.nix
+++ overlays/default.nix
@@ -8,9 +8,47 @@
   # You can change versions, add patches, set compilation flags, anything really.
   # https://nixos.wiki/wiki/Overlays
   modifications = final: prev: {
-    # example = prev.example.overrideAttrs (oldAttrs: rec {
-    # ...
-    # });
+    openstackclientpp = prev.openstackclient.overrideAttrs (oldAttrs: rec {
+      propagatedBuildInputs = oldAttrs.propagatedBuildInputs ++ [ final.python3Packages.designateclient ];
+    });
+
+    python3Packages = prev.python3Packages // {
+      designateclient = prev.python3Packages.buildPythonPackage rec {
+        pname = "python-designateclient";
+        version = "5.3.0";
+
+        src = prev.fetchPypi {
+          inherit pname version;
+          sha256 = "sha256-7nroQeq/8cw4ncRYI4c2btV0o1PEaxapb7QRJT1GmEQ=";
+        };
+
+        propagatedBuildInputs = with prev.python3Packages; [
+          pbr
+          pip
+          debtcollector
+          requests
+          stevedore
+          keystoneauth1
+          oslo-utils
+          oslo-serialization
+          osc-lib
+          jsonschema
+          tzdata
+          tempest
+          pkgs.reno
+          requests-mock
+          oslotest
+          coverage
+          hacking
+        ];
+
+        meta = {
+          description = "Python client library for OpenStack Designate DNS service.";
+          license = prev.lib.licenses.asl20;
+          maintainers = [ prev.maintainers.pschmitt ];
+        };
+      };
+    };
   };
 
   # When applied, the unstable nixpkgs set (declared in the flake inputs) will
@@ -27,3 +65,4 @@
     };
   };
 }
+
