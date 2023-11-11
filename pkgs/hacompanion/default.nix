87670f2a070290c7d0b7bd4cf0da1ff189e37119
Philipp Schmitt <philipp@schmitt.co>
2023-11-11 23:33:30 +0100

    Update meta

--- pkgs/hacompanion/default.nix
+++ pkgs/hacompanion/default.nix
@@ -1,4 +1,7 @@
-{ lib, buildGoModule, fetchFromGitHub }:
+{ lib
+, buildGoModule
+, fetchFromGitHub
+}:
 
 buildGoModule rec {
   pname = "hacompanion";
@@ -6,21 +9,24 @@ buildGoModule rec {
 
   src = fetchFromGitHub {
     owner = "tobias-kuendig";
-    repo = pname;
+    repo = "hacompanion";
     rev = "v${version}";
-    sha256 = "sha256-UAJNvN/FN7DNWFdv45zUmn6WpSpnhnau/rMeo2LytaQ=";
+    hash = "sha256-UAJNvN/FN7DNWFdv45zUmn6WpSpnhnau/rMeo2LytaQ=";
   };
 
-  vendorSha256 = "sha256-ZZ8nxN+zUeFhSXyoHLMgzeFllnIkKdoVnbVK5KjrLEQ=";
+  vendorHash = "sha256-ZZ8nxN+zUeFhSXyoHLMgzeFllnIkKdoVnbVK5KjrLEQ=";
 
-  # If the Go application has any build flags or environment variables, set them here.
-  # buildFlagsArray = [ "-ldflags=-X main.Version=${version}" ];
+  ldflags = [
+    "-s"
+    "-w"
+    "-X=main.Version=${version}"
+  ];
 
   meta = with lib; {
     description = "Daemon that sends local hardware information to Home Assistant";
     homepage = "https://github.com/tobias-kuendig/hacompanion";
     license = licenses.mit;
-    maintainers = [ maintainers.pschmitt ];
-    platforms = platforms.unix;
+    maintainers = with maintainers; [ pschmitt ];
+    mainProgram = "hacompanion";
   };
 }
