1f9becb2345419218ad79ed4bad8803efc1dd82d
Philipp Schmitt <philipp@schmitt.co>
2023-11-11 09:33:11 +0100

    Spin out theme

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -5,6 +5,7 @@
     ./firefox.nix
     ./nvim.nix
     ./obs-studio.nix
+    ./theme.nix
     ./work.nix
   ];
 
@@ -93,24 +94,6 @@
 
   gtk = {
     enable = true;
-    theme = {
-      name = "Adwaita-dark";
-      package = pkgs.gnome.gnome-themes-extra;
-    };
-    iconTheme = {
-      name = "Colloid-nord-dark";
-      # Colloid-nord-dark is not technically part of colloid-icon-theme
-      # package = pkgs.colloid-icon-theme;
-    };
-    font = {
-      name = "Noto Sans 10";
-      package = pkgs.noto-fonts;
-    };
-    cursorTheme = {
-      name = "Bibata-Modern-Ice";
-      package = pkgs.bibata-cursors;
-    };
-
     # https://hoverbear.org/blog/declarative-gnome-configuration-in-nixos/
     gtk3 = {
       extraConfig = { gtk-application-prefer-dark-theme = 1; };
@@ -127,20 +110,5 @@
         "file://${config.home.homeDirectory}/Videos"
       ];
     };
-
-    gtk4 = { extraConfig = { gtk-application-prefer-dark-theme = 1; }; };
-  };
-
-  qt = {
-    enable = true;
-    platformTheme = "gnome";
-    style = {
-      name = "adwaita-dark";
-      package = pkgs.adwaita-qt6;
-    };
-  };
-
-  dconf.settings = {
-    "org/gnome/desktop/interface" = { color-scheme = "prefer-dark"; };
   };
 }
