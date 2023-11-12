c83552f6e4f5d0df507c7a33125e39d0340a643d
Philipp Schmitt <philipp@schmitt.co>
2023-11-12 16:18:11 +0100

    Add bitwarden sync service

---
+++ home-manager/bitwarden.nix
@@ -0,0 +1,40 @@
+{ inputs, lib, config, nur, pkgs, ... }:
+{
+  home.packages = with pkgs; [
+    bitwarden-cli
+  ];
+
+  systemd.user.services.bitwarden-cli-sync = {
+    Unit = {
+      Description = "Bitwarden CLI Sync (bww)";
+    };
+
+    Service = {
+      Type = "oneshot";
+      Environment = "PATH=$PATH:${lib.makeBinPath [
+        pkgs.zsh
+        pkgs.bitwarden-cli
+      ] }";
+      ExecStart = "/home/pschmitt/bin/zhj 'bwp sync; gec::bwp sync'";
+    };
+
+    Install = {
+      WantedBy = [ "default.target" ];
+    };
+  };
+
+  systemd.user.timers.bitwarden-cli-sync = {
+    Unit = {
+      Description = "Bitwarden CLI Sync (bww)";
+    };
+
+    Timer = {
+      OnCalendar = "*-*-* 00/2:00:00";
+      Persistent = true;
+    };
+
+    Install = {
+      WantedBy = [ "timers.target" ];
+    };
+  };
+}
