06ca1ef471eeb5bdcec079f0680b96a0e4343d01
Philipp Schmitt <philipp@schmitt.co>
2023-11-13 07:50:51 +0100

    WIP: Comic Code with age encryption
    Former-commit-id: f2bf42cc7a1967d137bc3e8af44365221c5cbcb7

--- modules/custom.nix
+++ modules/custom.nix
@@ -13,5 +13,11 @@
       default = "/home/pschmitt";
       description = "Main user's home directory.";
     };
+
+    custom.sshKey = lib.mkOption {
+      type = lib.types.str;
+      default = "/home/pschmitt/.ssh/id_ed25519";
+      description = "Main SSH key (used for age decryption)";
+    };
   };
 }
