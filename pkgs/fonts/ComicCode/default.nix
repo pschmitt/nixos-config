ba7803c805bba4c5ee45c214d434dcda433c3787
Philipp Schmitt <philipp@schmitt.co>
2023-11-16 19:34:55 +0100

    Separate package for proprietary fonts
    Former-commit-id: b62570a60efccc8f2eda0463c511de27eea9ec78

---
+++ pkgs/fonts/ComicCode/default.nix
@@ -0,0 +1,56 @@
+{ lib, pkgs, stdenvNoCC, fetchurl, font-resizer }:
+
+stdenvNoCC.mkDerivation rec {
+  pname = "ComicCode";
+  version = "478c9f6";
+
+  # FIXME Why is fetchzip refusing to download this?
+  # src = fetchurl {
+  #   url = "file:///etc/nixos/pkgs/proprietary-fonts/ComicCode/ILT-220422-478c9f6.zip";
+  #   sha256 = "sha256-VS5kTzKd4Mi/kO68jEoLvvzv7AoFXs1eAN9XPJWAKSs=";
+  # };
+  src = ./ILT-220422-478c9f6.zip;
+
+  nativeBuildInputs = with pkgs; [
+    nerd-font-patcher
+    font-resizer
+    unzip
+  ];
+
+  phases = [ "buildPhase" ];
+
+  buildPhase = ''
+    mkdir -p $out/share/fonts/opentype extracted
+
+    unzip -j $src '*.otf' -d extracted
+
+    for f in extracted/*
+    do
+      # Copy original font
+      cp "$f" $out/share/fonts/opentype
+
+      # Nerdify font
+      nerd-font-patcher \
+        --complete \
+        --no-progressbars \
+        --outputdir "$out/share/fonts/opentype" \
+        "$f" || true  # /r/SomeOfYouMayDie
+    done
+
+    # Resize Nerdfied fonts
+    for f in "$out/share/fonts/opentype"/*Nerd*.otf
+    do
+      font-resizer --scale 0.8 "$f"
+    done
+
+    # Remove original NerdFonts
+    find "$out" -type f \( -name '*Nerd*' -and -not -name '*-resized*' \) -exec rm -f {} \;
+  '';
+
+  meta = with lib; {
+    homepage = "https://tosche.net/fonts/comic-code";
+    description = "Comic Code is a monospaced adaptation of the most infamous yet most popular casual font";
+    license = licenses.unfree;
+    platforms = platforms.all;
+  };
+}
---
+++ pkgs/fonts/MonoLisa-Custom/default.nix
@@ -0,0 +1,42 @@
+{ lib
+, pkgs
+, stdenvNoCC
+, fetchzip
+, python3Packages
+}:
+
+stdenvNoCC.mkDerivation rec {
+  pname = "MonoLisa-Custom";
+  version = "1.808";
+
+  src = ./MonoLisa-Plus-Custom-1.808.zip;
+
+  nativeBuildInputs = with pkgs; [
+    nerd-font-patcher
+    unzip
+  ];
+
+  phases = [ "buildPhase" ];
+
+  buildPhase = ''
+    mkdir -p $out/share/fonts/truetype extracted
+    unzip -j $src -d extracted
+
+    for f in extracted/*
+    do
+      # Copy original font
+      cp $f $out/share/fonts/truetype
+
+      # Nerdify font
+      nerd-font-patcher $f --complete --no-progressbars \
+        --outputdir $out/share/fonts/truetype
+    done
+  '';
+
+  meta = with lib; {
+    homepage = "https://www.monolisa.dev";
+    description = "font follows function";
+    license = licenses.unfree;
+    platforms = platforms.all;
+  };
+}
