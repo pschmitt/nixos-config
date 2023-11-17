9b52c83c175e6884180ad230f52624738525591f
Philipp Schmitt <philipp@schmitt.co>
2023-11-17 21:53:48 +0100

    Add separate packages for NF fonts

--- pkgs/fonts/MonoLisa-CustomNF/default.nix
+++ pkgs/fonts/MonoLisa-CustomNF/default.nix
@@ -5,20 +5,28 @@
 }:
 
 stdenvNoCC.mkDerivation rec {
-  pname = "MonoLisa-Custom";
+  pname = "MonoLisa-CustomNF";
   version = "1.808";
 
   src = ../src/MonoLisa-Plus-Custom-1.808.zip;
 
   nativeBuildInputs = with pkgs; [
+    nerd-font-patcher
     unzip
   ];
 
   phases = [ "buildPhase" ];
 
   buildPhase = ''
-    mkdir -p $out/share/fonts/truetype
-    unzip -j $src -d $out/share/fonts/truetype
+    mkdir -p $out/share/fonts/truetype extracted
+    unzip -j $src -d extracted
+
+    for f in extracted/*
+    do
+      # Nerdify font
+      nerd-font-patcher $f --complete --no-progressbars \
+        --outputdir $out/share/fonts/truetype
+    done
   '';
 
   meta = with lib; {
