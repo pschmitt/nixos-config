83e01cdbfb11c86918de52c3593f1f1d28b25cba
Philipp Schmitt <philipp@schmitt.co>
2023-11-28 16:08:53 +0100

    DRY

--- common/sshfs/default.nix
+++ common/sshfs/default.nix
@@ -1,4 +1,20 @@
-{ inputs, outputs, lib, config, pkgs, ... }: {
+{ inputs, outputs, lib, config, pkgs, ... }:
+let
+  opts = [
+      "noauto"
+      "_netdev"
+      "allow_other"
+      "reconnect"
+      "follow_symlinks"
+      "x-systemd.automount"
+      # "x-gvfs-hide"
+      "IdentityFile=${config.custom.homeDirectory}/.ssh/id_ed25519"
+      "StrictHostKeyChecking=no"
+      "UserKnownHostsFile=/dev/null"
+      "ServerAliveInterval=10"
+    ];
+
+in {
   # NOTE Arch Linux equivalent /etc/fstab entry:
   # root@hass-fnuc.lan:/config  /mnt/hass-fnuc   fuse.sshfs    defaults,_netdev,noauto,x-systemd.automount,allow_other,reconnect,UserKnownHostsFile=/dev/null,StrictHostKeyChecking=no,IdentityFile=/home/pschmitt/.ssh/id_ed25519    0  0
 
@@ -8,54 +24,18 @@
   fileSystems."/mnt/hass" = {
     fsType = "fuse";
     device = "${pkgs.sshfs-fuse}/bin/sshfs#root@hass-fnuc.schmitt.co.beta.tailscale.net:/config";
-    options = [
-      "noauto"
-      "_netdev"
-      "allow_other"
-      "reconnect"
-      "follow_symlinks"
-      "x-systemd.automount"
-      # "x-gvfs-hide"
-      "IdentityFile=/home/pschmitt/.ssh/id_ed25519"
-      "StrictHostKeyChecking=no"
-      "UserKnownHostsFile=/dev/null"
-      "ServerAliveInterval=10"
-    ];
+    options = opts;
   };
 
   fileSystems."/mnt/turris" = {
     fsType = "fuse";
     device = "${pkgs.sshfs-fuse}/bin/sshfs#root@turris.schmitt.co.beta.tailscale.net:/";
-    options = [
-      "noauto"
-      "_netdev"
-      "allow_other"
-      "reconnect"
-      "follow_symlinks"
-      "x-systemd.automount"
-      # "x-gvfs-hide"
-      "IdentityFile=/home/pschmitt/.ssh/id_ed25519"
-      "StrictHostKeyChecking=no"
-      "UserKnownHostsFile=/dev/null"
-      "ServerAliveInterval=10"
-    ];
+    options = opts;
   };
 
   fileSystems."/mnt/rofl" = {
     fsType = "fuse";
     device = "${pkgs.sshfs-fuse}/bin/sshfs#ubuntu@rofl-01.schmitt.co.beta.tailscale.net:/mnt/data";
-    options = [
-      "noauto"
-      "_netdev"
-      "allow_other"
-      "reconnect"
-      "follow_symlinks"
-      "x-systemd.automount"
-      # "x-gvfs-hide"
-      "IdentityFile=/home/pschmitt/.ssh/id_ed25519"
-      "StrictHostKeyChecking=no"
-      "UserKnownHostsFile=/dev/null"
-      "ServerAliveInterval=10"
-    ];
+    options = opts;
   };
 }
