a00a9ba1522ac321f1dd5a015dad117660e5964a
Philipp Schmitt <philipp@schmitt.co>
2023-11-29 12:07:42 +0100

    Add terminfo

--- pkgs/wezterm-bin/default.nix
+++ pkgs/wezterm-bin/default.nix
@@ -13,6 +13,7 @@
 , libiconv
 , libxcb
 , libxkbcommon
+, ncurses
 , openssl
 , perl
 , pkg-config
@@ -25,7 +26,6 @@
 , xcbutilkeysyms
 , xcbutilwm # contains xcb-ewmh among others
 , zlib
-
 }:
 
 let
@@ -56,31 +56,41 @@ stdenv.mkDerivation rec {
   pname = "wezterm-bin";
   version = "nightly-20231129";
 
+  outputs = [ "out" "terminfo" ];
+
   src = fetchurl {
     url = "https://github.com/wez/wezterm/releases/download/nightly/wezterm-nightly.Ubuntu22.04.tar.xz";
     sha256 = "sha256-WP9iMQl0vELaSX92+F5sPiUmlfoy/IqlklIJSXok3bU=";
   };
 
-  terminfo = fetchurl {
+  terminfoSrc = fetchurl {
     url = "https://raw.githubusercontent.com/wez/wezterm/90ca1117bc68e3644b1763460e17cf4b6ffbf1c3/termwiz/data/wezterm.terminfo";
     sha256 = "sha256-P+mUyBjCvblCtqOmNZlc2bqUU32tMNWpYO9g25KAgNs=";
   };
 
-  nativeBuildInputs = [ pkg-config python3 perl openssl ];
+  nativeBuildInputs = [ pkg-config python3 perl openssl ncurses ];
 
   # prevent further changes to the RPATH
   dontPatchELF = true;
 
   installPhase = ''
-    mkdir -p $out/share/terminfo/w
+    runHook preInstall
+    mkdir -p "$out"
     cp -a ./usr/* $out/
-    cp -a $terminfo $out/share/terminfo/w/wezterm
+
+    mkdir -p $terminfo/share/terminfo/w
+    tic -xe wezterm -o "$terminfo/share/terminfo" "$terminfoSrc"
+
+    mkdir -p "$out/nix-support"
+    echo "$terminfo" >> "$out/nix-support/propagated-user-env-packages"
+
+    runHook postInstall
   '';
 
   postFixup = ''
-    for artifact in wezterm wezterm-gui wezterm-mux-server strip-ansi-escapes; do
-     patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" "$out/bin/$artifact" || true
-     patchelf --set-rpath "${rpath}" $out/bin/$artifact
+    for bin in wezterm wezterm-gui wezterm-mux-server strip-ansi-escapes; do
+      patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" "$out/bin/$bin" || true
+      patchelf --set-rpath "${rpath}" "$out/bin/$bin"
     done
   '';
 
@@ -88,7 +98,7 @@ stdenv.mkDerivation rec {
     description = "Wezterm terminal (pre-compiled binary version)";
     homepage = "https://github.com/wez/wezterm";
     license = licenses.gpl3Plus;
-    maintainers = [ maintainers.pinpox ];
+    maintainers = [ maintainers.pschmitt ];
     platforms = platforms.all;
   };
 }
