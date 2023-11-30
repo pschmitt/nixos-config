75d0c81982a03da890fdb7a5e8f2db1fa85f00fe
Philipp Schmitt <philipp@schmitt.co>
2023-11-30 22:23:15 +0100

    Move apps that are not stricktly hyprland related out

---
+++ common/gui/apps.nix
@@ -0,0 +1,34 @@
+{ inputs, config, pkgs, ... }:
+
+{
+  environment.systemPackages = with pkgs; [
+    alacritty
+    foot
+    kitty
+    # wezterm  # og package, from upstream nixpkgs
+    # wezterm-bin
+    wezterm-nightly
+
+    # files and docs
+    gnome.eog
+    gnome.evince
+    gnome.gnome-font-viewer
+    gnome.file-roller
+    gnome.nautilus
+    gnome.seahorse
+    gnome.sushi
+
+    # wayland
+    lemonade
+    nwg-displays
+    remmina
+    wayvnc
+    wdisplays
+    wlr-randr
+    wlrctl
+    wtype
+    ydotool
+  ];
+}
+
+# vim: set ft=nix et ts=2 sw=2 :
