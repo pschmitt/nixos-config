81640ab1f46338f2028d6a00b44d0b2f10441bab
Philipp Schmitt <philipp@schmitt.co>
2023-12-03 18:06:30 +0100

    Write a custom zshrc with all the nix-specific stuff

---
+++ home-manager/zsh.nix
@@ -0,0 +1,18 @@
+{ inputs, pkgs, ... }:
+{
+  home.file = {
+    ".config/zsh/custom/os/nixos/system.zsh" = {
+      text = ''
+        # command-not-found integration
+        source ${inputs.nix-index-database.packages.${pkgs.system}.nix-index-with-db}/etc/profile.d/command-not-found.sh
+
+        # DEPRECATED: Use wezterm.sh instead
+        # source ${pkgs.vte}/etc/profile.d/vte.sh
+
+        # FIXME the osc7 shell func produces output which p10k complains about
+        # on startup (hence the WEZTERM_SHELL_SKIP_CWD)
+        WEZTERM_SHELL_SKIP_CWD=1 source ${pkgs.wezterm-nightly}/etc/profile.d/wezterm.sh
+      '';
+    };
+  };
+}
