32cf977642a66cdabc2aed4faeaf3fd5df690a95
Philipp Schmitt <philipp@schmitt.co>
2023-12-07 15:34:45 +0100

    Add host keys to recipients list

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,18 +1,31 @@
 let
-  authorizedKeys = builtins.readFile (builtins.fetchurl {
+  lib = import <nixpkgs/lib>;
+
+  authorizedKeysContent = lib.strings.fileContents(builtins.fetchurl {
     url = "https://github.com/pschmitt.keys";
     sha256 = "0z65rk873yc2bl6fk5y2czin260s5f5cqnzcw51bxyvshdwvp0kg";
   });
 
+  authorizedKeys = lib.filter (key: key != "") (lib.splitString "\n" authorizedKeysContent);
+
+  hostKeys = [
+    # ge2
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHxUgJAJlondg55FtQ1ez73QEiS3MR7u40K2+2SsJVQe"
+    # x13
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAII/PReiXvGknx3dHACrDg0ApbI7X57JhHaSAbHbKPfff"
+  ];
+
+  recipients = authorizedKeys ++ hostKeys;
+
 in
 {
-  "secret.age".publicKeys = [ authorizedKeys ];
+  "secret.age".publicKeys = recipients;
 
-  "ge2/restic-repository.age".publicKeys = [ authorizedKeys ];
-  "ge2/restic-password.age".publicKeys = [ authorizedKeys ];
-  "ge2/restic-env.age".publicKeys = [ authorizedKeys ];
+  "ge2/restic-repository.age".publicKeys = recipients;
+  "ge2/restic-password.age".publicKeys = recipients;
+  "ge2/restic-env.age".publicKeys = recipients;
 
-  "x13/restic-password.age".publicKeys = [ authorizedKeys ];
-  "x13/restic-repository.age".publicKeys = [ authorizedKeys ];
-  "x13/restic-env.age".publicKeys = [ authorizedKeys ];
+  "x13/restic-password.age".publicKeys = recipients;
+  "x13/restic-repository.age".publicKeys = recipients;
+  "x13/restic-env.age".publicKeys = recipients;
 }
