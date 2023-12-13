a5d2e6817149e877f556de0ef27b671929b53b22
Philipp Schmitt <philipp@schmitt.co>
2023-12-13 17:28:40 +0100

    Only source command-not-found and osc7 integration when running in an interactive session

--- home-manager/zsh.nix
+++ home-manager/zsh.nix
@@ -3,15 +3,18 @@
   home.file = {
     ".config/zsh/custom/os/nixos/system.zsh" = {
       text = ''
-        # command-not-found integration
-        source ${inputs.nix-index-database.packages.${pkgs.system}.nix-index-with-db}/etc/profile.d/command-not-found.sh
+        if [[ -o interactive && -o login ]]
+        then
+          # command-not-found integration
+          source ${inputs.nix-index-database.packages.${pkgs.system}.nix-index-with-db}/etc/profile.d/command-not-found.sh
 
-        # DEPRECATED: Use wezterm.sh instead
-        # source ${pkgs.vte}/etc/profile.d/vte.sh
+          # DEPRECATED: Use wezterm.sh instead
+          # source ${pkgs.vte}/etc/profile.d/vte.sh
 
-        # FIXME the osc7 shell func produces output which p10k complains about
-        # on startup (hence the WEZTERM_SHELL_SKIP_CWD)
-        WEZTERM_SHELL_SKIP_CWD=1 source ${pkgs.wezterm-nightly}/etc/profile.d/wezterm.sh
+          # FIXME the osc7 shell func produces output which p10k complains about
+          # on startup (hence the WEZTERM_SHELL_SKIP_CWD)
+          WEZTERM_SHELL_SKIP_CWD=1 source ${pkgs.wezterm-nightly}/etc/profile.d/wezterm.sh
+        fi
       '';
     };
   };
