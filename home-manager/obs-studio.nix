01bf22987ce143b8d94e89286efb85a8bf3f34ce
Philipp Schmitt <philipp@schmitt.co>
2023-12-14 16:24:45 +0100

    Add obs nvidia wrapper

--- home-manager/obs-studio.nix
+++ home-manager/obs-studio.nix
@@ -1,7 +1,25 @@
-{ inputs, lib, config, pkgs, ... }:
+{ inputs, lib, osConfig, config, pkgs, ... }:
+
+let
+  enableNvidiaOffload = osConfig.hardware.nvidia.prime.offload.enable;
+  obs-nvidia = pkgs.writeShellScriptBin "obs-nvidia" ''
+    nvidia-offload ${pkgs.obs-studio}/bin/obs "$@"
+  '';
+  obs-nvidia-custom = pkgs.writeShellScriptBin "obs-nvidia-custom" ''
+    ${obs-nvidia}/bin/obs-nvidia \
+        --minimize-to-tray \
+        --startvirtualcam \
+        --scene "Joining soon" \
+        "$@"
+  '';
+in
 
 {
-  home.packages = with pkgs; [ obs-cli ];
+  home.packages = [
+    pkgs.obs-cli
+  ]
+  ++ lib.optional enableNvidiaOffload obs-nvidia
+  ++ lib.optional enableNvidiaOffload obs-nvidia-custom;
 
   programs.obs-studio = {
     enable = true;
