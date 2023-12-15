d4165ad6fa9f3b9651963f8c797576deb7347cce
Philipp Schmitt <philipp@schmitt.co>
2023-12-15 17:10:48 +0100

    DRY

--- modules/custom.nix
+++ modules/custom.nix
@@ -19,5 +19,18 @@
       default = "/home/pschmitt/.ssh/id_ed25519";
       description = "Main SSH key (used for age decryption)";
     };
+
+    custom.authorizedKeys = lib.mkOption {
+      type = lib.types.listOf lib.types.str;
+      default =
+        let
+          authorizedKeysContent = lib.strings.fileContents (builtins.fetchurl {
+            url = "https://github.com/pschmitt.keys";
+            sha256 = "082ck5qhyswbinif0b0rb0n26i6m5rkvx6plhdsili3dyx5l7dqc";
+          });
+        in
+        lib.filter (key: key != "") (lib.splitString "\n" authorizedKeysContent);
+      description = "Main SSH authorized keys file";
+    };
   };
 }
