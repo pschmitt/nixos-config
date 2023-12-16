3b1bb43a8bde043b88dc877a5530bc34ae994906
Philipp Schmitt <philipp@schmitt.co>
2023-12-16 16:34:38 +0100

    Update custom udev callback for bt headsets

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -19,6 +19,7 @@ in
   obs-studio-plugins.obs-freeze-filter = pkgs.qt6Packages.callPackage ./obs-studio-plugins.obs-freeze-filter { };
   obs-studio-plugins.obs-replay-source = pkgs.qt6Packages.callPackage ./obs-studio-plugins.obs-replay-source { inherit libcaption; };
   timewarrior-jirapush = pkgs.callPackage ./timewarrior-jirapush { };
+  udev-custom-callback = pkgs.callPackage ./udev-custom-callback { };
   wezterm-bin = pkgs.callPackage ./wezterm/wezterm-bin { };
   wezterm-nightly = pkgs.callPackage ./wezterm/wezterm-nightly { };
   wezterm-nightly-appimage = pkgs.callPackage ./wezterm/wezterm-nightly-appimage { };
