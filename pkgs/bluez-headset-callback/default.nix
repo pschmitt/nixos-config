b6afba8a0c6c4fe5ca414672eca749ca425eb14d
Philipp Schmitt <philipp@schmitt.co>
2023-12-18 17:55:31 +0100

    bt headsets automation: switch from udev to bluez' dbus api

---
+++ pkgs/bluez-headset-callback/default.nix
@@ -0,0 +1,42 @@
+{ lib
+, makeWrapper
+, stdenvNoCC
+, bash
+, gnused
+, jq
+, libnotify
+, systemd
+, zsh
+}:
+
+stdenvNoCC.mkDerivation {
+  pname = "bluez-headset-callback";
+  version = "0.5";
+
+  src = ./bluez-headset-callback.sh;
+
+  phases = [ "installPhase" ];
+
+  nativeBuildInputs = [ makeWrapper ];
+
+  installPhase = ''
+    install -Dm 755 $src $out/bin/bluez-headset-callback.sh
+
+    wrapProgram $out/bin/bluez-headset-callback.sh \
+      --prefix PATH : "/run/wrappers/bin:${lib.makeBinPath [
+        bash
+        gnused
+        jq
+        libnotify
+        systemd
+        zsh
+      ]}"
+  '';
+
+  meta = with lib; {
+    description = "custom bluez dbus listener for headset setup";
+    license = licenses.gpl3Only;
+    platforms = platforms.linux;
+     mainProgram = "bluez-headset-callback.sh";
+  };
+}
