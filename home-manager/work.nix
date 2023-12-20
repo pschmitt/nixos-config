a2c64af00030c3e0c74c8801110c28477ff86d01
Philipp Schmitt <philipp@schmitt.co>
2023-12-20 10:24:13 +0100

    Rename tf bin

--- home-manager/work.nix
+++ home-manager/work.nix
@@ -26,6 +26,11 @@
     stern
     taskwarrior
     # terraform # 1.6+
+    (terraform.overrideAttrs (oldAttrs: {
+      postInstall = oldAttrs.postInstall + ''
+        mv $out/bin/terraform $out/bin/terraform-unfree
+      '';
+    }))
     pkgs.terraform-157.terraform
     terragrunt
     opentofu
