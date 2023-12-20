c7b5e5076d8fdd7db19b521aac81fd66f18cca88
Philipp Schmitt <philipp@schmitt.co>
2023-12-20 22:24:33 +0100

    Add myself as TU

--- common/global/default.nix
+++ common/global/default.nix
@@ -62,11 +62,13 @@ in
     # Making legacy nix commands consistent as well, awesome!
     nixPath = lib.mapAttrsToList (key: value: "${key}=${value.to.path}") config.nix.registry;
 
+
     settings = {
       # Enable flakes and new 'nix' command
       experimental-features = "nix-command flakes";
       # Deduplicate and optimize nix store
       auto-optimise-store = true;
+      trusted-users = [ "root" config.custom.username ];
     };
   };
 
