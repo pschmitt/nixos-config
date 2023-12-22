7c5c8b58e816bd00f2a8cb614e6bd30b3708e962
Philipp Schmitt <philipp@schmitt.co>
2023-12-22 17:00:30 +0100

    Add all them substituters

--- common/global/nix.nix
+++ common/global/nix.nix
@@ -15,12 +15,19 @@
       # Deduplicate and optimize nix store
       auto-optimise-store = true;
 
-      # allowed-users = [ config.custom.username ];
-      trusted-users = [ "root" config.custom.username ];
+      trusted-users = [ "root" "@wheel" ];
 
-      substituters = [ "https://hyprland.cachix.org" ];
+      substituters = [
+        "https://cache.nixos.org"
+        "https://hyprland.cachix.org"
+        "https://nix-community.cachix.org"
+        "https://pschmitt-nixos-config.cachix.org"
+      ];
       trusted-public-keys = [
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "pschmitt-nixos-config.cachix.org-1:cE7rK+O+QCIEewMOOk3C7JYOhSXxqgLzNpm+tdSMkMk="
       ];
     };
 
