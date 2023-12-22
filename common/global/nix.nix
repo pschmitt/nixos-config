4c270fe038da80d4e3e6c30ed6b09d92116ca2dd
Philipp Schmitt <philipp@schmitt.co>
2023-12-22 15:22:03 +0100

    Move nix settings to separate file

---
+++ common/global/nix.nix
@@ -0,0 +1,84 @@
+{ config, lib, outputs, pkgs, ... }:
+{
+  nix = {
+    # This will add each flake input as a registry
+    # To make nix3 commands consistent with your flake
+    registry = lib.mapAttrs (_: value: { flake = value; }) inputs;
+
+    # This will additionally add your inputs to the system's legacy channels
+    # Making legacy nix commands consistent as well, awesome!
+    nixPath = lib.mapAttrsToList (key: value: "${key}=${value.to.path}") config.nix.registry;
+
+    settings = {
+      # Enable flakes and new 'nix' command
+      experimental-features = "nix-command flakes";
+      # Deduplicate and optimize nix store
+      auto-optimise-store = true;
+      trusted-users = [ "root" config.custom.username ];
+
+      substituters = [ "https://hyprland.cachix.org" ];
+      trusted-public-keys = [
+        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+      ];
+    };
+
+    # Do not attempt to murder the laptop when running nixos rebuild
+    daemonCPUSchedPolicy = "idle";
+    daemonIOSchedClass = "idle";
+  };
+
+  nixpkgs = {
+    # You can add overlays here
+    overlays = [
+      # Add overlays your own flake exports (from overlays and pkgs dir):
+      outputs.overlays.additions
+      outputs.overlays.modifications
+      outputs.overlays.unstable-packages
+      outputs.overlays.old-packages
+
+      # You can also add overlays exported from other flakes:
+      # neovim-nightly-overlay.overlays.default
+
+      # Or define it inline, for example:
+      # (final: prev: {
+      #   hi = final.hello.overrideAttrs (oldAttrs: {
+      #     patches = [ ./change-hello-to-hi.patch ];
+      #   });
+      # })
+    ];
+    # Configure your nixpkgs instance
+    config = {
+      # Disable if you don't want unfree packages
+      allowUnfree = true;
+    };
+  };
+
+  programs.nix-ld = {
+    enable = true;
+    # libraries = [];
+  };
+
+  # Do not use /tmp (50% RAM tmpfs) for builds
+  systemd.services.nix-daemon.environment.TMPDIR = "/nix/tmp";
+  # Create /nix/tmp and clean it up every 48 hours (2 days)
+  systemd.tmpfiles.rules = [
+    "d /nix/tmp 0755 root root 2d"
+  ];
+
+  programs.command-not-found.enable = false;
+
+  environment.systemPackages = with pkgs; [
+    # nix
+    inputs.agenix.packages.${system}.default
+    nix-prefetch-git
+  ];
+
+  # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
+  system = {
+    stateVersion = "23.11";
+    # autoUpgrade = {
+    #   enable = true;
+    #   channel = "https://nixos.org/channels/nixos-23.05";
+    # };
+  };
+}
