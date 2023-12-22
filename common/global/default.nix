9e5397f03c6d72e04e758cff1432c2af27a65d60
Philipp Schmitt <philipp@schmitt.co>
2023-12-22 15:22:03 +0100

    Move nix settings to separate file

--- common/global/default.nix
+++ common/global/default.nix
@@ -24,53 +24,9 @@ in
 {
   imports = [
     ./appimage.nix
+    ./nix.nix
     ./printer.nix
   ];
-
-  nixpkgs = {
-    # You can add overlays here
-    overlays = [
-      # Add overlays your own flake exports (from overlays and pkgs dir):
-      outputs.overlays.additions
-      outputs.overlays.modifications
-      outputs.overlays.unstable-packages
-      outputs.overlays.old-packages
-
-      # You can also add overlays exported from other flakes:
-      # neovim-nightly-overlay.overlays.default
-
-      # Or define it inline, for example:
-      # (final: prev: {
-      #   hi = final.hello.overrideAttrs (oldAttrs: {
-      #     patches = [ ./change-hello-to-hi.patch ];
-      #   });
-      # })
-    ];
-    # Configure your nixpkgs instance
-    config = {
-      # Disable if you don't want unfree packages
-      allowUnfree = true;
-    };
-  };
-
-  nix = {
-    # This will add each flake input as a registry
-    # To make nix3 commands consistent with your flake
-    registry = lib.mapAttrs (_: value: { flake = value; }) inputs;
-
-    # This will additionally add your inputs to the system's legacy channels
-    # Making legacy nix commands consistent as well, awesome!
-    nixPath = lib.mapAttrsToList (key: value: "${key}=${value.to.path}") config.nix.registry;
-
-
-    settings = {
-      # Enable flakes and new 'nix' command
-      experimental-features = "nix-command flakes";
-      # Deduplicate and optimize nix store
-      auto-optimise-store = true;
-      trusted-users = [ "root" config.custom.username ];
-    };
-  };
 
   boot = {
     # Enable all MagicSysRq keys
@@ -135,10 +91,6 @@ in
   };
 
   environment.systemPackages = with pkgs; [
-    # nix
-    inputs.agenix.packages.${system}.default
-    nix-prefetch-git
-
     # core
     acpi
     autossh
@@ -286,25 +238,6 @@ in
     };
   };
 
-  programs.nix-ld = {
-    enable = true;
-    # libraries = [];
-  };
-
-  # Do not attempt to murder the laptop when running nixos rebuild
-  nix = {
-    daemonCPUSchedPolicy = "idle";
-    daemonIOSchedClass = "idle";
-  };
-  # Do not use /tmp (50% RAM tmpfs) for builds
-  systemd.services.nix-daemon.environment.TMPDIR = "/nix/tmp";
-  # Create /nix/tmp and clean it up every 48 hours (2 days)
-  systemd.tmpfiles.rules = [
-    "d /nix/tmp 0755 root root 2d"
-  ];
-
-  programs.command-not-found.enable = false;
-
   # started in user sessions.
   # programs.mtr.enable = true;
   programs.gnupg.agent = {
@@ -399,15 +332,6 @@ in
       owner = "root";
       group = "root";
     };
-  };
-
-  # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
-  system = {
-    stateVersion = "23.11";
-    # autoUpgrade = {
-    #   enable = true;
-    #   channel = "https://nixos.org/channels/nixos-23.05";
-    # };
   };
 
   # List services that you want to enable:
