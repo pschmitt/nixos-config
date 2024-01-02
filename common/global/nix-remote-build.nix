886652f51be6bbb6f681803ce5eae7b7a276038b
Philipp Schmitt <philipp@schmitt.co>
2024-01-02 12:00:31 +0100

    Add hostkey

--- common/global/nix-remote-build.nix
+++ common/global/nix-remote-build.nix
@@ -18,6 +18,7 @@ in
         hostName = "rofl-01";
         sshUser = "ubuntu";
         sshKey = config.age.secrets.ssh-privkey.path;
+        publicHostKey = "c3NoLWVkMjU1MTkgQUFBQUMzTnphQzFsWkRJMU5URTVBQUFBSUwrcTVrZ1o5TVRmYnpvbnE1MGZMTmRrV3UxZmlyVlJKaU5iSzhBUDJpekggcm9vdEByb2ZsaW5zdGFuY2UtMDEK";
         systems = [ "x86_64-linux" ];
         maxJobs = 4;
         speedFactor = 2;
