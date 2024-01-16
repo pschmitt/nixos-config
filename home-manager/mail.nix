5cda9e95e6d911450039bdf945a17acf36899ca1
Philipp Schmitt <philipp@schmitt.co>
2024-01-16 08:16:34 +0100

    Add gmailctl

--- home-manager/mail.nix
+++ home-manager/mail.nix
@@ -1,10 +1,16 @@
 { pkgs, ... }: {
   home.packages = [
     pkgs.aerc
+    pkgs.gmailctl
     pkgs.neomutt
   ];
 
   home.file.".config/neomutt/nix" = {
     source = "${pkgs.neomutt}/share/neomutt";
+  };
+
+  # Create cache dir
+  home.file.".cache/neomutt/bodies/.keep" = {
+    source = builtins.toFile "keep" "";
   };
 }
