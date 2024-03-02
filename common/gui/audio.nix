f04ba74d58e4eadf02319a579514ac870f9c5ae3
Philipp Schmitt <philipp@schmitt.co>
2024-03-02 14:09:42 +0100

    Split out audio config file

---
+++ common/gui/audio.nix
@@ -0,0 +1,18 @@
+{ ... }: {
+  imports = [
+    ./soundboard.nix
+  ];
+
+  # Enable sound with pipewire.
+  sound.enable = true;
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    jack.enable = true;
+  };
+}
--- common/gui/default.nix
+++ common/gui/default.nix
@@ -2,6 +2,7 @@
   imports = [
     # ./hardware-configuration.nix
     ./apps.nix
+    ./audio.nix
     ./bluetooth.nix
     ./btrfs.nix
     ./gec-vpn.nix
@@ -9,7 +10,6 @@
     ./hyprland.nix
     ./libvirt.nix
     ./logitech-mouse.nix
-    ./soundboard.nix
     ./theme.nix
   ];
 
@@ -52,24 +52,6 @@
   };
   services.gvfs.enable = true;
   services.tumbler.enable = true;
-
-  # Enable sound with pipewire.
-  sound.enable = true;
-  hardware.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
-  };
 
   # Enable lingering
   systemd.tmpfiles.rules = [
