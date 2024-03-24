303c4d8bcd351612a26bb72c3f891b4e838da355
Philipp Schmitt <philipp@schmitt.co>
2024-03-24 19:01:05 +0100

    Add garnix cache

--- common/global/nix.nix
+++ common/global/nix.nix
@@ -45,6 +45,7 @@
         "https://hyprland.cachix.org"
         "https://nix-community.cachix.org"
         "https://pschmitt-nixos-config.cachix.org"
+        "https://cache.garnix.io"
         "https://nix.rofl-01.heimat.dev/pschmitt"
         "https://nix-cache.heimat.dev"
       ];
@@ -59,6 +60,7 @@
         "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "pschmitt-nixos-config.cachix.org-1:cE7rK+O+QCIEewMOOk3C7JYOhSXxqgLzNpm+tdSMkMk="
+        "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
         "pschmitt:kKBIIQAJtCX5e0hfYWds4S+3wQAMPtX4PUEkpW2qqOs="
         "nix-cache.heimat.dev:k/zdgSv+6lcJ/9DRILjA7H18eIlFSA0OwzyqqXEwySM="
       ];
