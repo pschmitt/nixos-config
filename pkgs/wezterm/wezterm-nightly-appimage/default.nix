568c1e979c9ec7fad21971c57f27971afa90975b
Philipp Schmitt <philipp@schmitt.co>
2024-03-26 18:13:58 +0100

    Update checksum

--- pkgs/wezterm/wezterm-nightly-appimage/default.nix
+++ pkgs/wezterm/wezterm-nightly-appimage/default.nix
@@ -6,7 +6,7 @@ appimageTools.wrapType2 {
 
   src = fetchurl {
     url = "https://github.com/wez/wezterm/releases/download/nightly/WezTerm-nightly-Ubuntu20.04.AppImage";
-    hash = "sha256-GwOkvgONFm6aild8Qd2y/6Fdn2v+lgUAnCHt1JQTu2I=";
+    hash = "sha256-XcDaa6NKbYpBQDtJCB6N3j9JBGVwvEMVYhlYktqJYZU=";
 
   };
 
