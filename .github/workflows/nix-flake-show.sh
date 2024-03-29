c6976f70946deb6a5563981bd0c8c2ee9c8e607c
Philipp Schmitt <philipp@schmitt.co>
2024-03-29 17:49:09 +0100

    run nix flake show once

--- .github/workflows/nix-flake-show.sh
+++ .github/workflows/nix-flake-show.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-mapfile -t PKGS < <(nix flake show --json | \
-  jq -er '.packages["x86_64-linux"] | keys[]')
+NIX_FLAKE_SHOW=$(nix flake show --json)
+mapfile -t PKGS < <(jq -er '.packages["x86_64-linux"] | keys[]' <<< "$NIX_FLAKE_SHOW")
 
 PKGS_FREE=()
 PKGS_NONFREE=()
@@ -19,7 +19,7 @@ done
 JSON_PKGS=$(printf '%s\n' "${PKGS[@]}" | jq -Rcn '[inputs]')
 JSON_PKGS_FREE=$(printf '%s\n' "${PKGS_FREE[@]}" | jq -Rcn '[inputs]')
 JSON_PKGS_NONFREE=$(printf '%s\n' "${PKGS_NONFREE[@]}" | jq -Rcn '[inputs]')
-JSON_NIXOS_CONFIGS=$(nix flake show --json | jq -c '.nixosConfigurations | keys')
+JSON_NIXOS_CONFIGS=$(jq -c '.nixosConfigurations | keys' <<< "$NIX_FLAKE_SHOW")
 
 jq -cn \
   --argjson hosts "$JSON_NIXOS_CONFIGS" \
