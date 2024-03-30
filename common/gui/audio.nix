54eb0d6a8cf3e54962a5b9f44e9bb58c78a047b6
Philipp Schmitt <philipp@schmitt.co>
2024-03-30 08:15:15 +0100

    Remove sound.enable and disable jack

--- common/gui/audio.nix
+++ common/gui/audio.nix
@@ -4,16 +4,18 @@
   ];
 
   # Enable sound with pipewire.
-  sound.enable = true;
+  # https://nixos.wiki/wiki/PipeWire
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
 
   services.pipewire = {
     enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
+    alsa = {
+      enable = true;
+      support32Bit = true;
+    };
     pulse.enable = true;
-    jack.enable = true;
+    # jack.enable = true;
   };
 
   environment.systemPackages = with pkgs; [
