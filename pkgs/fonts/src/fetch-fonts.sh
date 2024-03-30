a4ad48863153054c63c4cf4e32607edd5a14d075
Philipp Schmitt <philipp@schmitt.co>
2024-03-30 17:48:49 +0100

    Squashed commit of the following:
    commit fee12c37a0dc7412d89d42a6ef113e22736b0c28
    Author: Philipp Schmitt <philipp@schmitt.co>
    Date:   Sat Mar 30 17:47:12 2024 +0100
    
        Default to hostname binary
    
    commit 55899cdb88d71bc9710c58db6a9599787216a9af
    Author: Philipp Schmitt <philipp@schmitt.co>
    Date:   Sat Mar 30 17:46:51 2024 +0100
    
        Update comments
    
    commit 08ec762fff2fc9973e0d237112b45f60174e1053
    Author: Philipp Schmitt <philipp@schmitt.co>
    Date:   Sat Mar 30 17:42:51 2024 +0100
    
        git add --intent-to-add --force .
    
    commit a3c8d604fae42bf4346b5b48e55e6516fe154655
    Author: Philipp Schmitt <philipp@schmitt.co>
    Date:   Sat Mar 30 17:42:02 2024 +0100
    
        git add --intent-to-add .
    
    commit ab6924bf058fdd92f91f5af8ad0a8e38356bec7c
    Author: Philipp Schmitt <philipp@schmitt.co>
    Date:   Sat Mar 30 17:31:11 2024 +0100
    
        Explicitly allow building unfree packages
    
    commit bd2aba273a931baf85670a335299bf7a8d65e2e0
    Author: Philipp Schmitt <philipp@schmitt.co>
    Date:   Sat Mar 30 17:28:00 2024 +0100
    
        Set ssh username
    
    commit 20476cb50156910206371f1629352b5d2c6d2bca
    Author: Philipp Schmitt <philipp@schmitt.co>
    Date:   Sat Mar 30 17:22:53 2024 +0100
    
        Set remote-host
    
    commit 1b2da95a11717fa127e57f5c0a2ce6a781f9f53a
    Author: Philipp Schmitt <philipp@schmitt.co>
    Date:   Sat Mar 30 17:19:12 2024 +0100
    
        Switch to webfactory/ssh-agent
    
    commit 5331a267fcef93d90d328fe671bff2b8e4697e9f
    Author: Philipp Schmitt <philipp@schmitt.co>
    Date:   Sat Mar 30 17:15:12 2024 +0100
    
        Cant use ${{ env.xxx }} in env:
    
        https://docs.github.com/en/actions/learn-github-actions/contexts\#context-availability
    
    commit b27a734271a8738002de37910d00c0d87a4a3f6d
    Author: Philipp Schmitt <philipp@schmitt.co>
    Date:   Sat Mar 30 17:11:14 2024 +0100
    
        split steps
    
    commit c654bebb8b979367b30be98da67cba26ef0690a1
    Author: Philipp Schmitt <philipp@schmitt.co>
    Date:   Sat Mar 30 17:01:37 2024 +0100
    
        Add params
    
    commit b0e97736660e7788fd0ec169724488043748916b
    Author: Philipp Schmitt <philipp@schmitt.co>
    Date:   Sat Mar 30 16:56:58 2024 +0100
    
        Fix syntax
    
    commit 35dca6d05104e2305270958463f3e7c993acb5e6
    Author: Philipp Schmitt <philipp@schmitt.co>
    Date:   Sat Mar 30 16:54:24 2024 +0100
    
        Remote builds using vanilla nix

--- pkgs/fonts/src/fetch-fonts.sh
+++ pkgs/fonts/src/fetch-fonts.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
-REMOTE_HOST=rofl-01
-REMOTE_PATH=/srv/nextcloud/data/nextcloud/pschmitt/files/Fonts
+usage() {
+  echo "Usage: $0 [--host HOSTNAME] [--remote-path PATH]"
+}
 
 list_fonts() {
   awk '{ print $2 }' ./sha256sum.txt
@@ -16,11 +17,11 @@ fetch_fonts() {
 
   for font in $(list_fonts)
   do
-    if [[ "$HOSTNAME" == "$REMOTE_HOST" ]]
+    if [[ "${HOSTNAME:-$(hostname)}" == "$REMOTE_HOST" ]]
     then
       cp "${REMOTE_PATH}/${font}" "$font"
     else
-      scp "${REMOTE_HOST}:${REMOTE_PATH}/${font}" "$font"
+      scp "${REMOTE_USER}@${REMOTE_HOST}:${REMOTE_PATH}/${font}" "$font"
     fi
   done
 
@@ -29,6 +30,35 @@ fetch_fonts() {
 
 if [[ "${BASH_SOURCE[0]}" == "${0}" ]]
 then
+  REMOTE_USER=${REMOTE_USER:-ubuntu}
+  REMOTE_HOST=${REMOTE_HOST:-rofl-01}
+  REMOTE_PATH=${REMOTE_PATH:-/srv/nextcloud/data/nextcloud/pschmitt/files/Fonts}
+
+  while [[ -n "$*" ]]
+  do
+    case "$1" in
+      -h|--help)
+        usage
+        exit 0
+        ;;
+      --user*|--remote-user*|-u)
+        REMOTE_USER="$2"
+        shift 2
+        ;;
+      --host|--hostname|--remote-host*|-H)
+        REMOTE_HOST="$2"
+        shift 2
+        ;;
+      --remote-path|-p)
+        REMOTE_PATH="$2"
+        shift 2
+        ;;
+      *)
+        break
+        ;;
+    esac
+  done
+
   cd "$(cd "$(dirname "$0")" >/dev/null 2>&1; pwd -P)" || exit 9
 
   if check_fonts --quiet --status 2>/dev/null
