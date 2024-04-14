c81a05e995f408a00b5b806d4629807551bb74ea
Philipp Schmitt <philipp@schmitt.co>
2024-04-14 20:37:31 +0200

    Add nixos-anywhere tf config

---
+++ hosts/nixos-optimist/default.nix
@@ -0,0 +1,26 @@
+{ lib, pkgs, inputs, ... }: {
+  imports = [
+    # inputs.hardware.nixosModules.common-cpu-amd
+    # inputs.hardware.nixosModules.common-gpu-amd
+    # inputs.hardware.nixosModules.common-pc-ssd
+
+    ./disk-config.nix
+    ./hardware-configuration.nix
+    ../../common/global
+  ];
+
+  xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+  xdg.portal.config.common.default = "*";
+
+  # Enable networking
+  networking = {
+    hostName = "nixos-optimist"; # Define your hostname.
+    # wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+    # Disable the firewall altogether.
+    firewall = {
+      enable = false;
+      # allowedTCPPorts = [ ... ];
+      # allowedUDPPorts = [ ... ];
+    };
+  };
+}
