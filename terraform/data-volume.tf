5e08891fb01fc86a5bf3c62d9bc4802aac903ffc
Philipp Schmitt <philipp@schmitt.co>
2024-04-16 12:06:47 +0200

    reorg tf code

--- terraform/data-volume.tf
+++ terraform/data-volume.tf
@@ -1,11 +1,11 @@
 resource "openstack_blockstorage_volume_v3" "data_volume" {
-  name        = "roflvolume-01"
-  size        = 4096 # GiB
+  name              = "roflvolume-01"
+  size              = 4096 # GiB
   availability_zone = var.availability_zone
 }
 
 resource "openstack_compute_volume_attach_v2" "va_data" {
-  instance_id = openstack_compute_instance_v2.nixos_anywhere_vm.id
+  instance_id = openstack_compute_instance_v2.rofl-02.id
   volume_id   = openstack_blockstorage_volume_v3.data_volume.id
 }
 
