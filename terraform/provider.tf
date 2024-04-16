b3ae8b6ccab19c301836a67d64bb3a5cf29b19a9
Philipp Schmitt <philipp@schmitt.co>
2024-04-16 17:51:10 +0200

    Migrate state to s3

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -5,6 +5,17 @@ terraform {
       version = "~> 1.44"
     }
   }
+
+  backend "s3" {
+    bucket                      = "terraform-state-heimat-dev"
+    key                         = "nixos-config.tfstate"
+    region                      = "eu-central-2"
+    endpoint                    = "s3.eu-central-2.wasabisys.com"
+    skip_region_validation      = true
+    skip_credentials_validation = true
+    # access_key=$AWS_ACCESS_KEY_ID
+    # secret_key=$AWS_SECRET_ACCESS_KEY
+  }
 }
 
 provider "openstack" {
