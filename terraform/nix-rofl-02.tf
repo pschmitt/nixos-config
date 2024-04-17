61e5f32093b06698df19cdadb3155b4ef93118f5
Philipp Schmitt <philipp@schmitt.co>
2024-04-17 09:30:10 +0200

    Rename tf objects

--- terraform/nix-rofl-02.tf
+++ terraform/nix-rofl-02.tf
@@ -13,7 +13,7 @@ module "install" {
   source            = "github.com/nix-community/nixos-anywhere//terraform/install"
   nixos_system      = module.system-build.result.out
   nixos_partitioner = module.disko.result.out
-  target_host       = openstack_networking_floatingip_v2.floating_ip.address
+  target_host       = openstack_networking_floatingip_v2.rofl_02_fip.address
   target_user       = var.nixos_anywhere_ssh_user
   instance_id       = openstack_compute_instance_v2.rofl-02.id
 }
