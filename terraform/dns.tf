a7c36fce72705fd685bc83d11f718d39ea214450
Philipp Schmitt <philipp@schmitt.co>
2024-04-18 19:48:32 +0200

    Add cloudflare DNS record

---
+++ terraform/dns.tf
@@ -0,0 +1,17 @@
+resource "cloudflare_account" "me" {
+  name = "philipp@schmitt.co"
+}
+
+resource "cloudflare_zone" "heimat_dev" {
+  zone = "heimat.dev"
+  plan = "free"
+  account_id = cloudflare_account.me.id
+}
+
+resource "cloudflare_record" "rofl-02" {
+  zone_id = cloudflare_zone.heimat_dev.id
+  name    = "rofl-02"
+  value   = openstack_networking_floatingip_v2.rofl_02_fip.address
+  type    = "A"
+  ttl     = 3600
+}
