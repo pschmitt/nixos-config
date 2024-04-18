273cea3d64117fddea2ec9fe74377653f6e8b1de
Philipp Schmitt <philipp@schmitt.co>
2024-04-18 19:02:48 +0200

    Don't destroy data volume

--- terraform/data-volume.tf
+++ terraform/data-volume.tf
@@ -2,6 +2,10 @@ resource "openstack_blockstorage_volume_v3" "data_volume" {
   name              = "roflvolume-01"
   size              = 4096 # GiB
   availability_zone = var.availability_zone
+
+  lifecycle {
+    prevent_destroy = true
+  }
 }
 
 resource "openstack_compute_volume_attach_v2" "va_data" {
