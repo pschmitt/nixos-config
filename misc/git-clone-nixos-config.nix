4b4b73f880a29dcc049563f029907be627a7c61b
Philipp Schmitt <philipp@schmitt.co>
2024-04-19 17:04:39 +0200

    Git clone config locally

---
+++ misc/git-clone-nixos-config.nix
@@ -0,0 +1,21 @@
+{ pkgs, ... }: {
+  # Define the systemd service
+  systemd.services."git-clone-nixos-config" = {
+    # Make sure the service waits for networking to be up
+    wantedBy = [ "multi-user.target" ];
+    after = [ "network-online.target" ];
+    requires = [ "network-online.target" ];
+
+    serviceConfig = {
+      Type = "oneshot";
+      RemainAfterExit = true;
+      ExecStart = "${pkgs.writeShellScriptBin "git-clone-nixos-config" ''
+        # Check if /etc/nixos is empty
+        if [[ -z "$(ls -A /etc/nixos)" ]]
+        then
+          ${pkgs.git}/bin/git clone https://github.com/pschmitt/nixos-config /etc/nixos
+        fi
+      ''}/bin/git-clone-nixos-config";
+    };
+  };
+}
