ff0f7dda985d8e568b9d671b9425920eea129392
Philipp Schmitt <philipp@schmitt.co>
2024-04-19 18:04:00 +0200

    Add font transfer script

---
+++ pkgs/fonts/src/transfer-files-to-builder.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]
+then
+  TARGET_USER="${TARGET_USER:-${USER}}"
+  TARGET_HOST="${TARGET_HOST:-rofl-03.heimat.dev}"
+
+  cd "$(cd "$(dirname "$0")" >/dev/null 2>&1; pwd -P)" || exit 9
+
+  if ! ./fetch-fonts.sh
+  then
+    echo "Failed to fetch font files" >&2
+    exit 1
+  fi
+
+  ssh "${TARGET_USER}@${TARGET_HOST}" mkdir -p src
+  scp ./*.zip "${TARGET_USER}@${TARGET_HOST}:src"
+fi
