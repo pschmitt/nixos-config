1a1f47cc629d5df97a076ca226f60f04cced053b
Philipp Schmitt <philipp@schmitt.co>
2024-04-19 13:50:52 +0200

    WIP: ipv6

--- terraform/network.tf
+++ terraform/network.tf
@@ -10,13 +10,29 @@ resource "openstack_networking_subnet_v2" "roflsubnet_v4" {
   ip_version = 4
 }
 
+resource "openstack_networking_subnetpool_v2" "customer_ipv6" {
+  name             = "customer-ipv6"
+  prefixes         = ["2a00:c320:1000::/48"]
+  address_scope_id = openstack_networking_addressscope_v2.bgpv6.id
+  is_default       = true
+  shared           = true
+}
+
+resource "openstack_networking_addressscope_v2" "bgpv6" {
+  name       = "bgpv6"
+  ip_version = 6
+  shared     = true
+}
+
 resource "openstack_networking_subnet_v2" "roflsubnet_v6" {
-  name              = "roflsubnet-v6"
-  network_id        = openstack_networking_network_v2.roflnet.id
-  cidr              = "2001:db8::/64" # Replace with your IPv6 range
-  ip_version        = 6
-  ipv6_address_mode = "slaac" # or dhcpv6-stateful, dhcpv6-stateless
-  ipv6_ra_mode      = "slaac" # or dhcpv6-stateful, dhcpv6-stateless
+  name          = "roflsubnet-v6"
+  network_id    = openstack_networking_network_v2.roflnet.id
+  subnetpool_id = openstack_networking_subnetpool_v2.customer_ipv6.id
+  ip_version    = 6
+  # ipv6_address_mode = "slaac" # or dhcpv6-stateful, dhcpv6-stateless
+  # ipv6_ra_mode      = "slaac" # or dhcpv6-stateful, dhcpv6-stateless
+  ipv6_address_mode = "dhcpv6-stateful"
+  ipv6_ra_mode      = "dhcpv6-stateful"
 }
 
 resource "openstack_networking_router_v2" "roflrouter" {
