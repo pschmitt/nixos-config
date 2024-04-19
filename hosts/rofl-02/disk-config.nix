aa53e036131b727a29a9a850d1abee4cf4e09eb8
Philipp Schmitt <philipp@schmitt.co>
2024-04-19 07:41:09 +0200

    Precise disk adressing

--- hosts/rofl-02/disk-config.nix
+++ hosts/rofl-02/disk-config.nix
@@ -1,8 +1,12 @@
 { lib, ... }:
 {
   disko.devices = {
-    disk.disk1 = {
-      device = lib.mkDefault "/dev/sda";
+    disk.system = {
+      # NOTE This needs to be updated along with the openstack volume id of the
+      # root disk
+      # See tf object at:
+      # openstack_blockstorage_volume_v3.rofl_02_boot_volume.id
+      device = lib.mkDefault "/dev/disk/by-id/scsi-0QEMU_QEMU_HARDDISK_212d29f8-a7e9-4a72-bb9b-61248e3b8b38";
       type = "disk";
       content = {
         type = "gpt";
@@ -10,13 +14,7 @@
           boot = {
             name = "boot";
             size = "1M";
-            # size = "512M";
             type = "EF02"; # mbr, bios
-            # content = {
-            #   type = "filesystem";
-            #   format = "ext4";
-            #   mountpoint = "/boot";
-            # };
           };
           esp = {
             name = "ESP";
--- terraform/data-volume.tf
+++ terraform/data-volume.tf
@@ -9,9 +9,9 @@ resource "openstack_blockstorage_volume_v3" "data_volume" {
   }
 }
 
-resource "openstack_compute_volume_attach_v2" "va_data" {
-  instance_id = openstack_compute_instance_v2.rofl-02.id
-  volume_id   = openstack_blockstorage_volume_v3.data_volume.id
-}
+# resource "openstack_compute_volume_attach_v2" "va_data" {
+#   instance_id = openstack_compute_instance_v2.rofl-02.id
+#   volume_id   = openstack_blockstorage_volume_v3.data_volume.id
+# }
 
 # vim: set ft=terraform
