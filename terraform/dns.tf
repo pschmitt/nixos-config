69c00164e89cbebf25033057b081c8d008f59a39
Philipp Schmitt <philipp@schmitt.co>
2024-04-19 17:51:35 +0200

    Add the remaining wildcards

--- terraform/dns.tf
+++ terraform/dns.tf
@@ -15,6 +15,21 @@ resource "cloudflare_record" "wildcard-rofl-01" {
   ttl     = 3600
 }
 
+resource "cloudflare_record" "wildcard-rofl-02" {
+  zone_id = cloudflare_zone.heimat_dev.id
+  name    = "*.rofl-02"
+  value   = openstack_networking_floatingip_v2.rofl_02_fip.address
+  type    = "A"
+  ttl     = 3600
+}
+
+resource "cloudflare_record" "wildcard-rofl-03" {
+  zone_id = cloudflare_zone.heimat_dev.id
+  name    = "*.rofl-03"
+  value   = openstack_networking_floatingip_v2.rofl_03_fip.address
+  type    = "A"
+  ttl     = 3600
+}
 
 resource "cloudflare_record" "rofl-02" {
   zone_id = cloudflare_zone.heimat_dev.id
