ac32d125f9779142ccbbbedd8decc5fff2159de4
Philipp Schmitt <philipp@schmitt.co>
2024-04-20 22:58:27 +0200

    Add rofl-03 to output

--- terraform/output.tf
+++ terraform/output.tf
@@ -8,4 +8,14 @@ output "rofl_02_fip" {
   description = "Floating IP address of rofl-02"
 }
 
+output "rofl_03_boot_volume_id" {
+  value       = openstack_blockstorage_volume_v3.rofl_03_boot_volume.id
+  description = "Volume ID of the root volume of rofl-03"
+}
+
+output "rofl_03_fip" {
+  value       = openstack_networking_floatingip_v2.rofl_03_fip.address
+  description = "Floating IP address of rofl-03"
+}
+
 # vim: set ft=terraform
