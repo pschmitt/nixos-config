6e693089b187fa3ddbcf3924fbb8ebeedca7b54e
Philipp Schmitt <philipp@schmitt.co>
2024-04-20 21:36:03 +0200

    WIP: remove ip=dhcp kernel param

--- common/global/network.nix
+++ common/global/network.nix
@@ -1,18 +1,5 @@
 { ... }:
 {
-  # Enable networking
-  networking = {
-    networkmanager = {
-      enable = true;
-      dns = "systemd-resolved";
-      # enableFccUnlock = true;  # deprecated
-    };
-  };
-
-  # FIXME Disable wait-online services, this somehow results in NM not being started at all.
-  # systemd.network.wait-online.enable = false;
-  # systemd.services.NetworkManager-wait-online.enable = false;
-
   services.resolved = {
     enable = true;
     dnssec = "false";
