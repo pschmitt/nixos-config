6aefb352a3f7b960b78364956be83c58d7bf4f90
Philipp Schmitt <philipp@schmitt.co>
2024-04-21 20:18:53 +0200

    More remote builders!

--- common/global/nix-remote-build.nix
+++ common/global/nix-remote-build.nix
@@ -10,13 +10,25 @@
     distributedBuilds = true;
     buildMachines = [
       {
+        hostName = "rofl-02.heimat.dev";
+        protocol = "ssh-ng";
+        sshUser = "pschmitt";
+        sshKey = config.age.secrets.ssh-privkey.path;
+        # base64 -w0 /etc/ssh/ssh_host_ed25519_key.pu3b
+        publicHostKey = "c3NoLWVkMjU1MTkgQUFBQUMzTnphQzFsWkRJMU5URTVBQUFBSUhqMWJ3eWtZSTR0QzRrdDNSZDRRQU9WMkQxc3JsY1ExNE5MQjl3M0pCWHAgcHNjaG1pdHRAZ2UyCg==";
+        systems = [ "x86_64-linux" ];
+        maxJobs = 2;
+        speedFactor = 1;
+        supportedFeatures = [ "nixos-test" "benchmark" "big-parallel" ];
+      }
+      {
         hostName = "rofl-03.heimat.dev";
         protocol = "ssh-ng";
         sshUser = "pschmitt";
         sshKey = config.age.secrets.ssh-privkey.path;
         # base64 -w0 /etc/ssh/ssh_host_ed25519_key.pub
         publicHostKey = "c3NoLWVkMjU1MTkgQUFBQUMzTnphQzFsWkRJMU5URTVBQUFBSUwvbStwRCtUc1NISnhTSFVIb3ltSHZxZXZGcnFPbWZBQmo3QWMxaFMzVFEgcm9vdEByb2ZsLTAzCg==";
-        systems = [ "x86_64-linux" ];
+        systems = [ "aarch64-linux" "x86_64-linux" ];
         maxJobs = 8;
         speedFactor = 2;
         supportedFeatures = [ "nixos-test" "benchmark" "big-parallel" ];
