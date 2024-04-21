054fbc4ac12c60fbc56d7ae1ffe7b91d2b36f0b8
Philipp Schmitt <philipp@schmitt.co>
2024-04-21 19:02:32 +0200

    More vars, more fixes. More problems.

--- hosts/rofl-02/scp-fonts-to-rofl-03.nix
+++ hosts/rofl-02/scp-fonts-to-rofl-03.nix
@@ -7,17 +7,18 @@
       Type = "simple";
       ExecStart = ''
         ${pkgs.openssh}/bin/scp \
-          -i /etc/ssh/ssh_host_ed25519_key \
+          -i "$${IDENTITY_FILE}" \
           -o UserKnownHostsFile=/dev/null \
           -o StrictHostKeyChecking=no \
           "$${SOURCE_DIR}/"* \
-          "$${REMOTE_USER}@$${REMOTE_USER}:$${DEST_DIR}/"
+          "$${REMOTE_USER}@$${REMOTE_HOST}:$${DEST_DIR}/"
       '';
       Environment = [
         "SOURCE_DIR=/mnt/data/srv/nextcloud/data/nextcloud/pschmitt/files/Fonts"
         "DEST_DIR=src"
         "REMOTE_USER=github-actions"
         "REMOTE_HOST=rofl-03.heimat.dev"
+        "IDENTITY_FILE=/etc/ssh/ssh_host_ed25519_key"
       ];
       Restart = "on-failure";
       RestartSec = 30;
