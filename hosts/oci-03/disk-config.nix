4517a0af797754192d76a7f333306bae871e0e5e
Philipp Schmitt <philipp@schmitt.co>
2024-04-21 11:53:41 +0200

    Add oci-03

---
+++ hosts/oci-03/disk-config.nix
@@ -0,0 +1,61 @@
+{ lib, ... }:
+{
+  disko.devices = {
+    disk.system = {
+      device = lib.mkDefault "/dev/disk/by-label/cloudimg-rootfs";
+      type = "disk";
+      content = {
+        type = "gpt";
+        partitions = {
+          boot = {
+            name = "boot";
+            size = "1M";
+            type = "EF02"; # mbr, bios
+          };
+          esp = {
+            name = "ESP";
+            size = "512M";
+            type = "EF00";
+            content = {
+              type = "filesystem";
+              format = "vfat";
+              mountpoint = "/boot";
+            };
+          };
+          luks = {
+            size = "100%";
+            content = {
+              type = "luks";
+              name = "encrypted";
+              settings = {
+                keyFile = "/tmp/disk-1.key";
+                # NOTE fallbackToPassword is implied when enabling systemd
+                # in initrd
+                # fallbackToPassword = true;
+                allowDiscards = true;
+              };
+              content = {
+                type = "btrfs";
+                extraArgs = [ "-f" ];
+                subvolumes = {
+                  "@root" = {
+                    mountpoint = "/";
+                    mountOptions = [ "compress=zstd" "noatime" ];
+                  };
+                  "@home" = {
+                    mountpoint = "/home";
+                    mountOptions = [ "compress=zstd" "noatime" ];
+                  };
+                  "@nix" = {
+                    mountpoint = "/nix";
+                    mountOptions = [ "compress=zstd" "noatime" ];
+                  };
+                };
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}
