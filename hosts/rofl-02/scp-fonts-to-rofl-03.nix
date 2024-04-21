c2ceb88524f34cad23b4edbe688068060a9e5565
Philipp Schmitt <philipp@schmitt.co>
2024-04-21 18:56:44 +0200

    Fix service

--- hosts/rofl-02/scp-fonts-to-rofl-03.nix
+++ hosts/rofl-02/scp-fonts-to-rofl-03.nix
@@ -1,14 +1,23 @@
 { pkgs, ... }: {
   systemd.services.scp-fonts-to-rolf-03 = {
-    description = "SCP files to github-actions@rofl-03.heimat.dev";
+    description = "SCP font files to github-actions@rofl-03.heimat.dev";
     after = [ "network.target" ];
     wantedBy = [ "multi-user.target" ];
     serviceConfig = {
       Type = "simple";
-      ExecStart = "${pkgs.openssh}/bin/scp -i /etc/ssh/ssh_host_ed25519_key '$SOURCE_DIR/'* 'github-actions@rofl-03.heimat.dev:$DEST_DIR/";
+      ExecStart = ''
+        ${pkgs.openssh}/bin/scp \
+          -i /etc/ssh/ssh_host_ed25519_key \
+          -o UserKnownHostsFile=/dev/null \
+          -o StrictHostKeyChecking=no \
+          "$SOURCE_DIR/"* \
+          "$REMOTE_USER@$REMOTE_USER:$DEST_DIR/"
+      '';
       Environment = [
         "SOURCE_DIR=/mnt/data/srv/nextcloud/data/nextcloud/pschmitt/files/Fonts"
         "DEST_DIR=src"
+        "REMOTE_USER=github-actions"
+        "REMOTE_HOST=rofl-03.heimat.dev"
       ];
       Restart = "on-failure";
       RestartSec = 30;
