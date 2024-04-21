c8b551dff86977a1bd394bf6049a0e0f2f5ccd34
Philipp Schmitt <philipp@schmitt.co>
2024-04-21 16:40:45 +0200

    Remove not-needed modules

--- hosts/oci-03/hardware-configuration.nix
+++ hosts/oci-03/hardware-configuration.nix
@@ -3,8 +3,7 @@
 { modulesPath, ... }:
 {
   imports = [ (modulesPath + "/profiles/qemu-guest.nix") ];
-  boot.initrd.availableKernelModules = [ "xhci_pci" "virtio_pci" "virtio_scsi" "usbhid" ];
-  # boot.initrd.kernelModules = [ "nvme" ];
+  boot.initrd.availableKernelModules = [ "xhci_pci" "virtio_scsi" ];
 
   boot.supportedFilesystems = [ "btrfs" ];
 
