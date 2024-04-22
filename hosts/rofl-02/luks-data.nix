f5fbab27c438cc7bf4744ecffcaf73ce3b87257c
Philipp Schmitt <philipp@schmitt.co>
2024-04-22 10:49:18 +0200

    Remove hostname var

--- hosts/rofl-02/luks-data.nix
+++ hosts/rofl-02/luks-data.nix
@@ -1,10 +1,6 @@
-{ config, ... }:
-let
-  hostname = config.networking.hostName;
-in
-{
+{ config, ... }: {
   # Data volume
-  age.secrets.luks-key-data.file = ../../secrets/${hostname}/luks-passphrase-data.age;
+  age.secrets.luks-key-data.file = ../../secrets/${config.networking.hostName}/luks-passphrase-data.age;
 
   environment.etc.crypttab.text = ''
     data UUID=371fa9e9-38f4-4022-bc96-227821c5eea7 ${config.age.secrets.luks-key-data.path}
