57a681fb9965099b63c0708461c26e0f5039617c
Philipp Schmitt <philipp@schmitt.co>
2024-04-22 18:14:42 +0200

    Explicitly start tailscaled before netbird

--- common/work/vpn/netbird.nix
+++ common/work/vpn/netbird.nix
@@ -4,4 +4,5 @@
   # Starting netbird before tailscaled ensures that (tailscale) hosts
   # resolution works as expected.
   systemd.services.netbird-wt0.after = [ "tailscaled.service" ];
+  systemd.services.tailscaled.before = [ "netbird-wt0.service" ];
 }
