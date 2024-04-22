36dff10a7f4fe681e77dd01b2e696a72231724a6
Philipp Schmitt <philipp@schmitt.co>
2024-04-22 09:42:01 +0200

    Add oci-03-data vol

--- opentofu/data-volume.tf
+++ opentofu/data-volume.tf
@@ -14,4 +14,17 @@ resource "openstack_compute_volume_attach_v2" "va_data" {
   volume_id   = openstack_blockstorage_volume_v3.data_volume.id
 }
 
-# vim: set ft=terraform
+resource "oci_core_volume" "oci_03_data" {
+  availability_domain = oci_core_instance.oci_03.availability_domain
+  compartment_id      = var.oci_compartment_id
+  display_name        = "oci-03-data"
+  size_in_gbs         = 50
+}
+
+resource "oci_core_volume_attachment" "oci_03_volume_attachment" {
+  attachment_type    = "paravirtualized"
+  instance_id        = oci_core_instance.oci_03.id
+  volume_id          = oci_core_volume.oci_03_data.id
+}
+
+# vim: set ft=terraform :
