407e45b852e7170162ddfbca425b971f44d05694
Philipp Schmitt <philipp@schmitt.co>
2024-04-22 18:09:38 +0200

    add note

--- common/work/vpn/netbird.nix
+++ common/work/vpn/netbird.nix
@@ -1,5 +1,7 @@
 { ... }: {
   services.netbird.enable = true;
 
+  # Starting netbird before tailscaled ensures that (tailscale) hosts
+  # resolution works as expected.
   systemd.services.netbird-wt0.after = [ "tailscaled.service" ];
 }
