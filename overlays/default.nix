35c26fd2f43c7f5b56b01859f351afbfe8438fcc
Philipp Schmitt <philipp@schmitt.co>
2024-04-23 10:45:35 +0200

    Update ratbag and piper to the latest master

--- overlays/default.nix
+++ overlays/default.nix
@@ -11,6 +11,8 @@
     (import ./brotab.nix { inherit final prev; }) //
     (import ./hyprpicker.nix { inherit final prev; }) //
     (import ./openstack-cli.nix { inherit final prev; }) //
+    (import ./libratbag.nix { inherit final prev; }) //
+    (import ./piper.nix { inherit final prev; }) //
     # FIXME Remove this overlay once this is fixed in nixpkgs
     # The issue is that the source of amrnb ie.
     # http://www.penguin.cz/~utx/ftp/amr/amrnb-11.0.0.0.tar.bz2
