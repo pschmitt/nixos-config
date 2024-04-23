b83b423b5da318afd45ab8027dacd94515a917a6
Philipp Schmitt <philipp@schmitt.co>
2024-04-23 09:20:16 +0200

    Fix extraUpFlags val if netbird disabled

--- common/global/tailscale.nix
+++ common/global/tailscale.nix
@@ -1,10 +1,10 @@
-{ config, lib, ... }: {
+{ config, ... }: {
   age.secrets.ts-auth-key.file = ../../secrets/tailscale-auth-key.age;
 
   services.tailscale = {
     enable = true;
     openFirewall = true;
-    extraUpFlags = lib.optionalString config.services.netbird.enable [ "--netfilter-mode=off" ];
+    extraUpFlags = if config.services.netbird.enable then [ "--netfilter-mode=off" ] else [];
     useRoutingFeatures = "both";
     authKeyFile = config.age.secrets.ts-auth-key.path;
   };
