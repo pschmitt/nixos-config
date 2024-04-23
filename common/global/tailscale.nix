9cf5183c6088cecc32643b397c9afed1e9be2821
Philipp Schmitt <philipp@schmitt.co>
2024-04-23 08:52:25 +0200

    add ts auth key

--- common/global/tailscale.nix
+++ common/global/tailscale.nix
@@ -1,8 +1,11 @@
 { config, lib, ... }: {
+  age.secrets.ts-auth-key.file = ../../secrets/tailscale-auth-key.age;
+
   services.tailscale = {
     enable = true;
     openFirewall = true;
     extraUpFlags = lib.optionalString config.services.netbird.enable [ "--netfilter-mode=off" ];
     useRoutingFeatures = "both";
+    authKeyFile = config.age.secrets.ts-auth-key.path;
   };
 }
