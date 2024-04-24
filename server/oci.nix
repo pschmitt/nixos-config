5ebbcb1c9e3f32741036fcdbf77f3badf7a6868e
Philipp Schmitt <philipp@schmitt.co>
2024-04-24 16:06:48 +0200

    oci: support volume consistent device paths https://docs.oracle.com/en-us/iaas/Content/Block/References/consistentdevicepaths.htm

--- server/oci.nix
+++ server/oci.nix
@@ -1,5 +1,9 @@
 # https://github.com/NixOS/nixpkgs/pull/119856/
-{ ... }: {
+{ inputs, pkgs, ... }:
+let
+  snapPkg = inputs.nix-snapd.packages.${pkgs.system}.default;
+in
+{
 
   boot.kernelParams = [
     "nvme.shutdown_timeout=10"
@@ -19,4 +23,16 @@
 
   # https://docs.oracle.com/en-us/iaas/Content/Compute/Tasks/configuringntpservice.htm#Configuring_the_Oracle_Cloud_Infrastructure_NTP_Service_for_an_Instance
   networking.timeServers = [ "169.254.169.254" ];
+
+  services.snap.enable = true;
+  systemd.services.snap-install-oracle-cloud-agent = {
+    wantedBy = [ "multi-user.target" ];
+    after = [ "network-online.target" "snapd.service" ];
+    requires = [ "network-online.target" "snapd.service" ];
+    script = "${snapPkg}/bin/snap install oracle-cloud-agent --classic";
+  };
+
+  # TODO Add the udev rules from ./99-systemoci-persistent-names.rules
+  services.udev.path = [ pkgs.oci-consistent-device-naming ];
+  services.udev.packages = [ pkgs.oci-consistent-device-naming ];
 }
