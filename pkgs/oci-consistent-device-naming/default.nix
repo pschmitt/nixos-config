a9386489d50e8c2e1fe8b799ca3cb2018a5ce645
Philipp Schmitt <philipp@schmitt.co>
2024-04-24 16:06:48 +0200

    oci: support volume consistent device paths https://docs.oracle.com/en-us/iaas/Content/Block/References/consistentdevicepaths.htm

---
+++ pkgs/oci-consistent-device-naming/default.nix
@@ -0,0 +1,30 @@
+{ lib, stdenv, bash }:
+
+stdenv.mkDerivation {
+  pname = "oci-udev-persistent-naming";
+  version = "1.0";
+
+  # These 2 files were extracted from an oci instance:
+  # /usr/local/sbin/oci_udev_persistent_naming
+  # /etc/udev/rules.d/99-systemoci-persistent-names.rules
+  src = ./oci_udev_persistent_naming;
+  rules = ./99-systemoci-persistent-names.rules;
+
+  buildInputs = [ bash ];
+  phases = [ "installPhase" ];
+
+  installPhase = ''
+    install -Dm 755 $src $out/bin/oci_udev_persistent_naming
+    patchShebangs $out/bin/oci_udev_persistent_naming
+
+    install -Dm 644 $rules $out/lib/udev/rules.d/99-systemoci-persistent-names.rules
+    sed -i "s#/usr/local/sbin/oci_udev_persistent_naming#$out/bin/oci_udev_persistent_naming#g" \
+      $out/lib/udev/rules.d/99-systemoci-persistent-names.rules
+  '';
+
+  meta = {
+    description = "Support for consistent device naming for Oracle Cloud Infrastructure on NixOS";
+    license = lib.licenses.gpl3Only;
+    maintainers = with lib.maintainers; [ pschmitt ];
+  };
+}
