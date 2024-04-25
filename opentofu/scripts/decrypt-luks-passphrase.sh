8f5dc900efefa659509f35f275f876fc7477dd4c
Philipp Schmitt <philipp@schmitt.co>
2024-04-25 16:21:57 +0200

    Read from env

--- opentofu/scripts/decrypt-luks-passphrase.sh
+++ opentofu/scripts/decrypt-luks-passphrase.sh
@@ -2,8 +2,8 @@
 
 cd "$(cd "$(dirname "$0")" >/dev/null 2>&1; pwd -P)" || exit 9
 
-AGENIX_DIR=/etc/nixos/secrets
-AGE_IDENTITY_FILE=/home/pschmitt/.ssh/id_ed25519
+AGENIX_DIR="${AGENIX_DIR:-/etc/nixos/secrets}"
+AGE_IDENTITY_FILE="${AGE_IDENTITY_FILE:-/home/pschmitt/.ssh/id_ed25519}"
 TARGET_HOST="${TARGET_HOST:-rofl-02}"
 
 age --decrypt --identity "$AGE_IDENTITY_FILE" \
