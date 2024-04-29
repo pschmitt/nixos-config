68d0ee28b7d5dd8fc90c6bbfd712f09fe3dfb069
Philipp Schmitt <philipp@schmitt.co>
2024-04-29 10:49:22 +0200

    Update remote builder config

--- common/global/nix-remote-build.nix
+++ common/global/nix-remote-build.nix
@@ -1,10 +1,10 @@
 { config, ... }: {
-  age = {
-    secrets = {
-      ssh-privkey.file = ../../secrets/ssh-key-nix-remote-builder.age;
-      # ssh-pubkey.file = ../../secrets/ssh-key-nix-remote-builder.pub.age;
-    };
-  };
+  # age = {
+  #   secrets = {
+  #     ssh-pubkey-rofl-02.file = ../../secrets/rofl-02/ssh_host_ed25519_key.age;
+  #     ssh-pubkey-rofl-03.file = ../../secrets/rofl-03/ssh_host_ed25519_key.age;
+  #   };
+  # };
 
   nix = {
     distributedBuilds = true;
@@ -12,9 +12,9 @@
       {
         hostName = "rofl-02.heimat.dev";
         protocol = "ssh-ng";
-        sshUser = "pschmitt";
-        sshKey = config.age.secrets.ssh-privkey.path;
-        # base64 -w0 /etc/ssh/ssh_host_ed25519_key.pu3b
+        sshUser = "nix-remote-builder";
+        sshKey = config.custom.sshKey;
+        # base64 -w0 /etc/ssh/ssh_host_ed25519_key.pub
         publicHostKey = "c3NoLWVkMjU1MTkgQUFBQUMzTnphQzFsWkRJMU5URTVBQUFBSUhqMWJ3eWtZSTR0QzRrdDNSZDRRQU9WMkQxc3JsY1ExNE5MQjl3M0pCWHAgcHNjaG1pdHRAZ2UyCg==";
         systems = [ "x86_64-linux" ];
         maxJobs = 2;
@@ -24,8 +24,8 @@
       {
         hostName = "rofl-03.heimat.dev";
         protocol = "ssh-ng";
-        sshUser = "pschmitt";
-        sshKey = config.age.secrets.ssh-privkey.path;
+        sshUser = "nix-remote-builder";
+        sshKey = config.custom.sshKey;
         # base64 -w0 /etc/ssh/ssh_host_ed25519_key.pub
         publicHostKey = "c3NoLWVkMjU1MTkgQUFBQUMzTnphQzFsWkRJMU5URTVBQUFBSUwvbStwRCtUc1NISnhTSFVIb3ltSHZxZXZGcnFPbWZBQmo3QWMxaFMzVFEgcm9vdEByb2ZsLTAzCg==";
         systems = [ "aarch64-linux" "x86_64-linux" ];
