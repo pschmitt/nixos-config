0546bdb841d37cbcd9c6bf78f2b80a142d59c204
Philipp Schmitt <philipp@schmitt.co>
2024-04-30 15:58:33 +0200

    Disable remote-builder again

--- common/global/nix-remote-build.nix
+++ common/global/nix-remote-build.nix
@@ -1,10 +1,12 @@
 { config, ... }: {
-  # age = {
-  #   secrets = {
-  #     ssh-pubkey-rofl-02.file = ../../secrets/rofl-02/ssh_host_ed25519_key.age;
-  #     ssh-pubkey-rofl-03.file = ../../secrets/rofl-03/ssh_host_ed25519_key.age;
-  #   };
-  # };
+  age = {
+    secrets = {
+      # ssh-pubkey-rofl-02.file = ../../secrets/rofl-02/ssh_host_ed25519_key.age;
+      # ssh-pubkey-rofl-03.file = ../../secrets/rofl-03/ssh_host_ed25519_key.age;
+      ssh-privkey-nix-remote-builder.file = ../../secrets/ssh-key-nix-remote-builder.age;
+      # ssh-pubkey.file = ../../secrets/ssh-key-nix-remote-builder.pub.age;
+    };
+  };
 
   nix = {
     distributedBuilds = true;
@@ -13,7 +15,7 @@
         hostName = "rofl-02.heimat.dev";
         protocol = "ssh-ng";
         sshUser = "nix-remote-builder";
-        sshKey = config.custom.sshKey;
+        sshKey = config.age.secrets.ssh-privkey-nix-remote-builder.path;
         # ssh rofl-02.heimat.dev base64 -w0 /etc/ssh/ssh_host_ed25519_key.pub
         publicHostKey = "c3NoLWVkMjU1MTkgQUFBQUMzTnphQzFsWkRJMU5URTVBQUFBSUhqMWJ3eWtZSTR0QzRrdDNSZDRRQU9WMkQxc3JsY1ExNE5MQjl3M0pCWHAgcHNjaG1pdHRAZ2UyCg==";
         systems = [ "x86_64-linux" ];
@@ -25,12 +27,12 @@
         hostName = "rofl-03.heimat.dev";
         protocol = "ssh-ng";
         sshUser = "nix-remote-builder";
-        sshKey = config.custom.sshKey;
+        sshKey = config.age.secrets.ssh-privkey-nix-remote-builder.path;
         # ssh rofl-03.heimat.dev base64 -w0 /etc/ssh/ssh_host_ed25519_key.pub
         publicHostKey = "c3NoLWVkMjU1MTkgQUFBQUMzTnphQzFsWkRJMU5URTVBQUFBSUwvbStwRCtUc1NISnhTSFVIb3ltSHZxZXZGcnFPbWZBQmo3QWMxaFMzVFEgcm9vdEByb2ZsLTAzCg==";
         systems = [ "aarch64-linux" "x86_64-linux" ];
-        maxJobs = 8;
-        speedFactor = 2;
+        maxJobs = 14;
+        speedFactor = 3;
         supportedFeatures = [ "nixos-test" "benchmark" "big-parallel" ];
       }
     ];
