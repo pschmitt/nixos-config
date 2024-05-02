49b41666c87c013ed49721e574aecec1e0ca44a8
Philipp Schmitt <philipp@schmitt.co>
2024-05-02 09:29:44 +0200

    Use stable nvidia package

--- hosts/ge2/nvidia.nix
+++ hosts/ge2/nvidia.nix
@@ -47,7 +47,8 @@
     nvidiaSettings = true;
 
     # Optionally, you may need to select the appropriate driver version for your specific GPU.
-    package = config.boot.kernelPackages.nvidiaPackages.beta;
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+    # package = config.boot.kernelPackages.nvidiaPackages.beta;
     # package = config.boot.kernelPackages.nvidiaPackages.vulkan_beta;
 
     prime = {
