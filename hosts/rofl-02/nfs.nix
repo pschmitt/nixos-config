87630c210cfcb372a5147871c532af5ecdff152c
Philipp Schmitt <philipp@schmitt.co>
2024-05-06 09:36:48 +0200

    nfs: Less code reps + client config

--- hosts/rofl-02/nfs.nix
+++ hosts/rofl-02/nfs.nix
@@ -1,42 +1,23 @@
-{ ... }: {
-  fileSystems."/export/backups" = {
-    device = "/mnt/data/backups";
-    options = [ "bind" ];
-  };
-  fileSystems."/export/books" = {
-    device = "/mnt/data/books";
-    options = [ "bind" ];
-  };
-  fileSystems."/export/documents" = {
-    device = "/mnt/data/documents";
-    options = [ "bind" ];
-  };
-  fileSystems."/export/mnt" = {
-    device = "/mnt/data/mnt";
-    options = [ "bind" ];
-  };
-  fileSystems."/export/srv" = {
-    device = "/mnt/data/srv";
-    options = [ "bind" ];
-  };
-  fileSystems."/export/tmp" = {
-    device = "/mnt/data/tmp";
-    options = [ "bind" ];
-  };
-  fileSystems."/export/videos" = {
-    device = "/mnt/data/videos";
-    options = [ "bind" ];
-  };
+{ lib, ... }:
+let
+  basePath = "/mnt/data";
+  exportPath = "/export";
+  subDirs = [ "backups" "books" "documents" "mnt" "srv" "tmp" "videos" ];
+  nfsNetwork = "100.64.0.0/10";
+  exportOptions = "rw,nohide,insecure,no_subtree_check";
+in
+{
+  fileSystems = builtins.listToAttrs (map
+    (dir: {
+      name = "${exportPath}/${dir}";
+      value.device = "${basePath}/${dir}";
+      value.options = [ "bind" ];
+    })
+    subDirs);
 
   services.nfs.server.enable = true;
   services.nfs.server.exports = ''
-    /export           100.64.0.0/10(rw,fsid=0,no_subtree_check)
-    /export/backups   100.64.0.0/10(rw,nohide,insecure,no_subtree_check)
-    /export/books     100.64.0.0/10(rw,nohide,insecure,no_subtree_check)
-    /export/documents 100.64.0.0/10(rw,nohide,insecure,no_subtree_check)
-    /export/mnt       100.64.0.0/10(rw,nohide,insecure,no_subtree_check)
-    /export/srv       100.64.0.0/10(rw,nohide,insecure,no_subtree_check)
-    /export/tmp       100.64.0.0/10(rw,nohide,insecure,no_subtree_check)
-    /export/videos    100.64.0.0/10(rw,nohide,insecure,no_subtree_check)
+    ${exportPath} ${nfsNetwork}(rw,fsid=0,no_subtree_check)
+    ${lib.concatStringsSep "\n" (map (dir: "${exportPath}/${dir} ${nfsNetwork}(${exportOptions})") subDirs)}
   '';
 }
