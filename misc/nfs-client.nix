cae936f810dea77325256d921f65047746c6f8ca
Philipp Schmitt <philipp@schmitt.co>
2024-05-06 10:53:51 +0200

    Update nfs config

---
+++ misc/nfs-client.nix
@@ -0,0 +1,17 @@
+{ ... }:
+
+let
+  serverAddress = "rofl-02.netbird.cloud:/export";
+  mounts = [ "backups" "books" "documents" "mnt" "srv" "tmp" "videos" ];
+in
+{
+  fileSystems = builtins.listToAttrs (map
+    (dir: {
+      name = "/mnt/data/${dir}";
+      value.device = "${serverAddress}/${dir}";
+      value.fsType = "nfs";
+      value.options = [ "noauto" "x-systemd.automount" "x-systemd.idle-timeout=600" ];
+    })
+    mounts);
+}
+
