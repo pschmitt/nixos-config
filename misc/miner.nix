8fee313360933114ea790781ec69d13f1b2951e7
Philipp Schmitt <philipp@schmitt.co>
2024-05-07 13:42:57 +0200

    Add xmrig

---
+++ misc/miner.nix
@@ -0,0 +1,39 @@
+{ config, ... }: {
+  services.xmrig = {
+    enable = true;
+    settings = {
+      autosave = true;
+      opencl = false;
+      cuda = false;
+      cpu = {
+        # https://xmrig.com/docs/miner/config/cpu
+        enabled = true;
+        priority = 1;
+        # 25% of $nproc threads
+        max-threads-hint = 25;
+      };
+      pools = [
+        {
+          url = "pool.hashvault.pro:443";
+          # NOTE yes, this looks weird, and yes this works.
+          user = ''''${HASHVAULT_USER}'';
+          keepalive = true;
+          tls = true;
+        }
+        {
+          url = "xmrpool.eu:9999";
+          # NOTE yes, this looks weird, and yes this works.
+          user = ''''${XMRPOOL_USER}'';
+          keepalive = true;
+          tls = true;
+        }
+      ];
+    };
+  };
+
+  age.secrets.wallets.file = ../secrets/wallets.age;
+
+  systemd.services.xmrig.serviceConfig = {
+    EnvironmentFile = config.age.secrets.wallets.path;
+  };
+}
