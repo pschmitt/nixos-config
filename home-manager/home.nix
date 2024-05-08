8c7fcfd4fd20ba2e4af5b100b0a0fd1a9630dc0e
Philipp Schmitt <philipp@schmitt.co>
2024-05-08 23:25:28 +0200

    fmt

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -1,27 +1,26 @@
 { inputs, lib, config, osConfig, pkgs, ... }:
 {
-  imports =
-    lib.concatLists [
-      [
-        inputs.nur.hmModules.nur
-        inputs.nix-index-database.hmModules.nix-index
-        ./bitwarden.nix
-        ./devel.nix
-        ./mail.nix
-        ./networking.nix
-        ./nvim.nix
-        ./work.nix
-        ./zsh.nix
-        ./zellij.nix
-      ]
-      (lib.optional (osConfig.hardware.bluetooth.enable) ./bluetooth.nix)
-      (lib.optionals (osConfig.services.xserver.enable) [
-        ./browser.nix
-        ./dotool.nix
-        ./media.nix
-        ./theme.nix
-      ])
-    ];
+  imports = lib.concatLists [
+    [
+      inputs.nur.hmModules.nur
+      inputs.nix-index-database.hmModules.nix-index
+      ./bitwarden.nix
+      ./devel.nix
+      ./mail.nix
+      ./networking.nix
+      ./nvim.nix
+      ./work.nix
+      ./zsh.nix
+      ./zellij.nix
+    ]
+    (lib.optional (osConfig.hardware.bluetooth.enable) ./bluetooth.nix)
+    (lib.optionals (osConfig.services.xserver.enable) [
+      ./browser.nix
+      ./dotool.nix
+      ./media.nix
+      ./theme.nix
+    ])
+  ];
 
   # FIXME Do we need that for anything?
   # nixpkgs.overlays = [
