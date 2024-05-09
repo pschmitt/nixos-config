f5e0cd1934b7ec9e361239ca665b97023a58c0ee
Philipp Schmitt <philipp@schmitt.co>
2024-05-09 19:50:52 +0200

    tofu re-org

--- tofu/rofl-03.tf
+++ tofu/rofl-03.tf
@@ -58,4 +58,25 @@ resource "openstack_networking_floatingip_associate_v2" "rofl_03_fip_associate" 
   port_id     = openstack_networking_port_v2.rofl_03_port.id
 }
 
+module "nix-rofl-03" {
+  depends_on             = [openstack_compute_instance_v2.rofl-03]
+  source                 = "github.com/numtide/nixos-anywhere//terraform/all-in-one"
+  nixos_system_attr      = "..#nixosConfigurations.rofl-03.config.system.build.toplevel"
+  nixos_partitioner_attr = "..#nixosConfigurations.rofl-03.config.system.build.diskoScript"
+  target_host            = openstack_networking_floatingip_v2.rofl_03_fip.address
+  install_user           = var.nixos_anywhere_ssh_user
+  instance_id            = openstack_compute_instance_v2.rofl-03.id
+  debug_logging          = true
+  extra_environment = {
+    TARGET_HOST = "rofl-03"
+  }
+  disk_encryption_key_scripts = [
+    {
+      path   = "/tmp/disk-1.key",
+      script = "${path.module}/scripts/decrypt-luks-passphrase.sh"
+    }
+  ]
+  extra_files_script = "${path.module}/scripts/decrypt-ssh-secrets.sh"
+}
+
 # vim: set ft=terraform
