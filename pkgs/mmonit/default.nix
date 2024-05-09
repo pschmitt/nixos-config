ed35742beabb84c01d60193c0854cb8acde5b237
Philipp Schmitt <philipp@schmitt.co>
2024-05-09 12:54:56 +0200

    Clean up

--- pkgs/mmonit/default.nix
+++ pkgs/mmonit/default.nix
@@ -39,13 +39,34 @@ stdenv.mkDerivation rec {
     # M/Monit tries to write to this pidfile on startup
     ln -sfv /var/lib/mmonit/mmonit.pid $out/logs/mmonit.pid
 
+    # Patch default server configuration
+    # https://mmonit.com/documentation/mmonit_manual.pdf#page=67
+    cp $out/conf/server.xml $out/conf/server.xml.orig
+    substituteInPlace $out/conf/server.xml \
+      --replace-fail 'sqlite:///db/mmonit.db' 'sqlite://${mmonitHome}/db/mmonit.db' \
+      --replace-fail 'Logger directory="logs"' 'Logger directory="${mmonitHome}/logs"' \
+      --replace-fail '<License file="license.xml"' '<License file="${mmonitHome}/license.xml"' \
+      --replace-fail '<Connector address="*" port="8080"' '<Connector address="127.0.0.1" port="${toString port}"' \
+      --replace-fail '<CACertificatePath path="/path/to/ca/certs" />' '--><CACertificatePath path="${cacert}/etc/ssl/certs/ca-bundle.crt" /><!--'
+
+    # Create systemd service
+    mkdir -p $out/lib/systemd/system
+
+    # Wrapper to handle DB copy and startup
+    makeWrapper $out/bin/mmonit $out/bin/mmonit.wrapped \
+      --prefix PATH : ${lib.makeBinPath [ curl coreutils ]} \
+      --run "mkdir -p ${mmonitHome}/logs" \
+      --run "if ! test -f ${mmonitHome}/conf || test -L ${mmonitHome}/conf; then rm -f ${mmonitHome}/conf; ln -sfv $out/conf ${mmonitHome}/conf; fi" \
+      --run "if ! test -f ${mmonitHome}/db/mmonit.db; then mkdir -p ${mmonitHome}/db && cp -v $out/db.og/mmonit.db ${mmonitHome}/db/mmonit.db; fi" \
+      --run "if ! test -f ${mmonitHome}/license.xml; then test -f /etc/mmonit/license.xml && ln -sfv /etc/mmonit/license.xml ${mmonitHome}/license.xml; fi"
+
+    # M/Monitor upgrade script
     substituteInPlace $out/upgrade/script/data.sh \
       --replace-fail '/bin/echo' '${coreutils}/bin/echo' \
       --replace-fail 'cp ' '${coreutils}/bin/cp ' \
       --replace-fail 'mv ' '${coreutils}/bin/mv ' \
       --replace-fail 'awk ' '${gawk}/bin/awk'
 
-    # ln -sfv $out/upgrade/upgrade $out/bin/mmonit-upgrade
     cat > $out/bin/mmonit-upgrade <<EOF
     #!/usr/bin/env sh
     export PATH=${lib.makeBinPath [ coreutils gawk ]}
@@ -70,35 +91,6 @@ stdenv.mkDerivation rec {
     rm -rf "\''${MMONIT_OLD_HOME}" "\''${MMONIT_NEW_HOME}"
     EOF
     chmod +x $out/bin/mmonit-upgrade
-
-    # mkShellScriptBin
-    # makeWrapper $out/upgrade/upgrade $out/bin/mmonit-upgrade \
-    #   --prefix PATH : ${lib.makeBinPath [ coreutils gawk ]} \
-    #   --run "mkdir -p $MMONIT_OLD_HOME" \
-    #   --run "cp -a ${mmonitHome}/db ${mmonitHome}/db.bak" \
-    #   --run "cp -a ${mmonitHome}/db $MMONIT_OLD_HOME/db" \
-    #   --run "ln -sf ${mmonitHome}/conf/server.xml.orig $MMONIT_OLD_HOME/conf/server.xml" \
-
-    # Patch default server configuration
-    # https://mmonit.com/documentation/mmonit_manual.pdf#page=67
-    cp $out/conf/server.xml $out/conf/server.xml.orig
-    substituteInPlace $out/conf/server.xml \
-      --replace-fail 'sqlite:///db/mmonit.db' 'sqlite://${mmonitHome}/db/mmonit.db' \
-      --replace-fail 'Logger directory="logs"' 'Logger directory="${mmonitHome}/logs"' \
-      --replace-fail '<License file="license.xml"' '<License file="${mmonitHome}/license.xml"' \
-      --replace-fail '<Connector address="*" port="8080"' '<Connector address="127.0.0.1" port="${toString port}"' \
-      --replace-fail '<CACertificatePath path="/path/to/ca/certs" />' '--><CACertificatePath path="${cacert}/etc/ssl/certs/ca-bundle.crt" /><!--'
-
-    # Create systemd service
-    mkdir -p $out/lib/systemd/system
-
-    # Wrapper to handle DB copy and startup
-    makeWrapper $out/bin/mmonit $out/bin/mmonit.wrapped \
-      --prefix PATH : ${lib.makeBinPath [ curl coreutils ]} \
-      --run "mkdir -p ${mmonitHome}/logs" \
-      --run "if ! test -f ${mmonitHome}/conf || test -L ${mmonitHome}/conf; then rm -f ${mmonitHome}/conf; ln -sfv $out/conf ${mmonitHome}/conf; fi" \
-      --run "if ! test -f ${mmonitHome}/db/mmonit.db; then mkdir -p ${mmonitHome}/db && cp -v $out/db.og/mmonit.db ${mmonitHome}/db/mmonit.db; fi" \
-      --run "if ! test -f ${mmonitHome}/license.xml; then test -f /etc/mmonit/license.xml && ln -sfv /etc/mmonit/license.xml ${mmonitHome}/license.xml; fi"
 
     # systemd service - https://mmonit.com/wiki/MMonit/Setup
     cat > $out/lib/systemd/system/mmonit.service <<EOF
