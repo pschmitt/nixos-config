a3f2ed4fd67ac63e90b18b15076e6e74adefeefd
Philipp Schmitt <philipp@schmitt.co>
2024-05-10 08:53:25 +0200

    rename distributed nix build config file

--- common/laptop/default.nix
+++ common/laptop/default.nix
@@ -2,7 +2,7 @@
   imports = [
     inputs.hardware.nixosModules.common-pc-laptop-acpi_call
     ./network.nix
-    ../../misc/nix-remote-build.nix
+    ../../misc/nix-distributed-build.nix
   ];
 
   # services.logind.lidSwitchExternalPower = "ignore";
