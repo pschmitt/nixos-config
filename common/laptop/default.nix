366edd68e9ee577e3e0c0eda8177d0027d82c82d
Philipp Schmitt <philipp@schmitt.co>
2024-05-10 08:17:30 +0200

    Don't setup remote-builds for servers, only laptops require this

--- common/laptop/default.nix
+++ common/laptop/default.nix
@@ -2,6 +2,7 @@
   imports = [
     inputs.hardware.nixosModules.common-pc-laptop-acpi_call
     ./network.nix
+    ../../misc/nix-remote-build.nix
   ];
 
   services.logind.lidSwitchExternalPower = "ignore";
