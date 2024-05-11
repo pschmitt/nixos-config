e88293e0c36180f65a9ba64bfe91d137211b8132
Philipp Schmitt <philipp@schmitt.co>
2024-05-11 23:46:00 +0200

    ge2: ignore lid closure events

--- common/laptop/default.nix
+++ common/laptop/default.nix
@@ -1,9 +1,9 @@
-{ inputs, ... }: {
+{ inputs, lib, ... }: {
   imports = [
     inputs.hardware.nixosModules.common-pc-laptop-acpi_call
     ./network.nix
     ../../misc/nix-distributed-build.nix
   ];
 
-  # services.logind.lidSwitchExternalPower = "ignore";
+  services.logind.lidSwitchExternalPower = lib.mkDefault "suspend";
 }
