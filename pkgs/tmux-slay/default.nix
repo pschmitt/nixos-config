5b46872d341b8eaefb8fc482e6be8e8a10332dc7
Philipp Schmitt <philipp@schmitt.co>
2024-05-12 01:02:58 +0200

    Add tmux-slay pkg

---
+++ pkgs/tmux-slay/default.nix
@@ -0,0 +1,44 @@
+{ lib
+, stdenv
+, fetchFromGitHub
+, makeWrapper
+, gawk
+, gnugrep
+, gnused
+, tmux
+}:
+
+stdenv.mkDerivation rec {
+  pname = "tmux-slay";
+  version = "b5a4db5e0247afbaa52e955993986aaed4575b85";
+
+  src = fetchFromGitHub {
+    owner = "pschmitt";
+    repo = "${pname}";
+    rev = "${version}";
+    hash = "sha256-1BtrN2r3DKvGVU7bhl77uYEt0ZGiTrYpz/eOugmFNwk=";
+  };
+
+  nativeBuildInputs = [ makeWrapper ];
+
+  installPhase = ''
+    mkdir -p $out/bin
+    cp $src/tmux-slay $out/bin/tmux-slay
+
+    wrapProgram $out/bin/tmux-slay --prefix PATH : ${lib.makeBinPath [
+      gawk
+      gnugrep
+      gnused
+      tmux
+    ]}
+  '';
+
+  meta = with lib; {
+    description = "TMUX script to run commands in a background session. Similar to abduco/dtach.";
+    homepage = "https://github.com/pschmitt/tmux-slay";
+    license = licenses.gpl3Only;
+    maintainers = with maintainers; [ pschmitt ];
+    mainProgram = "tmux-slay";
+    platforms = platforms.all;
+  };
+}
