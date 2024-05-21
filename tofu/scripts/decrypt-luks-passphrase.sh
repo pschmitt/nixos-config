7026fc9453a78538acf1ddb72013aa39598c8c81
Philipp Schmitt <philipp@schmitt.co>
2024-05-21 15:07:50 +0200

    sops compat

--- tofu/scripts/decrypt-luks-passphrase.sh
+++ tofu/scripts/decrypt-luks-passphrase.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env bash
 
 cd "$(cd "$(dirname "$0")" >/dev/null 2>&1; pwd -P)" || exit 9
-
-AGENIX_DIR="${AGENIX_DIR:-/etc/nixos/secrets}"
-AGE_IDENTITY_FILE="${AGE_IDENTITY_FILE:-/home/pschmitt/.ssh/id_ed25519}"
 TARGET_HOST="${TARGET_HOST:-rofl-02}"
 
-age --decrypt --identity "$AGE_IDENTITY_FILE" \
-  "${AGENIX_DIR}/${TARGET_HOST}/luks-passphrase-root.age" | \
+# age
+# AGENIX_DIR="${AGENIX_DIR:-/etc/nixos/secrets}"
+# AGE_IDENTITY_FILE="${AGE_IDENTITY_FILE:-/home/pschmitt/.ssh/id_ed25519}"
+#
+# age --decrypt --identity "$AGE_IDENTITY_FILE" \
+#   "${AGENIX_DIR}/${TARGET_HOST}/luks-passphrase-root.age" | \
+#   tr -d '\n'
+
+# sops
+sops --decrypt --extract '["luks"]["root"]' \
+  "../../hosts/${TARGET_HOST}/luks.sops.yaml" | \
   tr -d '\n'
