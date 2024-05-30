bb5ef98bd589a36596f1d535ca836ba28681e3e7
Philipp Schmitt <philipp@schmitt.co>
2024-05-30 13:27:49 +0200

    Use a fixed tdarr version

--- misc/tdarr.nix
+++ misc/tdarr.nix
@@ -1,9 +1,8 @@
 { config, ... }:
 {
-
   config.virtualisation.oci-containers.containers = {
     tdarr = {
-      image = "ghcr.io/haveagitgat/tdarr_node:latest";
+      image = "ghcr.io/haveagitgat/tdarr_node:2.19.01";
       volumes = [
         # NOTE podman will *not* create these directories on the host
         # "/srv/tdarr/config/tdarr:/app/configs"
@@ -15,20 +14,28 @@
         "/mnt/data/videos:/media"
       ];
       autoStart = true;
-      environment = {
-        TZ = "Europe/Berlin";
-        PUID = "1000";
-        PGID = "1000";
-        UMASK_SET = "002";
-        nodeName = "${config.networking.hostName}";
-        serverIP = "100.122.144.144"; # rofl-02.netbird.cloud
-        serverPort = "8266";
-        inContainer = "true";
-        ffmpegVersion = "6";
-        NVIDIA_DRIVER_CAPABILITIES = "all";
-        NVIDIA_VISIBLE_DEVICES = "all";
-      };
-      extraOptions = [ "--gpus=all" ];
+      environment =
+        {
+          TZ = "Europe/Berlin";
+          PUID = "1000";
+          PGID = "1000";
+          UMASK_SET = "002";
+          nodeName = "${config.networking.hostName}";
+          serverIP = "rofl-02.netbird.cloud";
+          serverPort = "8266";
+          inContainer = "true";
+          ffmpegVersion = "6";
+        }
+        // (
+          if config.hardware.nvidia-container-toolkit.enable then
+            {
+              NVIDIA_VISIBLE_DEVICES = "all";
+              NVIDIA_DRIVER_CAPABILITIES = "all";
+            }
+          else
+            { }
+        );
+      extraOptions = if config.hardware.nvidia-container-toolkit.enable then [ "--gpus=all" ] else [ ];
     };
   };
 }
