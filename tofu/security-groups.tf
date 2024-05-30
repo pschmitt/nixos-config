e8685d8e67e92881e765e2f3dca656a2e0f92ed5
Philipp Schmitt <philipp@schmitt.co>
2024-05-30 11:57:20 +0200

    Add http(s)-alt

--- tofu/security-groups.tf
+++ tofu/security-groups.tf
@@ -48,6 +48,26 @@ resource "openstack_networking_secgroup_rule_v2" "secgroup_rule_http_v6" {
   security_group_id = openstack_networking_secgroup_v2.secgroup_http.id
 }
 
+resource "openstack_networking_secgroup_rule_v2" "secgroup_rule_http_alt_v4" {
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "tcp"
+  port_range_min    = 8080
+  port_range_max    = 8080
+  remote_ip_prefix  = "0.0.0.0/0"
+  security_group_id = openstack_networking_secgroup_v2.secgroup_http.id
+}
+
+resource "openstack_networking_secgroup_rule_v2" "secgroup_rule_http_alt_v6" {
+  direction         = "ingress"
+  ethertype         = "IPv6"
+  protocol          = "tcp"
+  port_range_min    = 8080
+  port_range_max    = 8080
+  remote_ip_prefix  = "::/0"
+  security_group_id = openstack_networking_secgroup_v2.secgroup_http.id
+}
+
 resource "openstack_networking_secgroup_rule_v2" "secgroup_rule_https_v4" {
   direction         = "ingress"
   ethertype         = "IPv4"
@@ -64,6 +84,26 @@ resource "openstack_networking_secgroup_rule_v2" "secgroup_rule_https_v6" {
   protocol          = "tcp"
   port_range_min    = 443
   port_range_max    = 443
+  remote_ip_prefix  = "::/0"
+  security_group_id = openstack_networking_secgroup_v2.secgroup_http.id
+}
+
+resource "openstack_networking_secgroup_rule_v2" "secgroup_rule_https_alt_v4" {
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "tcp"
+  port_range_min    = 8443
+  port_range_max    = 8443
+  remote_ip_prefix  = "0.0.0.0/0"
+  security_group_id = openstack_networking_secgroup_v2.secgroup_http.id
+}
+
+resource "openstack_networking_secgroup_rule_v2" "secgroup_rule_https_alt_v6" {
+  direction         = "ingress"
+  ethertype         = "IPv6"
+  protocol          = "tcp"
+  port_range_min    = 8443
+  port_range_max    = 8443
   remote_ip_prefix  = "::/0"
   security_group_id = openstack_networking_secgroup_v2.secgroup_http.id
 }
