ce439bb77e20cfe58050c9a473142a6b9f34e679
Philipp Schmitt <philipp@schmitt.co>
2024-09-18 14:30:18 +0200

    DRY + set OCR languages to en + de + fr

--- hosts/rofl-02/paperless-ngx.nix
+++ hosts/rofl-02/paperless-ngx.nix
@@ -28,34 +28,39 @@ in
       PAPERLESS_ALLOWED_HOSTS = lib.concatStringsSep "," hostnames;
       PAPERLESS_CORS_ALLOWED_HOSTS = lib.concatStringsSep "," hostnamesWithSchema;
       PAPERLESS_CSRF_TRUSTED_ORIGINS = lib.concatStringsSep "," hostnamesWithSchema;
+
+      PAPERLESS_OCR_LANGUAGE = "deu+fra+eng";
+      PAPERLESS_CONSUMER_RECURSIVE = true;
     };
     passwordFile = config.sops.secrets."paperless-ngx/adminPassword".path;
   };
 
   # networking.firewall.allowedTCPPorts = [ 28981 ];
 
-  services.nginx = {
-    virtualHosts =
-      let
-        commonConfig = {
-          enableACME = true;
-          # FIXME https://github.com/NixOS/nixpkgs/issues/210807
-          acmeRoot = null;
-          forceSSL = true;
-          locations."/".extraConfig = ''
-            proxy_pass http://127.0.0.1:${toString config.services.paperless.port};
-            proxy_set_header Host $host;
-            proxy_redirect http:// https://;
-            # proxy_http_version 1.1;
-            proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-            proxy_set_header Upgrade $http_upgrade;
-            proxy_set_header Connection $connection_upgrade;
-          '';
-        };
-      in
-      {
-        "paperless.${config.networking.hostName}.brkn.lol" = commonConfig;
-        "paperless.${config.networking.hostName}.heimat.dev" = commonConfig;
-      };
-  };
+  services.nginx =
+    let
+      virtualHosts = builtins.listToAttrs (
+        map (hostname: {
+          name = hostname;
+          value = {
+            enableACME = true;
+            # FIXME https://github.com/NixOS/nixpkgs/issues/210807
+            acmeRoot = null;
+            forceSSL = true;
+            locations."/".extraConfig = ''
+              proxy_pass http://127.0.0.1:${toString config.services.paperless.port};
+              proxy_set_header Host $host;
+              proxy_redirect http:// https://;
+              # proxy_http_version 1.1;
+              proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+              proxy_set_header Upgrade $http_upgrade;
+              proxy_set_header Connection $connection_upgrade;
+            '';
+          };
+        }) hostnames
+      );
+    in
+    {
+      virtualHosts = virtualHosts;
+    };
 }
