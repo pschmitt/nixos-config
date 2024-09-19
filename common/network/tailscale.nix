a3c16837adc988bfce68c42ef68514494ee4db3d
Philipp Schmitt <philipp@schmitt.co>
2024-09-19 10:26:30 +0200

    fmt

--- common/network/tailscale.nix
+++ common/network/tailscale.nix
@@ -5,7 +5,14 @@
   services.tailscale = {
     enable = true;
     openFirewall = true;
-    extraUpFlags = if config.services.netbird.enable then [ "--netfilter-mode=off" ] else [ ];
+    extraUpFlags =
+      if config.services.netbird.enable then
+        [
+          "--netfilter-mode=off"
+          "--accept-dns=false"
+        ]
+      else
+        [ "--accept-dns=false" ];
     useRoutingFeatures = "both";
     authKeyFile = config.sops.secrets."tailscale/auth-key".path;
   };
