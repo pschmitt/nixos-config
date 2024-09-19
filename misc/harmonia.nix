2b244eb26a3068bf5c9ef6ad7bd1d4f4a5129527
Philipp Schmitt <philipp@schmitt.co>
2024-09-19 10:27:29 +0200

    signKeyPath -> signKeyPaths

--- misc/harmonia.nix
+++ misc/harmonia.nix
@@ -6,7 +6,7 @@
 
   services.harmonia = {
     enable = true;
-    signKeyPath = config.sops.secrets."nix/store/privkey".path;
+    signKeyPaths = [ config.sops.secrets."nix/store/privkey".path ];
     settings = {
       bind = "127.0.0.1:5000";
     };
