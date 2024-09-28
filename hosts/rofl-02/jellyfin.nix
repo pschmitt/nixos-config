c7d30e18dbaa6c873ea74d5d1ffc8b25f1381eab
Philipp Schmitt <philipp@schmitt.co>
2024-09-28 17:00:18 +0200

    jellyfin: more host entries

--- hosts/rofl-02/jellyfin.nix
+++ hosts/rofl-02/jellyfin.nix
@@ -1,29 +1,48 @@
-{ config, ... }:
+{ config, lib, ... }:
+let
+  domains = [
+    "brkn.lol"
+    "heimat.dev"
+  ];
+  subdomains = [
+    "jelly"
+    "jellyfin"
+    "media"
+    "tv"
+  ];
+  hostnames = lib.concatMap (
+    domain:
+    lib.concatMap (subdomain: [
+      "${subdomain}.${domain}"
+      "${subdomain}.${config.networking.hostName}.${domain}"
+    ]) subdomains
+  ) domains;
+in
 {
-  services.nginx = {
-    virtualHosts =
-      let
-        commonConfig = {
-          enableACME = true;
-          # FIXME https://github.com/NixOS/nixpkgs/issues/210807
-          acmeRoot = null;
-          forceSSL = true;
-          locations."/".extraConfig = ''
-            proxy_pass http://127.0.0.1:8096;
-            proxy_set_header Host $host;
-            proxy_redirect http:// https://;
-            # proxy_http_version 1.1;
-            proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-            proxy_set_header Upgrade $http_upgrade;
-            proxy_set_header Connection $connection_upgrade;
-          '';
-        };
-      in
-      {
-        "jelly.${config.networking.hostName}.brkn.lol" = commonConfig;
-        "jelly.${config.networking.hostName}.heimat.dev" = commonConfig;
-        # legacy
-        "jelly.rofl-01.heimat.dev" = commonConfig;
-      };
-  };
+  services.nginx =
+    let
+      virtualHosts = builtins.listToAttrs (
+        map (hostname: {
+          name = hostname;
+          value = {
+            enableACME = true;
+            # FIXME https://github.com/NixOS/nixpkgs/issues/210807
+            acmeRoot = null;
+            forceSSL = true;
+            locations."/".extraConfig = ''
+              proxy_pass http://127.0.0.1:8096;
+              proxy_set_header Host $host;
+              proxy_redirect http:// https://;
+              # proxy_http_version 1.1;
+              proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+              proxy_set_header Upgrade $http_upgrade;
+              proxy_set_header Connection $connection_upgrade;
+            '';
+          };
+        }) hostnames
+      );
+    in
+    {
+      virtualHosts = virtualHosts;
+    };
 }
