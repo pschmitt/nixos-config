cdef4ece776ceb0eac373a024520cf5547bca64f
Philipp Schmitt <philipp@schmitt.co>
2024-09-30 22:29:22 +0200

    Add immich-face-to-album systemd service

--- hosts/rofl-02/immich.nix
+++ hosts/rofl-02/immich.nix
@@ -1,4 +1,9 @@
-{ config, lib, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 let
   domains = [
     "brkn.lol"
@@ -51,4 +56,39 @@ in
     {
       virtualHosts = virtualHosts;
     };
+
+  sops.secrets."immich/immich-face-to-album/apiKey" = {
+    sopsFile = config.custom.sopsFile;
+  };
+  sops.secrets."immich/immich-face-to-album/faces/anika" = {
+    sopsFile = config.custom.sopsFile;
+  };
+  sops.secrets."immich/immich-face-to-album/albums/anika" = {
+    sopsFile = config.custom.sopsFile;
+  };
+  sops.templates."immich-face-to-album-anika".content = ''
+    API_KEY="${config.sops.placeholder."immich/immich-face-to-album/apiKey"}"
+    FACE="${config.sops.placeholder."immich/immich-face-to-album/faces/anika"}"
+    ALBUM="${config.sops.placeholder."immich/immich-album-to-album/albums/anika"}"
+  '';
+
+  # Define the systemd service
+  systemd.services.immich-face-to-album-anika = {
+    description = "Run immich-face-to-album for Anika";
+    serviceConfig = {
+      EnvironmentFile = "${config.sops.templates."immich-face-to-album-anika".path}";
+      ExecStart = "${pkgs.immich-face-to-album} --server http://localhost:${toString config.services.immich.port} --key $API_KEY --face $FACE --album $ALBUM";
+    };
+    wantedBy = [ "multi-user.target" ];
+  };
+
+  # Define the systemd timer
+  systemd.timers.immich-face-to-album-anika = {
+    description = "Run immich-face-to-album every hour";
+    wantedBy = [ "timers.target" ];
+    timerConfig = {
+      OnCalendar = "daily";
+      Persistent = true;
+    };
+  };
 }
