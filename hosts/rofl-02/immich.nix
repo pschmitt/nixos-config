338007b919052fdf625fd3783d615c78581d7838
Philipp Schmitt <philipp@schmitt.co>
2024-09-30 22:42:52 +0200

    Add TODO

--- hosts/rofl-02/immich.nix
+++ hosts/rofl-02/immich.nix
@@ -76,6 +76,25 @@ in
   systemd.services.immich-face-to-album-anika = {
     description = "Run immich-face-to-album for Anika";
     serviceConfig = {
+      # TODO Switch to LoadCredential once we figured out a simple way
+      # to have the credentials read from the files.
+      # We might need to wrap the service in a shell script that reads these
+      # script = ''
+      #    API_KEY=$(cat $API_KEY_FILE) # or cat $CREDENTIALS_DIRECTORY/api_key
+      #    ALBUM=$(cat $ALBUM_FILE)
+      #    FACE=$(cat $FACE_FILE)
+      #    exec ${pkgs.immich-face-to-album}/bin/immich-face-to-album --server http://localhost:${toString config.services.immich.port} --key $API_KEY --face $FACE --album $ALBUM
+      # '';
+      # LoadCredential = [
+      #  "api_key:${config.sops.secrets."immich/immich-face-to-album/apiKey".path}"
+      #  "face:${config.sops.secrets."immich/immich-face-to-album/faces/anika".path}"
+      #  "album:${config.sops.secrets."immich/immich-face-to-album/albums/anika".path}"
+      #  ];
+      # Environment = {
+      #   "API_KEY_FILE=%d/api_key"
+      #   "FACE_FILE=%d/face"
+      #   "ALBUM_FILE=%d/album"
+      # };
       EnvironmentFile = "${config.sops.templates."immich-face-to-album-anika".path}";
       ExecStart = "${pkgs.immich-face-to-album}/bin/immich-face-to-album --server http://localhost:${toString config.services.immich.port} --key $API_KEY --face $FACE --album $ALBUM";
     };
