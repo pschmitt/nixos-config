cc1276cee7059ecaea7e9eab720fa8aba731d207
Philipp Schmitt <philipp@schmitt.co>
2024-10-01 21:00:34 +0200

    Restart services

--- hosts/rofl-02/docker-compose-netbird-ip-fix.nix
+++ hosts/rofl-02/docker-compose-netbird-ip-fix.nix
@@ -4,9 +4,11 @@
     description = "Update the netbird ip in docker-compose files";
     wantedBy = [ "multi-user.target" ];
     path = [
+      pkgs.bash
       pkgs.findutils
       pkgs.gnused
       pkgs.jq
+      pkgs.docker-compose
     ];
     serviceConfig = {
       Type = "oneshot";
@@ -27,8 +29,19 @@
           return 1
         fi
 
-        find -L /srv -maxdepth 2 -iname docker-compose.yaml -exec \
-          sed -r -i "s#100\.122\.[0-9]+\.[0-9]+#''${netbird_ip}#g" {} \;
+        find -L /srv -maxdepth 2 -iname docker-compose.yaml -exec sh -c '
+          file="$1"
+          netbird_ip="$2"
+
+          checksum1=$(sha256sum "$file")
+          sed -r -i "s#100\.122\.[0-9]+\.[0-9]+#''${netbird_ip}#g" "$file"
+          checksum2=$(sha256sum "$file")
+
+          if [[ "$checksum1" != "$checksum2" ]]
+          then
+            echo "$file"
+          fi
+        ' -- {} "$netbird_ip" \;
       }
 
       if ! NETBIRD_IP=$(netbird_ip) || [[ -z "$NETBIRD_IP" ]]
@@ -39,7 +52,13 @@
 
       echo "Updating all compose files in /srv to bind to $NETBIRD_IP"
 
-      patch_compose_files "$NETBIRD_IP"
+      mapfile -t UPDATED_FILES < <(patch_compose_files "$NETBIRD_IP")
+      for COMPOSE_FILE in "''${UPDATED_FILES[@]}"
+      do
+        echo "Updated $COMPOSE_FILE"
+        echo "Restarting services..."
+        docker-compose -f $COMPOSE_FILE up --force-recreate -d
+      done
     '';
   };
 
