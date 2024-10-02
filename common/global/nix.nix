8f483ae8b1f2dba99643407ae96df2b8c0f7a7f1
Philipp Schmitt <philipp@schmitt.co>
2024-10-02 15:31:48 +0200

    Fix typo

--- common/global/nix.nix
+++ common/global/nix.nix
@@ -62,7 +62,7 @@
           # "https://nix-cache.heimat.dev"
         ]
         # don't use local http cache on the same host
-        ++ lib.optionals (config.networking.hostName != "rofl-02") [ "https://cache.rofl-02.nb.brkn.lol" ]
+        ++ lib.optionals (config.networking.hostName != "rofl-02") [ "https://cache.rofl-02.nb.brkn.lol" ];
         # FIXME rofl-03 has some issues with netbird
         # ++ lib.optionals (config.networking.hostName != "rofl-03") [ "https://cache.rofl-03.nb.brkn.lol" ];
 
