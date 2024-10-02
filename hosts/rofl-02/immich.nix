b560f190611779d3505383c3c75a7251c0e6a515
Philipp Schmitt <philipp@schmitt.co>
2024-10-02 15:23:44 +0200

    Simplify immich nginx settings

--- hosts/rofl-02/immich.nix
+++ hosts/rofl-02/immich.nix
@@ -40,17 +40,16 @@ in
             # FIXME https://github.com/NixOS/nixpkgs/issues/210807
             acmeRoot = null;
             forceSSL = true;
-            locations."/".extraConfig = ''
-              proxy_pass http://127.0.0.1:${toString config.services.immich.port};
-              proxy_set_header Host $host;
-              proxy_redirect http:// https://;
-              # proxy_http_version 1.1;
-              proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-              proxy_set_header Upgrade $http_upgrade;
-              proxy_set_header Connection $connection_upgrade;
+            locations."/" = {
+              proxyPass = "http://127.0.0.1:${toString config.services.immich.port}";
+              recommendedProxySettings = true;
+              proxyWebsockets = true;
+              # Allow uploading large files
               # https://immich.app/docs/FAQ/#why-are-only-photos-and-not-videos-being-uploaded-to-immich
-              client_max_body_size 50000M;
-            '';
+              extraConfig = ''
+                client_max_body_size 50000M;
+              '';
+            };
           };
         }) hostnames
       );
