31c8179cdf255921e0828fd6ab49a8d15cbb1df5
Philipp Schmitt <philipp@schmitt.co>
2024-10-04 12:10:31 +0200

    Simplify nginx settings for harmonia

--- misc/harmonia.nix
+++ misc/harmonia.nix
@@ -19,18 +19,15 @@
       acmeRoot = null;
       forceSSL = true;
 
-      locations."/".extraConfig = ''
-        proxy_pass http://127.0.0.1:5000;
-        proxy_set_header Host $host;
-        proxy_redirect http:// https://;
-        proxy_http_version 1.1;
-        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-        proxy_set_header Upgrade $http_upgrade;
-        proxy_set_header Connection $connection_upgrade;
-
-        zstd on;
-        zstd_types application/x-nix-archive;
-      '';
+      locations."/" = {
+        proxyPass = "http://${config.services.harmonia.settings.bind}";
+        recommendedProxySettings = true;
+        proxyWebsockets = true;
+        extraConfig = ''
+          zstd on;
+          zstd_types application/x-nix-archive;
+        '';
+      };
     };
   };
 }
