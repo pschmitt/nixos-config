e44baca1f3742380d54bee4f9ad3c5661276d79e
Philipp Schmitt <philipp@schmitt.co>
2024-10-05 23:04:15 +0200

    Simplify paperless-ngx nginx config

--- hosts/rofl-02/paperless-ngx.nix
+++ hosts/rofl-02/paperless-ngx.nix
@@ -47,15 +47,11 @@ in
             # FIXME https://github.com/NixOS/nixpkgs/issues/210807
             acmeRoot = null;
             forceSSL = true;
-            locations."/".extraConfig = ''
-              proxy_pass http://127.0.0.1:${toString config.services.paperless.port};
-              proxy_set_header Host $host;
-              proxy_redirect http:// https://;
-              # proxy_http_version 1.1;
-              proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-              proxy_set_header Upgrade $http_upgrade;
-              proxy_set_header Connection $connection_upgrade;
-            '';
+            locations."/" = {
+              proxyPass = "http://${config.services.paperless.address}:${toString config.services.paperless.port}";
+              proxyWebsockets = true;
+              recommendedProxySettings = true;
+            };
           };
         }) hostnames
       );
