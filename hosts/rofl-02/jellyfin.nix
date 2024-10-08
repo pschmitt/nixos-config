937bde29a79deb17897b5c791855ca7c51715240
Philipp Schmitt <philipp@schmitt.co>
2024-10-08 22:54:09 +0200

    Shorten jellyfin nginx conf

--- hosts/rofl-02/jellyfin.nix
+++ hosts/rofl-02/jellyfin.nix
@@ -29,15 +29,11 @@ in
             # FIXME https://github.com/NixOS/nixpkgs/issues/210807
             acmeRoot = null;
             forceSSL = true;
-            locations."/".extraConfig = ''
-              proxy_pass http://127.0.0.1:8096;
-              proxy_set_header Host $host;
-              proxy_redirect http:// https://;
-              # proxy_http_version 1.1;
-              proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-              proxy_set_header Upgrade $http_upgrade;
-              proxy_set_header Connection $connection_upgrade;
-            '';
+            locations."/" = {
+              proxyPass = "http://127.0.0.1:8096";
+              proxyWebsockets = true;
+              recommendedProxySettings = true;
+            };
           };
         }) hostnames
       );
