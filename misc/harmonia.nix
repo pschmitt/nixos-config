83f6e6300529205236e0d378c94997ca0acdcade
Philipp Schmitt <philipp@schmitt.co>
2024-10-08 21:58:22 +0200

    Public nix cache (with basic auth)

--- misc/harmonia.nix
+++ misc/harmonia.nix
@@ -1,7 +1,14 @@
 { config, ... }:
 {
-  sops.secrets."nix/store/privkey" = {
-    sopsFile = config.custom.sopsFile;
+  sops = {
+    secrets = {
+      "nix/store/privkey" = {
+        sopsFile = config.custom.sopsFile;
+      };
+      "nix/credentials/htpasswd" = {
+        owner = "nginx";
+      };
+    };
   };
 
   services.harmonia = {
@@ -18,6 +25,24 @@
       # FIXME https://github.com/NixOS/nixpkgs/issues/210807
       acmeRoot = null;
       forceSSL = true;
+
+      locations."/" = {
+        proxyPass = "http://${config.services.harmonia.settings.bind}";
+        recommendedProxySettings = true;
+        proxyWebsockets = true;
+        extraConfig = ''
+          zstd on;
+          zstd_types application/x-nix-archive;
+        '';
+      };
+    };
+
+    virtualHosts."cache.${config.networking.hostName}.brkn.lol" = {
+      enableACME = true;
+      # FIXME https://github.com/NixOS/nixpkgs/issues/210807
+      acmeRoot = null;
+      forceSSL = true;
+      basicAuthFile = config.sops.secrets."nix/credentials/htpasswd".path;
 
       locations."/" = {
         proxyPass = "http://${config.services.harmonia.settings.bind}";
