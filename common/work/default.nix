7b6fd5ee975591addc12835489de19591b9922d1
Philipp Schmitt <philipp@schmitt.co>
2024-10-12 10:06:53 +0200

    Remove openconnect and schuler-vpn

--- common/work/default.nix
+++ common/work/default.nix
@@ -1,17 +1,9 @@
-{
-  inputs,
-  lib,
-  config,
-  pkgs,
-  ...
-}:
+{ ... }:
 {
   imports = [
     ./deckmaster.nix
     ./jcalapi.nix
-    ./vpn/schuler-vpn.nix
     ./vpn/netbird.nix
-    # ./vpn/openconnect.nix
     ./vpn/openvpn.nix
   ];
 }
