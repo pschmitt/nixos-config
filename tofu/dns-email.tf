77f1ad660a0baa0d2056cea4387d1bbc1ee22cee
Philipp Schmitt <philipp@schmitt.co>
2024-10-15 08:46:39 +0200

    tofu fmt

--- tofu/dns-email.tf
+++ tofu/dns-email.tf
@@ -85,7 +85,7 @@ resource "cloudflare_record" "dmarc" {
   # value = "v=DMARC1; p=none; sp=none; fo=0; adkim=r; aspf=r; pct=100; rf=afrf; ri=86400; rua=mailto:p@schmitt.co; ruf=mailto:p@schmitt.co"
 
   # Strict
-  value = "v=DMARC1; p=quarantine; sp=quarantine; fo=0; adkim=r; aspf=r; pct=100; rf=afrf; ri=86400; rua=mailto:p@schmitt.co; ruf=mailto:p@schmitt.co"
+  value   = "v=DMARC1; p=quarantine; sp=quarantine; fo=0; adkim=r; aspf=r; pct=100; rf=afrf; ri=86400; rua=mailto:p@schmitt.co; ruf=mailto:p@schmitt.co"
   ttl     = 3600
   comment = var.dns_email_comment
 }
