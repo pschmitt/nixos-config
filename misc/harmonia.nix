6698d8106d8ae80e2067bff091e65de5f505410e
Philipp Schmitt <philipp@schmitt.co>
2024-10-15 10:44:07 +0200

    Fix harmonia hosts

--- misc/harmonia.nix
+++ misc/harmonia.nix
@@ -1,4 +1,55 @@
 { config, lib, ... }:
+let
+  # List of Harmonia hosts with their respective configurations
+  harmonia_hosts = [
+    {
+      domain = "cache.${config.networking.hostName}.nb.brkn.lol";
+      basicAuth = false;
+    }
+    { domain = "cache.${config.networking.hostName}.brkn.lol"; }
+  ];
+
+  # primary host
+  conditional_hosts = lib.optional (config.networking.hostName == "rofl-02") [
+    { domain = "cache.brkn.lol"; }
+    { domain = "nix-cache.brkn.lol"; }
+  ];
+
+  # Function to generate virtual host configuration
+  generateVHost =
+    {
+      domain,
+      basicAuth ? true,
+    }:
+    {
+      enableACME = true;
+      # FIXME https://github.com/NixOS/nixpkgs/issues/210807
+      acmeRoot = null;
+      forceSSL = true;
+      basicAuthFile = if basicAuth then config.sops.secrets."nix/credentials/htpasswd".path else null;
+
+      locations."/" = {
+        proxyPass = "http://${config.services.harmonia.settings.bind}";
+        recommendedProxySettings = true;
+        proxyWebsockets = true;
+        extraConfig = ''
+          zstd on;
+          zstd_types application/x-nix-archive;
+        '';
+      };
+    };
+
+  # Merge the common and conditional hosts
+  all_hosts = harmonia_hosts ++ lib.flatten conditional_hosts;
+
+  # Generate virtual hosts for each host
+  virtualHosts = builtins.listToAttrs (
+    map (host: {
+      name = host.domain;
+      value = generateVHost host;
+    }) all_hosts
+  );
+in
 {
   sops = {
     secrets = {
@@ -19,55 +70,5 @@
     };
   };
 
-  services.nginx =
-    let
-      # A function to generate the virtual host configuration
-      generateVHost =
-        {
-          domain,
-          useBasicAuth ? false,
-        }:
-        {
-          enableACME = true;
-          acmeRoot = null; # FIXME https://github.com/NixOS/nixpkgs/issues/210807
-          forceSSL = true;
-
-          basicAuthFile = if useBasicAuth then config.sops.secrets."nix/credentials/htpasswd".path else null;
-
-          locations."/" = {
-            proxyPass = "http://${config.services.harmonia.settings.bind}";
-            recommendedProxySettings = true;
-            proxyWebsockets = true;
-            extraConfig = ''
-              zstd on;
-              zstd_types application/x-nix-archive;
-            '';
-          };
-        };
-    in
-    {
-      virtualHosts =
-        let
-          commonHosts = {
-            "cache.${config.networking.hostName}.nb.brkn.lol" = generateVHost {
-              domain = "cache.${config.networking.hostName}.nb.brkn.lol";
-            };
-            "cache.${config.networking.hostName}.brkn.lol" = generateVHost {
-              domain = "cache.${config.networking.hostName}.brkn.lol";
-              useBasicAuth = true;
-            };
-          };
-          conditionalHosts = lib.mkIf (config.networking.hostName == "rofl-02") {
-            "cache.brkn.lol" = generateVHost {
-              domain = "cache.brkn.lol";
-              useBasicAuth = true;
-            };
-            "nix-cache.brkn.lol" = generateVHost {
-              domain = "nix-cache.brkn.lol";
-              useBasicAuth = true;
-            };
-          };
-        in
-        commonHosts // conditionalHosts;
-    };
+  services.nginx.virtualHosts = virtualHosts;
 }
