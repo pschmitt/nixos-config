95bc672919d525435dd154bf6476eabd225dbe11
Philipp Schmitt <philipp@schmitt.co>
2024-10-16 10:50:41 +0200

    Prevent zone destruction

--- tofu/dns.tf
+++ tofu/dns.tf
@@ -6,48 +6,72 @@ resource "cloudflare_zone" "bergmann_schmitt_de" {
   zone       = "bergmann-schmitt.de"
   plan       = "free"
   account_id = cloudflare_account.me.id
+  lifecycle {
+    prevent_destroy = true
+  }
 }
 
 resource "cloudflare_zone" "brkn_lol" {
   zone       = "brkn.lol"
   plan       = "free"
   account_id = cloudflare_account.me.id
+  lifecycle {
+    prevent_destroy = true
+  }
 }
 
 resource "cloudflare_zone" "curl-pipe-sh" {
   zone       = "curl-pipe.sh"
   plan       = "free"
   account_id = cloudflare_account.me.id
+  lifecycle {
+    prevent_destroy = true
+  }
 }
 
 resource "cloudflare_zone" "heimat_dev" {
   zone       = "heimat.dev"
   plan       = "free"
   account_id = cloudflare_account.me.id
+  lifecycle {
+    prevent_destroy = true
+  }
 }
 
 resource "cloudflare_zone" "pschmitt_dev" {
   zone       = "pschmitt.dev"
   plan       = "free"
   account_id = cloudflare_account.me.id
+  lifecycle {
+    prevent_destroy = true
+  }
 }
 
 resource "cloudflare_zone" "ovm5_de" {
   zone       = "ovm5.de"
   plan       = "free"
   account_id = cloudflare_account.me.id
+  lifecycle {
+    prevent_destroy = true
+  }
 }
 
 resource "cloudflare_zone" "schmitt_co" {
   zone       = "schmitt.co"
   plan       = "free"
   account_id = cloudflare_account.me.id
+  lifecycle {
+    prevent_destroy = true
+  }
 }
 
 resource "cloudflare_zone" "schmi-tt" {
   zone       = "schmi.tt"
   plan       = "free"
   account_id = cloudflare_account.me.id
+  lifecycle {
+    prevent_destroy = true
+  }
 }
 
 resource "cloudflare_record" "wildcard-heimat-dev" {
