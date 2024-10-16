3aa67c3417793c4378299d7bbe11d903296b7067
Philipp Schmitt <philipp@schmitt.co>
2024-10-16 14:15:08 +0200

    Add schmi.tt

---
+++ tofu/dns-schmi-tt.tf
@@ -0,0 +1,17 @@
+resource "cloudflare_record" "wildcard_schmi-tt" {
+  zone_id = cloudflare_zone.schmi-tt.id
+  name    = "*"
+  value   = oci_core_instance.oci_01.public_ip
+  type    = "A"
+  proxied = false
+  ttl     = 1
+}
+
+resource "cloudflare_record" "schmi-tt" {
+  zone_id = cloudflare_zone.schmi-tt.id
+  name    = "@"
+  value   = oci_core_instance.oci_01.public_ip
+  type    = "A"
+  proxied = false
+  ttl     = 1
+}
