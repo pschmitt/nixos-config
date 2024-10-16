9b3220ced17e3821b0ed54155984e4726b72cd6b
Philipp Schmitt <philipp@schmitt.co>
2024-10-16 15:16:36 +0200

    WIP: update cf provider

--- tofu/dns-email.tf
+++ tofu/dns-email.tf
@@ -7,27 +7,27 @@ variable "domains" {
   default = {
     "pschmitt.dev" = {
       dkim_public_key = "v=DKIM1; h=sha256; k=rsa; p=MIICIjANBgkqhkiG9w0BAQEFAAOCAg8AMIICCgKCAgEAz2Yz+Sxm+fvWjnQyTpMRJFUUBBjBYENNWB/+rlQ25J9RJxWwSyIhRJQdcGquXARoKf9WT31KgbxglL4eUcRjWvGRP6Kz9e7bvseT1y4HR4lx0/yCzt4BmzsdX0BBGUL/PvWLFr0p/8hC6KNGzDpHgSAWXS8hF1yIyAe5yMnVzSogJo+cJTc+nmcH8g+j86naXmtRKXxuL8GfM97dpVwflulfUPWMhAoeTUzpUU90t45B0tz7GOCRXM4unIY0ZJDnboXlSX92vFTiVRvzgm5eBE+qUkKBZKHTcNhxZizQGZAULVhGNs3fsP8jl7ni2Pt9fEoGIzmAfQc/FYmVQtYQ5JZhGgk8SjDP8D7KzjR4Eg0kQN+oRuhDLnGrXwyJFc5bWiGt8+a5Fiy2sNAzEn8iIeucan9HFrB1oeLnvIwQMQXXhNsqPQjwmC7/2CNbaENQFFXTmZSsdY0UPkc7jBdgvO3mmokehVtAm3rGJJi/DjNDMp9a42tvvHQHHNcbdmEO3xEUFxfLSGY0dMGlWm0wGzhw6Uu6DjG3Sc5QO3AHY3Q4L4BnEIZZABw+aC1yFKDpTUbeUAhsJirH3NUFOAK4OB4CNiV4RisTAkyt7xN2P4VuAivSUY5tW88Dp76BDuwAJNa5e6O8Gjr0Qi8KeKSr9/v6ee/M5tXq59nfs/nUSgMCAwEAAQ=="
-      cloudflare_mx = false
+      cloudflare_mx   = false
     },
     "heimat.dev" = {
       dkim_public_key = "v=DKIM1; h=sha256; k=rsa;p=MIICIjANBgkqhkiG9w0BAQEFAAOCAg8AMIICCgKCAgEAxFeSdBleS75MJrFYl9HZ1aR6zOVBvRdhY4PLtCS02Ed9oLenFMHwQ5D4s7Ddq6zTHu0MRFprzJWMYcETsM0MEER5HUDuKPkpqChTLLQTqMJ4/pjleHj/zRr4GNIkJhp8xZNAcsNCDaDlfzzoY1c7euZj8BhYR8oc/0702408SYKgLq2w8fj/jVHaDcLoesYiNXlJdI4pN99lV/pv9usuMkxdwiADZl9N+PfG8Jm/ZFpkhm9qyECDLnp/G4MOr6/BzouP0ACjiYofsH8fUzXFclS3yGVv4Czd8vy4GlEOqTas6FrVtOKxJ6oFn/y0yfkwKeDmtdg76pHHB2aCTFPrajEfQ9jveEcbK7oladtsjJSJYk2Q4KwSXY0wc5icJjUWHbq5Q3rVH+BEqOGYFEJ6KKq75bQ2/SrQgwnbgKIg8uYX/4tl5ltEjYgiprUPNjxLgrQbHL+CWu9+/DMaaELi0PQ42NQc6yd1WxkMxFlKmYo3AcERn+okq6ldlF/DHrKptgrgFJTkcskdxhIro4W7vyP0nm3bZol4maCmIjxBej5ve/M1m/sVTv8w1FKSSlYe2IGGRFgQJ1dOdqehRQ5przXep2Lzn0oZ8a6fT+WfZOjHGD0d7SlXwyizq8JP9gVpDcz2mX51MaUXv+FXLztXqm9Ti6rW8RwcxeyogYrAY1sCAwEAAQ=="
-      cloudflare_mx = false
+      cloudflare_mx   = false
     },
     "brkn.lol" = {
       dkim_public_key = "v=DKIM1; h=sha256; k=rsa; p=NONE"
-      cloudflare_mx = false
+      cloudflare_mx   = false
     },
     "curl-pipe.sh" = {
       dkim_public_key = "v=DKIM1; h=sha256; k=rsa; p=NONE"
-      cloudflare_mx = true
+      cloudflare_mx   = true
     },
     "schmi.tt" = {
       dkim_public_key = "v=DKIM1; h=sha256; k=rsa; p=NONE"
-      cloudflare_mx = true
+      cloudflare_mx   = true
     },
     "ovm5.de" = {
       dkim_public_key = "v=DKIM1; h=sha256; k=rsa; p=NONE"
-      cloudflare_mx = true
+      cloudflare_mx   = true
     }
   }
 }
@@ -76,8 +76,8 @@ resource "cloudflare_record" "mx" {
   name    = "@"
   type    = "MX"
   ttl     = 3600
-  # value    = "mail.${each.key}"
-  value    = var.main_mail_domain
+  # content    = "mail.${each.key}"
+  content  = var.main_mail_domain
   priority = 10
   comment  = var.dns_email_comment
 }
@@ -89,7 +89,7 @@ resource "cloudflare_record" "mail" {
 
   zone_id = data.cloudflare_zone.zones[each.key].id
   name    = "mail"
-  value   = oci_core_instance.oci_01.public_ip
+  content = oci_core_instance.oci_01.public_ip
   type    = "A"
   ttl     = 3600
   comment = var.dns_email_comment
@@ -103,8 +103,8 @@ resource "cloudflare_record" "spf" {
   zone_id = data.cloudflare_zone.zones[each.key].id
   type    = "TXT"
   name    = "@"
-  # value   = "v=spf1 a:mail.${each.key} -all"
-  value   = "v=spf1 a:${var.main_mail_domain} -all"
+  # content   = "v=spf1 a:mail.${each.key} -all"
+  content = "v=spf1 a:${var.main_mail_domain} -all"
   ttl     = 3600
   comment = var.dns_email_comment
 }
@@ -120,13 +120,13 @@ resource "cloudflare_record" "dmarc" {
   type    = "TXT"
   name    = "_dmarc"
   # Low
-  # value   = "v=DMARC1; p=none"
+  # content   = "v=DMARC1; p=none"
 
   # Mid
-  # value = "v=DMARC1; p=none; sp=none; fo=0; adkim=r; aspf=r; pct=100; rf=afrf; ri=86400; rua=mailto:p@schmitt.co; ruf=mailto:p@schmitt.co"
+  # content = "v=DMARC1; p=none; sp=none; fo=0; adkim=r; aspf=r; pct=100; rf=afrf; ri=86400; rua=mailto:p@schmitt.co; ruf=mailto:p@schmitt.co"
 
   # Strict
-  value   = "v=DMARC1; p=quarantine; sp=quarantine; fo=0; adkim=r; aspf=r; pct=100; rf=afrf; ri=86400; rua=mailto:${var.dmarc_report_email}; ruf=mailto:${var.dmarc_report_email}"
+  content = "v=DMARC1; p=quarantine; sp=quarantine; fo=0; adkim=r; aspf=r; pct=100; rf=afrf; ri=86400; rua=mailto:${var.dmarc_report_email}; ruf=mailto:${var.dmarc_report_email}"
   ttl     = 3600
   comment = var.dns_email_comment
 }
@@ -139,7 +139,7 @@ resource "cloudflare_record" "dmarc-report" {
   zone_id = cloudflare_zone.schmitt_co.id
   type    = "TXT"
   name    = "${each.key}._report._dmarc"
-  value   = "v=DMARC1;"
+  content = "v=DMARC1;"
   ttl     = 3600
   comment = var.dns_email_comment
 }
@@ -152,7 +152,7 @@ resource "cloudflare_record" "dkim" {
   zone_id = data.cloudflare_zone.zones[each.key].id
   type    = "TXT"
   name    = "mail._domainkey"
-  value   = var.domains[each.key].dkim_public_key
+  content = var.domains[each.key].dkim_public_key
   ttl     = 3600
   comment = var.dns_email_comment
 }
@@ -165,7 +165,7 @@ resource "cloudflare_record" "mailconf" {
   zone_id = data.cloudflare_zone.zones[each.key].id
   type    = "TXT"
   name    = "@"
-  value   = "mailconf=https://autoconfig.${each.key}/mail/config-v1.1.xml"
+  content = "mailconf=https://autoconfig.${each.key}/mail/config-v1.1.xml"
   ttl     = 3600
   comment = var.dns_email_comment
 }
@@ -178,7 +178,7 @@ resource "cloudflare_record" "autoconfig" {
   zone_id = data.cloudflare_zone.zones[each.key].id
   type    = "A"
   name    = "autoconfig"
-  value   = oci_core_instance.oci_01.public_ip
+  content = oci_core_instance.oci_01.public_ip
   ttl     = 3600
   comment = var.dns_email_comment
 }
@@ -191,11 +191,12 @@ resource "cloudflare_record" "autoconfigure" {
   zone_id = data.cloudflare_zone.zones[each.key].id
   type    = "A"
   name    = "autoconfigure"
-  value   = oci_core_instance.oci_01.public_ip
+  content = oci_core_instance.oci_01.public_ip
   ttl     = 3600
   comment = var.dns_email_comment
 }
 
+# srv records
 resource "cloudflare_record" "srv-autodiscover" {
   for_each = {
     for domain, config in var.domains : domain => config if config.cloudflare_mx == false
@@ -208,9 +209,6 @@ resource "cloudflare_record" "srv-autodiscover" {
   comment = var.dns_email_comment
 
   data {
-    service  = "_autodiscover"
-    proto    = "_tcp"
-    name     = "autodiscover-srv"
     priority = 0
     weight   = 0
     port     = 443
@@ -231,9 +229,6 @@ resource "cloudflare_record" "srv-imap" { # starttls
   comment = var.dns_email_comment
 
   data {
-    service  = "_imap"
-    proto    = "_tcp"
-    name     = "imap-srv"
     priority = 0
     weight   = 0
     port     = 143
@@ -254,9 +249,6 @@ resource "cloudflare_record" "srv-imaps" {
   comment = var.dns_email_comment
 
   data {
-    service  = "_imaps"
-    proto    = "_tcp"
-    name     = "imaps-srv"
     priority = 0
     weight   = 0
     port     = 993
@@ -277,9 +269,6 @@ resource "cloudflare_record" "srv-imaps" {
 #   comment = var.dns_email_comment
 #
 #   data {
-#     service  = "_pop3s"
-#     proto    = "_tcp"
-#     name     = "pop3s-srv"
 #     priority = 0
 #     weight   = 0
 #     port     = 995
@@ -300,9 +289,6 @@ resource "cloudflare_record" "srv-submission" { # starttls
   comment = var.dns_email_comment
 
   data {
-    service  = "_submission"
-    proto    = "_tcp"
-    name     = "submission-srv"
     priority = 0
     weight   = 0
     port     = 587
@@ -323,9 +309,6 @@ resource "cloudflare_record" "srv-submissions" {
   comment = var.dns_email_comment
 
   data {
-    service  = "_submissions"
-    proto    = "_tcp"
-    name     = "submissions-srv"
     priority = 0
     weight   = 0
     port     = 465
@@ -343,7 +326,7 @@ resource "cloudflare_record" "cname-smtp" {
   zone_id = data.cloudflare_zone.zones[each.key].id
   type    = "CNAME"
   name    = "smtp"
-  value   = var.main_mail_domain
+  content = var.main_mail_domain
   ttl     = 3600
   comment = var.dns_email_comment
 }
@@ -356,7 +339,7 @@ resource "cloudflare_record" "cname-imap" {
   zone_id = data.cloudflare_zone.zones[each.key].id
   type    = "CNAME"
   name    = "imap"
-  value   = var.main_mail_domain
+  content = var.main_mail_domain
   ttl     = 3600
   comment = var.dns_email_comment
 }
