d4a51295a47b53d8cbe9b1475cec9c17389e57d0
Philipp Schmitt <philipp@schmitt.co>
2024-10-16 15:16:36 +0200

    WIP: update cf provider

--- tofu/dns-bergmann-schmitt-de.tf
+++ tofu/dns-bergmann-schmitt-de.tf
@@ -1,37 +1,37 @@
 resource "cloudflare_record" "bergmann_schmitt_de_mx1" {
-  zone_id = cloudflare_zone.bergmann_schmitt_de.id
-  name    = "bergmann-schmitt.de"
-  value   = "mx.zoho.com"
-  type    = "MX"
+  zone_id  = cloudflare_zone.bergmann_schmitt_de.id
+  name     = "bergmann-schmitt.de"
+  content  = "mx.zoho.com"
+  type     = "MX"
   priority = 10
-  proxied = false
-  ttl     = 1
+  proxied  = false
+  ttl      = 1
 }
 
 resource "cloudflare_record" "bergmann_schmitt_de_mx2" {
-  zone_id = cloudflare_zone.bergmann_schmitt_de.id
-  name    = "bergmann-schmitt.de"
-  value   = "mx2.zoho.com"
-  type    = "MX"
+  zone_id  = cloudflare_zone.bergmann_schmitt_de.id
+  name     = "bergmann-schmitt.de"
+  content  = "mx2.zoho.com"
+  type     = "MX"
   priority = 20
-  proxied = false
-  ttl     = 1
+  proxied  = false
+  ttl      = 1
 }
 
 resource "cloudflare_record" "bergmann_schmitt_de_mx3" {
-  zone_id = cloudflare_zone.bergmann_schmitt_de.id
-  name    = "bergmann-schmitt.de"
-  value   = "mx3.zoho.com"
-  type    = "MX"
+  zone_id  = cloudflare_zone.bergmann_schmitt_de.id
+  name     = "bergmann-schmitt.de"
+  content  = "mx3.zoho.com"
+  type     = "MX"
   priority = 50
-  proxied = false
-  ttl     = 1
+  proxied  = false
+  ttl      = 1
 }
 
 resource "cloudflare_record" "bergmann_schmitt_de_spf" {
   zone_id = cloudflare_zone.bergmann_schmitt_de.id
   name    = "bergmann-schmitt.de"
-  value   = "v=spf1 include:zoho.com ~all"
+  content = "v=spf1 include:zoho.com ~all"
   type    = "TXT"
   proxied = false
   ttl     = 1
@@ -40,7 +40,7 @@ resource "cloudflare_record" "bergmann_schmitt_de_spf" {
 resource "cloudflare_record" "bergmann_schmitt_de_zoho_verification" {
   zone_id = cloudflare_zone.bergmann_schmitt_de.id
   name    = "bergmann-schmitt.de"
-  value   = "zoho-verification=zb83290461.zmverify.zoho.com"
+  content = "zoho-verification=zb83290461.zmverify.zoho.com"
   type    = "TXT"
   proxied = false
   ttl     = 1
@@ -49,7 +49,7 @@ resource "cloudflare_record" "bergmann_schmitt_de_zoho_verification" {
 resource "cloudflare_record" "bergmann_schmitt_de_dkim" {
   zone_id = cloudflare_zone.bergmann_schmitt_de.id
   name    = "zmail._domainkey"
-  value   = "v=DKIM1; k=rsa; p=MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCeRQJ2jB2YF0d6o28fYPIXVgfg+IEbBXJAV2Pgp9HFQqsuuy0Owh3EAgBz9l9GLa312kDohBSqP/x7EAB1om2e1MuEe7E+s31u6zPk/UfagbviDyQ6ICFyFd522HMuNjrjbVkGjwwrMAPtfbRQDwxi2ZlLUdTAA9OHCPZNVEIe5wIDAQAB"
+  content = "v=DKIM1; k=rsa; p=MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCeRQJ2jB2YF0d6o28fYPIXVgfg+IEbBXJAV2Pgp9HFQqsuuy0Owh3EAgBz9l9GLa312kDohBSqP/x7EAB1om2e1MuEe7E+s31u6zPk/UfagbviDyQ6ICFyFd522HMuNjrjbVkGjwwrMAPtfbRQDwxi2ZlLUdTAA9OHCPZNVEIe5wIDAQAB"
   type    = "TXT"
   proxied = false
   ttl     = 1
--- tofu/dns-curl-pipe-sh.tf
+++ tofu/dns-curl-pipe-sh.tf
@@ -1,7 +1,7 @@
 resource "cloudflare_record" "wildcard_curl-pipe-sh" {
   zone_id = cloudflare_zone.curl-pipe-sh.id
   name    = "*"
-  value   = oci_core_instance.oci_01.public_ip
+  content = oci_core_instance.oci_01.public_ip
   type    = "A"
   proxied = false
   ttl     = 1
@@ -10,7 +10,7 @@ resource "cloudflare_record" "wildcard_curl-pipe-sh" {
 resource "cloudflare_record" "curl-pipe-sh" {
   zone_id = cloudflare_zone.curl-pipe-sh.id
   name    = "@"
-  value   = oci_core_instance.oci_01.public_ip
+  content = oci_core_instance.oci_01.public_ip
   type    = "A"
   proxied = false
   ttl     = 1
