9b3962382bb31ee6ece520dbd827c30c5eecfa41
Philipp Schmitt <philipp@schmitt.co>
2024-10-17 08:58:12 +0200

    Add server-globuli.de

--- tofu/dns-email.tf
+++ tofu/dns-email.tf
@@ -29,6 +29,10 @@ variable "domains" {
       dkim_public_key = "v=DKIM1; h=sha256; k=rsa; p=NONE"
       cloudflare_mx   = true
     }
+    "server-globuli.de" = {
+      dkim_public_key = "v=DKIM1; h=sha256; k=rsa; p=NONE"
+      cloudflare_mx   = true
+    }
   }
 }
 
