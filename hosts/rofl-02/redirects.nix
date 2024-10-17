44701c4bdb7ae8a41269c1c0660ad31b659420e2
Philipp Schmitt <philipp@schmitt.co>
2024-10-17 22:29:28 +0200

    Add redirects

---
+++ hosts/rofl-02/redirects.nix
@@ -0,0 +1,14 @@
+{ config, ... }:
+{
+  services.nginx.virtualHosts = {
+    "y.${config.custom.mainDomain}" = {
+      enableACME = true;
+      # FIXME https://github.com/NixOS/nixpkgs/issues/210807
+      acmeRoot = null;
+      forceSSL = true;
+      locations."/" = {
+        return = "301 https://raw.githubusercontent.com/pschmitt/yadm-init/master/init.sh";
+      };
+    };
+  };
+}
