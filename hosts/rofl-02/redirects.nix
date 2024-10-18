c95d4aadca3f2cd7e5e3a86d719e762230cfe1db
Philipp Schmitt <philipp@schmitt.co>
2024-10-18 12:34:06 +0200

    Inline the pschmitt-dev package

--- hosts/rofl-02/redirects.nix
+++ hosts/rofl-02/redirects.nix
@@ -1,4 +1,5 @@
 { config, pkgs, ... }:
+
 {
   services.nginx.virtualHosts = {
     "y.${config.custom.mainDomain}" = {
@@ -17,7 +18,12 @@
       acmeRoot = null;
       forceSSL = true;
       locations."/" = {
-        root = pkgs.pschmitt-dev;
+        root = pkgs.fetchFromGitHub {
+          owner = "pschmitt";
+          repo = "pschmitt.dev";
+          rev = "35f63e78171ea9775572c57d7849681b1ffbabe4";
+          hash = "sha256-n7EA/v9lKTEFCbPJNG3iSp6u8XJ7KNu//FI0wH6ieu8=";
+        };
       };
     };
   };
