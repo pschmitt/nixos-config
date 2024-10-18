642ce0a4989973ea1504c88f444184302272ec49
Philipp Schmitt <philipp@schmitt.co>
2024-10-18 17:58:55 +0200

    Add xdg default apps

---
+++ home-manager/default-apps.nix
@@ -0,0 +1,15 @@
+{
+  xdg.mimeApps = {
+    enable = true;
+    defaultApplications = {
+      "text/html" = "firefox.desktop";
+      "x-scheme-handler/http" = "firefox.desktop";
+      "x-scheme-handler/https" = "firefox.desktop";
+      "x-scheme-handler/about" = "firefox.desktop";
+      "x-scheme-handler/unknown" = "firefox.desktop";
+
+      # FIXME This does not seem to work?
+      "application/pdf" = "org.gnome.Evince.desktop";
+    };
+  };
+}
