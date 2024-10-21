bdd2f468c61c002dd68e835760946378e83fa46d
Philipp Schmitt <philipp@schmitt.co>
2024-10-21 23:51:53 +0200

    Add bw-backup

---
+++ services/bw-backup.nix
@@ -0,0 +1,24 @@
+{ config, pkgs, ... }:
+
+{
+  sops.secrets."bw-backup" = {
+    sopsFile = config.custom.sopsFile;
+  };
+  sops
+  virtualisation.oci-containers.containers = {
+    bw-backup = {
+      image = "ghcr.io/pschmitt/bw-backup:latest";
+      autoStart = true;
+      environmentFiles = [
+        config.sops.secrets."bw-backup".path
+      ];
+      environment = {
+        CRON = "0 0 * * *";
+        # DEBUG = "true";
+      };
+      volumes = [
+        "/srv/bw-backup:/data"
+      ];
+    };
+  };
+}
