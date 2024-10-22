b83f95581f799b8c21a0c4cf454f156828b50bc4
Philipp Schmitt <philipp@schmitt.co>
2024-10-22 09:43:51 +0200

    fix path

--- hosts/oci-03/monit.nix
+++ hosts/oci-03/monit.nix
@@ -3,6 +3,7 @@ let
   mmonitVersionCheck = pkgs.writeShellScript "mmonit-version-check" ''
     export PATH=${
       pkgs.lib.makeBinPath [
+        pkgs.coreutils
         pkgs.curl
         pkgs.gawk
         pkgs.gnugrep
