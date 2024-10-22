1919a0f71624f60218c341fa07d8d0f0e0ba2445
Philipp Schmitt <philipp@schmitt.co>
2024-10-22 12:05:21 +0200

    Update hypridle and hyprlock

--- flake.nix
+++ flake.nix
@@ -51,7 +51,6 @@
       type = "git";
       url = "https://github.com/hyprwm/Hyprland";
       # https://github.com/hyprwm/Hyprland/releases
-      # v0.44.1
       ref = "refs/tags/v0.44.1";
       # git ls-remote --tags https://github.com/hyprwm/Hyprland | sort --version-sort -k 2 | tail -1 | awk '{ print $1 }'
       # rev = "4520b30d498daca8079365bdb909a8dea38e8d55";
