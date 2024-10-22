7c50b9c91cb43217501e747201ca59fa6cead67c
Philipp Schmitt <philipp@schmitt.co>
2024-10-22 12:02:48 +0200

    Add bw creds

--- common/laptop/default.nix
+++ common/laptop/default.nix
@@ -4,6 +4,7 @@
     inputs.hardware.nixosModules.common-pc-laptop-acpi_call
     ./network.nix
     ../../misc/nix-distributed-build.nix
+    ../../services/bitwarden.nix
   ];
 
   services.logind.lidSwitchExternalPower = lib.mkDefault "suspend";
