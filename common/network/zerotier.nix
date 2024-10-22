c9bce1999c10c5bd3d9924fad220b749b540c84c
Philipp Schmitt <philipp@schmitt.co>
2024-10-22 21:18:43 +0200

    Add zerotier

---
+++ common/network/zerotier.nix
@@ -0,0 +1,24 @@
+{ config, ... }:
+let
+  networkId = "12ac4a1e716b9d7d";
+in
+{
+  sops.secrets."zerotier/api-token" = {
+    owner = "zeronsd";
+  };
+
+  services.zerotierone = {
+    enable = true;
+    joinNetworks = [ networkId ];
+    localConf = { };
+  };
+
+  services.zeronsd = {
+    servedNetworks."${networkId}" = {
+      settings = {
+        domain = "zerotier.internal";
+        token = config.sops.secrets."zerotier/api-token".path;
+      };
+    };
+  };
+}
