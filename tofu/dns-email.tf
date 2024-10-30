e49ba56f181b23500eaa61e035a8e96163cc6f45
Philipp Schmitt <philipp@schmitt.co>
2024-10-30 13:52:14 +0100

    DRY email DNS entries

--- tofu/dns-email.tf
+++ tofu/dns-email.tf
@@ -1,41 +1,47 @@
 variable "domains" {
   type = map(object({
-    dkim_public_key = string
-    cloudflare_mx   = bool
+    dkim_public_key = optional(string, null)
+    mx_provider     = string
+    dmarc_policy    = optional(string, null)
   }))
+
+  validation {
+    condition     = alltrue([for domain in var.domains : contains(["custom", "google", "cloudflare"], domain.mx_provider)])
+    error_message = "The mx_provider must be one of 'custom', 'google', or 'cloudflare'."
+  }
 
   default = {
     "pschmitt.dev" = {
       dkim_public_key = "v=DKIM1; h=sha256; k=rsa; p=MIICIjANBgkqhkiG9w0BAQEFAAOCAg8AMIICCgKCAgEAz2Yz+Sxm+fvWjnQyTpMRJFUUBBjBYENNWB/+rlQ25J9RJxWwSyIhRJQdcGquXARoKf9WT31KgbxglL4eUcRjWvGRP6Kz9e7bvseT1y4HR4lx0/yCzt4BmzsdX0BBGUL/PvWLFr0p/8hC6KNGzDpHgSAWXS8hF1yIyAe5yMnVzSogJo+cJTc+nmcH8g+j86naXmtRKXxuL8GfM97dpVwflulfUPWMhAoeTUzpUU90t45B0tz7GOCRXM4unIY0ZJDnboXlSX92vFTiVRvzgm5eBE+qUkKBZKHTcNhxZizQGZAULVhGNs3fsP8jl7ni2Pt9fEoGIzmAfQc/FYmVQtYQ5JZhGgk8SjDP8D7KzjR4Eg0kQN+oRuhDLnGrXwyJFc5bWiGt8+a5Fiy2sNAzEn8iIeucan9HFrB1oeLnvIwQMQXXhNsqPQjwmC7/2CNbaENQFFXTmZSsdY0UPkc7jBdgvO3mmokehVtAm3rGJJi/DjNDMp9a42tvvHQHHNcbdmEO3xEUFxfLSGY0dMGlWm0wGzhw6Uu6DjG3Sc5QO3AHY3Q4L4BnEIZZABw+aC1yFKDpTUbeUAhsJirH3NUFOAK4OB4CNiV4RisTAkyt7xN2P4VuAivSUY5tW88Dp76BDuwAJNa5e6O8Gjr0Qi8KeKSr9/v6ee/M5tXq59nfs/nUSgMCAwEAAQ=="
-      cloudflare_mx   = false
+      mx_provider     = "custom"
     },
     "heimat.dev" = {
       dkim_public_key = "v=DKIM1; h=sha256; k=rsa;p=MIICIjANBgkqhkiG9w0BAQEFAAOCAg8AMIICCgKCAgEAxFeSdBleS75MJrFYl9HZ1aR6zOVBvRdhY4PLtCS02Ed9oLenFMHwQ5D4s7Ddq6zTHu0MRFprzJWMYcETsM0MEER5HUDuKPkpqChTLLQTqMJ4/pjleHj/zRr4GNIkJhp8xZNAcsNCDaDlfzzoY1c7euZj8BhYR8oc/0702408SYKgLq2w8fj/jVHaDcLoesYiNXlJdI4pN99lV/pv9usuMkxdwiADZl9N+PfG8Jm/ZFpkhm9qyECDLnp/G4MOr6/BzouP0ACjiYofsH8fUzXFclS3yGVv4Czd8vy4GlEOqTas6FrVtOKxJ6oFn/y0yfkwKeDmtdg76pHHB2aCTFPrajEfQ9jveEcbK7oladtsjJSJYk2Q4KwSXY0wc5icJjUWHbq5Q3rVH+BEqOGYFEJ6KKq75bQ2/SrQgwnbgKIg8uYX/4tl5ltEjYgiprUPNjxLgrQbHL+CWu9+/DMaaELi0PQ42NQc6yd1WxkMxFlKmYo3AcERn+okq6ldlF/DHrKptgrgFJTkcskdxhIro4W7vyP0nm3bZol4maCmIjxBej5ve/M1m/sVTv8w1FKSSlYe2IGGRFgQJ1dOdqehRQ5przXep2Lzn0oZ8a6fT+WfZOjHGD0d7SlXwyizq8JP9gVpDcz2mX51MaUXv+FXLztXqm9Ti6rW8RwcxeyogYrAY1sCAwEAAQ=="
-      cloudflare_mx   = false
+      mx_provider     = "custom"
     },
     "brkn.lol" = {
       dkim_public_key = "v=DKIM1; h=sha256; k=rsa; p=MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAzYK3JqwjHHIhk9OxYBE/FHIZxb7SkzZQLMx8XboOtoEAMsNJ99iKJ0mvywtadIt1giywb/q5xaFFqjsByWxA3vBIdqxTXSjNtTqV93qfq8Lw5YTwk7BGXZoVmMblg9aYOJRkF4LhH0uxOURnQz+Lhoj/cz4bnehfHEE/qii1cKXUhW4wTN4HypC22A3IVcNf3z+eTOcu8KPljpKARka7r0gmgPiunLpppafgbSZKtnycIvLdyTu45AD3+aTi836aZsX0vTty9wTl2JyB1buSpiq54IJyma8YSiwphCzBo4PI9OCXlIHTcuRLcU0id99BN6q3A4kE/Q85ks6DPv0dUQIDAQAB"
-      cloudflare_mx   = false
+      mx_provider     = "custom"
     },
     "curl-pipe.sh" = {
-      dkim_public_key = "v=DKIM1; h=sha256; k=rsa; p=NONE"
-      cloudflare_mx   = true
+      mx_provider = "cloudflare"
     },
     "schmi.tt" = {
-      dkim_public_key = "v=DKIM1; h=sha256; k=rsa; p=NONE"
-      cloudflare_mx   = true
+      mx_provider = "cloudflare"
     },
     "ovm5.de" = {
-      dkim_public_key = "v=DKIM1; h=sha256; k=rsa; p=NONE"
-      cloudflare_mx   = true
+      mx_provider = "cloudflare"
     }
     "server-globuli.de" = {
-      dkim_public_key = "v=DKIM1; h=sha256; k=rsa; p=NONE"
-      cloudflare_mx   = true
+      mx_provider = "cloudflare"
     }
     "bergmann-schmitt.de" = {
-      dkim_public_key = "v=DKIM1; h=sha256; k=rsa; p=NONE"
-      cloudflare_mx   = true
+      mx_provider = "cloudflare"
+    }
+    "schmitt.co" = {
+      mx_provider     = "google"
+      dkim_public_key = "v=DKIM1; k=rsa; p=MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCFfKjXJaCES5Z7YSp5OF+aavonYY4me9FzNZ1XgeQYPDOl9dPP1M7w2X8c9j2jgLPeOU7bs2ZDh+MiYU2OeHYFwl6uIO5BEeqhvQcJRJtfNorUvgfJ4v4Hyk5GbSS8OKs3AyskX4m+ImzVnwzjISVh89yLnTNxOs9sWPhpH3sRpQIDAQAB"
+      dmarc_policy    = "mid"
     }
   }
 }
@@ -67,7 +73,7 @@ variable "main_mail_domain" {
 
 resource "cloudflare_email_routing_settings" "cf_mail_routing" {
   for_each = {
-    for domain, config in var.domains : domain => config if config.cloudflare_mx == true
+    for domain, config in var.domains : domain => config if config.mx_provider == "cloudflare"
   }
   zone_id = data.cloudflare_zone.zones[each.key].id
   # FIXME Having this set to false raises an error when applying. Let's just not
@@ -77,22 +83,22 @@ resource "cloudflare_email_routing_settings" "cf_mail_routing" {
 
 resource "cloudflare_record" "mx" {
   for_each = {
-    for domain, config in var.domains : domain => config if config.cloudflare_mx == false
+    for domain, config in var.domains : domain => config if config.mx_provider != "cloudflare"
   }
 
   zone_id = data.cloudflare_zone.zones[each.key].id
   name    = "@"
   type    = "MX"
   ttl     = 3600
-  # content    = "mail.${each.key}"
-  content  = var.main_mail_domain
-  priority = 10
+  content = each.value.mx_provider == "google" ? "smtp.google.com" : var.main_mail_domain
+  # TODO Guess we could just use prio=1 for our custom domains as well
+  priority = each.value.mx_provider == "google" ? 1 : 10
   comment  = var.dns_email_comment
 }
 
 resource "cloudflare_record" "mail" {
   for_each = {
-    for domain, config in var.domains : domain => config if config.cloudflare_mx == false
+    for domain, config in var.domains : domain => config if config.mx_provider == "custom"
   }
 
   zone_id = data.cloudflare_zone.zones[each.key].id
@@ -105,14 +111,13 @@ resource "cloudflare_record" "mail" {
 
 resource "cloudflare_record" "spf" {
   for_each = {
-    for domain, config in var.domains : domain => config if config.cloudflare_mx == false
+    for domain, config in var.domains : domain => config if config.mx_provider != "cloudflare"
   }
 
   zone_id = data.cloudflare_zone.zones[each.key].id
   type    = "TXT"
   name    = "@"
-  # content   = "v=spf1 a:mail.${each.key} -all"
-  content = "v=spf1 a:${var.main_mail_domain} -all"
+  content = each.value.mx_provider == "google" ? "v=spf1 include:_spf.google.com ~all" : "v=spf1 a:${var.main_mail_domain} -all"
   ttl     = 3600
   comment = var.dns_email_comment
 }
@@ -121,20 +126,20 @@ resource "cloudflare_record" "spf" {
 resource "cloudflare_record" "dmarc" {
   # for_each = data.cloudflare_zone.zones
   for_each = {
-    for domain, config in var.domains : domain => config if config.cloudflare_mx == false
+    for domain, config in var.domains : domain => config if config.mx_provider != "cloudflare"
   }
 
   zone_id = data.cloudflare_zone.zones[each.key].id
   type    = "TXT"
   name    = "_dmarc"
   # Low
-  # content   = "v=DMARC1; p=none"
-
+  # content = "v=DMARC1; p=none"
   # Mid
-  # content = "v=DMARC1; p=none; sp=none; fo=0; adkim=r; aspf=r; pct=100; rf=afrf; ri=86400; rua=mailto:p@schmitt.co; ruf=mailto:p@schmitt.co"
-
+  # content = "v=DMARC1; p=none; sp=none; fo=0; adkim=r; aspf=r; pct=100; rf=afrf; ri=86400; rua=mailto:${var.dmarc_report_email}; ruf=mailto:${var.dmarc_report_email}"
   # Strict
-  content = "v=DMARC1; p=quarantine; sp=quarantine; fo=0; adkim=r; aspf=r; pct=100; rf=afrf; ri=86400; rua=mailto:${var.dmarc_report_email}; ruf=mailto:${var.dmarc_report_email}"
+  # content = "v=DMARC1; p=quarantine; sp=quarantine; fo=0; adkim=r; aspf=r; pct=100; rf=afrf; ri=86400; rua=mailto:${var.dmarc_report_email}; ruf=mailto:${var.dmarc_report_email}"
+
+  content = var.domains[each.key].dmarc_policy == "mid" ? "v=DMARC1; p=none; sp=none; fo=0; adkim=r; aspf=r; pct=100; rf=afrf; ri=86400; rua=mailto:${var.dmarc_report_email}; ruf=mailto:${var.dmarc_report_email}" : "v=DMARC1; p=quarantine; sp=quarantine; fo=0; adkim=r; aspf=r; pct=100; rf=afrf; ri=86400; rua=mailto:${var.dmarc_report_email}; ruf=mailto:${var.dmarc_report_email}"
   ttl     = 3600
   comment = var.dns_email_comment
 }
@@ -142,7 +147,7 @@ resource "cloudflare_record" "dmarc" {
 # Allow receiving DMARC reports for other zones/domains
 resource "cloudflare_record" "dmarc-report" {
   for_each = {
-    for domain, config in var.domains : domain => config if config.cloudflare_mx == false
+    for domain, config in var.domains : domain => config if config.mx_provider != "cloudflare" && domain != "schmitt.co"
   }
   zone_id = cloudflare_zone.schmitt_co.id
   type    = "TXT"
@@ -154,7 +159,7 @@ resource "cloudflare_record" "dmarc-report" {
 
 resource "cloudflare_record" "dkim" {
   for_each = {
-    for domain, config in var.domains : domain => config if config.cloudflare_mx == false
+    for domain, config in var.domains : domain => config if config.mx_provider == "custom" && config.dkim_public_key != null
   }
 
   zone_id = data.cloudflare_zone.zones[each.key].id
@@ -165,9 +170,22 @@ resource "cloudflare_record" "dkim" {
   comment = var.dns_email_comment
 }
 
+resource "cloudflare_record" "dkim_google" {
+  for_each = {
+    for domain, config in var.domains : domain => config if config.mx_provider == "google" && config.dkim_public_key != null
+  }
+
+  zone_id = data.cloudflare_zone.zones[each.key].id
+  type    = "TXT"
+  name    = "${each.key}._domainkey"
+  content = var.domains[each.key].dkim_public_key
+  ttl     = 3600
+  comment = var.dns_email_comment
+}
+
 resource "cloudflare_record" "mailconf" {
   for_each = {
-    for domain, config in var.domains : domain => config if config.cloudflare_mx == false
+    for domain, config in var.domains : domain => config if config.mx_provider != "cloudflare"
   }
 
   zone_id = data.cloudflare_zone.zones[each.key].id
@@ -180,7 +198,7 @@ resource "cloudflare_record" "mailconf" {
 
 resource "cloudflare_record" "autoconfig" {
   for_each = {
-    for domain, config in var.domains : domain => config if config.cloudflare_mx == false
+    for domain, config in var.domains : domain => config if config.mx_provider != "cloudflare"
   }
 
   zone_id = data.cloudflare_zone.zones[each.key].id
@@ -193,7 +211,7 @@ resource "cloudflare_record" "autoconfig" {
 
 resource "cloudflare_record" "autoconfigure" {
   for_each = {
-    for domain, config in var.domains : domain => config if config.cloudflare_mx == false
+    for domain, config in var.domains : domain => config if config.mx_provider != "cloudflare"
   }
 
   zone_id = data.cloudflare_zone.zones[each.key].id
@@ -207,7 +225,7 @@ resource "cloudflare_record" "autoconfigure" {
 # srv records
 resource "cloudflare_record" "srv-autodiscover" {
   for_each = {
-    for domain, config in var.domains : domain => config if config.cloudflare_mx == false
+    for domain, config in var.domains : domain => config if config.mx_provider != "cloudflare"
   }
 
   zone_id = data.cloudflare_zone.zones[each.key].id
@@ -220,14 +238,13 @@ resource "cloudflare_record" "srv-autodiscover" {
     priority = 0
     weight   = 0
     port     = 443
-    # target   = "mail.${each.key}"
-    target = "autoconfig.brkn.lol"
+    target   = "autoconfig.${each.key}"
   }
 }
 
 resource "cloudflare_record" "srv-imap" { # starttls
   for_each = {
-    for domain, config in var.domains : domain => config if config.cloudflare_mx == false
+    for domain, config in var.domains : domain => config if config.mx_provider != "cloudflare"
   }
 
   zone_id = data.cloudflare_zone.zones[each.key].id
@@ -240,14 +257,13 @@ resource "cloudflare_record" "srv-imap" { # starttls
     priority = 0
     weight   = 0
     port     = 143
-    # target   = "mail.${each.key}"
-    target = var.main_mail_domain
+    target   = each.value.mx_provider == "google" ? "imap.gmail.com" : var.main_mail_domain
   }
 }
 
 resource "cloudflare_record" "srv-imaps" {
   for_each = {
-    for domain, config in var.domains : domain => config if config.cloudflare_mx == false
+    for domain, config in var.domains : domain => config if config.mx_provider != "cloudflare"
   }
 
   zone_id = data.cloudflare_zone.zones[each.key].id
@@ -260,14 +276,13 @@ resource "cloudflare_record" "srv-imaps" {
     priority = 0
     weight   = 0
     port     = 993
-    # target   = "mail.${each.key}"
-    target = var.main_mail_domain
+    target   = each.value.mx_provider == "google" ? "imap.gmail.com" : var.main_mail_domain
   }
 }
 
 # resource "cloudflare_record" "srv-pop3s" {
 #   for_each = {
-#     for domain, config in var.domains : domain => config if config.cloudflare_mx == false
+#     for domain, config in var.domains : domain => config if config.mx_provider != "cloudflare"
 #   }
 #
 #   zone_id = data.cloudflare_zone.zones[each.key].id
@@ -280,14 +295,13 @@ resource "cloudflare_record" "srv-imaps" {
 #     priority = 0
 #     weight   = 0
 #     port     = 995
-#     # target   = "mail.${each.key}"
-#     target  = var.main_mail_domain
+#     target   = each.value.mx_provider == "google" ? "pop.gmail.com" : var.main_mail_domain
 #   }
 # }
 
 resource "cloudflare_record" "srv-submission" { # starttls
   for_each = {
-    for domain, config in var.domains : domain => config if config.cloudflare_mx == false
+    for domain, config in var.domains : domain => config if config.mx_provider != "cloudflare"
   }
 
   zone_id = data.cloudflare_zone.zones[each.key].id
@@ -300,14 +314,13 @@ resource "cloudflare_record" "srv-submission" { # starttls
     priority = 0
     weight   = 0
     port     = 587
-    # target   = "mail.${each.key}"
-    target = var.main_mail_domain
+    target   = each.value.mx_provider == "google" ? "smtp.gmail.com" : var.main_mail_domain
   }
 }
 
 resource "cloudflare_record" "srv-submissions" {
   for_each = {
-    for domain, config in var.domains : domain => config if config.cloudflare_mx == false
+    for domain, config in var.domains : domain => config if config.mx_provider != "cloudflare"
   }
 
   zone_id = data.cloudflare_zone.zones[each.key].id
@@ -320,34 +333,33 @@ resource "cloudflare_record" "srv-submissions" {
     priority = 0
     weight   = 0
     port     = 465
-    # target   = "mail.${each.key}"
-    target = var.main_mail_domain
+    target   = each.value.mx_provider == "google" ? "smtp.gmail.com" : var.main_mail_domain
   }
 }
 
 # cnames
 resource "cloudflare_record" "cname-smtp" {
   for_each = {
-    for domain, config in var.domains : domain => config if config.cloudflare_mx == false
+    for domain, config in var.domains : domain => config if config.mx_provider != "cloudflare"
   }
 
   zone_id = data.cloudflare_zone.zones[each.key].id
   type    = "CNAME"
   name    = "smtp"
-  content = var.main_mail_domain
+  content = each.value.mx_provider == "google" ? "smtp.gmail.com" : var.main_mail_domain
   ttl     = 3600
   comment = var.dns_email_comment
 }
 
 resource "cloudflare_record" "cname-imap" {
   for_each = {
-    for domain, config in var.domains : domain => config if config.cloudflare_mx == false
+    for domain, config in var.domains : domain => config if config.mx_provider != "cloudflare"
   }
 
   zone_id = data.cloudflare_zone.zones[each.key].id
   type    = "CNAME"
   name    = "imap"
-  content = var.main_mail_domain
+  content = each.value.mx_provider == "google" ? "imap.gmail.com" : var.main_mail_domain
   ttl     = 3600
   comment = var.dns_email_comment
 }
