813023a5f478ff42a805f24324c93a7ecaf783de
Philipp Schmitt <philipp@schmitt.co>
2024-10-30 15:41:37 +0100

    cachix for lan-mouse

--- common/gui/apps.nix
+++ common/gui/apps.nix
@@ -1,11 +1,18 @@
 { inputs, pkgs, ... }:
 {
+  # https://app.cachix.org/cache/lan-mouse
+  nix.settings = {
+    # lan-mouse flake
+    substituters = [ "https://lan-mouse.cachix.org" ];
+    trusted-public-keys = [ "lan-mouse.cachix.org-1:KlE2AEZUgkzNKM7BIzMQo8w9yJYqUpor1CAUNRY6OyM=" ];
+  };
+
   environment.systemPackages = with pkgs; [
     alacritty
     foot
     kitty
     inputs.wezterm.packages.${pkgs.system}.default
-    inputs.lan-mouse.packages.${pkgs.system}.default
+    # inputs.lan-mouse.packages.${pkgs.system}.default
 
     # files and docs
     eog
