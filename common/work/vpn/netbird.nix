11577a782bc8b30b11e5a47843fe4f8e96d9c53e
Philipp Schmitt <philipp@schmitt.co>
2024-11-13 17:39:00 +0100

    add wiit2 and make sure we use master.netbird

--- common/work/vpn/netbird.nix
+++ common/work/vpn/netbird.nix
@@ -17,7 +17,7 @@ let
       # TODO Stupidly prepending sudo to the command doesn't work and just
       # breaks the netbird cli
       # exec /run/wrappers/bin/sudo ${pkgs.netbird}/bin/netbird "$@"
-      exec ${pkgs.netbird}/bin/netbird "$@"
+      exec ${pkgs.master.netbird}/bin/netbird "$@"
     '';
 
   netbirdScripts = lib.mapAttrsToList createNetbirdScript config.services.netbird.tunnels;
@@ -30,6 +30,14 @@ in
         port = 51821;
         environment = {
           NB_MANAGEMENT_URL = "https://nb.gec.io";
+          NB_ALLOW_SSH = "false";
+        };
+      };
+      wiit2 = {
+        port = 51822;
+        environment = {
+          NB_MANAGEMENT_URL = "https://grpc.nb2.gec.io";
+          NB_ADMIN_URL = "https://nb2.gec.io";
           NB_ALLOW_SSH = "false";
         };
       };
