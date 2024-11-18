ee53e056acfdfb4b68fc4d832504982e4db5383a
Philipp Schmitt <philipp@schmitt.co>
2024-11-18 14:10:47 +0100

    Add wireguard-tools (with wg-json)

--- common/network/default.nix
+++ common/network/default.nix
@@ -1,9 +1,13 @@
-{ ... }:
+{ pkgs, ... }:
 {
   imports = [
     ./network.nix
     ./netbird.nix
     ./tailscale.nix
     ./zerotier.nix
+  ];
+
+  environment.systemPackages = with pkgs; [
+    wireguard-tools
   ];
 }
