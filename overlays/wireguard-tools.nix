af7b3470090e8f9e571880529893a46394365074
Philipp Schmitt <philipp@schmitt.co>
2024-11-18 14:10:47 +0100

    Add wireguard-tools (with wg-json)

---
+++ overlays/wireguard-tools.nix
@@ -0,0 +1,21 @@
+{ final, prev }:
+
+{
+  wireguard-tools = prev.wireguard-tools.overrideAttrs (oldAttrs: {
+    postInstall =
+      oldAttrs.postInstall or ""
+      + ''
+        # Add wg-json script to the output
+        cp $src/contrib/json/wg-json $out/bin/
+        chmod +x $out/bin/wg-json
+      '';
+
+    postFixup =
+      ''
+        # Replace "wg" with the full path to the wg binary in wg-json
+        substituteInPlace $out/bin/wg-json \
+          --replace "wg show" "$out/bin/wg show"
+      ''
+      + (oldAttrs.postFixup or "");
+  });
+}
