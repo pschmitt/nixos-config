d357f7093923824682dd4e5d5b3cd09df2f27af3
Philipp Schmitt <philipp@schmitt.co>
2024-11-20 12:10:58 +0100

    Fix HID_NAME

--- common/gui/logitech-mouse.nix
+++ common/gui/logitech-mouse.nix
@@ -6,7 +6,7 @@
   services.udev.extraRules = ''
     ACTION=="bind", SUBSYSTEM=="hid", ENV{HID_NAME}=="MX Master 3S", \
     RUN+="${pkgs.libratbag}/bin/ratbagctl 'MX Master 3S' dpi set 3000"
-    ACTION=="bind", SUBSYSTEM=="hid", ENV{HID_NAME}=="MX Vertical", \
+    ACTION=="bind", SUBSYSTEM=="hid", ENV{HID_NAME}=="Logitech MX Vertical Advanced Ergonomic Mouse", \
     RUN+="${pkgs.libratbag}/bin/ratbagctl 'MX Vertical' dpi set 3000"
   '';
 }
