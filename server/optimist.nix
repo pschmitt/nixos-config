c970122273346769784388af43280809450349a3
Philipp Schmitt <philipp@schmitt.co>
2024-11-23 09:05:19 +0100

    netbird: Force masquerading

--- server/optimist.nix
+++ server/optimist.nix
@@ -3,4 +3,12 @@
 {
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   custom.netbirdSetupKey = lib.mkForce "optimist";
+
+  # HACK Fix netbird port forwarding
+  # Set up NAT (Masquerading)
+  networking.nat = {
+    enable = true;
+    externalInterface = "ens3";
+    internalIPs = [ "100.122.0.0/16" ]; # Netbird subnet
+  };
 }
