d21b79a2e34a878ab5d026cc920132b0bde630eb
Philipp Schmitt <philipp@schmitt.co>
2024-11-23 22:32:23 +0100

    enable autoprune

--- common/global/containers.nix
+++ common/global/containers.nix
@@ -1,8 +1,28 @@
 { ... }:
 {
-  # FIXME as of 2024-10-21 podman is failing to start more than one container
-  # as root
-  # Error: netavark: code: 1, msg: iptables: Chain already exists.
-  # repro: sudo podman run -ti --rm ghcr.io/pschmitt/debug
-  virtualisation.oci-containers.backend = "docker";
+  virtualisation = {
+    # FIXME as of 2024-10-21 podman is failing to start more than one container
+    # as root
+    # Error: netavark: code: 1, msg: iptables: Chain already exists.
+    # repro: sudo podman run -ti --rm ghcr.io/pschmitt/debug
+    oci-containers.backend = "docker";
+
+    podman = {
+      enable = true;
+      dockerCompat = false;
+      defaultNetwork.settings = {
+        dns_enabled = true;
+      };
+      autoPrune = {
+        enable = true;
+      };
+    };
+    docker = {
+      enable = true;
+      storageDriver = "btrfs";
+      autoPrune = {
+        enable = true;
+      };
+    };
+  };
 }
