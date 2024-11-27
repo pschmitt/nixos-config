467b25d246b2071dce47e8a9ce09817e9c567321
Philipp Schmitt <philipp@schmitt.co>
2024-11-27 13:21:38 +0100

    add postup script to provision routes over the netbird interface (as opposed to the ovpn intf)

--- common/work/vpn/netbird.nix
+++ common/work/vpn/netbird.nix
@@ -20,7 +20,31 @@ let
       exec ${pkgs.master.netbird}/bin/netbird "$@"
     '';
 
-  netbirdScripts = lib.mapAttrsToList createNetbirdScript config.services.netbird.tunnels;
+  netbirdScripts = lib.genAttrs (lib.attrNames config.services.netbird.tunnels) (
+    name: createNetbirdScript name config.services.netbird.tunnels.${name}
+  );
+
+  netbirdWiitForceRoutes = pkgs.writeShellScriptBin "netbird-wiit-force-routes" ''
+    ACTION="add"
+
+    case "$1" in
+      -h|--help)
+        echo "Usage: $(basename "$0") [--delete]"
+        exit 0
+        ;;
+      *undo|*remove|*del*|*rm*|*clear*)
+        ACTION="delete"
+        ;;
+    esac
+
+    NB_INTERFACE_NAME="wiit"
+
+    ${netbirdScripts.wiit}/bin/netbird-wiit routes list | \
+      ${pkgs.gawk}/bin/awk '/Network: 10\./ { print $2 }' | \
+      ${pkgs.coreutils}/bin/sort -u | \
+      ${pkgs.findutils}/bin/xargs --verbose -I {} \
+        ${pkgs.iproute2}/bin/ip route "$ACTION" {} dev "$NB_INTERFACE_NAME"
+  '';
 in
 {
   services.netbird = {
@@ -36,5 +60,8 @@ in
     };
   };
 
-  environment.systemPackages = netbirdScripts;
+  # FIXME This does not seem to get triggered when the service starts
+  systemd.services.netbird-wiit.postStart = "${netbirdWiitForceRoutes}/bin/netbird-wiit-force-routes";
+
+  environment.systemPackages = lib.attrValues netbirdScripts ++ [ netbirdWiitForceRoutes ];
 }
