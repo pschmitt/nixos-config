7ae1f642cbdae6f173d51e7d84a42f06e0fbeb95
Philipp Schmitt <philipp@schmitt.co>
2024-12-01 12:07:22 +0100

    Merge libratbag and piper overlays

--- overlays/libratbag.nix
+++ overlays/libratbag.nix
@@ -1,10 +1,12 @@
+# TODO remove this overlay once this is merged:
+# https://github.com/NixOS/nixpkgs/pull/345782
 { final, prev }:
 {
   libratbag = prev.libratbag.overrideAttrs (
     oldAttrs:
     let
       # see https://github.com/libratbag/libratbag/commits/master/
-      version = "8444ceb638b19c3fbeb073a5cd29f17c6d34dd07";
+      version = "v0.18";
     in
     {
       pname = "libratbag";
@@ -14,7 +16,28 @@
         owner = "libratbag";
         repo = "libratbag";
         rev = version;
-        sha256 = "sha256-9rlnGQ7kcXqX+8mFb0imnzLo0X6Nuca6fcMv+H6ZwEw=";
+        hash = "sha256-dAWKDF5hegvKhUZ4JW2J/P9uSs4xNrZLNinhAff6NSc=";
+      };
+    }
+  );
+
+  piper = prev.piper.overrideAttrs (
+    oldAttrs:
+    let
+      # see https://github.com/libratbag/piper/commits/master/
+      version = "0.8";
+    in
+    {
+      pname = "piper";
+      version = version;
+
+      mesonFlags = [ "-Druntime-dependency-checks=false" ];
+
+      src = prev.fetchFromGitHub {
+        owner = "libratbag";
+        repo = "piper";
+        rev = version;
+        sha256 = "sha256-j58fL6jJAzeagy5/1FmygUhdBm+PAlIkw22Rl/fLff4=";
       };
     }
   );
