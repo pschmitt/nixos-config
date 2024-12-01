c28485c0f4ba57b6fe2a18764fe97516dc06cc13
Philipp Schmitt <philipp@schmitt.co>
2024-12-01 21:11:32 +0100

    Add NOTE about snapper and the .snapshots subvol

--- common/gui/btrfs.nix
+++ common/gui/btrfs.nix
@@ -1,7 +1,4 @@
 {
-  inputs,
-  lib,
-  config,
   pkgs,
   ...
 }:
@@ -14,7 +11,9 @@
     configs = {
       home = {
         FSTYPE = "btrfs";
-        SUBVOLUME = "/home";
+        # NOTE make sure that there is a .snapshots subvolume!
+        # sudo btrfs subvolume create /home/.snapshots
+        SUBVOLUME = "/home"; # @home won't work here
         ALLOW_USERS = [ "pschmitt" ];
         TIMELINE_CREATE = true;
         TIMELINE_CLEANUP = true;
