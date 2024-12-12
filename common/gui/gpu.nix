5783cdcd848337c70e7b68bb47a779b09a64b248
Philipp Schmitt <philipp@schmitt.co>
2024-12-12 14:43:32 +0100

    Add -render symlinks

--- common/gui/gpu.nix
+++ common/gui/gpu.nix
@@ -3,8 +3,10 @@
   services.udev.extraRules = ''
     # Persistent symlink for INTEL GPU
     KERNEL=="card*", SUBSYSTEMS=="pci", ATTRS{vendor}=="0x8086", ATTRS{class}=="0x03[0-9]*", SYMLINK+="dri/intel-gpu"
+    KERNEL=="renderD*", SUBSYSTEMS=="pci", ATTRS{vendor}=="0x8086", ATTRS{class}=="0x03[0-9]*", SYMLINK+="dri/intel-gpu-render"
 
     # Persistent symlink for NVIDIA GPU
     KERNEL=="card*", SUBSYSTEMS=="pci", ATTRS{vendor}=="0x10de", ATTRS{class}=="0x03[0-9]*", SYMLINK+="dri/nvidia-gpu"
+    KERNEL=="renderD*", SUBSYSTEMS=="pci", ATTRS{vendor}=="0x10de", ATTRS{class}=="0x03[0-9]*", SYMLINK+="dri/nvidia-gpu-render"
   '';
 }
