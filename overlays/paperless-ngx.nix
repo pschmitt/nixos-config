2211b4f52ff395eb51c9200584208e2b1f046ac8
Philipp Schmitt <philipp@schmitt.co>
2024-12-27 18:11:27 +0100

    next attempt to disable paperless-ngx tests

--- overlays/paperless-ngx.nix
+++ overlays/paperless-ngx.nix
@@ -1,6 +1,7 @@
 { final, prev }:
 {
   paperless-ngx = prev.paperless-ngx.overrideAttrs (oldAttrs: {
+    doCheck = false;
     disabledTests = (oldAttrs.disabledTests or [ ]) ++ [
       "test_consume_file"
     ];
