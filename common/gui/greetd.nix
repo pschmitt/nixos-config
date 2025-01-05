52f408f7e486d97210b0c8b1f4e0cdbfad3b8a33
Philipp Schmitt <philipp@schmitt.co>
2025-01-05 17:00:32 +0100

    uwsm+greetd: Support initial start of more wayland compositors

--- common/gui/greetd.nix
+++ common/gui/greetd.nix
@@ -4,17 +4,59 @@
   ...
 }:
 let
-  hyprland-uwsm = pkgs.writeShellApplication {
-    name = "hyprland-uwsm";
+  # Single script that dynamically extracts and runs the Exec= line
+  # from the matching “-uwsm.desktop” file, given a compositor name.
+  uwsm-run = pkgs.writeShellApplication {
+    name = "uwsm-run";
     runtimeInputs = [
       pkgs.gawk
       pkgs.util-linux
     ];
     text = ''
-      CMD=$(awk -F '=' '/^Exec=/ { print $2; exit }' \
-       "${config.services.displayManager.sessionData.desktops}/share/wayland-sessions/hyprland-uwsm.desktop")
-      logger -t greetd "Starting session: $CMD"
-      eval "$CMD"
+      log() {
+        logger -t greetd "$*"
+      }
+
+      COMPOSITOR="''${1:-hyprland}"
+      DESKTOP_FILE="${config.services.displayManager.sessionData.desktops}/share/wayland-sessions/''${COMPOSITOR}-uwsm.desktop"
+
+      if [[ ! -f "$DESKTOP_FILE" ]]
+      then
+        log "Desktop file not found: $DESKTOP_FILE"
+        exit 1
+      fi
+
+      # extract cmd from desktop file
+      CMD=$(awk -F '=' '/^Exec=/ { print $2; exit }' "$DESKTOP_FILE")
+
+      # FIXME This won't work as CMD is the entire line!
+      # example: uwsm-xxx ARGS ACTUAL_COMMAND
+      # if [[ ! -x "$CMD" ]]
+      # then
+      #   logger -t greetd "$CMD is not executable"
+      #   exit 1
+      # fi
+
+      log "Starting uwsm desktop session using command: $CMD"
+      log "UWSM Desktop file: $DESKTOP_FILE"
+      exec $CMD
+    '';
+
+  };
+
+  hyprland-uwsm = pkgs.writeShellApplication {
+    name = "hyprland-uwsm";
+    runtimeInputs = [ ];
+    text = ''
+      exec ${uwsm-run}/bin/uwsm-run hyprland
+    '';
+  };
+
+  sway-uwsm = pkgs.writeShellApplication {
+    name = "sway-uwsm";
+    runtimeInputs = [ ];
+    text = ''
+      exec ${uwsm-run}/bin/uwsm-run sway
     '';
   };
 in
@@ -27,6 +69,7 @@ in
     settings = {
       initial_session = {
         command = "${hyprland-uwsm}/bin/hyprland-uwsm";
+        # command = "${sway-uwsm}/bin/sway-uwsm";
         user = config.custom.username;
       };
 
