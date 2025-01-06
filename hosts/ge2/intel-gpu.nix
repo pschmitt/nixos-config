53ef3781a7f0a33593ee1fdad03aee544d99638f
Philipp Schmitt <philipp@schmitt.co>
2025-01-06 07:34:44 +0100

    xe driver + kernel 6.12.8 seems to work on ge2 :)

--- hosts/ge2/intel-gpu.nix
+++ hosts/ge2/intel-gpu.nix
@@ -11,6 +11,12 @@
     inputs.hardware.nixosModules.common-gpu-intel
   ];
 
+  # Force xe driver
+  # https://wiki.archlinux.org/title/Intel_graphics#Testing_the_new_experimental_Xe_driver
+  boot.kernelParams = [
+    "i915.force_probe=!a7a0"
+    "xe.force_probe=a7a0"
+  ];
   hardware.intelgpu.driver = "xe";
-  # hardware.intelgpu.vaapiDriver = "intel-media-driver";
+  hardware.intelgpu.vaapiDriver = "intel-media-driver";
 }
