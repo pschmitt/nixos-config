d9c6ec939b4825192ca2b85b05cd9db764f937dc
Philipp Schmitt <philipp@schmitt.co>
2025-01-12 15:41:57 +0100

    Add a few more gnome settings

--- home-manager/gui/gnome.nix
+++ home-manager/gui/gnome.nix
@@ -1,10 +1,34 @@
 { ... }:
 {
   dconf.settings = {
+    "org/gnome/desktop/interface" = {
+      show-battery-percentage = true;
+    };
+
     # Resize windows with super+right mouse
     # https://wiki.archlinux.org/title/GNOME#Resize_windows_by_mouse
     "org/gnome/desktop/wm/preferences" = {
       resize-with-right-button = true;
+    };
+
+    # close with Super+Shift+C
+    "org/gnome/desktop/wm/keybindings" = {
+      close = [ "<Shift><Super>c" ];
+    };
+    "org/gnome/settings-daemon/plugins/media-keys" = {
+      next = [ "<Shift><Control>Right" ];
+      previous = [ "<Shift><Control>Left" ];
+      play = [ "<Shift><Control>Up" ];
+
+      # Reference custom key bindings from below
+      custom-keybindings = [
+        "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/"
+      ];
+    };
+    "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
+      name = "kitty";
+      command = "kitty";
+      binding = "<Super>Return";
     };
   };
 }
