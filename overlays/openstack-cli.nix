0c0f438fd220c66f21b835bbeecb8b906cd96f9e
Philipp Schmitt <philipp@schmitt.co>
2025-01-12 20:51:44 +0100

    no need to include neutronclient anymore

--- overlays/openstack-cli.nix
+++ overlays/openstack-cli.nix
@@ -8,14 +8,6 @@ let
   python-openstackclient = py.python-openstackclient;
 
   cliPlugins = (python-openstackclient.passthru.optional-dependencies.cli-plugins or [ ]) ++ [
-    # FIX build error:
-    # > Finished creating a wheel...
-    # > Finished executing pypaBuildPhase
-    # > Running phase: pythonRuntimeDepsCheckHook
-    # > Executing pythonRuntimeDepsCheck
-    # > Checking runtime dependencies for python_octaviaclient-3.8.0-py3-none-any.whl
-    # >   - python-neutronclient not installed
-    py.python-neutronclient
     py.python-octaviaclient
   ];
 
