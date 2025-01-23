059dd3763372a09ae45371bdff0eb60e59558d60
Philipp Schmitt <philipp@schmitt.co>
2025-01-23 18:12:46 +0100

    Openstack-cli -> py3.12
    https://nixpk.gs/pr-tracker.html?pr=363661

--- overlays/openstack-cli.nix
+++ overlays/openstack-cli.nix
@@ -1,10 +1,7 @@
 { final, prev }:
 
 let
-  # TODO bump to 3.12 once it is available
-  # https://github.com/NixOS/nixpkgs/pull/363661
-  # https://nixpk.gs/pr-tracker.html?pr=363661
-  py = prev.python311Packages;
+  py = prev.python312Packages;
   python-openstackclient = py.python-openstackclient;
 
   cliPlugins = (python-openstackclient.passthru.optional-dependencies.cli-plugins or [ ]) ++ [
