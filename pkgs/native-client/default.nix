490ab6eec1480d8ebab3759a8abeb9792c3d12f7
Philipp Schmitt <philipp@schmitt.co>
2025-01-24 22:07:58 +0100

    Add APP_NAME

--- pkgs/native-client/default.nix
+++ pkgs/native-client/default.nix
@@ -27,12 +27,14 @@ stdenv.mkDerivation rec {
   phases = [ "buildPhase" ];
 
   buildPhase = ''
+    # code
     mkdir -p $out/lib/mozilla/native-messaging-hosts $out/lib/chromium/NativeMessagingHosts
     cp $src/config.js $src/follow-redirects.js $src/host.js $src/messaging.js \
       $out/lib/mozilla/native-messaging-hosts
     cp $src/config.js $src/follow-redirects.js $src/host.js $src/messaging.js \
       $out/lib/chromium/NativeMessagingHosts
 
+    APP_NAME="com.add0n.node"
     BINARY=$out/bin/run.sh
     # Mediator manifests
     get_extension_ids() {
@@ -46,27 +48,29 @@ stdenv.mkDerivation rec {
 
     EXT_FIREFOX=$(get_extension_ids firefox)
     jq -ner \
+      --arg name "$APP_NAME" \
       --arg bin "$BINARY" \
       --argjson ext "$EXT_FIREFOX" '
       {
-        "name": "com.add0n.node",
+        "name": $name,
         "description": "Node Host for Native Messaging",
         "path": $bin,
         "type": "stdio",
         "allowed_extensions": $ext
-      }' > $out/lib/mozilla/native-messaging-hosts/com.add0n.node.json
+      }' > "$out/lib/mozilla/native-messaging-hosts/$APP_NAME.json"
 
     EXT_CHROME=$(get_extension_ids chrome)
     jq -ner \
+      --arg name "$APP_NAME" \
       --arg bin "$BINARY" \
       --argjson ext "$EXT_CHROME" '
       {
-        "name": "com.add0n.node",
+        "name": $name,
         "description": "Node Host for Native Messaging",
         "path": $bin,
         "type": "stdio",
         "allowed_origins": $ext
-      }' > $out/lib/chromium/NativeMessagingHosts/com.add0n.node.json
+      }' > "$out/lib/chromium/NativeMessagingHosts/$APP_NAME.json"
 
     makeWrapper ${nodejs}/bin/node "$BINARY" \
       --add-flags $out/lib/mozilla/native-messaging-hosts/host.js
@@ -77,7 +81,7 @@ stdenv.mkDerivation rec {
     homepage = "https://github.com/andy-portmen/native-client";
     license = lib.licenses.mpl20;
     maintainers = with lib.maintainers; [ pschmitt ];
-    mainProgram = "native-client";
+    # mainProgram = "run.sh";
     platforms = lib.platforms.all;
   };
 }
