aedfacf7979838619c3a7769815c04ef2681cccd
Philipp Schmitt <philipp@schmitt.co>
2025-01-28 09:24:34 +0100

    more tests to ignore

--- overlays/paperless-ngx.nix
+++ overlays/paperless-ngx.nix
@@ -3,6 +3,7 @@
   paperless-ngx = prev.paperless-ngx.overrideAttrs (oldAttrs: {
     doCheck = false;
     disabledTests = (oldAttrs.disabledTests or [ ]) ++ [
+      "test_barcodes"
       "test_consume_file"
       "test_management_consumer"
       "test_preprocessor"
