94ee7e74e9f1631852316fc825f199dfcb833c0b
Philipp Schmitt <philipp@schmitt.co>
2025-01-30 17:52:11 +0100

    Remove commented out config

--- common/network/netbird.nix
+++ common/network/netbird.nix
@@ -11,9 +11,6 @@ in
     secrets.netbird-setup-key = {
       key = "netbird/setup-keys/netbird-io/${config.custom.netbirdSetupKey}";
     };
-    templates.netbird-auth.content = ''
-      NB_SETUP_KEY=${config.sops.placeholder.netbird-setup-key}
-    '';
   };
 
   users.users."${config.custom.username}".extraGroups = [ "netbird-netbird-io" ];
@@ -24,12 +21,6 @@ in
     clients = {
       netbird-io = {
         port = 51820;
-        # environment = {
-        #   NB_MANAGEMENT_URL = "https://api.netbird.io";
-        #   # Below won't work
-        #   # You will end up with NB_SETUP_KEY=<SOPS:xxxx:PLACEHOLDER>
-        #   # NB_SETUP_KEY = "${config.sops.placeholder.netbird-setup-key}";
-        # };
       };
     };
   };
