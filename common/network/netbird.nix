4e96142b76266971631b6886d344a30b07f6c28e
Philipp Schmitt <philipp@schmitt.co>
2025-01-30 17:11:00 +0100

    netbird tunnel -> clients

--- common/network/netbird.nix
+++ common/network/netbird.nix
@@ -1,27 +1,10 @@
 {
   config,
   pkgs,
-  lib,
   ...
 }:
 let
   netbirdPkg = pkgs.master.netbird;
-  createNetbirdScript =
-    tunnelName: tunnelConfig:
-    pkgs.writeShellScriptBin "netbird-${tunnelName}" ''
-      # Set environment variables
-      ${lib.concatStringsSep "\n" (
-        lib.mapAttrsToList (name: value: ''export ${name}="${value}"'') tunnelConfig.environment
-      )}
-
-      # Run netbird
-      # TODO Stupidly prepending sudo to the command doesn't work and just
-      # breaks the netbird cli
-      # exec /run/wrappers/bin/sudo ${netbirdPkg}/bin/netbird "$@"
-      exec ${netbirdPkg}/bin/netbird "$@"
-    '';
-
-  netbirdScripts = lib.mapAttrsToList createNetbirdScript config.services.netbird.tunnels;
 in
 {
   sops = {
@@ -33,48 +16,24 @@ in
     '';
   };
 
+  users.users."${config.custom.username}".extraGroups = [ "netbird-netbird-io" ];
+
   services.netbird = {
     enable = true;
     package = netbirdPkg;
-    tunnels = {
+    clients = {
       netbird-io = {
         port = 51820;
-        environment = {
-          NB_MANAGEMENT_URL = "https://api.netbird.io";
-          # Below won't work
-          # You will end up with NB_SETUP_KEY=<SOPS:xxxx:PLACEHOLDER>
-          # NB_SETUP_KEY = "${config.sops.placeholder.netbird-setup-key}";
-        };
+        # environment = {
+        #   NB_MANAGEMENT_URL = "https://api.netbird.io";
+        #   # Below won't work
+        #   # You will end up with NB_SETUP_KEY=<SOPS:xxxx:PLACEHOLDER>
+        #   # NB_SETUP_KEY = "${config.sops.placeholder.netbird-setup-key}";
+        # };
       };
     };
   };
 
   # mask netbird-wt0 service
-  systemd.services.netbird-wt0.enable = false;
-
-  systemd.services.netbird-netbird-io = {
-    postStart = ''
-      export PATH=${
-        pkgs.lib.makeBinPath (
-          netbirdScripts
-          ++ [
-            pkgs.coreutils
-            pkgs.gnugrep
-          ]
-        )
-      }
-      # sleep 5
-      if netbird-netbird-io status | \
-        grep -q 'NeedsLogin'
-      then
-        netbird-netbird-io up --setup-key-file "${config.sops.secrets.netbird-setup-key.path}"
-      fi
-    '';
-
-    # FIXME Below does not seem to be working... The NB_SETUP_KEY is correctly
-    # set but netbird seems to just ignore it.
-    serviceConfig.EnvironmentFile = config.sops.templates."netbird-auth".path;
-  };
-
-  environment.systemPackages = netbirdScripts;
+  # systemd.services.netbird-wt0.enable = false;
 }
