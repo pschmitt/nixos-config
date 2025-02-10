90933efb911a75a29f23b5a0d032a3eff345b8a9
Philipp Schmitt <philipp@schmitt.co>
2025-02-10 17:28:22 +0100

    Make target host configurable at runtime

--- build-iso.sh
+++ build-iso.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
+TARGET_HOST="${1:-iso}" # or iso-xmr for example
+
 cd "$(cd "$(dirname "$0")" >/dev/null 2>&1; pwd -P)" || exit 9
 
 rm -f result
-nix build '.#nixosConfigurations.iso.config.system.build.isoImage'
+nix build ".#nixosConfigurations.${TARGET_HOST}.config.system.build.isoImage"
 tree result
