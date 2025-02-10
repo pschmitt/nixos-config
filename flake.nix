9930ed91878ca2ad277ce9cbd55d0bb9e217eddd
Philipp Schmitt <philipp@schmitt.co>
2025-02-10 17:27:13 +0100

    Add a propper host config

--- flake.nix
+++ flake.nix
@@ -316,9 +316,8 @@
           modules = [
             "${nixpkgs}/nixos/modules/installer/cd-dvd/channel.nix"
             "${nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix"
-            ./hosts/iso
+            ./hosts/iso-xmr
             ./modules/custom.nix
-            ./services/xmrig-iso.nix
           ];
         };
       };
