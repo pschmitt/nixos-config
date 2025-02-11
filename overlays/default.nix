0bbd5b0b2c5c9c38526887b941b8673bee60e062
Philipp Schmitt <philipp@schmitt.co>
2025-02-11 13:44:22 +0100

    oops

--- overlays/default.nix
+++ overlays/default.nix
@@ -12,7 +12,6 @@
     # // (import ./netbird.nix { inherit final prev; })
     // (import ./openstack-cli.nix { inherit final prev; })
     // (import ./paperless-ngx.nix { inherit final prev; })
-    // (import ./pmbootstrap.nix { inherit final prev; })
     // (import ./tela-icon-theme.nix { inherit final prev; })
     // (import ./wireguard-tools.nix { inherit final prev; })
     # // (import ./tmux.nix { inherit final prev; })
