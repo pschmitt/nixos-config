2a7351ed64220671d3ab316be5a83040e4ed6528
Philipp Schmitt <philipp@schmitt.co>
2025-02-15 17:41:01 +0100

    Remove bitwarden-cli-sync service

--- home-manager/bitwarden.nix
+++ home-manager/bitwarden.nix
@@ -1,45 +1,8 @@
-{ lib, pkgs, ... }:
+{ pkgs, ... }:
 {
   home.packages = with pkgs; [
     bitwarden
     master.bitwarden-cli
     rbw
   ];
-
-  systemd.user.services.bitwarden-cli-sync = {
-    Unit = {
-      Description = "Bitwarden CLI Sync (bww)";
-    };
-
-    Service = {
-      Type = "oneshot";
-      Environment = "PATH=$PATH:${
-        lib.makeBinPath [
-          pkgs.bash
-          pkgs.zsh
-          pkgs.master.bitwarden-cli
-        ]
-      }";
-      ExecStart = "/home/pschmitt/bin/zhj 'bwp sync --force; gec::bwp sync --force'";
-    };
-
-    Install = {
-      WantedBy = [ "default.target" ];
-    };
-  };
-
-  systemd.user.timers.bitwarden-cli-sync = {
-    Unit = {
-      Description = "Bitwarden CLI Sync (bww)";
-    };
-
-    Timer = {
-      OnCalendar = "*-*-* 00/2:00:00";
-      Persistent = true;
-    };
-
-    Install = {
-      WantedBy = [ "timers.target" ];
-    };
-  };
 }
