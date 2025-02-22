19f14a6f049cb65f1c6d560bd2bca64e27dc3db8
Philipp Schmitt <philipp@schmitt.co>
2025-02-23 00:08:17 +0100

    Remove groups

--- hosts/rofl-02/turris-ssh-tunnel.nix
+++ hosts/rofl-02/turris-ssh-tunnel.nix
@@ -2,7 +2,6 @@
 {
   users.users."ssh-tunnel-turris" = {
     isSystemUser = true;
-    # groups = {};
     group = "ssh-tunnel-turris";
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIF945oD6xv+KL/P7m4JxdnOxljsm1UcKVNfE+KASu2ZX root@turris"
