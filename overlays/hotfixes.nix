c9b6b3490fdd5594fcaaec1c7d7cf1dd3a55bd54
Philipp Schmitt <philipp@schmitt.co>
2025-03-04 10:10:07 +0100

    Flake lock

--- overlays/hotfixes.nix
+++ overlays/hotfixes.nix
@@ -6,4 +6,7 @@
 {
   droidcam-obs-patched =
     inputs.droidcam-obs.legacyPackages.${final.system}.obs-studio-plugins.droidcam-obs;
+
+  aerc = inputs.aerc.legacyPackages.${final.system}.aerc;
+  notmuch = inputs.notmuch.legacyPackages.${final.system}.notmuch;
 }
