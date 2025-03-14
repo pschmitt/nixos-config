bb537c48cff9694fd2fb3124ca39f73641959511
Philipp Schmitt <philipp@schmitt.co>
2025-03-14 14:58:08 +0100

    Remove broken packages: terragrunt and python-ironicclient

--- overlays/openstack-cli.nix
+++ overlays/openstack-cli.nix
@@ -4,9 +4,17 @@ let
   py = prev.python312Packages;
   python-openstackclient = py.python-openstackclient;
 
-  cliPlugins = (python-openstackclient.passthru.optional-dependencies.cli-plugins or [ ]) ++ [
-    py.python-octaviaclient
-  ];
+  # cliPlugins = (python-openstackclient.passthru.optional-dependencies.cli-plugins or [ ]) ++ [
+  #   py.python-octaviaclient
+  # ];
+
+  # FIXME ironicclient is not building as of 2025-03-14 (special thanks to @kbudde!)
+  cliPlugins = builtins.filter (p: p != py.python-ironicclient) (
+    (python-openstackclient.passthru.optional-dependencies.cli-plugins or [ ])
+    ++ [
+      py.python-octaviaclient
+    ]
+  );
 
   openstackclient-full = py.python.withPackages (ps: cliPlugins ++ [ python-openstackclient ]);
 in
