b770f266052fc1555a6c22fbe0c92c77ec14fe7f
Philipp Schmitt <philipp@schmitt.co>
2025-03-22 18:42:36 +0100

    Add gimp flatpak

--- home-manager/flatpak.nix
+++ home-manager/flatpak.nix
@@ -5,11 +5,13 @@
       "flathub" = "https://dl.flathub.org/repo/flathub.flatpakrepo";
       "flathub-beta" = "https://dl.flathub.org/beta-repo/flathub-beta.flatpakrepo";
     };
-    # packages = [
-    #   # NOTE The "//" are here cause we omitted the cpu arch
-    #   "flathub:app/com.obsproject.Studio//stable"
-    #   "flathub:runtime/com.obsproject.Studio.Plugin.DroidCam//stable"
-    # ];
+    packages = [
+      # NOTE The "//" are here cause we omitted the cpu arch
+      # "flathub:app/com.obsproject.Studio//stable"
+      # "flathub:runtime/com.obsproject.Studio.Plugin.DroidCam//stable"
+
+      "flathub:app/org.gimp.GIMP//stable"
+    ];
     # overrides = {
     #   "com.obsproject.Studio" = {
     #     filesystems = [
