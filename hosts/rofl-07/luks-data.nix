cfbbcee8dfffc9a8f72144683f6d22f1b8e806ec
Philipp Schmitt <philipp@schmitt.co>
2025-04-02 12:04:25 +0200

    Switch back to agenix for rofl-07's data vol

--- hosts/rofl-07/luks-data.nix
+++ hosts/rofl-07/luks-data.nix
@@ -1,26 +1,25 @@
-{ config, ... }:
+{ config, lib, ... }:
 {
   # Data volume
 
-  sops.secrets."luks/data" = {
-    sopsFile = ./luks.sops.yaml;
-  };
-
-  sops.templates.crypttab.content = ''
-    data PARTLABEL=disk-data-luks ${config.sops.secrets."luks/data".path}
-  '';
-
-  environment.etc.crypttab = {
-    source = config.sops.templates.crypttab.path;
-    mode = "0400";
-  };
-
-  fileSystems."/mnt/data".neededForBoot = false;
-
-  # TODO delete?
-  # agenix
-  # age.secrets.luks-key-data.file = ../../secrets/${config.networking.hostName}/luks-passphrase-data.age;
-  # environment.etc.crypttab.text = ''
-  #   data PARTLABEL=disk-data-luks ${config.age.secrets.luks-key-data.path}
+  # FIXME sops-nix decryption of the data volume
+  # sops.secrets."luks/data" = {
+  #   sopsFile = ./luks.sops.yaml;
+  # };
+  # sops.templates.crypttab.content = ''
+  #   data PARTLABEL=disk-data-luks ${config.sops.secrets."luks/data".path}
   # '';
+  # environment.etc.crypttab = {
+  #   source = config.sops.templates.crypttab.path;
+  #   mode = "0400";
+  # };
+
+  fileSystems."/mnt/data".neededForBoot = lib.mkForce false;
+
+  # TODO Replace with sops-nix
+  # agenix
+  age.secrets.luks-key-data.file = ../../secrets/${config.networking.hostName}/luks-passphrase-data.age;
+  environment.etc.crypttab.text = ''
+    data PARTLABEL=disk-data-luks ${config.age.secrets.luks-key-data.path}
+  '';
 }
