3fe3f1e0842629512ad5f975cccef222ed69aa7c
Philipp Schmitt <philipp@schmitt.co>
2025-04-11 18:04:26 +0200

    just keep what we need

--- hosts/rofl-07/luks-data.nix
+++ hosts/rofl-07/luks-data.nix
@@ -1,46 +1,7 @@
-{ config, lib, ... }:
-let
-  # keyFile = "/mnt-root/luks-data.keyfile";
-  # keyFile = "/sysroot/luks-data.keyfile";
-  keyFile = "/luks-data.keyfile";
-in
+{ lib, ... }:
 {
   # Data volume
-
-  # FIXME sops-nix decryption of the data volume
-  # sops.secrets."luks/data" = {
-  #   sopsFile = ./luks.sops.yaml;
-  # };
-  # sops.templates.crypttab.content = ''
-  #   data PARTLABEL=disk-data-luks ${config.sops.secrets."luks/data".path}
-  # '';
-  # environment.etc.crypttab = {
-  #   source = config.sops.templates.crypttab.path;
-  #   mode = "0400";
-  # };
-
-  # TODO Replace with sops-nix
-  # agenix
-  # age.secrets.luks-key-data.file = ../../secrets/${config.networking.hostName}/luks-passphrase-data.age;
-  # environment.etc.crypttab.text = ''
-  #   data PARTLABEL=disk-data-luks ${config.age.secrets.luks-key-data.path}
-  # '';
-
-  environment.etc.crypttab.text = ''
-    data PARTLABEL=disk-data-luks ${keyFile}
-  '';
-
-  # fileSystems."/mnt/data" = {
-  #   encrypted = {
-  #     # TODO Try explicitly disabling it with lib.mkForce false
-  #     # and use crypttab?
-  #     enable = lib.mkForce false;
-  #     keyFile = "/mnt-root/luks-data.keyfile";
-  #   };
-  # };
-
   boot.initrd.luks.devices.data-encrypted = {
-    keyFile = lib.mkForce "/sysroot${keyFile}";
-    fallbackToPassword = lib.mkForce false;
+    keyFile = lib.mkForce "/sysroot/luks-data.keyfile";
   };
 }
