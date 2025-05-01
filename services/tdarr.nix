81f84cffc7ca5327ae31ab16072ee9a5f05541d8
Philipp Schmitt <philipp@schmitt.co>
2025-05-01 11:35:15 +0200

    Mount videos dir

--- services/tdarr.nix
+++ services/tdarr.nix
@@ -1,5 +1,12 @@
 { config, ... }:
 {
+  imports = [
+    (import ./nfs-client.nix {
+      server = "rofl-07.netbird.cloud";
+      exports = [ "videos" ];
+      mountPoint = "/mnt/data";
+    })
+  ];
   sops = {
     secrets.tdarr-api-key = {
       key = "tdarr/api-key";
