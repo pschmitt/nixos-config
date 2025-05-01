9e1ddcd19013f1aab19f9e5f8d33d92030454704
Philipp Schmitt <philipp@schmitt.co>
2025-05-01 11:31:28 +0200

    fix mountpoint

--- services/nfs-client-all.nix
+++ services/nfs-client-all.nix
@@ -8,7 +8,7 @@
     (import ./nfs-client.nix {
       server = "rofl-07.netbird.cloud";
       exports = [ "videos" ];
-      mountPoint = "/mnt/data/videos";
+      mountPoint = "/mnt/data";
     })
   ];
 }
