9cf1fbdd4e143812df2c78c0b022e121f56dfa96
Philipp Schmitt <philipp@schmitt.co>
2025-05-01 22:38:08 +0200

    Dynamically publish tdarr server port to ts/nb ip

--- services/tdarr-server.nix
+++ services/tdarr-server.nix
@@ -1,4 +1,16 @@
-{ lib, ... }:
+{ pkgs, lib, ... }:
+let
+  tdarrInterfaces = [
+    {
+      name = "tailscale";
+      iface = "tailscale0";
+    }
+    {
+      name = "netbird";
+      iface = "netbird-netbird-io";
+    }
+  ];
+in
 {
 
   imports = [ ./tdarr-node.nix ];
@@ -16,13 +28,69 @@
       };
       ports = [
         "127.0.0.1:8265:8265" # web UI port
-        # FIXME dynamically publish server port netbird/ts IP
         "127.0.0.1:8266:8266" # server port
-        "100.122.139.168:8266:8266" # netbird
-        "100.114.226.109:8266:8266" # tailscale
+
+        # NOTE see below for the dynamically publishing of the server port
+        # to netbird/ts IPs
+        # "100.122.139.168:8266:8266" # netbird
+        # "100.114.226.109:8266:8266" # tailscale
 
         # "127.0.0.1:8267:8267" # Internal node port
       ];
     };
   };
+
+  # Dynamically forward tdarr server port to the netbird/ts IPs
+  systemd.services = lib.listToAttrs (
+    map (iface: {
+      name = "tdarr-socat-${iface.name}";
+      value = {
+        description = "Socat proxy on ${iface.iface}:8266 → localhost:8266";
+        wants = [ "docker-tdarr.service" ];
+        after = [ "docker-tdarr.service" ];
+        wantedBy = [ "multi-user.target" ];
+        path = [
+          pkgs.jq
+          pkgs.socat
+          pkgs.tailscale
+        ];
+        script = ''
+          netbird_ip() {
+            # FIXME We should use the pkgs.netbird-netbird-io path here...
+            /run/current-system/sw/bin/netbird-netbird-io status --json | \
+              jq -er '.netbirdIp | gsub("/.*"; "")'
+          }
+
+          case "${iface.iface}" in
+            *netbird*)
+              LISTEN_IP=$(netbird_ip)
+              ;;
+            *tailscale*)
+              LISTEN_IP=$(tailscale ip -4)
+              ;;
+            *)
+              echo "Unknown interface type: '${iface.name}'" >&2
+              exit 2
+              ;;
+          esac
+
+          if [[ -z "$LISTEN_IP" ]]
+          then
+            echo "Failed to determine ${iface.name} IP" >&2
+            exit 1
+          fi
+
+          # NOTE below still attempts to listen on 0.0.0.0:8266
+          # socat TCP4-LISTEN:8266,so-bindtodevice=${iface.iface},reuseaddr,fork TCP4:127.0.0.1:8266
+
+          socat TCP4-LISTEN:8266,bind=''${LISTEN_IP},reuseaddr,fork TCP4:127.0.0.1:8266;
+        '';
+        serviceConfig = {
+          Restart = "always";
+          RestartSec = 5;
+        };
+      };
+    }) tdarrInterfaces
+  );
+
 }
