4fb719204ed5da0b5e0d351e0313f48d2709b824
Philipp Schmitt <philipp@schmitt.co>
2025-05-22 16:04:19 +0200

    Update nfs host

--- services/nfs/nfs-client-rofl-08.nix
+++ services/nfs/nfs-client-rofl-08.nix
@@ -2,7 +2,7 @@
 {
   imports = [
     (import ./nfs-client.nix {
-      server = "rofl-07.netbird.cloud";
+      server = "rofl-08.netbird.cloud";
       exports = [ "videos" ];
       mountPoint = "/mnt/data";
     })
