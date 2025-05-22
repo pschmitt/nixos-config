9a665c9952888ee6ae2d644f8bee31bf1ee003b2
Philipp Schmitt <philipp@schmitt.co>
2025-05-22 18:13:42 +0200

    Remove blob vol

--- tofu/data-volumes.tf
+++ tofu/data-volumes.tf
@@ -9,17 +9,6 @@ resource "openstack_blockstorage_volume_v3" "data_volume" {
   }
 }
 
-resource "openstack_blockstorage_volume_v3" "blob_volume" {
-  name                 = "blob-vol"
-  size                 = 4096 # GiB
-  availability_zone    = var.availability_zone
-  enable_online_resize = true
-
-  lifecycle {
-    prevent_destroy = true
-  }
-}
-
 resource "openstack_blockstorage_volume_v3" "blobarr_volume" {
   name                 = "blobarr-vol"
   size                 = 4096 # GiB
@@ -34,11 +23,6 @@ resource "openstack_blockstorage_volume_v3" "blobarr_volume" {
 resource "openstack_compute_volume_attach_v2" "va_data" {
   instance_id = openstack_compute_instance_v2.rofl-02.id
   volume_id   = openstack_blockstorage_volume_v3.data_volume.id
-}
-
-resource "openstack_compute_volume_attach_v2" "va_blob" {
-  instance_id = openstack_compute_instance_v2.rofl-07.id
-  volume_id   = openstack_blockstorage_volume_v3.blob_volume.id
 }
 
 resource "openstack_compute_volume_attach_v2" "va_blobarr" {
