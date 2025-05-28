60ac03400422c7a9085dd74fb35aac9ac782749f
Philipp Schmitt <philipp@schmitt.co>
2025-05-28 16:29:25 +0200

    Cripple rofl-02

--- hosts/rofl-02/default.nix
+++ hosts/rofl-02/default.nix
@@ -10,31 +10,31 @@
 
     # services
     # backups services
-    ../../services/backups/autorestic.nix
-    ../../services/backups/bitwarden.nix
-    ../../services/backups/evernote.nix
-    ../../services/changedetection-io.nix
-    ../../services/harmonia.nix
-    ../../services/forgejo.nix
-    ../../services/http.nix
-    ../../services/http-static.nix
-    ../../services/immich.nix
-    ../../services/luks-ssh-unlock-homelab.nix
+    # ../../services/backups/autorestic.nix
+    # ../../services/backups/bitwarden.nix
+    # ../../services/backups/evernote.nix
+    # ../../services/changedetection-io.nix
+    # ../../services/harmonia.nix
+    # ../../services/forgejo.nix
+    # ../../services/http.nix
+    # ../../services/http-static.nix
+    # ../../services/immich.nix
+    # ../../services/luks-ssh-unlock-homelab.nix
     # ../../services/mealie.nix # it's a container now!
-    ../../services/paperless-ngx.nix
-    ../../services/podsync.nix
-    ../../services/rclone-bisync.nix
-    ../../services/turris-ssh-tunnel.nix
-    (import ../../services/nfs/nfs-server.nix { inherit lib; })
+    # ../../services/paperless-ngx.nix
+    # ../../services/podsync.nix
+    # ../../services/rclone-bisync.nix
+    # ../../services/turris-ssh-tunnel.nix
+    # (import ../../services/nfs/nfs-server.nix { inherit lib; })
 
     # misc
-    ../../misc/docker-compose-netbird-ip-fix.nix
-    ../../misc/rsync-fonts-to-rofl-03.nix
+    # ../../misc/docker-compose-netbird-ip-fix.nix
+    # ../../misc/rsync-fonts-to-rofl-03.nix
 
     # host-specific service config
-    ./container-services.nix
-    ./monit.nix
-    ./restic.nix
+    # ./container-services.nix
+    # ./monit.nix
+    # ./restic.nix
   ];
 
   # Enable networking
