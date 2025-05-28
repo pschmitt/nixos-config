48034b42fad0ac59187b2079460a9a18f4fe2bc5
Philipp Schmitt <philipp@schmitt.co>
2025-05-28 16:49:18 +0200

    Add r9 key

--- misc/users/github-actions.nix
+++ misc/users/github-actions.nix
@@ -15,8 +15,11 @@
       # Github Actions
       # https://github.com/pschmitt/nixos-config/settings/secrets/actions
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIN6Luwh05e39m7lx9CDyZNicRpHYerLx8fXMwn3o5xHW github-actions@nixos-config"
-      # rofl-02 host key
+      # rofl-02 host key TODO Remove
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHj1bwykYI4tC4kt3Rd4QAOV2D1srlcQ14NLB9w3JBXp root@rofl-02"
+      # rofl-09 host key
+      # ssh r9 sudo cat /etc/ssh/ssh_host_ed25519_key.pub
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIM1RQuD12+CL5NzJHrVge49uK9QyPlISobQG5MNgIZHo root@rofl-09"
     ];
   };
 
