9268ccaf026cc5edf85d90a10440b20fac5e1c74
Philipp Schmitt <philipp@schmitt.co>
2025-05-28 16:29:15 +0200

    Remove dupe

--- hosts/rofl-08/default.nix
+++ hosts/rofl-08/default.nix
@@ -38,24 +38,5 @@
     };
   };
 
-  systemd.services.docker-compose-bulk-up = {
-    after = [
-      "network.target"
-      "docker.service"
-      "mnt-data.mount"
-    ];
-    requires = [
-      "docker.service"
-      "mnt-data.mount"
-    ];
-    wantedBy = [ "multi-user.target" ];
-
-    serviceConfig = {
-      Type = "oneshot";
-      RemainAfterExit = true;
-      ExecStart = "${pkgs.docker-compose-bulk}/bin/docker-compose-bulk up -d";
-    };
-  };
-
   environment.systemPackages = with pkgs; [ yt-dlp ];
 }
