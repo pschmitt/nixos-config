bc80ef40e22244954350123e0bb9f4d188932f8d
Philipp Schmitt <philipp@schmitt.co>
2025-05-29 14:51:07 +0200

    Remove rofl-02

--- tofu/output.tf
+++ tofu/output.tf
@@ -1,10 +1,5 @@
-output "rofl_02_boot_volume_id" {
-  value       = openstack_blockstorage_volume_v3.rofl_02_boot_volume.id
-  description = "Volume ID of the root volume of rofl-02"
-}
-
-output "rofl_02_fip" {
-  value       = openstack_networking_floatingip_v2.rofl_02_fip.address
+output "rofl-09_fip" {
+  value       = openstack_networking_floatingip_v2.rofl-09_fip.address
   description = "Floating IP address of rofl-02"
 }
 
