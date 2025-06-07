d141955b91876bb7662c058524f336e8751562b4
Philipp Schmitt <philipp@schmitt.co>
2025-06-07 10:07:56 +0200

    hyprland 0.49.1 does not build. Revert to 0.48.1

--- flake.nix
+++ flake.nix
@@ -63,12 +63,18 @@
       type = "git";
       url = "https://github.com/hyprwm/Hyprland";
       # https://github.com/hyprwm/Hyprland/releases
-      ref = "refs/tags/v0.49.0";
+      ref = "refs/tags/v0.48.1";
+      # ref = "refs/tags/v0.49.0";
       # git ls-remote --tags https://github.com/hyprwm/Hyprland | sort --version-sort -k 2 | tail -1 | awk '{ print $1 }'
       # rev = "4520b30d498daca8079365bdb909a8dea38e8d55";
       submodules = true;
-      inputs.nixpkgs.follows = "nixpkgs";
+      # inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    # xdph = {
+    #   url = "github:hyprwm/xdg-desktop-portal-hyprland";
+    #   inputs.nixpkgs.follows = "nixpkgs";
+    # };
 
     hypridle = {
       url = "github:hyprwm/hypridle";
