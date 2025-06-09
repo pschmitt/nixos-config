2c5a70d9fe59145afec56c769e6b2f8c27890653
Philipp Schmitt <philipp@schmitt.co>
2025-06-09 11:23:51 +0200

    sort

--- services/http-static.nix
+++ services/http-static.nix
@@ -15,15 +15,6 @@
         '';
       };
     };
-    "y.${config.custom.mainDomain}" = {
-      enableACME = true;
-      # FIXME https://github.com/NixOS/nixpkgs/issues/210807
-      acmeRoot = null;
-      forceSSL = true;
-      locations."/" = {
-        return = "301 https://raw.githubusercontent.com/pschmitt/yadm-init/main/init.sh";
-      };
-    };
 
     "p.${config.custom.mainDomain}" = {
       enableACME = true;
@@ -37,6 +28,16 @@
           rev = "b6d5e9cc361cede2756c81e5e9ce4f34c78b3824";
           hash = "sha256-w/BOiyBQIBsxlSdMhx0jx6Q0qKWHZQti0ayfLaBjINY=";
         };
+      };
+    };
+
+    "y.${config.custom.mainDomain}" = {
+      enableACME = true;
+      # FIXME https://github.com/NixOS/nixpkgs/issues/210807
+      acmeRoot = null;
+      forceSSL = true;
+      locations."/" = {
+        return = "301 https://raw.githubusercontent.com/pschmitt/yadm-init/main/init.sh";
       };
     };
   };
