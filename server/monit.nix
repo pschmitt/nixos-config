25e8dbccc6a63c0c10c5e1a5a24bfedd22ead043
Philipp Schmitt <philipp@schmitt.co>
2025-07-01 19:42:22 +0200

    Strip ansi from output

--- server/monit.nix
+++ server/monit.nix
@@ -28,7 +28,7 @@ let
   '';
 
   needsReboot = pkgs.writeShellScript "needs-reboot" ''
-    OUTPUT=$(${inputs.nixos-needsreboot.packages.${pkgs.system}.default}/bin/nixos-needsreboot 2>&1)
+    OUTPUT=$(${inputs.nixos-needsreboot.packages.${pkgs.system}.default}/bin/nixos-needsreboot 2>&1 | ${pkgs.strip-ansi}/bin/strip-ansi)
     echo "$OUTPUT"
     ${pkgs.gnugrep}/bin/grep -q 'No reboot required' <<< "$OUTPUT"
   '';
