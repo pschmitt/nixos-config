0bb3e0ca7972df9d9b7deca235f2ec41aa8cc2d3
Philipp Schmitt <philipp@schmitt.co>
2025-07-24 16:56:39 +0200

    Set XDG_RUNTIME_DIR and DBUS_SESSION_BUS_ADDRESS

--- common/gui/hacompanion.nix
+++ common/gui/hacompanion.nix
@@ -20,11 +20,19 @@
       "/etc/profiles/per-user/${config.custom.username}"
     ];
 
+    # NOTE We probably need these 2 vars for playerctl
+    # We might also need to restart the service once the user session has started
+    environment = {
+      XDG_RUNTIME_DIR = "/run/user/1000";
+      DBUS_SESSION_BUS_ADDRESS = "unix:path=/run/user/1000/bus";
+    };
+
     serviceConfig = {
       User = "${config.custom.username}";
       # NOTE We can't use %E here since we are running as a system service
       EnvironmentFile = "${config.custom.homeDirectory}/.config/hacompanion/secrets";
       ExecStart = "${pkgs.hacompanion}/bin/hacompanion -quiet -config ~/.config/hacompanion/hacompanion.toml";
+
       Restart = "always";
       RestartSec = 5;
     };
