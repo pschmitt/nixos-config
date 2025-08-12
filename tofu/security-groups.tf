8abde25b4bf4b3104bd9cd4e77a59eb6295166f5
Philipp Schmitt <philipp@schmitt.co>
2025-08-12 10:22:48 +0200

    Add stratum etc

--- tofu/security-groups.tf
+++ tofu/security-groups.tf
@@ -238,4 +238,64 @@ resource "openstack_networking_secgroup_rule_v2" "secgroup_rule_xmrig_proxy_v6" 
   security_group_id = openstack_networking_secgroup_v2.secgroup_xmr.id
 }
 
+resource "openstack_networking_secgroup_rule_v2" "secgroup_rule_p2pool_v4" {
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "tcp"
+  port_range_min    = 37888
+  port_range_max    = 37890
+  remote_ip_prefix  = "0.0.0.0/0"
+  security_group_id = openstack_networking_secgroup_v2.secgroup_xmr.id
+}
+
+resource "openstack_networking_secgroup_rule_v2" "secgroup_rule_p2pool_v6" {
+  direction         = "ingress"
+  ethertype         = "IPv6"
+  protocol          = "tcp"
+  port_range_min    = 37888
+  port_range_max    = 37890
+  remote_ip_prefix  = "::/0"
+  security_group_id = openstack_networking_secgroup_v2.secgroup_xmr.id
+}
+
+resource "openstack_networking_secgroup_rule_v2" "secgroup_rule_stratum_v4" {
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "tcp"
+  port_range_min    = 3333
+  port_range_max    = 3333
+  remote_ip_prefix  = "0.0.0.0/0"
+  security_group_id = openstack_networking_secgroup_v2.secgroup_xmr.id
+}
+
+resource "openstack_networking_secgroup_rule_v2" "secgroup_rule_stratum_v6" {
+  direction         = "ingress"
+  ethertype         = "IPv6"
+  protocol          = "tcp"
+  port_range_min    = 3333
+  port_range_max    = 3333
+  remote_ip_prefix  = "::/0"
+  security_group_id = openstack_networking_secgroup_v2.secgroup_xmr.id
+}
+
+resource "openstack_networking_secgroup_rule_v2" "secgroup_rule_stratum_alt_v4" {
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "tcp"
+  port_range_min    = 13333
+  port_range_max    = 13333
+  remote_ip_prefix  = "0.0.0.0/0"
+  security_group_id = openstack_networking_secgroup_v2.secgroup_xmr.id
+}
+
+resource "openstack_networking_secgroup_rule_v2" "secgroup_rule_stratum_alt_v6" {
+  direction         = "ingress"
+  ethertype         = "IPv6"
+  protocol          = "tcp"
+  port_range_min    = 13333
+  port_range_max    = 13333
+  remote_ip_prefix  = "::/0"
+  security_group_id = openstack_networking_secgroup_v2.secgroup_xmr.id
+}
+
 # vim: set ft=terraform
