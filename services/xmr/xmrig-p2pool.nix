58771c648f4b9ce85f8a4bb5d7d8cc15024b0a96
Philipp Schmitt <philipp@schmitt.co>
2025-08-12 10:45:21 +0200

    Prioritize p2pool

--- services/xmr/xmrig-p2pool.nix
+++ services/xmr/xmrig-p2pool.nix
@@ -1,17 +1,40 @@
-{ config, ... }:
-{
-  services.xmrig.settings = {
-    # NOTE This will put p2pool on the top of the list of pools!
-    # xmrig does fallback, ie if p2pool is down, it will try the next pool.
-    pools = [
-      {
-        url = "127.0.0.1:${toString config.services.p2pool.stratum.port}";
-        user = "x"; # or "x+10000" for fixed diff
-        pass = config.networking.hostName;
-        keepalive = true;
-        tls = false; # true only if p2pool has TLS certs
-        "rig-id" = config.networking.hostName;
-      }
-    ];
+{ config, lib, ... }:
+
+let
+  common = {
+    keepalive = true;
+    pass = config.networking.hostName;
   };
+
+  # p2pool first
+  p2pool = common // {
+    url = "127.0.0.1:${toString config.services.p2pool.stratum.port}";
+    user = "x"; # or "x+<fixed-diff>"
+    tls = false; # enable only if you gave p2pool TLS
+    "rig-id" = config.networking.hostName;
+  };
+
+  publicPools = [
+    (
+      common
+      // {
+        url = "pool.hashvault.pro:443";
+        user = "\${HASHVAULT_USER}";
+        tls = true;
+      }
+    )
+    (
+      common
+      // {
+        url = "xmrpool.eu:9999";
+        user = "\${XMRPOOL_USER}";
+        tls = true;
+      }
+    )
+  ];
+in
+{
+  # We need to force this, because otherwise we would just append to the list
+  # of pools and have duplicate entries.
+  services.xmrig.settings.pools = lib.mkForce ([ p2pool ] ++ publicPools);
 }
