8819c8a9380d5d958af058b5edbf455b993b44ae
Philipp Schmitt <philipp@schmitt.co>
2025-08-21 18:20:21 +0200

    Refactor

--- hosts/pica4/hardware-configuration.nix
+++ hosts/pica4/hardware-configuration.nix
@@ -1,6 +1,22 @@
-{ inputs, ... }:
+{
+  inputs,
+  lib,
+  pkgs,
+  ...
+}:
 {
   imports = [
     inputs.hardware.nixosModules.raspberry-pi-4
   ];
+
+  # Here for force-set a few settings that are set by bootloader.nix, which
+  # overrides the nixos-hardware settings.
+  boot = {
+    kernelPackages = lib.mkForce pkgs.linuxKernel.packages.linux_rpi4;
+    loader = {
+      grub.enable = lib.mkForce false;
+      systemd-boot.enable = lib.mkForce false;
+    };
+  };
+
 }
