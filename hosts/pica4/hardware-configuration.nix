f30d572a112452facd73bdac3fbbbe68463046d0
Philipp Schmitt <philipp@schmitt.co>
2025-08-22 17:50:10 +0200

    Very WIP; very much no fucking camera

--- hosts/pica4/hardware-configuration.nix
+++ hosts/pica4/hardware-configuration.nix
@@ -5,7 +5,29 @@
   ...
 }:
 {
-  imports = [ inputs.hardware.nixosModules.raspberry-pi-4 ];
+  # imports = [ inputs.hardware.nixosModules.raspberry-pi-4 ];
+  # imports = [
+  #   inputs.nixos-raspberrypi.nixosModules.raspberry-pi-4.base
+  # ];
+
+  hardware = {
+    i2c.enable = true;
+
+    # raspberry-pi."4" = {
+    #   bluetooth.enable = true;
+    #   i2c0.enable = false;
+    #   i2c1.enable = true;
+    # };
+
+    raspberry-pi.config.all = {
+      dt-overlays = {
+        imx219 = {
+          enable = true;
+          params = { };
+        };
+      };
+    };
+  };
 
   nixpkgs.hostPlatform = lib.mkDefault "aarch64-linux";
 
@@ -16,8 +38,28 @@
   hardware.enableAllHardware = lib.mkForce false;
   services.fwupd.enable = lib.mkForce false;
 
+  # mount the fw partition as /boot/firmware
+  # FIXME This fails to activate:
+  # Error: Failed to open unit file /nix/store/br5yy8zkrwrlk6rvsxv1j68d5klkxh4s-nixos-system-pica4-sd-card-25.11.20250819.2007595/etc/systemd/system/boot-firmware.mount
+  # Caused by:
+  #     No such file or directory (os error 2)
+  # fileSystems."/boot/firmware" = {
+  #   device = "/dev/disk/by-label/FIRMWARE";
+  #   fsType = "vfat";
+  #   options = lib.mkForce [
+  #     "auto"
+  #     "nofail"
+  #     # root-only by default
+  #     "fmask=0077"
+  #     "dmask=0077"
+  #   ];
+  # };
+
   environment.systemPackages = with pkgs; [
     libraspberrypi
     raspberrypi-eeprom
   ];
+
+  # Can't use btrfs storage driver!
+  virtualisation.docker.storageDriver = lib.mkForce null;
 }
---
+++ hosts/pica4/ssh.nix
@@ -0,0 +1,55 @@
+{ config, ... }:
+let
+  secrets = [
+    "ssh/host_keys/rsa/privkey"
+    "ssh/host_keys/rsa/pubkey"
+    "ssh/host_keys/ed25519/privkey"
+    "ssh/host_keys/ed25519/pubkey"
+  ];
+in
+{
+  sops.secrets = builtins.listToAttrs (
+    map (secret: {
+      name = secret;
+      value = {
+        sopsFile = config.custom.sopsFile;
+      };
+    }) secrets
+  );
+
+  environment.etc."ssh/ssh_host_rsa_key" = {
+    source = config.sops.secrets."ssh/host_keys/rsa/privkey".path;
+    user = "root";
+    group = "root";
+    mode = "0400";
+  };
+  environment.etc."ssh/ssh_host_ed25519_key" = {
+    source = config.sops.secrets."ssh/host_keys/ed25519/privkey".path;
+    user = "root";
+    group = "root";
+    mode = "0400";
+  };
+  environment.etc."ssh/ssh_host_rsa_key.pub" = {
+    source = config.sops.secrets."ssh/host_keys/rsa/pubkey".path;
+    mode = "0444";
+    user = "root";
+    group = "root";
+  };
+  environment.etc."ssh/ssh_host_ed25519_key.pub" = {
+    source = config.sops.secrets."ssh/host_keys/ed25519/pubkey".path;
+    mode = "0444";
+    user = "root";
+    group = "root";
+  };
+
+  # services.openssh.hostKeys = [
+  #   {
+  #     path = config.sops.secrets."ssh/host_keys/ed25519/privkey".path;
+  #     type = "ed25519";
+  #   }
+  #   {
+  #     path = config.sops.secrets."ssh/host_keys/rsa/privkey".path;
+  #     type = "rsa";
+  #   }
+  # ];
+}
