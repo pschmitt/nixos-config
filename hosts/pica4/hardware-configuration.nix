4e9726ab50eba164c29f26840d75ee51a75436eb
Philipp Schmitt <philipp@schmitt.co>
2025-08-22 12:19:03 +0200

    Found the real culprit: hardware.enableAllHardware

--- hosts/pica4/hardware-configuration.nix
+++ hosts/pica4/hardware-configuration.nix
@@ -9,21 +9,12 @@
 
   nixpkgs.hostPlatform = lib.mkDefault "aarch64-linux";
 
-  boot = {
-    kernelPackages = lib.mkForce pkgs.linuxKernel.packages.linux_rpi4;
+  boot.kernelPackages = lib.mkForce pkgs.linuxKernel.packages.linux_rpi4;
 
-    # Including the default modules leads to build errors:
-    # modprobe: FATAL: Module dw-hdmi not found in directory /nix/store/…
-    initrd.availableKernelModules = lib.mkForce [
-      "usbhid"
-      "usb_storage"
-      "vc4"
-      "pcie_brcmstb" # required for the pcie bus to work
-      "reset-raspberrypi" # required for vl805 firmware to load
-    ];
-    # initrd.includeDefaultModules = false;
-    # initrd.kernelModules = lib.mkForce [ ];
-  };
+  # Enabling all hardware leads to build errors:
+  # modprobe: FATAL: Module dw-hdmi not found in directory /nix/store/…
+  hardware.enableAllHardware = lib.mkForce false;
+  services.fwupd.enable = lib.mkForce false;
 
   environment.systemPackages = with pkgs; [
     libraspberrypi
