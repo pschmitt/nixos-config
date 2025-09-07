5a411e866be49d5add3dc784005b82812add9979
Philipp Schmitt <philipp@schmitt.co>
2025-09-07 11:47:46 +0200

    fix dep

--- hosts/rofl-08/monit.nix
+++ hosts/rofl-08/monit.nix
@@ -30,7 +30,6 @@ let
     check program "gluetun" with path "${pkgs.curl}/bin/curl -fsSL -x $TAILSCALE_IP:8888 https://myip.wtf/json"
       every 5 cycles
       group piracy
-      depends on "docker compose services"
       restart program = "${pkgs.docker}/bin/docker compose -f /srv/piracy/docker-compose.yaml restart gluetun"
       if status != 0 then restart
       if 2 restarts within 10 cycles then alert
