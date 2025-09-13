e025e7a4a0805b62bf9984c4b874bb317adee9dc
Philipp Schmitt <philipp@schmitt.co>
2025-09-13 18:10:24 +0200

    Undebug

--- hosts/rofl-11/default.nix
+++ hosts/rofl-11/default.nix
@@ -9,24 +9,6 @@
 
   custom.cattle = true;
 
-  # Increase verbosity only on this host to debug
-  # initrd networking/SSH bring-up and early boot.
-  boot = {
-    initrd.verbose = true;
-    initrd.network.ssh.extraConfig = "LogLevel DEBUG3";
-
-    # Kernel verbosity on consoles
-    consoleLogLevel = 7;
-    # Add verbose logging for initrd/systemd and udev
-    kernelParams = [
-      "loglevel=7"
-      "rd.systemd.log_target=console"
-      # Also increase regular systemd logging to console for completeness
-      "systemd.log_target=console"
-    ];
-
-  };
-
   # Enable networking
   networking = {
     hostName = lib.strings.trim (builtins.readFile ./HOSTNAME);
