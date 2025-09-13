ba881b3e48ecee47fe0946d74566f275f322d598
Philipp Schmitt <philipp@schmitt.co>
2025-09-13 18:17:08 +0200

    Add rofl-12

---
+++ hosts/rofl-12/default.nix
@@ -0,0 +1,58 @@
+{ config, lib, ... }:
+{
+  imports = [
+    ./disk-config.nix
+    ./hardware-configuration.nix
+
+    ../../server
+    ../../server/optimist.nix
+
+    (import ../../services/nfs/nfs-client.nix { })
+
+    ../../services/http.nix
+
+    ../../services/xmr/monero-wallet-rpc.nix
+    ../../services/xmr/monerod.nix
+    ../../services/xmr/p2pool.nix
+    ../../services/xmr/xmrig-p2pool.nix
+    ../../services/xmr/xmrig-proxy.nix
+    ../../services/xmr/xmrig.nix
+  ];
+
+  custom.cattle = true;
+  custom.promptColor = "#ff6600";
+
+  # Enable networking
+  networking = {
+    hostName = lib.strings.trim (builtins.readFile ./HOSTNAME);
+    # Disable the firewall altogether.
+    firewall = {
+      enable = false;
+      # allowedTCPPorts = [ ... ];
+      # allowedUDPPorts = [ ... ];
+    };
+  };
+
+  services = {
+    xmrig.settings.cpu.max-threads-hint = lib.mkForce 15;
+
+    monero.extraConfig = ''
+      # add for p2pool's quick template updates
+      zmq-pub=tcp://127.0.0.1:18083
+    '';
+
+    p2pool = {
+      enable = true;
+      walletSecret = "p2pool/wallet";
+      sopsFile = config.custom.sopsFile;
+      mode = "mini"; # or "nano" for faster sync
+      stratum.port = 13333; # 3333 is used by xmrig-proxy!
+      openFirewall = true;
+      extraArgs = [
+        # examples:
+        # "--disable-upnp"
+        # "--loglevel" "2"
+      ];
+    };
+  };
+}
