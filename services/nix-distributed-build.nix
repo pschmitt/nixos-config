37deabf0ba85c48fb6dfbe198caa2c69452d2a53
Philipp Schmitt <philipp@schmitt.co>
2025-09-14 15:27:12 +0200

    Update remote builders

--- services/nix-distributed-build.nix
+++ services/nix-distributed-build.nix
@@ -4,12 +4,12 @@
     distributedBuilds = true;
     buildMachines = [
       {
-        hostName = "rofl-09.brkn.lol";
+        hostName = "rofl-10.brkn.lol";
         protocol = "ssh-ng";
         sshUser = "nix-remote-builder";
         sshKey = config.sops.secrets."ssh/nix-remote-builder/privkey".path;
         # NOTE we rely on the public keys being setup by programs.ssh.knownHosts
-        # ssh rofl-09.brkn.lol base64 -w0 /etc/ssh/ssh_host_ed25519_key.pub
+        # ssh rofl-10.brkn.lol base64 -w0 /etc/ssh/ssh_host_ed25519_key.pub
         # publicHostKey = "c3NoLWVkMjU1MTkgQUFBQUMzTnphQzFsWkRJMU5URTVBQUFBSU0xUlF1RDEyK0NMNU56SkhyVmdlNDl1SzlReVBsSVNvYlFHNU1OZ0laSG8gcm9vdEByb2ZsLTA5Cg==";
         systems = [ "x86_64-linux" ];
         maxJobs = 2;
@@ -21,12 +21,12 @@
         ];
       }
       {
-        hostName = "rofl-03.brkn.lol";
+        hostName = "rofl-13.brkn.lol";
         protocol = "ssh-ng";
         sshUser = "nix-remote-builder";
         sshKey = config.sops.secrets."ssh/nix-remote-builder/privkey".path;
         # NOTE we rely on the public keys being setup by programs.ssh.knownHosts
-        # ssh rofl-03.brkn.lol base64 -w0 /etc/ssh/ssh_host_ed25519_key.pub
+        # ssh rofl-13.brkn.lol base64 -w0 /etc/ssh/ssh_host_ed25519_key.pub
         # publicHostKey = "c3NoLWVkMjU1MTkgQUFBQUMzTnphQzFsWkRJMU5URTVBQUFBSUwvbStwRCtUc1NISnhTSFVIb3ltSHZxZXZGcnFPbWZBQmo3QWMxaFMzVFEgcm9vdEByb2ZsLTAzCg==";
         systems = [
           "aarch64-linux"
