4bc6bc41f167b0dd6f06c4a3d7364c6799bc5f77
Philipp Schmitt <philipp@schmitt.co>
2025-09-14 12:07:57 +0200

    Disable services on rofl-08

--- hosts/rofl-08/default.nix
+++ hosts/rofl-08/default.nix
@@ -8,21 +8,21 @@
     ../../server
     ../../server/optimist.nix
 
-    (import ../../services/nfs/nfs-client.nix { mountPoint = "/mnt/rofl-09"; })
-    (import ../../services/nfs/nfs-server.nix {
-      inherit lib;
-      exports = [
-        "srv"
-        "videos"
-      ];
-    })
-    ../../services/http.nix
-    ../../services/stash.nix
-    ../../services/tdarr-server.nix
+    # (import ../../services/nfs/nfs-client.nix { mountPoint = "/mnt/rofl-09"; })
+    # (import ../../services/nfs/nfs-server.nix {
+    #   inherit lib;
+    #   exports = [
+    #     "srv"
+    #     "videos"
+    #   ];
+    # })
+    # ../../services/http.nix
+    # ../../services/stash.nix
+    # ../../services/tdarr-server.nix
 
-    ./container-services.nix
-    ./monit.nix
-    ./restic.nix
+    # ./container-services.nix
+    # ./monit.nix
+    # ./restic.nix
   ];
 
   custom.cattle = true;
