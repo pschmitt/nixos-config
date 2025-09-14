0ddd98b1ab8cd06bc82b133a1955837b6625643c
Philipp Schmitt <philipp@schmitt.co>
2025-09-14 15:14:06 +0200

    rofl-09 -> rofl-10

--- hosts/rofl-09/default.nix
+++ hosts/rofl-09/default.nix
@@ -10,35 +10,35 @@
 
     # services
     # backups services
-    ../../services/backups/autorestic.nix
-    ../../services/backups/bitwarden.nix
-    ../../services/backups/evernote.nix
-    ../../services/changedetection-io.nix
-    ../../services/harmonia.nix
-    ../../services/forgejo.nix
-    ../../services/http.nix
-    ../../services/http-static.nix
-    ../../services/immich.nix
-    ../../services/luks-ssh-unlock-homelab.nix
-    # ../../services/mealie.nix # it's a container now!
-    ../../services/paperless-ngx.nix
-    ../../services/podsync.nix
-    ../../services/postgresql.nix
-    ../../services/rclone-bisync.nix
-    ../../services/searxng.nix
-    ../../services/turris-ssh-tunnel.nix
-    (import ../../services/nfs/nfs-server.nix { inherit lib; })
-
-    # misc
-    ../../misc/rsync-fonts-to-rofl-13.nix
-
-    # host-specific service config
-    ./container-services.nix
-    ./monit.nix
-    ./restic.nix
+    # ../../services/backups/autorestic.nix
+    # ../../services/backups/bitwarden.nix
+    # ../../services/backups/evernote.nix
+    # ../../services/changedetection-io.nix
+    # ../../services/harmonia.nix
+    # ../../services/forgejo.nix
+    # ../../services/http.nix
+    # ../../services/http-static.nix
+    # ../../services/immich.nix
+    # ../../services/luks-ssh-unlock-homelab.nix
+    # # ../../services/mealie.nix # it's a container now!
+    # ../../services/paperless-ngx.nix
+    # ../../services/podsync.nix
+    # ../../services/postgresql.nix
+    # ../../services/rclone-bisync.nix
+    # ../../services/searxng.nix
+    # ../../services/turris-ssh-tunnel.nix
+    # (import ../../services/nfs/nfs-server.nix { inherit lib; })
+    #
+    # # misc
+    # ../../misc/rsync-fonts-to-rofl-13.nix
+    #
+    # # host-specific service config
+    # ./container-services.nix
+    # ./monit.nix
+    # ./restic.nix
   ];
 
-  custom.cattle = false;
+  custom.cattle = true;
   custom.promptColor = "#088A39"; # nginx green
 
   # Enable networking
