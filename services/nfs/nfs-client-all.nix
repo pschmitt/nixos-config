4fc613269f59b5cd6dc158501a2be830880ac4a0
Philipp Schmitt <philipp@schmitt.co>
2025-09-14 15:19:22 +0200

    Update default nfs host

--- services/nfs/nfs-client-all.nix
+++ services/nfs/nfs-client-all.nix
@@ -1,9 +1,7 @@
 { ... }:
 {
   imports = [
-    ./nfs-client-rofl-09.nix
-    # TODO Uncomment once migration is done
-    # ./nfs-client-rofl-10.nix
+    ./nfs-client-rofl-10.nix
     ./nfs-client-rofl-11.nix
   ];
 }
