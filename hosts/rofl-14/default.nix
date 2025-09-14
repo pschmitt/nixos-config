e434628136cf91b7d8f922527169d8c134eb30cf
Philipp Schmitt <philipp@schmitt.co>
2025-09-14 14:17:08 +0200

    Add rofl-14

---
+++ hosts/rofl-14/default.nix
@@ -0,0 +1,32 @@
+{ lib, ... }:
+{
+  imports = [
+    ./disk-config.nix
+    ./hardware-configuration.nix
+
+    ../../server
+    ../../server/optimist.nix
+
+    ../../services/harmonia.nix
+    ../../services/http.nix
+    ../../services/nfs/nfs-client-rofl-11.nix
+    ../../services/tdarr-node.nix
+    ../../services/xmr/xmrig.nix
+  ];
+
+  custom.cattle = true;
+  custom.promptColor = "magenta";
+
+  # Enable networking
+  networking = {
+    hostName = lib.strings.trim (builtins.readFile ./HOSTNAME);
+    # Disable the firewall altogether.
+    firewall = {
+      enable = false;
+      # allowedTCPPorts = [ ... ];
+      # allowedUDPPorts = [ ... ];
+    };
+  };
+
+  # environment.systemPackages = with pkgs; [ ];
+}
