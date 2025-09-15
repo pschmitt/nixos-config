b8fd649c23f68f3035811ab08f0591bdcb2fca3d
Philipp Schmitt <philipp@schmitt.co>
2025-09-15 10:41:30 +0200

    Brotab: add dev mode extension id

--- overlays/brotab.nix
+++ overlays/brotab.nix
@@ -1,10 +1,26 @@
 { final, prev }:
 {
   brotab = prev.brotab.overrideAttrs (oldAttrs: {
+    nativeBuildInputs = (oldAttrs.nativeBuildInputs or [ ]) ++ [ final.jq ];
+
     postInstall = ''
       mkdir -p $out/lib/mozilla/native-messaging-hosts $out/lib/chromium/NativeMessagingHosts
-      sed -r "s#(\"path\":).*#\1 \"$out/bin/bt_mediator\",#" $out/lib/python3*/site-packages/brotab/mediator/firefox_mediator.json > $out/lib/mozilla/native-messaging-hosts/brotab_mediator.json
-      sed -r "s#(\"path\":).*#\1 \"$out/bin/bt_mediator\",#" $out/lib/python3*/site-packages/brotab/mediator/chromium_mediator.json > $out/lib/chromium/NativeMessagingHosts/brotab_mediator.json
+
+      # Firefox mediator: only patch the path
+      jq --arg out "$out" '.path = "\($out)/bin/bt_mediator"' \
+        $out/lib/python3*/site-packages/brotab/mediator/firefox_mediator.json \
+        > $out/lib/mozilla/native-messaging-hosts/brotab_mediator.json
+
+      # Chromium mediator: patch the path and add the extra allowed origin
+      jq --arg out "$out" --arg ext "knldjmfmopnpolahpmmgbagdohdnhkik" '
+        .path = "\($out)/bin/bt_mediator"
+        | .allowed_origins = (
+            (.allowed_origins // [])
+            + ["chrome-extension://\($ext)/"]
+          | unique)
+      ' \
+        $out/lib/python3*/site-packages/brotab/mediator/chromium_mediator.json \
+        > $out/lib/chromium/NativeMessagingHosts/brotab_mediator.json
     '';
   });
 }
