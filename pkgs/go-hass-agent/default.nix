f82d92ef59491ab89604e253cd9885a4c1381eb3
Philipp Schmitt <philipp@schmitt.co>
2025-09-18 09:36:16 +0200

    Add go-hass-agent

---
+++ pkgs/go-hass-agent/default.nix
@@ -0,0 +1,91 @@
+# Based off of:
+# https://github.com/nix-community/nur-combined/blob/main/repos/sebrut/pkgs/go-hass-agent/default.nix
+{
+  lib,
+  pkgs,
+  buildGoModule, # workaround till buildGoModule uses go 1.23 by default
+  pkg-config,
+  glfw,
+  libX11,
+  libXcursor,
+  libXrandr,
+  libXinerama,
+  libXi,
+  libXxf86vm,
+  mage,
+  writeShellScriptBin,
+  git,
+  ...
+}:
+
+buildGoModule rec {
+  pname = "go-hass-agent";
+  version = "13.3.3";
+
+  src = pkgs.fetchFromGitHub {
+    owner = "joshuar";
+    repo = "go-hass-agent";
+    rev = "v${version}";
+    hash = "sha256-6OgEhP2G8tnaYsa9rpPx/VoQ1akrGDFmwjkBuzjPVPQ=";
+  };
+
+  vendorHash = "sha256-m+4kPNlfZBSW3TGsoYjburKdEo10ysV5AJCr5oDy9qU=";
+
+  doCheck = false;
+
+  nativeBuildInputs =
+    let
+      fakeGit = writeShellScriptBin "git" ''
+        if [[ $@ = "describe --tags --always --dirty" ]]; then
+            echo "${version}"
+        elif [[ $@ = "rev-parse --short HEAD" ]]; then
+            echo "dummyrev"
+        elif [[ $@ = "log --date=iso8601-strict -1 --pretty=%ct" ]]; then
+            echo "0"
+        else
+            ${git}/bin/git $@
+        fi
+      '';
+    in
+    [
+      fakeGit
+      pkg-config
+      mage
+    ];
+
+  buildInputs = [
+    glfw
+    libX11
+    libXcursor
+    libXrandr
+    libXinerama
+    libXi
+    libXxf86vm
+  ];
+
+  buildPhase = ''
+    runHook preBuild
+
+    # Fixes “Error: error compiling magefiles” during build.
+    export HOME=$(mktemp -d)
+
+    mage -d build/magefiles -w . build:full
+
+    runHook postBuild
+  '';
+
+  installPhase = ''
+      runHook preInstall
+      mv dist/go-hass-agent-amd64 dist/go-hass-agent
+    install -Dt $out/bin dist/go-hass-agent
+    runHook postInstall
+  '';
+
+  meta = {
+    description = "A Home Assistant, native app for desktop/laptop devices.";
+    homepage = "https://github.com/joshuar/go-hass-agent";
+    license = lib.licenses.mit;
+    maintainers = with lib.maintainers; [ sebrut ];
+    mainProgram = "go-hass-agent";
+  };
+}
