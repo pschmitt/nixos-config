972714383aa5df677c2b259999baae912541edd7
Philipp Schmitt <philipp@schmitt.co>
2025-09-28 10:17:44 +0200

    Add settings

--- home-manager/gui/vicinae.nix
+++ home-manager/gui/vicinae.nix
@@ -7,5 +7,18 @@ in
 
   services.vicinae = {
     enable = true;
+    settings = {
+      faviconService = "twenty"; # "twenty" | "google" | "none"
+      font = {
+        normal = "Comic Code";
+        size = 16;
+      };
+      theme.name = "vicinae-dark";
+      window = {
+        csd = true;
+        opacity = 1;
+        rounding = 10;
+      };
+    };
   };
 }
