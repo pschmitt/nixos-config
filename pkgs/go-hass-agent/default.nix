e919908b45db05bf2f28a56916cf6a4f7074a672
Philipp Schmitt <philipp@schmitt.co>
2025-09-29 09:47:39 +0200

    Update go-hass-agent

--- pkgs/go-hass-agent/default.nix
+++ pkgs/go-hass-agent/default.nix
@@ -1,91 +1,38 @@
 # Based off of:
 # https://github.com/nix-community/nur-combined/blob/main/repos/sebrut/pkgs/go-hass-agent/default.nix
+
 {
   lib,
   pkgs,
-  buildGoModule, # workaround till buildGoModule uses go 1.23 by default
-  pkg-config,
-  glfw,
-  libX11,
-  libXcursor,
-  libXrandr,
-  libXinerama,
-  libXi,
-  libXxf86vm,
-  mage,
-  writeShellScriptBin,
-  git,
+  buildGoModule,
   ...
 }:
 
 buildGoModule rec {
   pname = "go-hass-agent";
-  version = "13.3.3";
+  version = "14.0.0";
 
   src = pkgs.fetchFromGitHub {
     owner = "joshuar";
     repo = "go-hass-agent";
     rev = "v${version}";
-    hash = "sha256-gvb/XuNAnypfgHqMnxFwn+8BXTJm4r1TvC8fwLmO5Bk=";
+    hash = "sha256-828CIwaexjXGKmGD5uGpPOMZJowFb+hZ8Lkj1jyikPE=";
   };
 
-  vendorHash = "sha256-m+4kPNlfZBSW3TGsoYjburKdEo10ysV5AJCr5oDy9qU=";
+  vendorHash = "sha256-A0uygP8ZwNfByWMA1yRMv0fN73vUSNYwyeCFGkPPyoA=";
 
-  doCheck = false;
-
-  nativeBuildInputs =
-    let
-      fakeGit = writeShellScriptBin "git" ''
-        if [[ $@ = "describe --tags --always --dirty" ]]; then
-            echo "${version}"
-        elif [[ $@ = "rev-parse --short HEAD" ]]; then
-            echo "dummyrev"
-        elif [[ $@ = "log --date=iso8601-strict -1 --pretty=%ct" ]]; then
-            echo "0"
-        else
-            ${git}/bin/git $@
-        fi
-      '';
-    in
-    [
-      fakeGit
-      pkg-config
-      mage
-    ];
-
-  buildInputs = [
-    glfw
-    libX11
-    libXcursor
-    libXrandr
-    libXinerama
-    libXi
-    libXxf86vm
+  ldflags = [
+    "-s"
+    "-w"
+    "-X github.com/joshuar/go-hass-agent/config.AppVersion=${version}"
   ];
-
-  buildPhase = ''
-    runHook preBuild
-
-    # Fixes “Error: error compiling magefiles” during build.
-    export HOME=$(mktemp -d)
-
-    mage -d build/magefiles -w . build:full
-
-    runHook postBuild
-  '';
-
-  installPhase = ''
-      runHook preInstall
-      mv dist/go-hass-agent-amd64 dist/go-hass-agent
-    install -Dt $out/bin dist/go-hass-agent
-    runHook postInstall
-  '';
 
   meta = {
     description = "A Home Assistant, native app for desktop/laptop devices.";
     homepage = "https://github.com/joshuar/go-hass-agent";
+    changelog = "https://github.com/joshuar/go-hass-agent/blob/${src.rev}/CHANGELOG.md";
     license = lib.licenses.mit;
-    maintainers = with lib.maintainers; [ sebrut ];
+    maintainers = with lib.maintainers; [ pschmitt ];
     mainProgram = "go-hass-agent";
   };
 }
