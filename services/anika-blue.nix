35fc21cda6cc0a5c2f55534848e28e3d3385e2e1
Philipp Schmitt <philipp@schmitt.co>
2025-10-03 21:09:21 +0200

    WIP: Anika blue

---
+++ services/anika-blue.nix
@@ -0,0 +1,30 @@
+{ config, ... }:
+{
+  sops.secrets."anika-blue/secretKey" = {
+    sopsFile = config.custom.sopsFile;
+  };
+
+  services.anika-blue = {
+    enable = true;
+    port = 5000;
+    dataDir = "/var/lib/anika-blue";
+    # Optional: path to file containing secret key
+    # secretKeyFile = "/run/secrets/anika-blue-secret";
+    secretKeyFile = config.sops.secrets."anika-blue/secretKey".path;
+  };
+
+  services.nginx.virtualHosts = {
+    "anika-blue.${config.custom.mainDomain}" = {
+      enableACME = true;
+      # FIXME https://github.com/NixOS/nixpkgs/issues/210807
+      acmeRoot = null;
+      forceSSL = true;
+
+      locations."/" = {
+        proxyPass = "http://127.0.0.1:${config.services.anika-blue.port}";
+        # proxyWebsockets = true;
+        recommendedProxySettings = true;
+      };
+    };
+  };
+}
