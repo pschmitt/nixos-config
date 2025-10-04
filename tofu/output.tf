a8504d454096df5e28c4b09b756f6780c0e9b389
Philipp Schmitt <philipp@schmitt.co>
2025-10-04 21:32:32 +0200

    Add hc

--- tofu/output.tf
+++ tofu/output.tf
@@ -3,4 +3,9 @@ output "rofl-10_fip" {
   description = "Floating IP address of rofl-10"
 }
 
+output "restic_backup_ping_urls" {
+  description = "Ping URLs for Restic backup checks"
+  value       = { for host, check in healthchecksio_check.restic_backup : host => check.ping_url }
+}
+
 # vim: set ft=terraform
