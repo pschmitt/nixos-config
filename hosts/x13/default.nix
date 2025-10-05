f17c5727ffb2f0b4a43d90bc5f274c5dda0c9f4f
Philipp Schmitt <philipp@schmitt.co>
2025-10-05 12:27:38 +0200

    laptops aint cattle

--- hosts/x13/default.nix
+++ hosts/x13/default.nix
@@ -20,6 +20,8 @@
     ../../services/nfs/nfs-client-all.nix
   ];
 
+  custom.cattle = false;
+
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
