f1bf74f64a11c6c06ad7ab21443655e9ea831924
Philipp Schmitt <philipp@schmitt.co>
2025-10-06 12:48:56 +0200

    add alt host

--- services/poor-tools.nix
+++ services/poor-tools.nix
@@ -1,18 +1,26 @@
-{ config, inputs, ... }: {
+{ config, inputs, ... }:
+{
   imports = [ inputs.poor-tools.nixosModules.default ];
 
   services.poor-tools-web = {
     enable = true;
   };
 
-  services.nginx.virtualHosts."poor.curl-pipe.sh" = {
-    enableACME = true;
-    forceSSL = false; # disabled on purpose!
+  services.nginx.virtualHosts =
+    let
+      nginxConfig = {
+        enableACME = true;
+        forceSSL = false; # disabled on purpose!
 
-    locations."/" = {
-      proxyPass = "http://${toString config.services.poor-tools-web.bindHost}:${toString config.services.poor-tools-web.bindPort}";
-      proxyWebsockets = true;
-      recommendedProxySettings = true;
+        locations."/" = {
+          proxyPass = "http://${toString config.services.poor-tools-web.bindHost}:${toString config.services.poor-tools-web.bindPort}";
+          proxyWebsockets = true;
+          recommendedProxySettings = true;
+        };
+      };
+    in
+    {
+      "poor.curl-pipe.sh" = nginxConfig;
+      "poor.${config.custom.mainDomain}" = nginxConfig;
     };
-  };
 }
