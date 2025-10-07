cfe009da05e83aec787186c66f0fad83d9f75c48
Philipp Schmitt <philipp@schmitt.co>
2025-10-07 07:40:39 +0200

    add poor.tools

---
+++ tofu/dns-poor-tools.tf
@@ -0,0 +1,17 @@
+resource "cloudflare_record" "wildcard_poor-tools" {
+  zone_id = cloudflare_zone.poor-tools.id
+  name    = "*"
+  content = openstack_networking_floatingip_v2.rofl-10_fip.address
+  type    = "A"
+  proxied = false
+  ttl     = 1
+}
+
+resource "cloudflare_record" "poor-tools" {
+  zone_id = cloudflare_zone.poor-tools.id
+  name    = "@"
+  content = openstack_networking_floatingip_v2.rofl-10_fip.address
+  type    = "A"
+  proxied = false
+  ttl     = 1
+}
