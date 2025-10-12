36dac341287d764cba9336d8bf48f536e7c1f24a
Philipp Schmitt <philipp@schmitt.co>
2025-10-12 15:34:32 +0200

    Dynamic .snapshot creation

--- common/gui/btrfs.nix
+++ common/gui/btrfs.nix
@@ -5,22 +5,72 @@
   ...
 }:
 let
-  createSnapshotsSubvolume = pkgs.writeShellScript "snapper-create-snapshots-subvolume" ''
-    if ${pkgs.btrfs-progs}/bin/btrfs subvolume show /home/.snapshots
-    then
-      echo "/home/.snapshots already exits, nothing to do."
-      exit 0
-    fi
+  snapperConfigs = config.services.snapper.configs or { };
 
-    echo "Creating /home/.snapshots subvolume for snapper..."
-    ${pkgs.btrfs-progs}/bin/btrfs subvolume create /home/.snapshots
-  '';
+  relevantConfigs = lib.pipe snapperConfigs [
+    (lib.mapAttrsToList (
+      name: cfg:
+      if (cfg ? FSTYPE) && cfg.FSTYPE == "btrfs" && (cfg ? SUBVOLUME) then
+        {
+          inherit name;
+          snapshotDir = "${cfg.SUBVOLUME}/.snapshots";
+        }
+      else
+        null
+    ))
+    (configs: lib.filter (config: config != null) configs)
+  ];
+
+  createSnapshotsSubvolumeScript =
+    if relevantConfigs == [ ] then
+      null
+    else
+      pkgs.writeShellScript "snapper-create-snapshots-subvolumes" ''
+        set -euo pipefail
+
+        btrfs_bin=${lib.escapeShellArg "${pkgs.btrfs-progs}/bin/btrfs"}
+
+        if [ -n "''${SNAPPER_BTRFS_BIN:-}" ]
+        then
+          btrfs_bin="$SNAPPER_BTRFS_BIN"
+        fi
+
+        configs=(
+          ${lib.concatMapStringsSep "\n          " (
+            config: lib.escapeShellArg "${config.name}:${config.snapshotDir}"
+          ) relevantConfigs}
+        )
+
+        for config_info in "''${configs[@]}"
+        do
+          config_name="''${config_info%%:*}"
+          snapshot_dir="''${config_info#*:}"
+
+          if [ -z "$config_name" ] || [ -z "$snapshot_dir" ]
+          then
+            echo "Skipping snapper config $config_name because the snapshot directory is missing."
+            continue
+          fi
+
+          echo "Ensuring $snapshot_dir exists for snapper config $config_name..."
+
+          if "$btrfs_bin" subvolume show "$snapshot_dir"
+          then
+            echo "$snapshot_dir already exists, nothing to do."
+            continue
+          fi
+
+          echo "Creating $snapshot_dir subvolume for snapper config $config_name..."
+          "$btrfs_bin" subvolume create "$snapshot_dir"
+        done
+      '';
 in
 {
   services.snapper = {
     snapshotRootOnBoot = false;
     snapshotInterval = "hourly";
     cleanupInterval = "1d";
+    persistentTimer = true;
 
     configs = {
       home = {
@@ -37,7 +87,7 @@ in
 
   environment.systemPackages = with pkgs; [ snapper-gui ];
 
-  systemd.services.snapperd.serviceConfig.ExecStartPre = lib.mkBefore [
-    createSnapshotsSubvolume
-  ];
+  systemd.services.snapperd.serviceConfig.ExecStartPre = lib.mkBefore (
+    lib.optional (createSnapshotsSubvolumeScript != null) createSnapshotsSubvolumeScript
+  );
 }
