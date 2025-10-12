43cc460fc5c7c86de00893640e63bf1218a6aa33
Philipp Schmitt <philipp@schmitt.co>
2025-10-12 15:06:03 +0200

    Ensure .snapshot dir exists

--- common/gui/btrfs.nix
+++ common/gui/btrfs.nix
@@ -1,7 +1,21 @@
 {
+  config,
+  lib,
   pkgs,
   ...
 }:
+let
+  createSnapshotsSubvolume = pkgs.writeShellScript "snapper-create-snapshots-subvolume" ''
+    if ${pkgs.btrfs-progs}/bin/btrfs subvolume show /home/.snapshots
+    then
+      echo "/home/.snapshots already exits, nothing to do."
+      exit 0
+    fi
+
+    echo "Creating /home/.snapshots subvolume for snapper..."
+    ${pkgs.btrfs-progs}/bin/btrfs subvolume create /home/.snapshots
+  '';
+in
 {
   services.snapper = {
     snapshotRootOnBoot = false;
@@ -14,7 +28,7 @@
         # NOTE make sure that there is a .snapshots subvolume!
         # sudo btrfs subvolume create /home/.snapshots
         SUBVOLUME = "/home"; # @home won't work here
-        ALLOW_USERS = [ "pschmitt" ];
+        ALLOW_USERS = [ config.custom.username ];
         TIMELINE_CREATE = true;
         TIMELINE_CLEANUP = true;
       };
@@ -22,4 +36,8 @@
   };
 
   environment.systemPackages = with pkgs; [ snapper-gui ];
+
+  systemd.services.snapperd.serviceConfig.ExecStartPre = lib.mkBefore [
+    createSnapshotsSubvolume
+  ];
 }
