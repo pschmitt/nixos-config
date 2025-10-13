1c1ebac61eb63873c253d73bc2692ffaf5997288
Philipp Schmitt <philipp@schmitt.co>
2025-10-13 23:39:22 +0200

    Add addressbook-export

--- home-manager/misc.nix
+++ home-manager/misc.nix
@@ -8,6 +8,11 @@
 
     # todoist cli
     inputs.tdc.packages."${system}".tdc
-    todoist
+
+    # Below allows exporting address books from Evolution
+    # we use this in ~zpl/contacts.zsh
+    (pkgs.writeShellScriptBin "addressbook-export" ''
+      exec ${pkgs.evolution-data-server}/libexec/evolution-data-server/addressbook-export "$@"
+    '')
   ];
 }
