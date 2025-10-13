3db50fee5bdc9bd13c25d742880c5b5285580927
Philipp Schmitt <philipp@schmitt.co>
2025-10-13 21:33:26 +0200

    Remove docs, we ain't fucking children

--- hosts/ge2/hardware-configuration.nix
+++ hosts/ge2/hardware-configuration.nix
@@ -29,7 +29,8 @@
     options = [ "subvol=@root" ];
   };
 
-  boot.initrd.luks.devices."encrypted".device = "/dev/disk/by-uuid/d4f522ea-e59a-4960-908d-dc8445e1ffcd";
+  boot.initrd.luks.devices."encrypted".device =
+    "/dev/disk/by-uuid/d4f522ea-e59a-4960-908d-dc8445e1ffcd";
 
   fileSystems."/boot" = {
     device = "/dev/disk/by-uuid/1E3C-2DA0";
@@ -50,12 +51,7 @@
 
   swapDevices = [ ];
 
-  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
-  # (the default) this is the recommended approach. When using systemd-networkd it's
-  # still possible to use this option, but it's recommended to use it in conjunction
-  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
   networking.useDHCP = lib.mkDefault true;
-  # networking.interfaces.wlp0s20f3.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
