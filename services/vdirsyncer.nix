0a2625e5722f02d51e5ca355ea6aca8681bb3b12
Philipp Schmitt <philipp@schmitt.co>
2025-10-14 11:37:06 +0200

    Rewrite config

--- services/vdirsyncer.nix
+++ services/vdirsyncer.nix
@@ -1,71 +1,53 @@
-{ config, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 let
   syncUser = "vdirsyncer";
   syncGroup = syncUser;
   stateDir = "/var/lib/${syncUser}";
-  serviceName = "vdirsyncer@google-to-nextcloud.service";
-  templateDir = "vdirsyncer";
-  templateName = "${templateDir}/google-to-nextcloud.conf";
   nextcloudHost = "c.${config.custom.mainDomain}";
-  nextcloudUrl = "https://${nextcloudHost}/remote.php/dav/addressbooks/users/${
+  nextcloudRootUrl = "https://${nextcloudHost}/remote.php/dav/addressbooks/users/${
     config.sops.placeholder."vdirsyncer/nextcloud/username"
   }/";
-  secretBase = {
-    sopsFile = config.custom.sopsFile;
-    restartUnits = [ serviceName ];
-  };
-  secretWith = extra: secretBase // extra;
-  syncSecret = secretWith {
-    owner = syncUser;
-    group = syncGroup;
-    mode = "0400";
-  };
 in
 {
   sops.secrets = {
-    "vdirsyncer/google/client-id" = secretBase;
-    "vdirsyncer/google/client-secret" = secretBase;
-    "vdirsyncer/google/token" = syncSecret;
-    "vdirsyncer/nextcloud/username" = syncSecret;
-    "vdirsyncer/nextcloud/password" = syncSecret;
-  };
-
-  sops.templates."${templateName}" = {
-    owner = syncUser;
-    group = syncGroup;
-    mode = "0400";
-    content = ''
-      [general]
-      status_path = "${stateDir}/google-to-nextcloud/status"
-
-      [pair google_to_nextcloud]
-      a = "google_contacts"
-      b = "nextcloud_contacts"
-      # collections = ["from a"]
-      # sync from google's "default" address book to nextcloud's "google" address book
-      collections = [["a", "default", "google"]]
-      conflict_resolution = "a wins"
-      # metadata = ["displayname", "color"]
-
-      [storage google_contacts]
-      type = "google_contacts"
-      read_only = true
-      client_id = "${config.sops.placeholder."vdirsyncer/google/client-id"}"
-      client_secret = "${config.sops.placeholder."vdirsyncer/google/client-secret"}"
-      # Below is what we used to let vdirsyncer write the token file itself (initial setup)
-      token_file = "${stateDir}/google-to-nextcloud/google-token.json"
-      # token_file = "${config.sops.placeholder."vdirsyncer/google/token"}"
-
-      [storage nextcloud_contacts]
-      type = "carddav"
-      url = "${nextcloudUrl}"
-      username = "${config.sops.placeholder."vdirsyncer/nextcloud/username"}"
-      password = "${config.sops.placeholder."vdirsyncer/nextcloud/password"}"
-    '';
+    "vdirsyncer/google/client-id" = {
+      sopsFile = config.custom.sopsFile;
+      owner = syncUser;
+      group = syncGroup;
+      mode = "0400";
+    };
+    "vdirsyncer/google/client-secret" = {
+      sopsFile = config.custom.sopsFile;
+      owner = syncUser;
+      group = syncGroup;
+      mode = "0400";
+    };
+    "vdirsyncer/google/token" = {
+      sopsFile = config.custom.sopsFile;
+      owner = syncUser;
+      group = syncGroup;
+      mode = "0400";
+    };
+    "vdirsyncer/nextcloud/username" = {
+      sopsFile = config.custom.sopsFile;
+      owner = syncUser;
+      group = syncGroup;
+      mode = "0400";
+    };
+    "vdirsyncer/nextcloud/password" = {
+      sopsFile = config.custom.sopsFile;
+      owner = syncUser;
+      group = syncGroup;
+      mode = "0400";
+    };
   };
 
   users.groups.${syncGroup} = { };
-
   users.users.${syncUser} = {
     isSystemUser = true;
     group = syncGroup;
@@ -75,17 +57,82 @@ in
 
   services.vdirsyncer = {
     enable = true;
+
     jobs.google-to-nextcloud = {
       enable = true;
       user = syncUser;
       group = syncGroup;
       forceDiscover = true;
+
+      config = {
+        general.status_path = "${stateDir}/google-to-nextcloud/status";
+
+        storages = {
+          google_contacts = {
+            type = "google_contacts";
+            read_only = true;
+            "client_id.fetch" = [
+              "command"
+              "cat"
+              config.sops.secrets."vdirsyncer/google/client-id".path
+            ];
+            "client_secret.fetch" = [
+              "command"
+              "cat"
+              config.sops.secrets."vdirsyncer/google/client-secret".path
+            ];
+            token_file = "${stateDir}/google-to-nextcloud/google-token.json";
+          };
+
+          nextcloud_contacts = {
+            type = "carddav";
+            url = nextcloudRootUrl;
+            "username.fetch" = [
+              "command"
+              "cat"
+              config.sops.secrets."vdirsyncer/nextcloud/username".path
+            ];
+            "password.fetch" = [
+              "command"
+              "cat"
+              config.sops.secrets."vdirsyncer/nextcloud/password".path
+            ];
+          };
+        };
+
+        pairs.google_to_nextcloud = {
+          a = "google_contacts";
+          b = "nextcloud_contacts";
+          collections = [
+            [
+              "google_sync"
+              "default"
+              "google"
+            ]
+          ];
+          conflict_resolution = "a wins";
+          metadata = [
+            "displayname"
+            "color"
+          ];
+        };
+      };
+
       timerConfig = {
         OnCalendar = "hourly";
         RandomizedDelaySec = "900";
         Persistent = true;
       };
-      configFile = config.sops.templates."${templateName}".path;
     };
   };
+
+  # Just delete the state before every run, this allows us to delete
+  # the "google" address book from nextcloud cleanly if we ever want to.
+  systemd.services."vdirsyncer@google-to-nextcloud".serviceConfig.ExecStartPre = lib.mkAfter [
+    (pkgs.writeShellScript "vdirsyncer-rm-status-google-to-nextcloud" ''
+      ${pkgs.coreutils}/bin/rm -rf -- \
+        "${stateDir}/google-to-nextcloud/google_to_nextcloud" \
+        "${stateDir}/google-to-nextcloud/google_to_nextcloud.collections"
+    '')
+  ];
 }
