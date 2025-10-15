58e0f7f8fd3c8923af9edd63c05f11902ad6f1f0
Philipp Schmitt <philipp@schmitt.co>
2025-10-15 11:57:32 +0200

    Add snapper to the path

--- services/snapper.nix
+++ services/snapper.nix
@@ -1,64 +1,66 @@
-{ config, lib, modulesPath, pkgs, ... }:
+{
+  config,
+  lib,
+  modulesPath,
+  pkgs,
+  ...
+}:
 let
   snapperConfigs = config.services.snapper.configs or { };
 
   relevantConfigs = lib.pipe snapperConfigs [
     (lib.mapAttrsToList (
       name: cfg:
-        if (cfg ? FSTYPE) && cfg.FSTYPE == "btrfs" && (cfg ? SUBVOLUME) then
-          {
-            inherit name;
-            snapshotDir = "${cfg.SUBVOLUME}/.snapshots";
-          }
-        else
-          null
+      if (cfg ? FSTYPE) && cfg.FSTYPE == "btrfs" && (cfg ? SUBVOLUME) then
+        {
+          inherit name;
+          snapshotDir = "${cfg.SUBVOLUME}/.snapshots";
+        }
+      else
+        null
     ))
     (configs: lib.filter (config: config != null) configs)
   ];
 
-  createSnapshotsSubvolumeScripts =
-    lib.forEach relevantConfigs (
-      configInfo:
-        pkgs.writeShellScript "snapper-create-${configInfo.name}-snapshots-subvolume" ''
-          # shellcheck shell=bash
-          set -euo pipefail
+  createSnapshotsSubvolumeScripts = lib.forEach relevantConfigs (
+    configInfo:
+    pkgs.writeShellScript "snapper-create-${configInfo.name}-snapshots-subvolume" ''
+      # shellcheck shell=bash
+      set -euo pipefail
 
-          btrfs_bin=${lib.escapeShellArg "${pkgs.btrfs-progs}/bin/btrfs"}
+      btrfs_bin=${lib.escapeShellArg "${pkgs.btrfs-progs}/bin/btrfs"}
 
-          if [ -n "''${SNAPPER_BTRFS_BIN:-}" ]
-          then
-            btrfs_bin="$SNAPPER_BTRFS_BIN"
-          fi
+      if [ -n "''${SNAPPER_BTRFS_BIN:-}" ]
+      then
+        btrfs_bin="$SNAPPER_BTRFS_BIN"
+      fi
 
-          config_name=${lib.escapeShellArg configInfo.name}
-          snapshot_dir=${lib.escapeShellArg configInfo.snapshotDir}
+      config_name=${lib.escapeShellArg configInfo.name}
+      snapshot_dir=${lib.escapeShellArg configInfo.snapshotDir}
 
-          if "$btrfs_bin" subvolume show "$snapshot_dir"
-          then
-            exit 0
-          fi
+      if "$btrfs_bin" subvolume show "$snapshot_dir"
+      then
+        exit 0
+      fi
 
-          echo "Creating $snapshot_dir subvolume for snapper config $config_name..."
-          "$btrfs_bin" subvolume create "$snapshot_dir"
-        ''
-    );
+      echo "Creating $snapshot_dir subvolume for snapper config $config_name..."
+      "$btrfs_bin" subvolume create "$snapshot_dir"
+    ''
+  );
 
-  snapperWrappers =
-    lib.mapAttrsToList (
-      name: _:
-        pkgs.writeShellScriptBin "snapper-${name}" ''
-          exec ${lib.escapeShellArg "${pkgs.snapper}/bin/snapper"} -c ${lib.escapeShellArg name} "$@"
-        ''
-    ) snapperConfigs;
+  snapperWrappers = lib.mapAttrsToList (
+    name: _:
+    pkgs.writeShellScriptBin "snapper-${name}" ''
+      exec ${lib.escapeShellArg "${pkgs.snapper}/bin/snapper"} -c ${lib.escapeShellArg name} "$@"
+    ''
+  ) snapperConfigs;
 in
 {
   imports = [ "${modulesPath}/services/misc/snapper.nix" ];
 
-  environment.systemPackages = snapperWrappers;
+  environment.systemPackages = [ pkgs.snapper ] ++ snapperWrappers;
 
-  systemd.services.snapperd.serviceConfig.ExecStartPre =
-    lib.mkBefore (
-      lib.optionals (createSnapshotsSubvolumeScripts != [ ])
-        createSnapshotsSubvolumeScripts
-    );
+  systemd.services.snapperd.serviceConfig.ExecStartPre = lib.mkBefore (
+    lib.optionals (createSnapshotsSubvolumeScripts != [ ]) createSnapshotsSubvolumeScripts
+  );
 }
