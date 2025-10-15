3ccffa6c5a223153d2d54e8918260c3426663722
Philipp Schmitt <philipp@schmitt.co>
2025-10-15 10:15:04 +0200

    Enable snapper on servers

---
+++ common/gui/snapper.nix
@@ -0,0 +1,21 @@
+{ config, pkgs, ... }:
+{
+  imports = [ ../../services/snapper.nix ];
+
+  services.snapper = {
+    snapshotRootOnBoot = false;
+    snapshotInterval = "hourly";
+    cleanupInterval = "1d";
+    persistentTimer = true;
+
+    configs.home = {
+      FSTYPE = "btrfs";
+      SUBVOLUME = "/home"; # @home won't work here
+      ALLOW_USERS = [ config.custom.username ];
+      TIMELINE_CREATE = true;
+      TIMELINE_CLEANUP = true;
+    };
+  };
+
+  environment.systemPackages = with pkgs; [ snapper-gui ];
+}
