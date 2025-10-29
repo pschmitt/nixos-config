ec7aee6b0252177c7186ee6a4d029f4b81a229a4
Philipp Schmitt <philipp@schmitt.co>
2025-10-29 09:15:55 +0100

    Add winboat

--- flake.nix
+++ flake.nix
@@ -232,6 +232,11 @@
       # inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    winboat = {
+      url = "github:TibixDev/winboat";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     zen-browser = {
       url = "github:0xc000022070/zen-browser-flake";
       inputs.nixpkgs.follows = "nixpkgs";
