922c2029876bc8c1299366a6fceaead144b0bf97
Philipp Schmitt <philipp@schmitt.co>
2025-10-30 18:19:05 +0100

    Fix google-chrome crashes when moving the window to another monitor

--- overlays/hotfixes.nix
+++ overlays/hotfixes.nix
@@ -24,6 +24,17 @@
   #   };
   # };
 
+  # FIX For google-chrome crashing on Hyprland when moving the window from
+  # one monitor to another.
+  # https://github.com/hyprwm/Hyprland/discussions/11843
+  google-chrome = prev.google-chrome.overrideAttrs (old: {
+    nativeBuildInputs = (old.nativeBuildInputs or [ ]) ++ [ prev.makeWrapper ];
+    postFixup = (old.postFixup or "") + ''
+      wrapProgram $out/bin/google-chrome-stable \
+        --add-flags "--disable-features=WaylandWpColorManagerV1"
+    '';
+  });
+
   # Ensure python313Packages uses the modified interpreter
   # python313Packages = final.python313.pkgs;
 }
