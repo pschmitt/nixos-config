eb95faee4295ffb7e8dcbdb16c0ca2ad4a45b9f9
Philipp Schmitt <philipp@schmitt.co>
2025-10-31 08:37:25 +0100

    Use host network

--- pkgs/oracle-cloud-agent/get-download-urls.sh
+++ pkgs/oracle-cloud-agent/get-download-urls.sh
@@ -4,4 +4,4 @@ cd "$(cd "$(dirname "$0")" >/dev/null 2>&1; pwd -P)" || exit 9
 
 docker build -t oracle-cloud-agent-urls .
 
-docker run -it --rm oracle-cloud-agent-urls
+docker run -it --rm --net=host oracle-cloud-agent-urls
