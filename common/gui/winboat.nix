ee6a9e9622aae6946d7d9831e35482a728b3c8f8
Philipp Schmitt <philipp@schmitt.co>
2025-10-31 12:41:00 +0100

    freerdp3 -> freerdp

--- common/gui/winboat.nix
+++ common/gui/winboat.nix
@@ -1,8 +1,8 @@
 { inputs, pkgs, ... }:
 {
   environment.systemPackages = [
-    # NOTE w/o adding freerdp3 here, winboat fails to detect it at runtime
-    pkgs.freerdp3
+    # NOTE w/o adding freerdp here, winboat fails to detect it at runtime
+    pkgs.freerdp
     inputs.winboat.packages.${pkgs.system}.winboat
   ];
 }
