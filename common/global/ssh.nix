dac6ff27267c04267860c83e54c94ec9ef45618a
Philipp Schmitt <philipp@schmitt.co>
2025-11-04 15:55:49 +0100

    fmt

--- common/global/ssh.nix
+++ common/global/ssh.nix
@@ -79,22 +79,20 @@ in
 
   # https://github.com/nix-community/srvos/blob/main/nixos/common/well-known-hosts.nix
   # Avoid TOFU MITM with known forges.
-  programs.ssh.knownHosts = (
-    {
-      "github.com".hostNames = [ "github.com" ];
-      "github.com".publicKey =
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOMqqnkVzrm0SdG6UOoqKLsabgH5C9okWi0dh2l9GKJl";
+  programs.ssh.knownHosts = {
+    "github.com".hostNames = [ "github.com" ];
+    "github.com".publicKey =
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOMqqnkVzrm0SdG6UOoqKLsabgH5C9okWi0dh2l9GKJl";
 
-      "gitlab.com".hostNames = [ "gitlab.com" ];
-      "gitlab.com".publicKey =
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAfuCHKVTjquxvt6CM6tdG4SLp1Btn/nOeHHE5UOzRdf";
+    "gitlab.com".hostNames = [ "gitlab.com" ];
+    "gitlab.com".publicKey =
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAfuCHKVTjquxvt6CM6tdG4SLp1Btn/nOeHHE5UOzRdf";
 
-      "git.sr.ht".hostNames = [ "git.sr.ht" ];
-      "git.sr.ht".publicKey =
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMZvRd4EtM7R+IHVMWmDkVU3VLQTSwQDSAvW0t2Tkj60";
-    }
-    // generatedHosts
-  );
+    "git.sr.ht".hostNames = [ "git.sr.ht" ];
+    "git.sr.ht".publicKey =
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMZvRd4EtM7R+IHVMWmDkVU3VLQTSwQDSAvW0t2Tkj60";
+  }
+  // generatedHosts;
 
   networking.firewall = {
     allowedTCPPorts = lib.mkBefore [ 22 ];
