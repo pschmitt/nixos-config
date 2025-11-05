0996452b97e61e999de11e6c0a7d604da9a1d498
Philipp Schmitt <philipp@schmitt.co>
2025-11-05 21:57:51 +0100

    a little bit less netbird issues

--- common/network/tailscale.nix
+++ common/network/tailscale.nix
@@ -16,14 +16,7 @@ in
     enable = true;
     package = tailscalePkg;
     openFirewall = true;
-    extraUpFlags =
-      if config.services.netbird.enable then
-        [
-          "--netfilter-mode=off"
-          "--accept-dns=false"
-        ]
-      else
-        [ "--accept-dns=false" ];
+    extraSetFlags = [ "--accept-dns=false" ];
     useRoutingFeatures = "both";
     authKeyFile = config.sops.secrets."tailscale/auth-key".path;
   };
