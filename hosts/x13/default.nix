ed2a79b54be42cbeb09b6ce5395f0ac21270ba5e
Philipp Schmitt <philipp@schmitt.co>
2025-11-09 19:10:40 +0100

    Simplify x13 hw config

--- hosts/x13/default.nix
+++ hosts/x13/default.nix
@@ -4,10 +4,6 @@
 }:
 {
   imports = [
-    inputs.hardware.nixosModules.common-cpu-amd
-    # inputs.hardware.nixosModules.common-gpu-amd
-    inputs.hardware.nixosModules.common-pc-ssd
-
     ./hardware-configuration.nix
 
     ../../common/global
