c21d8eb0a20c5eb1d9db96058c931d3263b6ddfb
Philipp Schmitt <philipp@schmitt.co>
2025-11-09 11:56:27 +0100

    Split dotfiles, add manydots-magic

---
+++ server/dotfiles/shell.nix
@@ -0,0 +1,170 @@
+{
+  environment = {
+    interactiveShellInit = ''
+      [[ "$SHELL" != *zsh ]] && alias where="which"
+
+      echo_info() {
+        echo "''${fg_bold[blue]}INF''${reset_color} ''${*}" >&2
+      }
+
+      echo_warning() {
+        echo "''${fg_bold[yellow]}WRN''${reset_color} ''${*}" >&2
+      }
+
+      echo_error() {
+        echo "''${fg_bold[red]}ERR''${reset_color} ''${*}" >&2
+      }
+
+      alias cdr='cd "$(git rev-parse --show-toplevel)"'
+      alias gst="git status"
+      alias gl="git pull"
+      alias gll="git pull --rebase --autostash"
+
+      if command -v monit &>/dev/null
+      then
+        alias monit="sudo monit"
+      fi
+
+      if ! command -v docker-compose &>/dev/null
+      then
+        alias docker-compose="docker compose"
+      fi
+
+      alias dc="docker compose"
+      alias dpl="docker compose pull"
+      alias ddwn="docker compose down"
+      alias drup="docker compose down; docker compose up --force-recreate --remove-orphans"
+      alias drupd="drup -d"
+
+      docker-compose::services() {
+        docker compose ls --format json | jq -er '.[].Name'
+      }
+
+      dlog() {
+        local ctrlc pid rc
+
+        trap '
+          ctrlc=1
+          if [ -n "$pid" ]
+          then
+            kill "$pid" 2>/dev/null
+          fi
+        ' INT
+
+        while :
+        do
+          docker compose logs -f "$@" &
+          pid=$!
+
+          wait "$pid"
+          rc=$?
+
+          if [ -n "$ctrlc" ]
+          then
+            break
+          fi
+
+          # If docker exited normally (no -f, or daemon restarted), retry after a short pause
+          sleep 1 || break
+        done
+
+        trap - INT
+      }
+      alias dlogs="dlog"
+
+      de() {
+        local svc
+
+        if [[ "$#" -eq 0 ]]
+        then
+          case "$PWD" in
+            /srv/*)
+              svc=$(basename "$PWD")
+              ;;
+            *)
+              echo_error "not in a /srv directory. Please provide target svc."
+              echo_info "Available services:"
+              docker-compose::services
+              return 1
+              ;;
+          esac
+        else
+          svc="$1"
+          shift
+        fi
+
+        local compose_file="/srv/''${svc}/docker-compose.yaml"
+
+        if [[ ! -r $compose_file ]]
+        then
+          echo_error "Invalid svc: $svc -> ''${fg_bold[red]}''${compose_file}''${reset_color} not found"
+          echo_info "Available services:"
+          docker-compose::services
+          return 1
+        fi
+
+        echo_info "Targeting compose service ''${fg_bold[yellow]}''${svc}''${reset_color}"
+        local cmd=("$@")
+
+        if [[ -n $cmd ]]
+        then
+          echo_info "Executing command \$ ''${fg_bold[magenta]}''${cmd}''${reset_color}"
+          docker compose -f "$compose_file" exec "$svc" "''${cmd[@]}"
+          return "$?"
+        fi
+
+        # no command provided: default to a shell
+        local shell rc res
+        for shell in bash ash sh
+        do
+          res=$(docker compose -f "$compose_file" exec "$svc" "$shell" -c exit 2>&1)
+          rc="$?"
+
+          case "$rc" in
+            0)
+              echo_info "Using shell ''${fg_bold[magenta]}''${shell}''${reset_color}"
+              docker compose -f "$compose_file" exec "$svc" "$shell"
+              return "$?"
+              ;;
+            125)
+              echo "$res" >&2
+              return "$rc"
+              ;;
+          esac
+        done
+
+        echo_error "Failed to find an available shell in $svc"
+        return 1
+      }
+
+      alias sc-cat="sudo systemctl cat"
+      alias sc-daemon-reload="sudo systemctl daemon-reload"
+      alias sc-disable-now="sudo systemctl disable --now"
+      alias sc-disable="sudo systemctl disable"
+      alias sc-enable-now="sudo systemctl enable --now"
+      alias sc-enable="sudo systemctl enable"
+      alias sc-restart="sudo systemctl restart"
+      alias sc-start="sudo systemctl start"
+      alias sc-status="sudo systemctl status"
+      alias sc-stop="sudo systemctl stop"
+
+      alias scu-cat="systemctl --user cat"
+      alias scu-daemon-reload="systemctl --user daemon-reload"
+      alias scu-disable-now="systemctl --user disable --now"
+      alias scu-disable="systemctl --user disable"
+      alias scu-enable-now="systemctl --user enable --now"
+      alias scu-enable="systemctl --user enable"
+      alias scu-start="systemctl --user start"
+      alias scu-status="systemctl --user status"
+      alias scu-stop="systemctl --user stop"
+
+      alias tmad="tmux -u new -A -D -s main"
+      alias tmam="tmux attach-session -t main"
+
+      alias grep="grep --color=auto"
+
+      alias cdnix="cd /etc/nixos"
+    '';
+
+  };
+}
---
+++ server/dotfiles/zsh.nix
@@ -0,0 +1,109 @@
+{ pkgs, ... }:
+let
+  # ... -> ../..
+  manydots-magic = pkgs.fetchFromGitHub {
+    owner = "knu";
+    repo = "zsh-manydots-magic";
+    rev = "main";
+    hash = "sha256-lv7e7+KBR/nxC43H0uvphLcI7fALPvxPSGEmBn0g8HQ=";
+  };
+in
+{
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+    vteIntegration = false; # see below for osc7
+    autosuggestions.enable = true;
+    syntaxHighlighting.enable = true;
+    shellInit = ''
+      export HOSTNAME="$HOST"
+    '';
+    interactiveShellInit = ''
+      # enable colors (exposes $fg etc)
+      autoload -U colors && colors
+
+      # cd to paths directly (no need to prepend with 'cd')
+      setopt autocd
+
+      # Enable command correction prompts
+      setopt correct
+
+      fpath=(${manydots-magic} $fpath)
+      autoload -Uz manydots-magic
+      manydots-magic
+
+      # OSC 7
+      function __osc7-pwd() {
+        emulate -L zsh # also sets localoptions for us
+        setopt extendedglob
+        local LC_ALL=C
+        # FIXME Below is supposed to leverage named dirs (ie: ~c for ~/.config) but it
+        # does not really work in wezterm (no path displayed and the hostname gets an
+        # extra '~' appended to it for some reason)
+        # local cwd="''${(z)$(print -P "%~")[1]}"
+        printf '\e]7;file://%s%s\e\' $HOST ''${PWD//(#m)([^@-Za-z&-;_~])/%''${(l:2::0:)$(([##16]#MATCH))}}
+      }
+
+      function __chpwd-osc7-pwd() {
+        (( ZSH_SUBSHELL )) || __osc7-pwd
+      }
+      __chpwd-osc7-pwd  # execute right away
+      # add-zsh-hook -Uz chpwd __chpwd-osc7-pwd
+      chpwd_functions+=(__chpwd-osc7-pwd)
+
+      # This little snippets set TERM to TERM_SSH_CLIENT which holds the ssh
+      # client's TERM value. It is sent by the ssh::fix-term zsh func.
+      case "$TERM_SSH_CLIENT" in
+        foot|*kitty*|wezterm)
+          export TERM=$TERM_SSH_CLIENT
+          ;;
+      esac
+
+      zmodload zsh/terminfo
+      terminfo_bind() {
+        local key="$1"
+        local func="$2"
+        if [[ -n "''${terminfo[$key]}" ]]
+        then
+          bindkey "''${terminfo[$key]}" "$func"
+          return 0
+        fi
+        return 1
+      }
+
+      # remove slash ("/") from what zsh considers a "word" character,
+      # so backward-kill-word will stop at each /
+      WORDCHARS=''${WORDCHARS//\/}
+
+      terminfo_bind kHOM5 beginning-of-line
+      terminfo_bind kend  end-of-line
+      terminfo_bind kLFT5 backward-word
+      terminfo_bind kRIT5 forward-word
+      terminfo_bind cub1  backward-kill-word
+
+      # We need to add some fallback bindings because some of the terminfos do
+      # not contain kLFT5, kRIT5 etc.
+      case "$TERM" in
+        screen*|*wezterm*)
+          bindkey '^[[1;5C' forward-word      # ctrl-right
+          bindkey '^[[1;5D' backward-word     # ctrl-left
+          bindkey '^[[3;5~' kill-word         # ctrl-delete
+        ;;
+      esac
+
+      # For Termux, since we are often ssh'ing through an intermediary host
+      bindkey "^[OH"    beginning-of-line # Home
+      bindkey "^[OF"    end-of-line       # End
+
+      # Undo/Redo
+      bindkey '^[z' undo  # alt-z
+      bindkey '^[y' redo  # alt-y
+
+      # ctrl-e to edit current line in editor
+      autoload -Uz edit-command-line
+      zle -N edit-command-line
+      bindkey '\C-e' edit-command-line
+      bindkey '^E' edit-command-line
+    '';
+  };
+}
