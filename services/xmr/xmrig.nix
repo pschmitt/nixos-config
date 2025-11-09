ffb71a6915a496f32490a8a86792ee0f37767411
Philipp Schmitt <philipp@schmitt.co>
2025-11-09 15:28:51 +0100

    lets accept dns again

--- services/xmr/xmrig.nix
+++ services/xmr/xmrig.nix
@@ -1,4 +1,39 @@
-{ config, lib, ... }:
+{ config, lib, ... }@args:
+let
+  useProxy = args.useProxy or false;
+  cpuUsage = args.cpuUsage or 25;
+
+  proxyPool = {
+    url = "xmrig-proxy.${config.networking.hostName}.nb.${config.custom.mainDomain}:8443";
+    user = config.networking.hostName;
+    pass = "\${XMRIG_PROXY_PASSWORD}";
+    keepalive = true;
+    tls = true;
+    nicehash = true;
+    "rig-id" = config.networking.hostName;
+  };
+
+  publicPools = [
+    {
+      url = "pool.hashvault.pro:443";
+      # NOTE yes, this looks weird, and yes this works.
+      user = "\${HASHVAULT_USER}";
+      pass = config.networking.hostName;
+      keepalive = true;
+      tls = true;
+    }
+    {
+      url = "xmrpool.eu:9999";
+      # NOTE yes, this looks weird, and yes this works.
+      user = "\${XMRPOOL_USER}";
+      pass = config.networking.hostName;
+      keepalive = true;
+      tls = true;
+    }
+  ];
+
+  pools = if useProxy then [ proxyPool ] else publicPools;
+in
 {
   sops.secrets."xmrig/env" = { };
   systemd.services.xmrig.serviceConfig = {
@@ -15,31 +50,14 @@
         # https://xmrig.com/docs/miner/config/cpu
         enabled = true;
         priority = 1;
-        # 25% of $nproc threads
-        max-threads-hint = lib.mkDefault 25;
+        # Configurable percentage of $nproc threads
+        max-threads-hint = lib.mkDefault cpuUsage;
       };
       # https://xmrig.com/docs/miner/hugepages
       randomx = {
         "1gb-pages" = true;
       };
-      pools = [
-        {
-          url = "pool.hashvault.pro:443";
-          # NOTE yes, this looks weird, and yes this works.
-          user = "\${HASHVAULT_USER}";
-          pass = config.networking.hostName;
-          keepalive = true;
-          tls = true;
-        }
-        {
-          url = "xmrpool.eu:9999";
-          # NOTE yes, this looks weird, and yes this works.
-          user = "\${XMRPOOL_USER}";
-          pass = config.networking.hostName;
-          keepalive = true;
-          tls = true;
-        }
-      ];
+      pools = pools;
     };
   };
 }
