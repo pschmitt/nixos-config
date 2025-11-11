36b3112eb14b2806f279b14b55f200abc27f1e99
Philipp Schmitt <philipp@schmitt.co>
2025-11-11 22:04:59 +0100

    Add gk4

---
+++ hosts/gk4/default.nix
@@ -0,0 +1,39 @@
+{ ... }:
+{
+  imports = [
+    ./hardware-configuration.nix
+
+    ../../common/global
+    ../../common/gui
+    ../../common/laptop
+    ../../common/restic
+    ../../common/snek
+    ../../common/sshfs
+    ../../common/work
+    ../../services/nfs/nfs-client-all.nix
+
+    ../../services/nixos-installer-boot-entry.nix
+  ];
+
+  custom.cattle = false;
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking = {
+    hostName = "gk4";
+    # wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+    # Disable the firewall altogether.
+    firewall = {
+      enable = false;
+      # allowedTCPPorts = [ ... ];
+      # allowedUDPPorts = [ ... ];
+    };
+  };
+
+  # boot.kernelPackages = lib.mkForce pkgs.linuxPackages_zen;
+
+  # services.fprintd.enable = true;
+}
