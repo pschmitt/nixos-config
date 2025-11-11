175863ea6af681f0cc03bcadc814f73c60a5540f
Philipp Schmitt <philipp@schmitt.co>
2025-11-11 22:04:59 +0100

    Add gk4

---
+++ hosts/gk4/README.md
@@ -0,0 +1,7 @@
+NOTE: for snapper you need to add a subvolume (/home/.snapshots)
+
+```shell
+sudo btrfs subvolume list /home/.snapshots
+```
+
+This should be something we can do with disko...
---
+++ hosts/gk4/disko-config.nix
@@ -0,0 +1,77 @@
+{
+  disko.devices.disk.main = {
+    type = "disk";
+    device = "/dev/disk/by-id/nvme-changeme";
+    content = {
+      type = "gpt";
+      partitions = {
+        mbr = {
+          label = "MBR"; # FIXME, this has no effect?
+          size = "1M";
+          type = "EF02"; # for grub MBR
+          priority = 1;
+        };
+        ESP = {
+          name = "ESP";
+          label = "EFI";
+          size = "10G";
+          type = "EF00";
+          content = {
+            type = "filesystem";
+            format = "vfat";
+            mountpoint = "/boot";
+            mountOptions = [ "defaults" ];
+            extraArgs = ["-n" "EFI"];
+          };
+        };
+        luks = {
+          size = "100%";
+          # label = "luks-root";
+          content = {
+            type = "luks";
+            name = "luks-root";
+            extraOpenArgs = [ "--allow-discards" ];
+            # if you want to use the key for interactive login be sure there is no trailing newline
+            # for example use `echo -n "password" > /tmp/secret.key`
+            # keyFile = "/tmp/secret.key"; # Interactive
+            # settings = {
+            #   keyFile = "/tmp/secret.key";
+            #   fallbackToPassword = true;
+            # };
+            # settings.keyFile = "/tmp/secret.key";
+            # additionalKeyFiles = ["/tmp/additionalSecret.key"];
+            content = {
+              type = "btrfs";
+              extraArgs = [ "-L" "luks-root" ];
+              subvolumes = {
+                "@root" = {
+                  mountpoint = "/";
+                  mountOptions = [
+                    "compress=zstd"
+                    "noatime"
+                  ];
+                };
+                "@home" = {
+                  mountpoint = "/home";
+                  mountOptions = [
+                    "compress=zstd"
+                    "noatime"
+                  ];
+                };
+                # FIXME Snapper
+                # "/home/.snapshots" = { };
+                "@nix" = {
+                  mountpoint = "/nix";
+                  mountOptions = [
+                    "compress=zstd"
+                    "noatime"
+                  ];
+                };
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}
