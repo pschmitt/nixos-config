80bd0de91b6fba37da1e52e73dd966376af471f4
Philipp Schmitt <philipp@schmitt.co>
2025-11-13 23:32:20 +0100

    Add hyprgrass config

--- home-manager/hyprland.nix
+++ home-manager/hyprland.nix
@@ -1,7 +1,4 @@
-{
-  config,
-  ...
-}:
+{ ... }:
 {
   imports = [
     ./hyprdynamicmonitor.nix
@@ -15,6 +12,23 @@
       #
       # Include the HyprDynamicMonitors output so Hyprland uses the generated layout.
       source = $config_dir/91-hdm-monitors.conf
+    '';
+  };
+
+  xdg.configFile."hypr/config.d/plugins/hyprgrass.conf" = {
+    text = ''
+      # Hyprgrass touch gesture tuning
+      plugin {
+        touch_gestures {
+          sensitivity = 4.0
+          workspace_swipe_fingers = 3
+          workspace_swipe_edge = d
+          long_press_delay = 400
+          resize_on_border_long_press = true
+          edge_margin = 10
+          emulate_touchpad_swipe = true
+        }
+      }
     '';
   };
 }
