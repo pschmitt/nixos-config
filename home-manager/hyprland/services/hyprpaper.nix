740af83895221904de1c55a7dce82ec93f968319
Philipp Schmitt <philipp@schmitt.co>
2025-11-14 13:10:13 +0100

    massive re-org

---
+++ home-manager/hyprland/services/hyprpaper.nix
@@ -0,0 +1,13 @@
+{ config, ... }:
+let
+  wallpaperPath = "${config.home.homeDirectory}/Pictures/Wallpapers/chill.png";
+in
+{
+  services.hyprpaper = {
+    enable = true;
+    settings = {
+      preload = [ wallpaperPath ];
+      wallpaper = [ ",${wallpaperPath}" ];
+    };
+  };
+}
