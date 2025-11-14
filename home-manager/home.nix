f1153ecd6a0f9c2308b68cbeb8927f5e5627a08c
Philipp Schmitt <philipp@schmitt.co>
2025-11-14 10:48:01 +0100

    Ensure ssh keys are owned by user

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -6,6 +6,26 @@
   pkgs,
   ...
 }:
+let
+  # DIRTYFIX for ssh keys transferred via nixos-anywhere's --extra-files option
+  # All files are then owned by root, which makes sops-nix unhappy
+  fixSshOwnership = pkgs.writeShellScript "hm-fix-ssh-ownership" ''
+    SUDO_BIN="/run/wrappers/bin/sudo"
+    DIR='${osConfig.custom.homeDirectory}/.ssh'
+
+    if [[ ! -d "$DIR" ]]
+    then
+      exit 0
+    fi
+
+    echo "Ensuring ownership on $DIR"
+    if ! "$SUDO_BIN" -n chown -R "${osConfig.custom.username}:${osConfig.custom.username}" "$DIR"
+    then
+      echo "Could not change ownership on $DIR (needs passwordless sudo)" >&2
+      exit 1
+    fi
+  '';
+in
 {
   imports = lib.concatLists [
     [
@@ -32,84 +52,89 @@
       ./zsh.nix
       # ./zellij.nix
     ]
-    (lib.optional (osConfig.hardware.bluetooth.enable) ./bluetooth.nix)
-    (lib.optional (osConfig.services.xserver.enable) ./gui)
+    (lib.optional osConfig.hardware.bluetooth.enable ./bluetooth.nix)
+    (lib.optional osConfig.services.xserver.enable ./gui)
   ];
 
-  # The home.stateVersion option does not have a default and must be set
-  home.stateVersion = osConfig.system.stateVersion;
-
   sops = {
-    defaultSopsFile = osConfig.sops.defaultSopsFile;
-    age.sshKeyPaths = [ "/home/pschmitt/.ssh/id_ed25519" ];
+    inherit (osConfig.sops) defaultSopsFile;
+    age.sshKeyPaths = [ "${osConfig.custom.homeDirectory}/.ssh/id_ed25519" ];
     age.generateKey = false;
   };
+
+  systemd.user.services.sops-nix.Service.ExecStartPre =
+    lib.mkBefore [ "${fixSshOwnership}" ];
 
   programs.home-manager.enable = true;
 
   programs.nix-index-database.comma.enable = true;
 
-  home.sessionPath = [
-    "${config.home.homeDirectory}/.local/bin"
-    "${config.home.homeDirectory}/.local/share/polaris/bin"
-    "${config.home.homeDirectory}/bin"
-  ];
-
   systemd.user.startServices = "sd-switch";
 
-  home.packages = with pkgs; [
-    home-manager
-    # nwg-displays
-    # thunderbird
+  home = {
+    # The home.stateVersion option does not have a default and must be set
+    inherit (osConfig.system) stateVersion;
 
-    # cli
-    age
-    atuin
-    bat
-    direnv
-    emoji-fzf
-    eza
-    fd
-    fzf
-    gyb
-    jc
-    jsonrepair
-    # rich-cli fails to build as of 2024-05-10 (rich-rst's test fail)
-    # rich-cli
-    sops
-    ssh-to-age
-    yadm
+    sessionPath = [
+      "${config.home.homeDirectory}/.local/bin"
+      "${config.home.homeDirectory}/.local/share/polaris/bin"
+      "${config.home.homeDirectory}/bin"
+    ];
 
-    # gui
-    gimp
-    nextcloud-client
+    packages = with pkgs; [
+      home-manager
+      # nwg-displays
+      # thunderbird
 
-    # chat
-    element-desktop
-    signal-desktop
+      # cli
+      age
+      atuin
+      bat
+      direnv
+      emoji-fzf
+      eza
+      fd
+      fzf
+      gyb
+      jc
+      jsonrepair
+      # rich-cli fails to build as of 2024-05-10 (rich-rst's test fail)
+      # rich-cli
+      sops
+      ssh-to-age
+      yadm
 
-    # iot
-    mosquitto
-    net-snmp
+      # gui
+      gimp
+      nextcloud-client
 
-    # virtualization
-    distrobox
-    # quickemu # fails to build as of 28.12.2024
-    # quickgui # fails to build as of 09.09.2024
+      # chat
+      element-desktop
+      signal-desktop
 
-  ];
+      # iot
+      mosquitto
+      net-snmp
 
-  # AccountService profile picture
-  home.file = {
-    ".face" = {
-      enable = true;
-      source = builtins.fetchurl {
-        # NOTE setting the extension to .png is required for hyprlock to detect
-        # the filetype correctly
-        # https://github.com/hyprwm/hyprlock/issues/317
-        name = "face.png";
-        url = "https://www.gravatar.com/avatar/8635e7a28259cb6da1c6a3c96c75b425.png?size=96";
-        sha256 = "1kg0x188q1g2mph13cs3sm4ybj3wsliq2yjz5qcw4qs8ka77l78p";
+      # virtualization
+      distrobox
+      # quickemu # fails to build as of 28.12.2024
+      # quickgui # fails to build as of 09.09.2024
+
+    ];
+
+    # AccountService profile picture
+    file = {
+      ".face" = {
+        enable = true;
+        source = builtins.fetchurl {
+          # NOTE setting the extension to .png is required for hyprlock to detect
+          # the filetype correctly
+          # https://github.com/hyprwm/hyprlock/issues/317
+          name = "face.png";
+          url = "https://www.gravatar.com/avatar/8635e7a28259cb6da1c6a3c96c75b425.png?size=96";
+          sha256 = "1kg0x188q1g2mph13cs3sm4ybj3wsliq2yjz5qcw4qs8ka77l78p";
+        };
       };
     };
   };
