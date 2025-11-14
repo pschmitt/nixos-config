f9e7385ca05a0f5b769a6a314df4bd76dea437d0
Philipp Schmitt <philipp@schmitt.co>
2025-11-14 21:00:22 +0100

    add gesture to toggle soft keyboard

--- home-manager/hyprland/plugins/hyprgrass.nix
+++ home-manager/hyprland/plugins/hyprgrass.nix
@@ -6,13 +6,20 @@
     ];
 
   wayland.windowManager.hyprland.settings.plugin.touch_gestures =
-    lib.mkDefault {
-      sensitivity = 4.0;
-      workspace_swipe_fingers = 3;
-      workspace_swipe_edge = "d";
-      long_press_delay = 400;
-      resize_on_border_long_press = true;
-      edge_margin = 10;
-      emulate_touchpad_swipe = true;
-    };
+    lib.mkMerge [
+      (lib.mkDefault {
+        sensitivity = 4.0;
+        workspace_swipe_fingers = 3;
+        workspace_swipe_edge = "d";
+        long_press_delay = 400;
+        resize_on_border_long_press = true;
+        edge_margin = 10;
+        emulate_touchpad_swipe = true;
+      })
+      {
+        hyprgrass-bind = [
+          ", edge:d:u, exec, $bin_dir/toggle-wvkbd.sh"
+        ];
+      }
+    ];
 }
