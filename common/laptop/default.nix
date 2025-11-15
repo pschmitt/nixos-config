e6bfdad0fda5dbfd6d63b7c95469867cfc7166fb
Philipp Schmitt <philipp@schmitt.co>
2025-11-15 09:09:57 +0100

    Let's give ppd a try

--- common/laptop/default.nix
+++ common/laptop/default.nix
@@ -11,5 +11,12 @@
     ./wireshark.nix
   ];
 
+  # power-profiles-daemon conflicts with tlp
+  # https://linrunner.de/tlp/faq/ppd.html
+  services = {
+    power-profiles-daemon.enable = lib.mkForce true;
+    tlp.enable = lib.mkForce false;
+  };
+
   services.logind.settings.Login.HandleLidSwitchExternalPower = lib.mkDefault "suspend";
 }
