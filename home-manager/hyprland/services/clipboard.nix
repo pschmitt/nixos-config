39d4a4baca31100f0a47df75d648a3ab62e2814f
Philipp Schmitt <philipp@schmitt.co>
2025-11-16 17:54:32 +0100

    Use hm modules for clipboard services

--- home-manager/hyprland/services/clipboard.nix
+++ home-manager/hyprland/services/clipboard.nix
@@ -1,52 +1,17 @@
-{ lib, pkgs, ... }:
+_:
 let
-  inherit (lib) escapeShellArgs getExe;
-  wlClipPersist = getExe pkgs.wl-clip-persist;
-  wlPaste = lib.getExe' pkgs.wl-clipboard "wl-paste";
-  cliphist = lib.getExe' pkgs.cliphist "cliphist";
-
-  mkClipboardService =
-    description: execArgs:
-    {
-      Unit = {
-        Description = description;
-        PartOf = [ "graphical-session.target" ];
-        After = [ "graphical-session.target" ];
-      };
-      Install = {
-        WantedBy = [ "graphical-session.target" ];
-      };
-      Service = {
-        ExecStart = escapeShellArgs execArgs;
-        Restart = "on-failure";
-        RestartSec = 2;
-      };
-    };
+  target = "graphical-session.target";
 in
 {
-  systemd.user.services = {
-    clipboard-persist = mkClipboardService "Persist clipboard contents" [
-      wlClipPersist
-      "--clipboard"
-      "both"
-    ];
+  services.cliphist = {
+    enable = true;
+    allowImages = true;
+    systemdTargets = [ target ];
+  };
 
-    clipboard-cliphist-text = mkClipboardService "Record text selections into cliphist" [
-      wlPaste
-      "--type"
-      "text"
-      "--watch"
-      cliphist
-      "store"
-    ];
-
-    clipboard-cliphist-image = mkClipboardService "Record image clipboard into cliphist" [
-      wlPaste
-      "--type"
-      "image"
-      "--watch"
-      cliphist
-      "store"
-    ];
+  services.wl-clip-persist = {
+    enable = true;
+    clipboardType = "both";
+    systemdTargets = [ target ];
   };
 }
