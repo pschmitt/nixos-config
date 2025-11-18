59403432259bd9f78acd5401b907d95175b1a2a0
Philipp Schmitt <philipp@schmitt.co>
2025-11-18 11:15:14 +0100

    Backup blobs

--- hosts/rofl-10/restic.nix
+++ hosts/rofl-10/restic.nix
@@ -1,6 +1,15 @@
 {
   services.restic.backups.main = {
-    paths = [ "/mnt/data/srv" ];
-    exclude = [ "/mnt/data/srv/monerod/data" ];
+    paths = [
+      "/mnt/data/srv"
+      "/mnt/data/blobs"
+    ];
+    exclude = [
+      "/mnt/data/srv/monerod/data"
+
+      # ignore nix isos/sd-card images
+      "/mnt/data/blobs/img"
+      "/mnt/data/blobs/iso"
+    ];
   };
 }
