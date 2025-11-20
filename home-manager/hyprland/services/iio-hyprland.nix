5d7e2dfb46491f4737db9ea2ad62f3324d6ad633
Philipp Schmitt <philipp@schmitt.co>
2025-11-20 20:37:46 +0100

    Merge iio-hyprland config files

--- home-manager/hyprland/services/iio-hyprland.nix
+++ home-manager/hyprland/services/iio-hyprland.nix
@@ -1,16 +1,11 @@
 {
   lib,
-  osConfig ? null,
+  osConfig,
   pkgs,
   ...
 }:
-let
-  inherit (osConfig.networking) hostName;
-  isGk4 = hostName == "gk4";
-  iioHyprlandBin = lib.getExe pkgs.iio-hyprland;
-in
 {
-  systemd.user.services."iio-hyprland" = lib.mkIf isGk4 {
+  systemd.user.services."iio-hyprland" = lib.mkIf osConfig.hardware.sensor.iio.enable {
     Unit = {
       Description = "Automatic display rotation via iio-hyprland";
       PartOf = [ "graphical-session.target" ];
@@ -22,7 +17,8 @@ in
     };
 
     Service = {
-      ExecStart = "${iioHyprlandBin} --transform 3,0,1,2";
+      # NOTE These --transform values are only relevant for the GPD Pocket 4
+      ExecStart = "${lib.getExe pkgs.iio-hyprland} --transform 3,0,1,2";
       Restart = "on-failure";
       RestartSec = 5;
     };
