45fd45dcdeccf14500e23773b03aa7f69d44e207
Philipp Schmitt <philipp@schmitt.co>
2025-11-21 11:15:07 +0100

    Move the gpdpocket4 keyboard/touchpad config out of the gk4 specific config

--- home-manager/hyprland/conf/host-specific/gk4.nix
+++ home-manager/hyprland/conf/host-specific/gk4.nix
@@ -1,21 +1,8 @@
+{ lib, ... }:
 {
-  wayland.windowManager.hyprland.settings = {
-    device = [
-      {
-        name = "hailuck-co.-ltd-usb-keyboard";
-        kb_layout = "gpdpocket4,us,de";
-      }
-      {
-        # Enable inverted scrolling for the built-in touchpad.
-        name = "hailuck-co.-ltd-usb-keyboard-mouse";
-        natural_scroll = true;
-      }
-    ];
-
-    input.touchdevice = {
-      enabled = true;
-      output = "eDP-1";
-      transform = 3;
-    };
+  wayland.windowManager.hyprland.settings.input.touchdevice = lib.mkMerge {
+    enabled = true;
+    output = "eDP-1";
+    transform = 3;
   };
 }
