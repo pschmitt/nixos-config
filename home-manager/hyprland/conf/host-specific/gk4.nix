01baa77ddcd7afe9fe527acb707082762928e5ce
Philipp Schmitt <philipp@schmitt.co>
2025-11-21 23:19:09 +0100

    Add F1-typo keybind for the scratchpad term

--- home-manager/hyprland/conf/host-specific/gk4.nix
+++ home-manager/hyprland/conf/host-specific/gk4.nix
@@ -1,7 +1,19 @@
+{ lib, ... }:
 {
-  wayland.windowManager.hyprland.settings.input.touchdevice = {
-    enabled = true;
-    output = "eDP-1";
-    transform = 3;
-  };
+  wayland.windowManager.hyprland.settings = lib.mkMerge [
+    {
+      bind = [
+        # fake f1 -> scratchpad terminal
+        # NOTE We might want to consider to set:
+        # resolve_binds_by_sym = 1
+        # -> would make this keybind less keymap-dependent.
+        "CTRL, dead_circumflex, exec, $bin_dir/scratchpad.sh term"
+      ];
+      input.touchdevice = {
+        enabled = true;
+        output = "eDP-1";
+        transform = 3;
+      };
+    }
+  ];
 }
