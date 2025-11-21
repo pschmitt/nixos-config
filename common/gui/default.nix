1d5b7410e5a0f97885c7023b2d19d396d9fcdf20
Philipp Schmitt <philipp@schmitt.co>
2025-11-21 12:59:40 +0100

    re-org

--- common/gui/default.nix
+++ common/gui/default.nix
@@ -1,10 +1,4 @@
 {
-  config,
-  inputs,
-  pkgs,
-  ...
-}:
-{
   imports = [
     ./apps.nix
     ./audio.nix
@@ -15,11 +9,16 @@
     ./gpu.nix
     ./go-hass-agent.nix
     ./hacompanion.nix
+    ./flatpak.nix
+    ./keyboard.nix
     ./libvirt.nix
     ./logitech-mouse.nix
     ./printer.nix
+    ./services.nix
     ./snapper.nix
+    ./touchpad.nix
     ./theme.nix
+    ./v4l2loopback.nix
 
     # Display Manager
     ./gdm.nix
@@ -30,140 +29,8 @@
     ./hyprland.nix
     ./niri.nix
     ./sway.nix
-
-    inputs.flatpaks.nixosModule
   ];
 
-  boot = {
-    extraModulePackages = [ config.boot.kernelPackages.v4l2loopback ];
-    kernelModules = [ "v4l2loopback" ];
-    extraModprobeConfig = ''
-      # exclusive_caps: Skype, Zoom, Teams etc. will only show device when actually streaming
-      # card_label: Name of virtual camera, how it'll show up in Skype, Zoom, Teams
-      # https://github.com/umlaeute/v4l2loopback
-      options v4l2loopback video_nr=10 exclusive_caps=1 card_label="OBS Virtual Camera"
-    '';
-  };
-
-  # Enable flatpak
-  xdg.portal.enable = true; # required for flatpak
-  xdg.portal.xdgOpenUsePortal = true; # fix xdg-open
-  services.flatpak = {
-    enable = true;
-    remotes = {
-      "flathub" = "https://dl.flathub.org/repo/flathub.flatpakrepo";
-      "flathub-beta" = "https://dl.flathub.org/beta-repo/flathub-beta.flatpakrepo";
-    };
-    packages = [ ];
-  };
-
-  services.xserver =
-    let
-      customKeymaps = pkgs.custom-keymaps;
-      symbolsFile = name: "${customKeymaps}/share/X11/xkb/symbols/${name}";
-    in
-    {
-      # Enable the X11 windowing system.
-      enable = true;
-      xkb = {
-        layout = "de";
-        variant = "";
-        extraLayouts = {
-          hhkb-de = {
-            description = "Custom HHKB DE layout by pschmitt";
-            languages = [ "deu" ];
-            symbolsFile = symbolsFile "hhkb-de";
-          };
-          gpdpocket4-us = {
-            description = "GPD Pocket 4 layout with swapped Y/Z and AltGr tweaks";
-            languages = [
-              "eng"
-              "deu"
-            ];
-            symbolsFile = symbolsFile "gpdpocket4-us";
-          };
-          gpdpocket4-de = {
-            description = "GPD Pocket 4 custom DE Layout";
-            languages = [ "deu" ];
-            symbolsFile = symbolsFile "gpdpocket4-de";
-          };
-        };
-      };
-    };
-
-  hardware.uinput.enable = true;
-  # Enable touchpad support (enabled by default in most desktopManager).
-  services.libinput.enable = true;
-
-  services = {
-    avahi = {
-      enable = true;
-      nssmdns4 = true;
-    };
-
-    dbus = {
-      enable = true;
-      implementation = "broker";
-    };
-
-    # enable sushi and rygel
-    gnome = {
-      gnome-online-accounts.enable = true;
-      sushi.enable = true;
-      rygel.enable = true;
-    };
-
-    gvfs.enable = true;
-    seatd.enable = true;
-    tumbler.enable = true;
-  };
-
-  # Enable lingering
-  systemd.tmpfiles.rules = [ "f /var/lib/systemd/linger/${config.custom.username}" ];
-  users.users."${config.custom.username}" = {
-    linger = true;
-
-    extraGroups = [
-      "input" # do we need this?
-      "uinput" # for dotool
-      "video" # do we need this?
-    ];
-  };
-
-  programs.adb.enable = true;
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  environment.systemPackages = with pkgs; [
-    gobject-introspection
-    gtk3 # gtk-update-icon-cache
-    iw
-    libinput # libinput debug-events
-    libsecret # secret-tool
-    pinentry-curses
-    pinentry-gnome3
-    tesseract
-
-    # media
-    deckmaster
-    imagemagick
-    yt-dlp
-
-    # apps
-    gparted
-    mullvad-vpn
-    usbimager # etcher alternative
-    virt-manager
-
-    # qrcode create/read
-    qrencode
-    zbar # provides zabarimg, for reading qr codes
-
-    # cli image viewers
-    chafa
-    # termimage
-  ];
 }
 
 # vim: set ft=nix et ts=2 sw=2 :
