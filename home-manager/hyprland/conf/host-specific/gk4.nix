d41fe01a15d2076cb987fc66da69f66ee1315e7a
Philipp Schmitt <philipp@schmitt.co>
2025-11-21 23:22:09 +0100

    lets add ctrl+esc as well

--- home-manager/hyprland/conf/host-specific/gk4.nix
+++ home-manager/hyprland/conf/host-specific/gk4.nix
@@ -7,7 +7,9 @@
         # NOTE We might want to consider to set:
         # resolve_binds_by_sym = 1
         # -> would make this keybind less keymap-dependent.
+        # We'd then map on "grave" instead of dead_circumflex.
         "CTRL, dead_circumflex, exec, $bin_dir/scratchpad.sh term"
+        "CTRL, escape, exec, $bin_dir/scratchpad.sh term"
       ];
       input.touchdevice = {
         enabled = true;
