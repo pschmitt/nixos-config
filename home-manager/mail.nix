40bdde059d18db779415b2de6a032754a59d635f
Philipp Schmitt <philipp@schmitt.co>
2025-11-21 09:43:28 +0100

    fmt

--- home-manager/mail.nix
+++ home-manager/mail.nix
@@ -1,19 +1,18 @@
 { inputs, pkgs, ... }:
 {
-  home = {
-    packages = with pkgs; [
-      inputs.myl.packages.${stdenv.hostPlatform.system}.myl
-      inputs.myl-discovery.packages.${stdenv.hostPlatform.system}.myl-discovery
-      inputs.sendmyl.packages.${stdenv.hostPlatform.system}.sendmyl
+  home.packages = with pkgs; [
+    inputs.myl.packages.${stdenv.hostPlatform.system}.myl
+    inputs.myl-discovery.packages.${stdenv.hostPlatform.system}.myl-discovery
+    inputs.sendmyl.packages.${stdenv.hostPlatform.system}.sendmyl
 
-      aerc
-      gmailctl
-      neomutt
-    ];
+    aerc
+    gmailctl
+    neomutt
+  ];
 
-    file.".config/neomutt/nix".source = "${pkgs.neomutt}/share/neomutt";
+  xdg = {
+    configFile."neomutt/nix".source = "${pkgs.neomutt}/share/neomutt";
+    # Create neomutt cache dir
+    cacheFile."neomutt/bodies/.keep".text = "";
   };
-
-  # Create cache dir
-  xdg.cacheFile."neomutt/bodies/.keep".text = "";
 }
