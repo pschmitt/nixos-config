d12ddbd4cc9c01ceea45afb501aefd25f110f201
Philipp Schmitt <philipp@schmitt.co>
2025-11-22 17:15:54 +0100

    Leverage serverAliases

--- services/immich.nix
+++ services/immich.nix
@@ -5,153 +5,145 @@
   ...
 }:
 let
-  domains = [ config.custom.mainDomain ];
-  subdomains = [
-    "img"
-    "immich"
+  hostnames = [
+    "img.${config.custom.mainDomain}"
+    "img.${config.networking.hostName}.${config.custom.mainDomain}"
+    "immich.${config.custom.mainDomain}"
+    "immich.${config.networking.hostName}.${config.custom.mainDomain}"
   ];
-  hostnames = lib.concatMap (
-    domain:
-    lib.concatMap (subdomain: [
-      "${subdomain}.${domain}"
-      "${subdomain}.${config.networking.hostName}.${domain}"
-    ]) subdomains
-  ) domains;
+  primaryHost = builtins.head hostnames;
+  serverAliases = lib.remove primaryHost hostnames;
 
   # immich-face-to-album
-  names = [
+  persons = [
     "anika"
     "maya"
   ];
-
-  # Function to generate configurations per name
-  generateConfigs = name: {
-    sopsSecrets = {
-      "immich/immich-face-to-album/faces/${name}" = {
-        sopsFile = config.custom.sopsFile;
-      };
-      "immich/immich-face-to-album/albums/${name}" = {
-        sopsFile = config.custom.sopsFile;
-      };
-    };
-
-    sopsTemplates = {
-      "immich-face-to-album-${name}" = {
-        content = ''
-          API_KEY="${config.sops.placeholder."immich/immich-face-to-album/apiKey"}"
-          FACE="${config.sops.placeholder."immich/immich-face-to-album/faces/${name}"}"
-          ALBUM="${config.sops.placeholder."immich/immich-face-to-album/albums/${name}"}"
-        '';
-        owner = config.services.immich.user;
-      };
-    };
-
-    systemdServices = {
-      "immich-face-to-album-${name}" = {
-        description = "Run immich-face-to-album for ${lib.toUpper name}";
-        after = [ "immich-server.service" ];
-        requires = [ "immich-server.service" ];
-        serviceConfig = {
-          EnvironmentFile = config.sops.templates."immich-face-to-album-${name}".path;
-          ExecStart = ''
-            ${pkgs.immich-face-to-album}/bin/immich-face-to-album \
-            --server http://${config.services.immich.host}:${toString config.services.immich.port} \
-            --key "$API_KEY" \
-            --face "$FACE" \
-            --album "$ALBUM"
-          '';
-          User = config.services.immich.user;
-          Type = "oneshot";
-        };
-      };
-    };
-
-    systemdTimers = {
-      "immich-face-to-album-${name}" = {
-        description = "Run immich-face-to-album regularly for ${lib.toUpper name}";
-        wantedBy = [ "timers.target" ];
-        timerConfig = {
-          OnCalendar = "hourly";
-          Persistent = true;
-        };
-      };
-    };
-  };
-
-  # Generate configurations for all names
-  perNameConfigs = map generateConfigs names;
-
-  # Combine configurations
-  combinedSopsSecrets = lib.foldl' (acc: cfg: acc // cfg.sopsSecrets) {
-    "immich/immich-face-to-album/apiKey" = {
-      sopsFile = config.custom.sopsFile;
-    };
-  } perNameConfigs;
-
-  combinedSopsTemplates = lib.foldl' (acc: cfg: acc // cfg.sopsTemplates) { } perNameConfigs;
-
-  combinedSystemdServices = lib.foldl' (acc: cfg: acc // cfg.systemdServices) { } perNameConfigs;
-
-  combinedSystemdTimers = lib.foldl' (acc: cfg: acc // cfg.systemdTimers) { } perNameConfigs;
-in
-{
-  services.immich = {
-    enable = true;
-    # immich fails to build on unstable as of 2024-12-29
-    # Fix:
-    # https://github.com/NixOS/nixpkgs/pull/369042
-    # https://nixpkgs-tracker.ocfox.me/?pr=369042
-    package = pkgs.master.immich;
-    # NOTE listening on "localhost" leads to immich only listening on IPv6
-    host = "127.0.0.1";
-    port = 2283;
-    mediaLocation = "/mnt/data/srv/immich/media";
-  };
-
-  services.nginx =
+  faceToAlbum =
     let
-      virtualHosts = builtins.listToAttrs (
-        map (hostname: {
-          name = hostname;
-          value = {
-            enableACME = true;
-            # FIXME https://github.com/NixOS/nixpkgs/issues/210807
-            acmeRoot = null;
-            forceSSL = true;
-            locations."/" = {
-              proxyPass = "http://${config.services.immich.host}:${toString config.services.immich.port}";
-              recommendedProxySettings = true;
-              proxyWebsockets = true;
-              # Allow uploading large files
-              # https://immich.app/docs/FAQ/#why-are-only-photos-and-not-videos-being-uploaded-to-immich
-              extraConfig = ''
-                client_max_body_size 50000M;
-              '';
+      baseSecretPath = "immich/immich-face-to-album";
+    in
+    lib.foldl'
+      (
+        acc: name:
+        let
+          templateName = "immich-face-to-album-${name}";
+        in
+        {
+          sopsSecrets = acc.sopsSecrets // {
+            "${baseSecretPath}/faces/${name}" = {
+              inherit (config.custom) sopsFile;
+            };
+            "${baseSecretPath}/albums/${name}" = {
+              inherit (config.custom) sopsFile;
             };
           };
-        }) hostnames
-      );
-    in
-    {
-      virtualHosts = virtualHosts;
+
+          sopsTemplates = acc.sopsTemplates // {
+            "${templateName}" = {
+              content = ''
+                API_KEY="${config.sops.placeholder."${baseSecretPath}/apiKey"}"
+                FACE="${config.sops.placeholder."${baseSecretPath}/faces/${name}"}"
+                ALBUM="${config.sops.placeholder."${baseSecretPath}/albums/${name}"}"
+              '';
+              owner = config.services.immich.user;
+            };
+          };
+
+          systemdServices = acc.systemdServices // {
+            "${templateName}" = {
+              description = "Run immich-face-to-album for ${lib.toUpper name}";
+              after = [ "immich-server.service" ];
+              requires = [ "immich-server.service" ];
+              serviceConfig = {
+                EnvironmentFile = config.sops.templates."${templateName}".path;
+                ExecStart = ''
+                  ${pkgs.immich-face-to-album}/bin/immich-face-to-album \
+                  --server http://${config.services.immich.host}:${toString config.services.immich.port} \
+                  --key "$API_KEY" \
+                  --face "$FACE" \
+                  --album "$ALBUM"
+                '';
+                User = config.services.immich.user;
+                Type = "oneshot";
+              };
+            };
+          };
+
+          systemdTimers = acc.systemdTimers // {
+            "${templateName}" = {
+              description = "Run immich-face-to-album regularly for ${lib.toUpper name}";
+              wantedBy = [ "timers.target" ];
+              timerConfig = {
+                OnCalendar = "hourly";
+                Persistent = true;
+              };
+            };
+          };
+        }
+      )
+      {
+        sopsSecrets = {
+          "${baseSecretPath}/apiKey" = {
+            inherit (config.custom) sopsFile;
+          };
+        };
+        sopsTemplates = { };
+        systemdServices = { };
+        systemdTimers = { };
+      }
+      persons;
+in
+{
+  services = {
+    immich = {
+      enable = true;
+      # immich fails to build on unstable as of 2024-12-29
+      # Fix:
+      # https://github.com/NixOS/nixpkgs/pull/369042
+      # https://nixpkgs-tracker.ocfox.me/?pr=369042
+      package = pkgs.master.immich;
+      # NOTE listening on "localhost" leads to immich only listening on IPv6
+      host = "127.0.0.1";
+      port = 2283;
+      mediaLocation = "/mnt/data/srv/immich/media";
     };
 
-  # immich-face-to-album
-  sops.secrets = combinedSopsSecrets;
-  sops.templates = combinedSopsTemplates;
-  systemd.services = combinedSystemdServices;
-  systemd.timers = combinedSystemdTimers;
+    nginx.virtualHosts."${primaryHost}" = {
+      inherit serverAliases;
+      enableACME = true;
+      # FIXME https://github.com/NixOS/nixpkgs/issues/210807
+      acmeRoot = null;
+      forceSSL = true;
+      locations."/" = {
+        proxyPass = "http://${config.services.immich.host}:${toString config.services.immich.port}";
+        recommendedProxySettings = true;
+        proxyWebsockets = true;
+        # Allow uploading large files
+        # https://immich.app/docs/FAQ/#why-are-only-photos-and-not-videos-being-uploaded-to-immich
+        extraConfig = ''
+          client_max_body_size 50000M;
+        '';
+      };
+    };
 
-  services.monit.config = lib.mkAfter ''
-    check host "immich" with address "img.${config.custom.mainDomain}"
-      group services
-      restart program = "${pkgs.systemd}/bin/systemctl restart immich-server"
-      if failed
-        port 443
-        protocol https
-        with timeout 15 seconds
-        and certificate valid for 5 days
-      then restart
-      if 5 restarts within 10 cycles then alert
-  '';
+    monit.config = lib.mkAfter ''
+      check host "immich" with address "${primaryHost}"
+        group services
+        restart program = "${pkgs.systemd}/bin/systemctl restart immich-server"
+        if failed
+          port 443
+          protocol https
+          with timeout 15 seconds
+          and certificate valid for 5 days
+        then restart
+        if 5 restarts within 10 cycles then alert
+    '';
+  };
+
+  # immich-face-to-album
+  sops.secrets = faceToAlbum.sopsSecrets;
+  sops.templates = faceToAlbum.sopsTemplates;
+  systemd.services = faceToAlbum.systemdServices;
+  systemd.timers = faceToAlbum.systemdTimers;
 }
--- services/paperless-ngx.nix
+++ services/paperless-ngx.nix
@@ -9,11 +9,13 @@ let
     "paperless.${config.custom.mainDomain}"
     "paperless.${config.networking.hostName}.${config.custom.mainDomain}"
   ];
+  primaryHost = builtins.head hostnames;
+  serverAliases = lib.remove primaryHost hostnames;
   hostnamesWithSchema = map (host: "https://${host}") hostnames;
 in
 {
   sops.secrets."paperless-ngx/adminPassword" = {
-    sopsFile = config.custom.sopsFile;
+    inherit (config.custom) sopsFile;
   };
 
   # FIXME THIS LOCKS THE FUCKING ROOT USER WHEN APPLIED
@@ -28,22 +30,40 @@ in
   #   ];
   # };
 
-  systemd.services.lsyncd = {
-    description = "Lsyncd - Live Sync Daemon";
-    script = ''
-      ${pkgs.lsyncd}/bin/lsyncd -nodaemon /etc/lsyncd/lsyncd.conf.lua
-    '';
-    serviceConfig = {
-      Type = "oneshot";
+  systemd = {
+    services.lsyncd = {
+      description = "Lsyncd - Live Sync Daemon";
+      script = ''
+        ${pkgs.lsyncd}/bin/lsyncd -nodaemon /etc/lsyncd/lsyncd.conf.lua
+      '';
+      serviceConfig = {
+        Type = "oneshot";
+      };
     };
-  };
 
-  systemd.timers.lsyncd = {
-    wantedBy = [ "timers.target" ];
-    timerConfig = {
-      OnCalendar = "hourly";
-      Persistent = true;
+    timers.lsyncd = {
+      wantedBy = [ "timers.target" ];
+      timerConfig = {
+        OnCalendar = "hourly";
+        Persistent = true;
+      };
     };
+
+    tmpfiles.settings."10-paperless" =
+      let
+        inherit (config.services.paperless) user;
+
+        defaultRule = {
+          inherit user;
+          group = user;
+          mode = "755"; # enforce mode for all dirs
+        };
+      in
+      {
+        "${config.services.paperless.dataDir}".d = defaultRule;
+        "${config.services.paperless.mediaDir}".d = defaultRule;
+        "${config.services.paperless.consumptionDir}".d = defaultRule;
+      };
   };
 
   environment.etc."lsyncd/lsyncd.conf.lua".text = ''
@@ -62,92 +82,71 @@ in
     }
   '';
 
-  services.paperless = {
-    enable = true;
-    # package = pkgs.master.paperless-ngx;
+  services = {
+    paperless = {
+      enable = true;
+      # package = pkgs.master.paperless-ngx;
 
-    domain = builtins.elemAt hostnames 0;
-    address = "127.0.0.1";
-    port = 28981;
+      domain = primaryHost;
+      address = "127.0.0.1";
+      port = 28981;
 
-    dataDir = "/mnt/data/srv/paperless-ngx/data";
-    mediaDir = "/mnt/data/srv/paperless-ngx/data/media";
-    consumptionDir = "/mnt/data/srv/paperless-ngx/data/consume";
-    consumptionDirIsPublic = false;
+      dataDir = "/mnt/data/srv/paperless-ngx/data";
+      mediaDir = "/mnt/data/srv/paperless-ngx/data/media";
+      consumptionDir = "/mnt/data/srv/paperless-ngx/data/consume";
+      consumptionDirIsPublic = false;
 
-    # https://docs.paperless-ngx.com/configuration/
-    settings = {
-      PAPERLESS_ADMIN_USER = config.custom.username;
+      # https://docs.paperless-ngx.com/configuration/
+      settings = {
+        PAPERLESS_ADMIN_USER = config.custom.username;
 
-      PAPERLESS_ALLOWED_HOSTS = lib.concatStringsSep "," hostnames;
-      PAPERLESS_CORS_ALLOWED_HOSTS = lib.concatStringsSep "," hostnamesWithSchema;
-      PAPERLESS_CSRF_TRUSTED_ORIGINS = lib.concatStringsSep "," hostnamesWithSchema;
+        PAPERLESS_ALLOWED_HOSTS = lib.concatStringsSep "," hostnames;
+        PAPERLESS_CORS_ALLOWED_HOSTS = lib.concatStringsSep "," hostnamesWithSchema;
+        PAPERLESS_CSRF_TRUSTED_ORIGINS = lib.concatStringsSep "," hostnamesWithSchema;
 
-      PAPERLESS_OCR_LANGUAGE = "deu+fra+eng";
-      PAPERLESS_CONSUMER_RECURSIVE = true;
+        PAPERLESS_OCR_LANGUAGE = "deu+fra+eng";
+        PAPERLESS_CONSUMER_RECURSIVE = true;
 
-      PAPERLESS_FILENAME_FORMAT = "{original_name}";
+        PAPERLESS_FILENAME_FORMAT = "{original_name}";
+      };
+      passwordFile = config.sops.secrets."paperless-ngx/adminPassword".path;
     };
-    passwordFile = config.sops.secrets."paperless-ngx/adminPassword".path;
-  };
 
-  # networking.firewall.allowedTCPPorts = [ 28981 ];
+    # networking.firewall.allowedTCPPorts = [ 28981 ];
 
-  services.nginx =
-    let
-      virtualHosts = builtins.listToAttrs (
-        map (hostname: {
-          name = hostname;
-          value = {
-            enableACME = true;
-            # FIXME https://github.com/NixOS/nixpkgs/issues/210807
-            acmeRoot = null;
-            forceSSL = true;
-            locations."/" = {
-              proxyPass = "http://${config.services.paperless.address}:${toString config.services.paperless.port}";
-              proxyWebsockets = true;
-              recommendedProxySettings = true;
-            };
+    nginx = {
+      virtualHosts = {
+        "${primaryHost}" = {
+          inherit serverAliases;
+          enableACME = true;
+          # FIXME https://github.com/NixOS/nixpkgs/issues/210807
+          acmeRoot = null;
+          forceSSL = true;
+          locations."/" = {
+            proxyPass = "http://${config.services.paperless.address}:${toString config.services.paperless.port}";
+            proxyWebsockets = true;
+            recommendedProxySettings = true;
           };
-        }) hostnames
-      );
-    in
-    {
-      virtualHosts = virtualHosts;
+        };
+      };
     };
+
+    monit.config = lib.mkAfter ''
+      check host "paperless-ngx" with address "${primaryHost}"
+        group services
+        restart program = "${pkgs.systemd}/bin/systemctl restart paperless-web.service"
+        if failed
+          port 443
+          protocol https
+          with timeout 15 seconds
+          and certificate valid for 5 days
+        then restart
+        if 5 restarts within 10 cycles then alert
+    '';
+  };
 
   users.users."${config.custom.username}" = {
     extraGroups = [ "paperless" ];
   };
 
-  # Override default tmpfile settings
-  # The ones provided by the module are not working too well.
-  systemd.tmpfiles.settings."10-paperless" =
-    let
-      user = config.services.paperless.user;
-
-      defaultRule = {
-        user = user;
-        group = user;
-        mode = "755"; # enforce mode for all dirs
-      };
-    in
-    {
-      "${config.services.paperless.dataDir}".d = defaultRule;
-      "${config.services.paperless.mediaDir}".d = defaultRule;
-      "${config.services.paperless.consumptionDir}".d = defaultRule;
-    };
-
-  services.monit.config = lib.mkAfter ''
-    check host "paperless-ngx" with address "${builtins.head hostnames}"
-      group services
-      restart program = "${pkgs.systemd}/bin/systemctl restart paperless-web.service"
-      if failed
-        port 443
-        protocol https
-        with timeout 15 seconds
-        and certificate valid for 5 days
-      then restart
-      if 5 restarts within 10 cycles then alert
-  '';
 }
