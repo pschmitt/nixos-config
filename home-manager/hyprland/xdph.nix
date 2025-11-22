5cb33114934e6b16c562cf41be40206f72301133
Philipp Schmitt <philipp@schmitt.co>
2025-11-22 10:55:36 +0100

    wip: custom xdph

--- home-manager/hyprland/xdph.nix
+++ home-manager/hyprland/xdph.nix
@@ -1,5 +1,8 @@
-{ ... }:
+# { inputs, pkgs, ... }:
 {
+  # wayland.windowManager.hyprland.portalPackage =
+  #   inputs.xdph.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
+
   # Mirror ~/.config/hypr/xdph.conf for xdg-desktop-portal-hyprland.
   xdg.configFile."hypr/xdph.conf".text = ''
     screencopy {
