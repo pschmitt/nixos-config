49154bcdfa6bef823c04de60f1dbd2cc12061da7
Philipp Schmitt <philipp@schmitt.co>
2025-11-24 22:00:16 +0100

    undebog

--- services/backups/bitwarden.nix
+++ services/backups/bitwarden.nix
@@ -12,7 +12,8 @@
       environmentFiles = [ config.sops.secrets."bw-backup".path ];
       environment = {
         CRON = "0 0 * * *";
-        DEBUG = "true";
+        # DEBUG = "true";
+        # START_RIGHT_NOW = "true";
       };
       volumes = [ "/srv/bw-backup:/data" ];
     };
