4561ce5b148f124e97e6cbd957d8623ada59e244
Philipp Schmitt <philipp@schmitt.co>
2025-11-25 17:42:40 +0100

    sort

--- home-manager/env.nix
+++ home-manager/env.nix
@@ -4,8 +4,8 @@
   xdg.enable = true;
 
   home.sessionVariables = {
-    GNUPGHOME = config.programs.gpg.homedir;
     CARGO_HOME = "${config.xdg.dataHome}/cargo";
+    GNUPGHOME = config.programs.gpg.homedir;
     GOPATH = "${config.xdg.dataHome}/go";
     SQLITE_HISTORY = "${config.xdg.dataHome}/sqlite_history";
   };
