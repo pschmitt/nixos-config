8a843fb9a7708aa29204e29246cef456321aaf4c
Philipp Schmitt <philipp@schmitt.co>
2025-11-25 21:55:49 +0100

    Install bw-desktop as a system app

--- home-manager/bitwarden.nix
+++ home-manager/bitwarden.nix
@@ -1,7 +1,10 @@
 { pkgs, ... }:
 {
   home.packages = with pkgs; [
-    bitwarden-desktop
+    # NOTE For the biometrics to work, the bitwarden-deskop pkg must be installed
+    # as a system package. See:
+    # https://github.com/NixOS/nixpkgs/pull/339384
+    # bitwarden-desktop
     master.bitwarden-cli
     rbw
   ];
