13e8ca1e339c3c0929f1200041bd8611bf4a7d23
Philipp Schmitt <philipp@schmitt.co>
2025-11-25 21:55:49 +0100

    Install bw-desktop as a system app

--- services/bitwarden.nix
+++ services/bitwarden.nix
@@ -1,7 +1,14 @@
-{ config, ... }:
+{ config, pkgs, ... }:
 {
   sops.secrets."bitwarden/password" = {
     inherit (config.custom) sopsFile;
     owner = config.custom.username;
   };
+
+  # NOTE For the biometrics to work, the bitwarden-deskop pkg must be installed
+  # as a system package. See:
+  # https://github.com/NixOS/nixpkgs/pull/339384
+  environment.systemPackages = with pkgs; [
+    bitwarden-desktop
+  ];
 }
