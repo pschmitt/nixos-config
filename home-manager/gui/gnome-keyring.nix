d54dca848d5fb529becb06ebfd962ae287ea08ec
Philipp Schmitt <philipp@schmitt.co>
2025-11-26 23:41:28 +0100

    Add gnome-keyring-unlock

--- home-manager/gui/gnome-keyring.nix
+++ home-manager/gui/gnome-keyring.nix
@@ -1,5 +1,26 @@
-{ config, ... }:
+{ config, pkgs, ... }:
 {
+  home.packages = [
+    # usage: gnome-keyring-unlock <<< PASSWORD
+    (pkgs.stdenvNoCC.mkDerivation {
+      pname = "gnome-keyring-unlock";
+      version = "cd3eb79";
+      src = pkgs.fetchFromGitea {
+        domain = "codeberg.org";
+        owner = "umglurf";
+        repo = "gnome-keyring-unlock";
+        rev = "cd3eb799ead710b930871224423a88f2d186cbea";
+        hash = "sha256-zVp+1TZZ2E7qwmcN1kgZZyhjgi407hIlv3y4zWundvY=";
+      };
+      nativeBuildInputs = [ pkgs.python3 ];
+      dontBuild = true;
+      installPhase = ''
+        install -Dm755 unlock.py $out/bin/gnome-keyring-unlock
+      '';
+      postPatch = "patchShebangs unlock.py";
+    })
+  ];
+
   systemd.user.services.gnome-keyring-auto-unlock = {
     Unit.Description = "Auto-unlock GNOME keyring";
 
