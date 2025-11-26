7b6104f88fdb962d294df7749502dcfdcec660f9
Philipp Schmitt <philipp@schmitt.co>
2025-11-26 23:27:32 +0100

    remove kubectl-1.21

--- home-manager/work/k8s.nix
+++ home-manager/work/k8s.nix
@@ -8,9 +8,6 @@
     krew
     kubecolor
     kubectl
-    (writeShellScriptBin "kubectl-1.21" ''
-      ${pkgs.kubectl-121.kubectl}/bin/kubectl "$@"
-    '')
     (writeShellScriptBin "kubectl-1.23" ''
       ${pkgs.kubectl-123.kubectl}/bin/kubectl "$@"
     '')
