11b420a29cf95c7120d0c21ca5a7696d3cc6563b
Philipp Schmitt <philipp@schmitt.co>
2025-11-26 22:24:21 +0100

    add py env

--- home-manager/devel/python.nix
+++ home-manager/devel/python.nix
@@ -8,5 +8,17 @@
     python3Packages.flake8
     python3Packages.ipython
     uv
+
+    (python3.withPackages (
+      ps: with ps; [
+        dbus-python
+        dnspython # for ansible
+        gst-python
+        pygobject3
+        pynvim
+        requests
+        rich
+      ]
+    ))
   ];
 }
