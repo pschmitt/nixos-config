f8829acd7718759bd4c13abb99a4dfdd278e94b2
Philipp Schmitt <philipp@schmitt.co>
2025-11-27 09:13:03 +0100

    refactor

--- services/jellyfin.nix
+++ services/jellyfin.nix
@@ -1,45 +1,37 @@
-{ config, ... }:
 {
-  services.jellyfin = {
-    enable = true;
-    # dataDir = "/mnt/data/jellyfin";
-  };
-
-  services.nginx =
-    let
-      hostNames = [
-        # main
-        "media.${config.custom.mainDomain}"
-        "tv.${config.custom.mainDomain}"
-        "jellyfin.${config.custom.mainDomain}"
-        "jelly.${config.custom.mainDomain}"
-
-        # including hostname
-        # "jelly.${config.networking.hostName}.${config.custom.mainDomain}"
-        # "jellyfin.${config.networking.hostName}.${config.custom.mainDomain}"
-
-        # vpn
-        # "media.${config.networking.hostName}.nb.${config.custom.mainDomain}"
-        # "media.${config.networking.hostName}.ts.${config.custom.mainDomain}"
-      ];
-      virtualHosts = builtins.listToAttrs (
-        map (hostName: {
-          name = hostName;
-          value = {
-            enableACME = true;
-            # FIXME https://github.com/NixOS/nixpkgs/issues/210807
-            acmeRoot = null;
-            forceSSL = true;
-            locations."/" = {
-              proxyPass = "http://127.0.0.1:8096";
-              recommendedProxySettings = true;
-              proxyWebsockets = true;
-            };
-          };
-        }) hostNames
-      );
-    in
-    {
-      virtualHosts = virtualHosts;
+  config,
+  lib,
+  ...
+}:
+let
+  hostnames = [
+    # main
+    "media.${config.custom.mainDomain}"
+    "tv.${config.custom.mainDomain}"
+    "jellyfin.${config.custom.mainDomain}"
+    "jelly.${config.custom.mainDomain}"
+  ];
+  primaryHost = builtins.head hostnames;
+  serverAliases = lib.remove primaryHost hostnames;
+in
+{
+  services = {
+    jellyfin = {
+      enable = true;
+      # dataDir = "/mnt/data/jellyfin";
     };
+
+    nginx.virtualHosts."${primaryHost}" = {
+      inherit serverAliases;
+      enableACME = true;
+      # FIXME https://github.com/NixOS/nixpkgs/issues/210807
+      acmeRoot = null;
+      forceSSL = true;
+      locations."/" = {
+        proxyPass = "http://127.0.0.1:8096";
+        recommendedProxySettings = true;
+        proxyWebsockets = true;
+      };
+    };
+  };
 }
