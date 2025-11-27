5062b9a7da9030d413aa44b3934956475d44fd36
Philipp Schmitt <philipp@schmitt.co>
2025-11-27 21:17:41 +0100

    back on the flake. disable xtra-dispatchers

--- home-manager/gui/hyprland/plugins/xtra-dispatchers.nix
+++ home-manager/gui/hyprland/plugins/xtra-dispatchers.nix
@@ -1,7 +1,7 @@
 { inputs, pkgs, ... }:
 {
   wayland.windowManager.hyprland.plugins = [
-    pkgs.master.hyprlandPlugins.xtra-dispatchers
-    # inputs.hyprland-plugins.packages.${pkgs.stdenv.hostPlatform.system}.xtra-dispatchers
+    # pkgs.master.hyprlandPlugins.xtra-dispatchers
+    inputs.hyprland-plugins.packages.${pkgs.stdenv.hostPlatform.system}.xtra-dispatchers
   ];
 }
