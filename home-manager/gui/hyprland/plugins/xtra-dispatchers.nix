17cfc575ac82a6dffa1dda46ec7415d1bf4a2e17
Philipp Schmitt <philipp@schmitt.co>
2025-11-27 14:34:14 +0100

    unfuck hyprland

--- home-manager/gui/hyprland/plugins/xtra-dispatchers.nix
+++ home-manager/gui/hyprland/plugins/xtra-dispatchers.nix
@@ -1,6 +1,6 @@
 { pkgs, ... }:
 {
-  wayland.windowManager.hyprland.plugins = with pkgs.hyprlandPlugins; [
+  wayland.windowManager.hyprland.plugins = with pkgs.master.hyprlandPlugins; [
     xtra-dispatchers
   ];
 }
