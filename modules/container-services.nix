a67995b34a75b5f40b4c2d3d80820be4ce48292f
Philipp Schmitt <philipp@schmitt.co>
2025-11-27 23:09:12 +0100

    fmt

--- modules/container-services.nix
+++ modules/container-services.nix
@@ -369,39 +369,39 @@ let
 
       # Internal location for Authelia authz check
       autheliaServerExtraConfig = optionalString wantsSsoAuth ''
-                set $upstream_authelia ${defaultAuthzURL};
+        set $upstream_authelia ${defaultAuthzURL};
 
-                ## Virtual endpoint created by nginx to forward auth requests.
-                location /internal/authelia/authz {
-                  ## Essential Proxy Configuration
-                  internal;
-        ${autheliaResolverDirectives}
-                  proxy_pass $upstream_authelia;
+        ## Virtual endpoint created by nginx to forward auth requests.
+        location /internal/authelia/authz {
+          ## Essential Proxy Configuration
+          internal;
+          ${autheliaResolverDirectives}
+          proxy_pass $upstream_authelia;
 
-                  ## Headers
-                  ## The headers starting with X-* are required.
-                  proxy_set_header X-Original-Method $request_method;
-                  proxy_set_header X-Original-URL $scheme://$http_host$request_uri;
-                  proxy_set_header X-Forwarded-For $remote_addr;
-                  proxy_set_header Content-Length "";
-                  proxy_set_header Connection "";
+          ## Headers
+          ## The headers starting with X-* are required.
+          proxy_set_header X-Original-Method $request_method;
+          proxy_set_header X-Original-URL $scheme://$http_host$request_uri;
+          proxy_set_header X-Forwarded-For $remote_addr;
+          proxy_set_header Content-Length "";
+          proxy_set_header Connection "";
 
-                  ## Basic Proxy Configuration
-                  proxy_pass_request_body off;
-                  proxy_next_upstream error timeout invalid_header http_500 http_502 http_503; # Timeout if the real server is dead
-                  proxy_redirect http:// $scheme://;
-                  proxy_http_version 1.1;
-                  proxy_cache_bypass $cookie_session;
-                  proxy_no_cache $cookie_session;
-                  proxy_buffers 4 32k;
-                  client_body_buffer_size 128k;
+          ## Basic Proxy Configuration
+          proxy_pass_request_body off;
+          proxy_next_upstream error timeout invalid_header http_500 http_502 http_503; # Timeout if the real server is dead
+          proxy_redirect http:// $scheme://;
+          proxy_http_version 1.1;
+          proxy_cache_bypass $cookie_session;
+          proxy_no_cache $cookie_session;
+          proxy_buffers 4 32k;
+          client_body_buffer_size 128k;
 
-                  ## Advanced Proxy Configuration
-                  send_timeout 5m;
-                  proxy_read_timeout 240;
-                  proxy_send_timeout 240;
-                  proxy_connect_timeout 240;
-                }
+          ## Advanced Proxy Configuration
+          send_timeout 5m;
+          proxy_read_timeout 240;
+          proxy_send_timeout 240;
+          proxy_connect_timeout 240;
+        }
       '';
     in
     {
