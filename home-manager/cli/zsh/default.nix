7ae8c4e1c62dbc2b3888e0b2dcb3f798a6e71646
Philipp Schmitt <philipp@schmitt.co>
2025-11-27 15:58:18 +0100

    merge nix-index-database config bits

--- home-manager/cli/zsh/default.nix
+++ home-manager/cli/zsh/default.nix
@@ -1,6 +1,5 @@
 {
   config,
-  inputs,
   pkgs,
   ...
 }:
@@ -31,74 +30,65 @@
       nix-your-shell
       zoxide
     ];
+  };
 
-    file = {
-      "${config.xdg.configHome}/zsh/custom/os/nixos/system.zsh" = {
-        text = ''
-          [[ -o interactive ]] || return
+  xdg.configFile = {
+    "zsh/custom/os/nixos/system.zsh".text = ''
+      [[ -o interactive ]] || return
 
-          # command-not-found integration
-          source ${
-            inputs.nix-index-database.packages.${pkgs.stdenv.hostPlatform.system}.nix-index-with-db
-          }/etc/profile.d/command-not-found.sh
+      # DEPRECATED: Use wezterm.sh instead
+      # source ${pkgs.vte}/etc/profile.d/vte.sh
 
-          # DEPRECATED: Use wezterm.sh instead
-          # source ${pkgs.vte}/etc/profile.d/vte.sh
+      # FIXME the osc7 shell func produces output which p10k complains about
+      # on startup (hence the WEZTERM_SHELL_SKIP_CWD)
+      # WEZTERM_SHELL_SKIP_CWD=1 source ${pkgs.wezterm}/etc/profile.d/wezterm.sh
 
-          # FIXME the osc7 shell func produces output which p10k complains about
-          # on startup (hence the WEZTERM_SHELL_SKIP_CWD)
-          # WEZTERM_SHELL_SKIP_CWD=1 source ${pkgs.wezterm}/etc/profile.d/wezterm.sh
+      # atuin
+      source ${
+        (pkgs.runCommand "atuin-init" { } ''
+          mkdir -p $out/home
+          HOME=$out/home ${pkgs.atuin}/bin/atuin init \
+            --disable-ctrl-r \
+            --disable-up-arrow \
+            zsh > $out/init.zsh
+          rm -rf $out/home
+        '')
+      }/init.zsh
+      # bindkey '^[r' _atuin_search_widget
 
-          # atuin
-          source ${
-            (pkgs.runCommand "atuin-init" { } ''
-              mkdir -p $out/home
-              HOME=$out/home ${pkgs.atuin}/bin/atuin init \
-                --disable-ctrl-r \
-                --disable-up-arrow \
-                zsh > $out/init.zsh
-              rm -rf $out/home
-            '')
-          }/init.zsh
-          # bindkey '^[r' _atuin_search_widget
+      # direnv
+      source ${
+        (pkgs.runCommand "direnv-init" { } ''
+          mkdir -p $out
+          ${pkgs.direnv}/bin/direnv hook zsh > $out/init.zsh
+        '')
+      }/init.zsh
 
-          # direnv
-          source ${
-            (pkgs.runCommand "direnv-init" { } ''
-              mkdir -p $out
-              ${pkgs.direnv}/bin/direnv hook zsh > $out/init.zsh
-            '')
-          }/init.zsh
+      # vivid
+      export LS_COLORS="$(cat ${
+        (pkgs.runCommand "vivid-generate" { } ''
+          mkdir -p $out
+          ${pkgs.vivid}/bin/vivid generate ${config.programs.vivid.activeTheme} > $out/ls_colors
+        '')
+      }/ls_colors)"
 
-          # vivid
-          export LS_COLORS="$(cat ${
-            (pkgs.runCommand "vivid-generate" { } ''
-              mkdir -p $out
-              ${pkgs.vivid}/bin/vivid generate ${config.programs.vivid.activeTheme} > $out/ls_colors
-            '')
-          }/ls_colors)"
+      # zoxide
+      source ${
+        (pkgs.runCommand "zoxide-init" { } ''
+          mkdir -p $out
+          ${pkgs.zoxide}/bin/zoxide init zsh --no-cmd > $out/init.zsh
+        '')
+      }/init.zsh
+      alias z=__zoxide_z
+      alias zz=__zoxide_zi
+    '';
 
-          # zoxide
-          source ${
-            (pkgs.runCommand "zoxide-init" { } ''
-              mkdir -p $out
-              ${pkgs.zoxide}/bin/zoxide init zsh --no-cmd > $out/init.zsh
-            '')
-          }/init.zsh
-          alias z=__zoxide_z
-          alias zz=__zoxide_zi
-        '';
-      };
-
-      # completions
-      "${config.xdg.configHome}/zsh/completions/source-me.zsh" = {
-        text = ''
-          # bashcompinit is not needed here since we already do this in zinit
-          # autoload -U +X bashcompinit && bashcompinit
-          complete -C "${pkgs.openbao}/bin/bao" bao
-          complete -C "${pkgs.vault}/bin/vault" vault
-        '';
-      };
-    };
+    # completions
+    "zsh/completions/source-me.zsh".text = ''
+      # bashcompinit is not needed here since we already do this in zinit
+      # autoload -U +X bashcompinit && bashcompinit
+      complete -C "${pkgs.openbao}/bin/bao" bao
+      complete -C "${pkgs.vault}/bin/vault" vault
+    '';
   };
 }
--- home-manager/nix-index-database.nix
+++ home-manager/nix-index-database.nix
@@ -1,9 +1,18 @@
 {
   inputs,
+  lib,
+  pkgs,
   ...
 }:
 {
   imports = [ inputs.nix-index-database.homeModules.nix-index ];
 
   programs.nix-index-database.comma.enable = true;
+
+  xdg.configFile."zsh/custom/os/nixos/system.zsh".text = lib.mkAfter ''
+    # command-not-found integration
+    source ${
+      inputs.nix-index-database.packages.${pkgs.stdenv.hostPlatform.system}.nix-index-with-db
+    }/etc/profile.d/command-not-found.sh
+  '';
 }
