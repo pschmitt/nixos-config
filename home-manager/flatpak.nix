e56a06dad67e79f911bc91f510244cb2117a6586
Philipp Schmitt <philipp@schmitt.co>
2025-11-28 08:34:55 +0100

    move portal config to gui/

--- home-manager/flatpak.nix
+++ home-manager/flatpak.nix
@@ -1,6 +1,5 @@
 {
   inputs,
-  pkgs,
   ...
 }:
 {
@@ -26,25 +25,5 @@
     #     ];
     #   };
     # };
-  };
-
-  # The quick and dirty config below:
-  # xdg.portal.config.common.default = "*";
-  # reproduces the < 1.17 behavior,  which uses the first portal
-  # implementation found in lexicographical order
-  xdg.portal = {
-    enable = true;
-    extraPortals = [
-      pkgs.xdg-desktop-portal-gtk
-      pkgs.xdg-desktop-portal-hyprland
-    ];
-    config.common = {
-      default = [
-        "hyprland"
-        "gtk"
-      ];
-      "org.freedesktop.impl.portal.FileChooser" = [ "gtk" ];
-      "org.freedesktop.impl.portal.Secret" = [ "gnome-keyring" ];
-    };
   };
 }
