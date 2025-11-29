751c1b7cb018a6b003588dbb89e38ba03486f021
Philipp Schmitt <philipp@schmitt.co>
2025-11-29 10:24:32 +0100

    shift

--- common/laptop/tor.nix
+++ common/laptop/tor.nix
@@ -1,7 +1,9 @@
-{ lib, ... }:
+{ lib, pkgs, ... }:
 {
   imports = [ ../../services/tor.nix ];
 
   # Disable the tor service by default
   systemd.services.tor.wantedBy = lib.mkForce [ ];
+
+  environment.systemPackages = [ pkgs.tor-browser ];
 }
