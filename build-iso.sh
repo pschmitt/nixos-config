088527c99b5dcb1ae93a18aaefd5829739e477de
Philipp Schmitt <philipp@schmitt.co>
2025-11-29 11:05:58 +0100

    merge iso/sd-image build scripts

--- build-iso.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-TARGET_HOST="${1:-iso}" # or iso-xmr for example
-
-cd "$(cd "$(dirname "$0")" >/dev/null 2>&1; pwd -P)" || exit 9
-
-rm -f result
-nix build ".#nixosConfigurations.${TARGET_HOST}.config.system.build.isoImage"
-tree result
