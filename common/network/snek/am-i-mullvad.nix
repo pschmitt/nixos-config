120c3c582c55fcd4d916771ff9d68b92ce66d17d
Philipp Schmitt <philipp@schmitt.co>
2025-11-30 17:51:14 +0100

    jq the json

--- common/network/snek/am-i-mullvad.nix
+++ common/network/snek/am-i-mullvad.nix
@@ -4,18 +4,27 @@
     # https://mullvad.net/en/check
     (pkgs.writeShellScriptBin "am-i-mullvad" ''
       URL=https://am.i.mullvad.net
+      JSON=""
 
       case "$1" in
         -j|--json)
           shift
           URL+="/json"
+          JSON=1
           ;;
         *)
           URL+="/connected"
           ;;
       esac
 
-      exec ${pkgs.curl}/bin/curl -fsSL "$URL" "$@"
+      ${pkgs.curl}/bin/curl -fsSL "$URL" "$@" | {
+        if [[ -z "$JSON" ]]
+        then
+          cat
+        else
+          ${pkgs.jq}/bin/jq '.'
+        fi
+      }
     '')
   ];
 }
