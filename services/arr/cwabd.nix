0bccfa4b607b2c71a1bbf22223c29c4bd64f4548
Philipp Schmitt <philipp@schmitt.co>
2025-11-30 19:15:39 +0100

    --net=ns:/run/netns/xxx is a thing (in podman)

--- services/arr/cwabd.nix
+++ services/arr/cwabd.nix
@@ -2,20 +2,14 @@
 {
   virtualisation.oci-containers.containers.cwabd = {
     image = "ghcr.io/calibrain/calibre-web-automated-book-downloader";
-    pull = "always";
     autoStart = true;
     extraOptions = [
-      # Use --network=host so the container shares the network namespace of the
-      # systemd service, which is confined to the VPN namespace.
-      "--network=host"
-      "--init"
+      "--net=ns:/run/netns/mullvad"
     ];
   };
 
-  systemd.services."${config.virtualisation.oci-containers.containers.cwabd.serviceName
-  }".vpnConfinement =
-    {
-      enable = true;
-      vpnNamespace = "mullvad";
-    };
+  systemd.services."${config.virtualisation.oci-containers.containers.cwabd.serviceName}" = {
+    after = [ "mullvad.service" ];
+    requires = [ "mullvad.service" ];
+  };
 }
