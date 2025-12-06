4e1e36ebec2ba01a6bc555decff24eb50ac22424
Philipp Schmitt <philipp@schmitt.co>
2025-12-06 09:45:58 +0100

    Remove hotfixes

--- overlays/hotfixes.nix
+++ overlays/hotfixes.nix
@@ -1,7 +1,7 @@
 {
-  final,
-  prev,
+  # final,
   # inputs,
+  prev,
   ...
 }:
 
@@ -35,57 +35,6 @@
     '';
   });
 
-  # TODO Remove once https://github.com/NixOS/nixpkgs/pull/467572 lands
-  v4l2loopback =
-    let
-      base =
-        prev.v4l2loopback or (prev.callPackage "${prev.path}/pkgs/os-specific/linux/v4l2loopback" {
-          kernel = null;
-          kernelModuleMakeFlags = [ ];
-        });
-    in
-    base.overrideAttrs (old: rec {
-      version = "0.15.3";
-
-      src = prev.fetchFromGitHub {
-        owner = "umlaeute";
-        repo = "v4l2loopback";
-        tag = "v${version}";
-        hash = "sha256-KXJgsEJJTr4TG4Ww5HlF42v2F1J+AsHwrllUP1n/7g8=";
-      };
-
-      passthru = (old.passthru or { }) // {
-        updateScript = prev."nix-update-script" { };
-      };
-    });
-
   # Ensure python313Packages uses the modified interpreter
   # python313Packages = final.python313.pkgs;
-
-  # Make sure kernel-specific package sets also use the patched v4l2loopback
-  linuxPackages = prev.linuxPackages.extend (
-    self: _super: {
-      v4l2loopback = final.v4l2loopback.override { kernel = self.kernel; };
-    }
-  );
-
-  linuxPackages_latest = prev.linuxPackages_latest.extend (
-    self: _super: {
-      v4l2loopback = final.v4l2loopback.override { kernel = self.kernel; };
-    }
-  );
-
-  linuxPackages_6_18 = prev.linuxPackages_6_18.extend (
-    self: _super: {
-      v4l2loopback = final.v4l2loopback.override { kernel = self.kernel; };
-    }
-  );
-
-  # https://github.com/NixOS/nixpkgs/pull/465400
-  termbench-pro = prev.termbench-pro.overrideAttrs (old: {
-    buildInputs = [
-      prev.fmt
-      (prev.glaze.override { enableSSL = false; })
-    ];
-  });
 }
