6c324483d5bd9a101358a32502d57d8309266cff
Philipp Schmitt <philipp@schmitt.co>
2025-12-06 10:46:32 +0100

    Add hardware dir

--- hosts/pica4/hardware-configuration.nix
+++ hosts/pica4/hardware-configuration.nix
@@ -1,57 +1,8 @@
-{
-  inputs,
-  lib,
-  pkgs,
-  ...
-}:
+{ ... }:
 {
   imports = [
-    inputs.hardware.nixosModules.raspberry-pi-4
+    ../../hardware/rpi4.nix
     ./config-txt.nix
     ./power-monitoring.nix
   ];
-
-  hardware.raspberry-pi."4" = {
-    bluetooth.enable = false;
-    leds = {
-      act.disable = true;
-      eth.disable = true;
-      pwr.disable = true;
-    };
-  };
-
-  nixpkgs.hostPlatform = lib.mkDefault "aarch64-linux";
-
-  boot.kernelPackages = lib.mkForce pkgs.linuxKernel.packages.linux_rpi4;
-
-  # Enabling all hardware leads to build errors:
-  # modprobe: FATAL: Module dw-hdmi not found in directory /nix/store/…
-  hardware.enableAllHardware = lib.mkForce false;
-  services.fwupd.enable = lib.mkForce false;
-
-  # mount the fw partition as /boot/firmware
-  # FIXME This fails to activate:
-  # Error: Failed to open unit file /nix/store/br5yy8zkrwrlk6rvsxv1j68d5klkxh4s-nixos-system-pica4-sd-card-25.11.20250819.2007595/etc/systemd/system/boot-firmware.mount
-  # Caused by:
-  #     No such file or directory (os error 2)
-  # fileSystems."/boot/firmware" = {
-  #   device = "/dev/disk/by-label/FIRMWARE";
-  #   fsType = "vfat";
-  #   options = lib.mkForce [
-  #     "auto"
-  #     "nofail"
-  #     # root-only by default
-  #     "fmask=0077"
-  #     "dmask=0077"
-  #   ];
-  # };
-
-  # FIXME libraspberrypi does not compile with cmake 4
-  # see https://github.com/NixOS/nixpkgs/pull/450828
-  environment.systemPackages = with pkgs; [
-    raspberrypi-eeprom
-  ];
-
-  # Can't use btrfs storage driver!
-  virtualisation.docker.storageDriver = lib.mkForce null;
 }
