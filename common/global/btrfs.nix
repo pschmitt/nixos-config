a7439a80a6e5feb2df8ff62e07ff7ab360ea874b
Philipp Schmitt <philipp@schmitt.co>
2025-12-06 09:52:11 +0100

    only enable btrfs auto scrubs if there a btrfs fses present

--- common/global/btrfs.nix
+++ common/global/btrfs.nix
@@ -1,7 +1,17 @@
 {
-  services.btrfs.autoScrub = {
-    enable = true;
-    # fileSystems = [ "/" ]; # default is all btrfs file systems
-    limit = "100M"; # max throughput
+  config,
+  lib,
+  ...
+}:
+let
+  hasBtrfsFs = lib.any (fs: fs.fsType == "btrfs") (builtins.attrValues config.fileSystems);
+in
+{
+  config = lib.mkIf hasBtrfsFs {
+    services.btrfs.autoScrub = {
+      enable = true;
+      # fileSystems = [ "/" ]; # default is all btrfs file systems
+      limit = "100M"; # max throughput
+    };
   };
 }
