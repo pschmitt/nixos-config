16f107f4789f134c2907a8ad2151c9a0d59f1fa0
Philipp Schmitt <philipp@schmitt.co>
2025-12-07 11:38:50 +0100

    Add micro- and shadowsocks

---
+++ services/arr/microsocks.nix
@@ -0,0 +1,36 @@
+{ config, pkgs, ... }:
+let
+  internalIP = config.vpnNamespaces.mullvad.namespaceAddress;
+  port = 1080;
+in
+{
+  services.microsocks = {
+    enable = true;
+    ip = "0.0.0.0";
+    inherit port;
+  };
+
+  services.monit.config = ''
+    check host "microsocks" with address ${internalIP}
+      group piracy
+      depends on mullvad-netns
+      restart program = "${pkgs.systemd}/bin/systemctl restart microsocks"
+      if failed port ${toString port} protocol default for 2 cycles then restart
+      if 2 restarts within 6 cycles then alert
+  '';
+
+  systemd.services.microsocks = {
+    vpnConfinement = {
+      enable = true;
+      vpnNamespace = "mullvad";
+    };
+  };
+
+  # Firewall still limits exposure to the tailscale/netbird interfaces.
+  vpnNamespaces.mullvad.portMappings = [
+    {
+      from = port;
+      to = port;
+    }
+  ];
+}
