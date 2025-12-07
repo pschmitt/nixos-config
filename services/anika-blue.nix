c8cffac78be3fe882bb5b5779b2a68bad0c851b3
Philipp Schmitt <philipp@schmitt.co>
2025-12-07 23:15:18 +0100

    anika.blue

--- services/anika-blue.nix
+++ services/anika-blue.nix
@@ -5,6 +5,14 @@
   pkgs,
   ...
 }:
+let
+  mainHost = "anika.blue";
+  serverAliases = [
+    "anika-blue.${config.custom.mainDomain}"
+    "anika-blue.bergmann-schmitt.de"
+    "blue.bergmann-schmitt.de"
+  ];
+in
 {
   imports = [ inputs.anika-blue.nixosModules.default ];
 
@@ -22,26 +30,19 @@
       secretKeyFile = config.sops.secrets."anika-blue/secretKey".path;
     };
 
-    nginx.virtualHosts =
-      let
-        nginxConfig = {
-          enableACME = true;
-          # FIXME https://github.com/NixOS/nixpkgs/issues/210807
-          acmeRoot = null;
-          forceSSL = true;
+    nginx.virtualHosts."${mainHost}" = {
+      enableACME = true;
+      # FIXME https://github.com/NixOS/nixpkgs/issues/210807
+      acmeRoot = null;
+      forceSSL = true;
+      inherit serverAliases;
 
-          locations."/" = {
-            proxyPass = "http://${config.services.anika-blue.bindHost}:${toString config.services.anika-blue.port}";
-            # proxyWebsockets = true;
-            recommendedProxySettings = true;
-          };
-
-        };
-      in
-      {
-        "anika-blue.${config.custom.mainDomain}" = nginxConfig;
-        "anika-blue.bergmann-schmitt.de" = nginxConfig;
+      locations."/" = {
+        proxyPass = "http://${config.services.anika-blue.bindHost}:${toString config.services.anika-blue.port}";
+        recommendedProxySettings = true;
+        # proxyWebsockets = true;
       };
+    };
 
     monit.config = lib.mkAfter ''
       check host "anika-blue" with address "anika-blue.${config.custom.mainDomain}"
