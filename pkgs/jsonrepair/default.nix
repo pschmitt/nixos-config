cd674e2d11e95334ee90d9dd95f074e7472cdb58
pschmitt <37886+pschmitt@users.noreply.github.com>
2025-12-08 04:44:42 +0000

    chore: update jsonrepair

--- pkgs/jsonrepair/default.nix
+++ pkgs/jsonrepair/default.nix
@@ -6,16 +6,16 @@
 
 buildNpmPackage rec {
   pname = "jsonrepair";
-  version = "3.4.0";
+  version = "3.13.1";
 
   src = fetchFromGitHub {
     owner = "josdejong";
     repo = pname;
     rev = "v${version}";
-    hash = "sha256-E7ilA+Rqq1zj2Qusr9RpDsxq6Um9VAv/2T0fwhOKsbM=";
+    hash = "sha256-SsTz4YZ+6TA5GxZZWUC0u+ratN5gA3Gfnajv2pISgNs=";
   };
 
-  npmDepsHash = "sha256-kvKjqmr4LodnIz+QNcf/fmzZ2SqB3msI41UBg0Rc2XQ=";
+  npmDepsHash = "sha256-Bgf623Ic4hFgKMmk+hml20LR2MHH8ssL34tprTNZ5KM=";
 
   # The prepack script runs the build script, which we'd rather do in the build phase.
   # npmPackFlags = [ "--ignore-scripts" ];
