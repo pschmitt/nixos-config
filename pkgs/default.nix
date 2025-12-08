283ab17e5f4628c9bd250d282b0fe4d1e15a72c3
Philipp Schmitt <philipp@schmitt.co>
2025-12-08 09:55:57 +0100

    org

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -10,11 +10,15 @@ let
   # libcaption = pkgs.callPackage ./libcaption { };
 in
 {
+  # local pkgs
   bluez-headset-callback = pkgs.callPackage ./bluez-headset-callback { };
-  cdpcurl = pkgs.callPackage ./cdpcurl { };
-  native-client = pkgs.callPackage ./native-client { };
   custom-keymaps = pkgs.callPackage ./custom-keymaps { };
   docker-compose-wrapper = pkgs.callPackage ./docker-compose-wrapper { };
+  udev-custom-callback = pkgs.callPackage ./udev-custom-callback { };
+
+  # external pkgs
+  cdpcurl = pkgs.callPackage ./cdpcurl { };
+  native-client = pkgs.callPackage ./native-client { };
   emoji-fzf = pkgs.callPackage ./emoji-fzf { };
   go-hass-agent = pkgs.callPackage ./go-hass-agent { };
   happy-hacking-gnu = pkgs.callPackage ./happy-hacking-gnu { };
@@ -24,20 +28,21 @@ in
   ketall = pkgs.callPackage ./ketall { };
   libfprint-focaltech = pkgs.callPackage ./libfprint-focaltech { };
   linkding-cli = pkgs.callPackage ./linkding-cli { };
-  luks-mount = pkgs.callPackage ./luks-mount { };
   mmonit = pkgs.callPackage ./mmonit { };
-  oci-consistent-device-naming = pkgs.callPackage ./oci-consistent-device-naming { };
   opsgenie-cli = pkgs.callPackage ./opsgenie-cli { };
-  oracle-cloud-agent = pkgs.callPackage ./oracle-cloud-agent { };
   timewarrior-jirapush = pkgs.callPackage ./timewarrior-jirapush { };
-  udev-custom-callback = pkgs.callPackage ./udev-custom-callback { };
   waypoint = pkgs.callPackage ./waypoint { };
   withoutbg = pkgs.python3Packages.callPackage ./withoutbg { };
   yank-osc52 = pkgs.callPackage ./yank-osc52 { };
 
   # custom packages, that should be flakes
   ldifj = pkgs.callPackage ./ldifj { };
+  luks-mount = pkgs.callPackage ./luks-mount { };
   tmux-slay = pkgs.callPackage ./tmux-slay { };
+
+  # oci pkgs
+  oci-consistent-device-naming = pkgs.callPackage ./oci/oci-consistent-device-naming { };
+  oracle-cloud-agent = pkgs.callPackage ./oci/oracle-cloud-agent { };
 
   # Fonts
   ComicCode = pkgs.callPackage ./fonts/ComicCode { };
