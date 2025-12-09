a3642ac8ea4719d79a42377ba01dd7898e45bc65
Philipp Schmitt <philipp@schmitt.co>
2025-12-09 17:59:36 +0100

    disable falcon-sensor autostart

--- common/work/crowdstrike-falcon-sensor.nix
+++ common/work/crowdstrike-falcon-sensor.nix
@@ -18,24 +18,29 @@
     sopsFile = ../../secrets/shared.sops.yaml;
   };
 
-  systemd.services.falcon-sensor.serviceConfig.ExecStartPre = lib.mkForce [
-    (pkgs.writeScript "falcon-init" ''
-      #!${pkgs.bash}/bin/bash
-      set -euo pipefail
-      ln -sf ${pkgs.falcon-sensor-unwrapped}/opt/CrowdStrike/* /opt/CrowdStrike/
-      ${pkgs.falcon-sensor}/bin/falconctl -s --trace=debug
+  systemd.services.falcon-sensor = {
+    # Let's just not start this garbage service automatically
+    wantedBy = lib.mkForce [ ];
 
-      if [ -f ${config.sops.secrets."crowdstrike/customerId".path} ]; then
-        CID=$(cat ${config.sops.secrets."crowdstrike/customerId".path})
-        ${pkgs.falcon-sensor}/bin/falconctl -s --cid="$CID" -f
-      else
-        echo "CID secret not found at ${config.sops.secrets."crowdstrike/customerId".path}!"
-        exit 1
-      fi
+    serviceConfig.ExecStartPre = lib.mkForce [
+      (pkgs.writeScript "falcon-init" ''
+        #!${pkgs.bash}/bin/bash
+        set -euo pipefail
+        ln -sf ${pkgs.falcon-sensor-unwrapped}/opt/CrowdStrike/* /opt/CrowdStrike/
+        ${pkgs.falcon-sensor}/bin/falconctl -s --trace=debug
 
-      ${pkgs.falcon-sensor}/bin/falconctl -g --cid
-    '')
-  ];
+        if [ -f ${config.sops.secrets."crowdstrike/customerId".path} ]; then
+          CID=$(cat ${config.sops.secrets."crowdstrike/customerId".path})
+          ${pkgs.falcon-sensor}/bin/falconctl -s --cid="$CID" -f
+        else
+          echo "CID secret not found at ${config.sops.secrets."crowdstrike/customerId".path}!"
+          exit 1
+        fi
+
+        ${pkgs.falcon-sensor}/bin/falconctl -g --cid
+      '')
+    ];
+  };
 
   nixpkgs.overlays = [
     (final: prev: {
