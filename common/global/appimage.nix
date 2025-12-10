01bcae56071eb0fdd7bc7a467dc6803138d0b970
Philipp Schmitt <philipp@schmitt.co>
2025-12-10 15:59:24 +0100

    yolo, flake lock

--- common/global/appimage.nix
+++ common/global/appimage.nix
@@ -1,15 +1,19 @@
 {
-  inputs,
-  outputs,
   lib,
-  config,
   pkgs,
   ...
 }:
 
-{
-  environment.systemPackages = with pkgs; [ appimage-run ];
+let
+  # XXX Disable AppImage support on non-x86_64 platforms for now
+  # Blame:
+  # https://github.com/NixOS/nixpkgs/commit/d151f99d0f0c460b2f5344c58a4e1b759b7dffcd
+  enableAppImage = pkgs.stdenv.hostPlatform.isx86_64;
+in
+lib.mkIf enableAppImage {
+  environment.systemPackages = [ pkgs.appimage-run ];
 
+  # https://nixos.wiki/wiki/Appimage
   boot.binfmt.registrations.appimage = {
     wrapInterpreterInShell = false;
     interpreter = "${pkgs.appimage-run}/bin/appimage-run";
