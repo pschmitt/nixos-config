ae2ab79ef1dbdf1900f2e2f4e5e21dfd57645b4a
Philipp Schmitt <philipp@schmitt.co>
2025-12-11 15:00:05 +0100

    load bcm2835_wdt on rpi

--- hardware/watchdog.nix
+++ hardware/watchdog.nix
@@ -1,24 +1,27 @@
 { config, lib, ... }:
 let
   cfg = config.hardware.watchdog;
-  isPhysical = cfg.implementation == "hardware";
+  isRpi = config.hardware.type == "rpi";
+  isHardware = cfg.implementation == "hardware";
   moduleName = if cfg.implementation == "softdog" then "softdog" else "virtio_watchdog";
 in
 {
   config = lib.mkIf cfg.enable (
     lib.mkMerge [
-      (lib.mkIf isPhysical {
-        services.watchdogd.enable = true;
+      (lib.mkIf (isHardware && isRpi) {
+        boot.kernelModules = [ "bcm2835_wdt" ];
       })
 
-      (lib.mkIf (!isPhysical) {
+      (lib.mkIf (!isHardware) {
         boot.kernelModules = [ moduleName ];
+      })
 
+      {
         systemd.settings.Manager = {
           RuntimeWatchdogSec = "30s";
           RebootWatchdogSec = "5min";
         };
-      })
+      }
     ]
   );
 }
