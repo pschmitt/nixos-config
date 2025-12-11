6b8d6cc5d0a1d49d2de133ee907186145f9f2a3f
Philipp Schmitt <philipp@schmitt.co>
2025-12-11 09:41:02 +0100

    fmt

--- modules/hardware.nix
+++ modules/hardware.nix
@@ -1,53 +1,50 @@
 { lib, ... }:
 {
   options = {
-    hardware =
-      let
-        deviceType = lib.types.nullOr (
+    hardware = {
+      type = lib.mkOption {
+        type = lib.types.nullOr (
           lib.types.enum [
-            "laptop"
-            "server"
             "installation-media"
             "rpi"
+
+            "laptop"
+            "server"
           ]
         );
-      in
-      {
-        type = lib.mkOption {
-          type = deviceType;
-          default = null;
-          description = "Device category for this host.";
-        };
-
-        biosBoot = lib.mkOption {
-          type = lib.types.bool;
-          default = false;
-          description = "Use BIOS instead of UEFI";
-        };
-
-        kvmGuest = lib.mkOption {
-          type = lib.types.bool;
-          default = true;
-          description = "Whether this is cloud-based server";
-        };
-
-        highDpi = lib.mkOption {
-          type = lib.types.bool;
-          default = false;
-          description = "Whether this host has a high DPI screen";
-        };
-
-        server = lib.mkOption {
-          type = lib.types.bool;
-          default = false;
-          description = "Whether or not this is a server";
-        };
-
-        cattle = lib.mkOption {
-          type = lib.types.bool;
-          default = true;
-          description = "Whether this is a cattle/throw-away server";
-        };
+        default = null;
+        description = "Device category for this host.";
       };
+
+      biosBoot = lib.mkOption {
+        type = lib.types.bool;
+        default = false;
+        description = "Use BIOS instead of UEFI";
+      };
+
+      cattle = lib.mkOption {
+        type = lib.types.bool;
+        default = true;
+        description = "Whether this is a cattle/throw-away server";
+      };
+
+      highDpi = lib.mkOption {
+        type = lib.types.bool;
+        default = false;
+        description = "Whether this host has a high DPI screen";
+      };
+
+      kvmGuest = lib.mkOption {
+        type = lib.types.bool;
+        default = true;
+        description = "Whether this is cloud-based server";
+      };
+
+      server = lib.mkOption {
+        type = lib.types.bool;
+        default = false;
+        description = "Whether or not this is a server";
+      };
+    };
   };
 }
