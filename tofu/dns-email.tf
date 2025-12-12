5d53f01e3c9b34875f93b8844b9d916c7aaecc5a
Philipp Schmitt <philipp@schmitt.co>
2025-12-12 22:31:02 +0100

    remove data.priority

--- tofu/dns-email.tf
+++ tofu/dns-email.tf
@@ -250,7 +250,6 @@ resource "cloudflare_dns_record" "srv-autodiscover" {
   comment = var.dns_email_comment
 
   data = {
-    priority = 0
     weight   = 0
     port     = 443
     target   = "autoconfig.${each.key}"
@@ -269,7 +268,6 @@ resource "cloudflare_dns_record" "srv-imap" { # starttls
   comment = var.dns_email_comment
 
   data = {
-    priority = 0
     weight   = 0
     port     = 143
     target   = each.value.mx_provider == "google" ? local.imap_google : var.main_mail_domain
@@ -288,7 +286,6 @@ resource "cloudflare_dns_record" "srv-imaps" {
   comment = var.dns_email_comment
 
   data = {
-    priority = 0
     weight   = 0
     port     = 993
     target   = each.value.mx_provider == "google" ? local.imap_google : var.main_mail_domain
@@ -307,7 +304,6 @@ resource "cloudflare_dns_record" "srv-imaps" {
 #   comment = var.dns_email_comment
 #
 #   data = {
-#     priority = 0
 #     weight   = 0
 #     port     = 995
 #     target   = each.value.mx_provider == "google" ? local.pop_google : var.main_mail_domain
@@ -326,7 +322,6 @@ resource "cloudflare_dns_record" "srv-submission" { # starttls
   comment = var.dns_email_comment
 
   data = {
-    priority = 0
     weight   = 0
     port     = 587
     target   = each.value.mx_provider == "google" ? local.smtp_google : var.main_mail_domain
@@ -345,7 +340,6 @@ resource "cloudflare_dns_record" "srv-submissions" {
   comment = var.dns_email_comment
 
   data = {
-    priority = 0
     weight   = 0
     port     = 465
     target   = each.value.mx_provider == "google" ? local.smtp_google : var.main_mail_domain
