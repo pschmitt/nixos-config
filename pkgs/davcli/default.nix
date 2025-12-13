3ed5835b743c9897953ad2175cd46f046e4b7fd4
Philipp Schmitt <philipp@schmitt.co>
2025-12-13 21:10:09 +0100

    fix nix-update for davcli

--- pkgs/davcli/default.nix
+++ pkgs/davcli/default.nix
@@ -4,27 +4,39 @@
   fetchFromSourcehut,
   stdenv,
   darwin,
+  nix-update-script,
 }:
 
-rustPlatform.buildRustPackage {
+rustPlatform.buildRustPackage rec {
   pname = "davcli";
-  version = "0.1.0";
+  version = "unstable-${lib.strings.substring 0 10 rev}";
+  rev = "1dd3673ef82660e6c15640dbcd517889cb691277";
 
   src = fetchFromSourcehut {
     owner = "~whynothugo";
     repo = "davcli";
-    rev = "main";
+    inherit rev;
     hash = "sha256-FEPWykjbJz/ANtxpbzkhNUK+ycfqD2u9scXKmmqcSfs=";
   };
 
   cargoHash = "sha256-qWKXAqlHy3mD1eoSdxKTbPcBXnnPASuSIxBvcwg9fkA=";
+
+  passthru.updateScript = nix-update-script {
+    extraArgs = [
+      "--flake"
+      "--version"
+      "branch"
+      "--version-regex"
+      "(?:0-)?(unstable-[0-9]{4}-[0-9]{2}-[0-9]{2})"
+    ];
+  };
 
   buildInputs = lib.optionals stdenv.isDarwin [
     darwin.apple_sdk.frameworks.Security
   ];
 
   meta = {
-    description = "";
+    description = "Command line CalDav and CardDav client";
     homepage = "https://git.sr.ht/~whynothugo/davcli";
     license = lib.licenses.isc;
     maintainers = with lib.maintainers; [ pschmitt ];
