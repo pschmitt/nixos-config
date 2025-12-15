b8134590566deebcf1ea9e609ac56a1376f57d48
Philipp Schmitt <philipp@schmitt.co>
2025-12-15 09:23:51 +0100

    simplify

--- common/global/appimage.nix
+++ common/global/appimage.nix
@@ -1,18 +1,10 @@
+{ pkgs, ... }:
 {
-  lib,
-  pkgs,
-  ...
-}:
-
-let
-  # XXX Disable AppImage support on non-x86_64 platforms for now
-  # Blame:
-  # https://github.com/NixOS/nixpkgs/commit/d151f99d0f0c460b2f5344c58a4e1b759b7dffcd
-  enableAppImage = pkgs.stdenv.hostPlatform.isx86_64;
-in
-lib.mkIf enableAppImage {
   programs.appimage = {
-    enable = true;
+    # XXX Disable AppImage support on non-x86_64 platforms for now
+    # Blame:
+    # https://github.com/NixOS/nixpkgs/commit/d151f99d0f0c460b2f5344c58a4e1b759b7dffcd
+    enable = pkgs.stdenv.hostPlatform.isx86_64;
     binfmt = true;
   };
 }
