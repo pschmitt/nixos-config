91ee7e2f67756c9a925abcaf442d61b607f66677
Philipp Schmitt <philipp@schmitt.co>
2025-12-15 10:28:00 +0100

    update clipcascade pkg

--- home-manager/gui/clipcascade.nix
+++ home-manager/gui/clipcascade.nix
@@ -1,19 +1,4 @@
-{ pkgs, lib, ... }:
-let
-  giTypelibs = lib.makeSearchPath "lib/girepository-1.0" [
-    pkgs.gdk-pixbuf
-    pkgs.gobject-introspection
-    pkgs.gtk3
-    pkgs.libayatana-appindicator
-  ];
-
-  xdgDataDirs = lib.makeSearchPath "share" [
-    pkgs.gsettings-desktop-schemas
-    pkgs.gtk3
-    pkgs.libayatana-appindicator
-    pkgs.shared-mime-info
-  ];
-in
+{ pkgs, ... }:
 {
   systemd.user.services.clipcascade = {
     Unit = {
@@ -30,11 +15,6 @@ in
       ExecStart = "${pkgs.clipcascade}/bin/clipcascade";
       Restart = "on-failure";
       RestartSec = 5;
-      Environment = [
-        "PYTHONUNBUFFERED=1"
-        "GI_TYPELIB_PATH=${giTypelibs}"
-        "XDG_DATA_DIRS=${xdgDataDirs}"
-      ];
     };
 
     Install.WantedBy = [ "graphical-session.target" ];
