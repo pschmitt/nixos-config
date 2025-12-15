e549034515b4e6d016df04fe4fa85cccf487f391
Philipp Schmitt <philipp@schmitt.co>
2025-12-15 09:19:36 +0100

    use programs.appimage

--- common/global/appimage.nix
+++ common/global/appimage.nix
@@ -11,15 +11,8 @@ let
   enableAppImage = pkgs.stdenv.hostPlatform.isx86_64;
 in
 lib.mkIf enableAppImage {
-  environment.systemPackages = [ pkgs.appimage-run ];
-
-  # https://nixos.wiki/wiki/Appimage
-  boot.binfmt.registrations.appimage = {
-    wrapInterpreterInShell = false;
-    interpreter = "${pkgs.appimage-run}/bin/appimage-run";
-    recognitionType = "magic";
-    offset = 0;
-    mask = "\\xff\\xff\\xff\\xff\\x00\\x00\\x00\\x00\\xff\\xff\\xff";
-    magicOrExtension = "\\x7fELF....AI\\x02";
+  programs.appimage = {
+    enable = true;
+    binfmt = true;
   };
 }
