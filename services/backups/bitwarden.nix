5c309e422f7b83478c2dc4b07274fba1413cf00e
Philipp Schmitt <philipp@schmitt.co>
2025-12-15 15:17:14 +0100

    add bw-sync

--- services/backups/bitwarden.nix
+++ services/backups/bitwarden.nix
@@ -9,22 +9,34 @@
     inputs.bw-backup.nixosModules.default
   ];
 
-  sops.secrets."bw-backup" = {
-    inherit (config.custom) sopsFile;
-    owner = "bw-backup";
-    group = "bw-backup";
+  sops.secrets = {
+    "bw-backup" = {
+      inherit (config.custom) sopsFile;
+      owner = config.services.bw-backup.user;
+      inherit (config.services.bw-backup) group;
+    };
+    "bw-sync" = {
+      inherit (config.custom) sopsFile;
+      owner = config.services.bw-sync.user;
+      inherit (config.services.bw-sync) group;
+    };
   };
 
-  bw-backup = {
-    backup = {
+  services = {
+    bw-backup = {
       enable = true;
       backupPath = "/srv/bw-backup/data";
       environmentFiles = [ config.sops.secrets."bw-backup".path ];
       retention = 30;
+      monit = {
+        enable = true;
+        thresholdSeconds = 86400;
+      };
     };
-    monit = {
+
+    bw-sync = {
       enable = true;
-      thresholdSeconds = 86400;
+      environmentFiles = [ config.sops.secrets."bw-sync".path ];
     };
   };
 }
