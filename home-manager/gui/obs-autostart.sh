48f207a94815dd6a3df42ad01aded2a03d4cb5cc
Philipp Schmitt <philipp@schmitt.co>
2025-12-16 16:13:50 +0100

    remove obs flatpak code path

--- home-manager/gui/obs-autostart.sh
+++ home-manager/gui/obs-autostart.sh
@@ -1,49 +1,30 @@
 #!/usr/bin/env bash
 
-obs-studio::command() {
-  # NOTE order matters here!
-  if command -v obs &>/dev/null
-  then
-    echo obs
-    return 0
-  fi
-
-  if flatpak list --user --app | grep -q "com.obsproject.Studio"
-  then
-    echo flatpak run --user com.obsproject.Studio
-    return 0
-  fi
-
-  return 1
-}
-
 # shellcheck disable=SC2120
 obs-studio::start() {
   local start_scene="${1:-🚬 brb}"
-  local obs_args=(--minimize-to-tray --startvirtualcam --scene "$start_scene")
-  local -a obs_cmd=()
+  local obs_bin
 
-  if ! mapfile -t obs_cmd < <(obs-studio::command)
+  if ! obs_bin=$(command -v obs 2>/dev/null) || [[ -z $obs_bin ]]
   then
     echo "OBS Studio is not installed" >&2
     return 1
   fi
 
-  local native_obs
-  if [[ "${obs_cmd[*]}" == "obs" ]]
+  if ! pgrep -af "$(command -v obs)" &>/dev/null
   then
-    native_obs=1
-    if ! pgrep -af "$(command -v obs)" &>/dev/null
-    then
-      rm -vrf ~/.config/obs-studio/.sentinel
-    fi
+    rm -vrf ~/.config/obs-studio/.sentinel
   fi
 
-  obs_cmd+=("${obs_args[@]}")
+  local -a obs_cmd=(
+    "$obs_bin"
+    --minimize-to-tray
+    --startvirtualcam
+    --scene "$start_scene"
+  )
 
   # Use NVIDIA card explicitly for OBS (if available and not targeting flatpak)
-  if [[ -n $native_obs ]] && \
-     [[ -e /dev/dri/card1 ]]
+  if [[ -e /dev/dri/card1 ]]
   then
     obs_cmd=(nvidia-offload "${obs_cmd[@]}")
   fi
