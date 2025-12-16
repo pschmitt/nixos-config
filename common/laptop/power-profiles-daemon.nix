1f3d6fe3a0e041c7b54a7c658eb9b35245310d4a
Philipp Schmitt <philipp@schmitt.co>
2025-12-16 11:47:58 +0100

    wip: ppd handler

---
+++ common/laptop/power-profiles-daemon.nix
@@ -0,0 +1,42 @@
+{
+  lib,
+  pkgs,
+  ...
+}:
+let
+  ppdReact = pkgs.writeShellApplication {
+    name = "ppd-react";
+    runtimeInputs = with pkgs; [
+      gawk
+      jq
+      libnotify
+      systemd
+      util-linux
+    ];
+    text = builtins.readFile ./ppd-react.sh;
+  };
+in
+{
+  # power-profiles-daemon conflicts with tlp
+  # https://linrunner.de/tlp/faq/ppd.html
+  services = {
+    power-profiles-daemon.enable = lib.mkForce true;
+    tlp.enable = lib.mkForce false;
+  };
+
+  systemd.services.ppd-react = {
+    description = "React to power-profiles-daemon profile changes";
+    wantedBy = [ "multi-user.target" ];
+    after = [
+      "dbus.service"
+      "power-profiles-daemon.service"
+    ];
+    wants = [ "power-profiles-daemon.service" ];
+
+    serviceConfig = {
+      ExecStart = "${ppdReact}/bin/ppd-react";
+      Restart = "always";
+      RestartSec = "1s";
+    };
+  };
+}
