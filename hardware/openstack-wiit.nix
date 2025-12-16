b247ef39fb7cc4819041aa1adcd2876ba8657ec7
Philipp Schmitt <philipp@schmitt.co>
2025-12-16 22:04:55 +0100

    haha really fix it

--- hardware/openstack-wiit.nix
+++ hardware/openstack-wiit.nix
@@ -1,56 +1,71 @@
 {
   lib,
   config,
-  modulesPath,
   ...
 }:
-lib.mkIf (config.hardware.serverType == "openstack") {
-  imports = [ (modulesPath + "/profiles/qemu-guest.nix") ];
+{
+  config = lib.mkIf (config.hardware.serverType == "openstack") {
+    hardware.kvmGuest = true;
 
-  hardware.kvmGuest = true;
+    nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+    custom.netbirdSetupKey = lib.mkForce "optimist";
 
-  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-  custom.netbirdSetupKey = lib.mkForce "optimist";
-
-  boot = {
-    initrd = {
-      availableKernelModules = [
-        "ata_piix"
-        "uhci_hcd"
-        "xen_blkfront"
-        "vmw_pvscsi"
-        # Below is required for ssh in initrd
-        "virtio_pci"
-        "virtio_net"
-      ];
-      kernelModules = [ "nvme" ];
+    boot = {
+      initrd = {
+        # From nixpkgs' `profiles/qemu-guest.nix`
+        availableKernelModules = [
+          "virtio_net"
+          "virtio_pci"
+          "virtio_mmio"
+          "virtio_blk"
+          "virtio_scsi"
+          "9p"
+          "9pnet_virtio"
+        ]
+        ++ [
+          "ata_piix"
+          "uhci_hcd"
+          "xen_blkfront"
+          "vmw_pvscsi"
+          # Below is required for ssh in initrd
+          "virtio_pci"
+          "virtio_net"
+        ];
+        kernelModules = [
+          "virtio_balloon"
+          "virtio_console"
+          "virtio_rng"
+          "virtio_gpu"
+          "nvme"
+        ];
+      };
+      supportedFilesystems = [ "btrfs" ];
     };
-    supportedFilesystems = [ "btrfs" ];
-  };
 
-  # TODO Put the respective internalInterfaces values into the netbird.nix
-  # and tailscale.nix files
-  # HACK Fix netbird port forwarding
-  networking.nat = {
-    enable = true;
-    externalInterface = "ens3";
-    # internalIPs = [
-    #   # Netbird subnet
-    #   "100.122.0.0/16"
-    #   # Tailscale
-    #   "100.64.0.0/10"
-    # ];
-    internalInterfaces = [
-      # netbird-netbird-io
-      "netbird-io"
-      # netbird-wiit
-      "wiit"
-      # default nb interface name
-      # "netbird0"
-      "wt0"
+    # TODO Put the respective internalInterfaces values into the netbird.nix
+    # and tailscale.nix files
+    # HACK Fix netbird port forwarding
+    networking.nat = {
+      enable = true;
+      externalInterface = "ens3";
+      # internalIPs = [
+      #   # Netbird subnet
+      #   "100.122.0.0/16"
+      #   # Tailscale
+      #   "100.64.0.0/10"
+      # ];
+      internalInterfaces = [
+        # netbird-netbird-io
+        "netbird-io"
+        # netbird-wiit
+        "wiit"
+        # default nb interface name
+        # "netbird0"
+        "wt0"
 
-      # tailscale default interface name
-      "tailscale0"
-    ];
+        # tailscale default interface name
+        "tailscale0"
+      ];
+    };
   };
 }
