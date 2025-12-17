c03eae412a9dfa40fb22f31de9888895a5955bc7
Philipp Schmitt <philipp@schmitt.co>
2025-12-17 15:11:46 +0100

    back to ssh

---
+++ pkgs/tewi/default.nix
@@ -0,0 +1,72 @@
+{
+  lib,
+  python3,
+  fetchFromGitHub,
+  fetchPypi,
+}:
+
+let
+  geoip2fast = python3.pkgs.buildPythonPackage rec {
+    pname = "geoip2fast";
+    version = "1.2.2";
+    pyproject = true;
+
+    src = fetchPypi {
+      inherit pname version;
+      hash = "sha256-OIFXAM7f6xl9UbS4czsNT3lls23hUUfBJVJxJPi0XWs=";
+    };
+
+    build-system = [
+      python3.pkgs.setuptools
+    ];
+
+    pythonImportsCheck = [
+      "geoip2fast"
+    ];
+  };
+in
+python3.pkgs.buildPythonApplication rec {
+  pname = "tewi";
+  version = "2.1.0";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "anlar";
+    repo = "tewi";
+    rev = "v${version}";
+    hash = "sha256-fl66nIGT0oM95COgqfZpwDZVm4zJQVnFXowEthgiezw=";
+  };
+
+  build-system = [
+    python3.pkgs.setuptools
+  ];
+
+  dependencies = with python3.pkgs; [
+    geoip2fast
+    platformdirs
+    pyperclip
+    qbittorrent-api
+    textual
+    transmission-rpc
+  ];
+
+  optional-dependencies = with python3.pkgs; {
+    dev = [
+      pytest
+      ruff
+    ];
+  };
+
+  pythonImportsCheck = [
+    "tewi"
+  ];
+
+  meta = {
+    description = "Text-based interface for BitTorrent clients (Transmission, qBittorrent, Deluge)";
+    homepage = "https://github.com/anlar/tewi";
+    changelog = "https://github.com/anlar/tewi/blob/v${version}/CHANGELOG.md";
+    license = lib.licenses.gpl3Only;
+    maintainers = with lib.maintainers; [ pschmitt ];
+    mainProgram = "tewi";
+  };
+}
