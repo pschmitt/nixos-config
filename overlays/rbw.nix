e8671102aede6f1d27aec7bd1a389fe917469a65
Philipp Schmitt <philipp@schmitt.co>
2026-01-01 12:33:10 +0100

    remove old hashes

--- overlays/rbw.nix
+++ overlays/rbw.nix
@@ -2,7 +2,6 @@
 {
   rbw = prev.rbw.overrideAttrs (old: rec {
     pname = "rbw";
-    # version = "1.14.1";
     version = "1.15.0";
 
     src = prev.fetchFromGitHub {
@@ -16,7 +15,6 @@
 
     cargoDeps = final.rustPlatform.fetchCargoVendor {
       inherit src;
-      # hash = "sha256-H1DSP3Kyklv8ncn7zDP0njDlwB8Qh+h7mqWRAJcpWrE=";
       hash = "sha256-N4IxnAXDvD+vp3LUB9CKYM+1C5i1Flihk+Pfb2c5IWY=";
     };
   });
