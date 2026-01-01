98fcb276ac16a4045f44e517a4619883001072c0
Philipp Schmitt <philipp@schmitt.co>
2026-01-01 12:32:40 +0100

    rbw 1.15.0

--- overlays/rbw.nix
+++ overlays/rbw.nix
@@ -2,20 +2,22 @@
 {
   rbw = prev.rbw.overrideAttrs (old: rec {
     pname = "rbw";
-    version = "1.14.1";
+    # version = "1.14.1";
+    version = "1.15.0";
 
     src = prev.fetchFromGitHub {
       owner = "pschmitt";
       repo = pname;
-      rev = "json-1.14";
-      hash = "sha256-d44LaEtZD68ucBlfp7Pwd3mvV09U5ITNJSi5LDVwq1Q=";
+      rev = "json-1.15";
+      hash = "sha256-3ZrKXaXal4J/qSl/pXwV1+eIPGQJgLSIGcQ+4XHsJFg=";
     };
 
     # cargoHash = null;
 
     cargoDeps = final.rustPlatform.fetchCargoVendor {
       inherit src;
-      hash = "sha256-H1DSP3Kyklv8ncn7zDP0njDlwB8Qh+h7mqWRAJcpWrE=";
+      # hash = "sha256-H1DSP3Kyklv8ncn7zDP0njDlwB8Qh+h7mqWRAJcpWrE=";
+      hash = "sha256-N4IxnAXDvD+vp3LUB9CKYM+1C5i1Flihk+Pfb2c5IWY=";
     };
   });
 }
