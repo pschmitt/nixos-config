3c86295bf11e13b4a37dfbda41ac604e2ce9aa96
Philipp Schmitt <philipp@schmitt.co>
2026-01-17 09:36:00 +0100

    Update services.resolved config syntax

--- common/network/dns.nix
+++ common/network/dns.nix
@@ -1,12 +1,15 @@
 _: {
   services.resolved = {
     enable = true;
-    dnssec = "false";
-    llmnr = "true";
-    # domains = [ "~." ];
-    fallbackDns = [
-      "1.1.1.1#one.one.one.one"
-      "1.0.0.1#one.one.one.one"
-    ];
+    settings.Resolve = {
+      DNSOverTLS = false;
+      DNSSEC = false;
+      # Domains = [ "~." ];
+      LLMNR = true;
+      FallbackDNS = [
+        "1.1.1.1#one.one.one.one"
+        "1.0.0.1#one.one.one.one"
+      ];
+    };
   };
 }
