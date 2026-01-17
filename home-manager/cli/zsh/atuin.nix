5d8897037aa7c902f2a9c0ce9a4aea58233c973f
Philipp Schmitt <philipp@schmitt.co>
2026-01-17 10:56:50 +0100

    atuin client config

--- home-manager/cli/zsh/atuin.nix
+++ home-manager/cli/zsh/atuin.nix
@@ -1,12 +1,22 @@
 {
   lib,
+  osConfig,
   pkgs,
   ...
 }:
 {
-  home.packages = with pkgs; [
-    atuin
-  ];
+  programs.atuin = {
+    enable = true;
+    enableZshIntegration = false; # We manage this manually below
+    settings = {
+      dialect = "uk";
+      auto_sync = true;
+      update_check = true;
+      sync_address = "https://atuin.${osConfig.domains.main}";
+      sync_frequency = "1h";
+      search_mode = "fuzzy";
+    };
+  };
 
   xdg.configFile."zsh/custom/os/nixos/system.zsh".text = lib.mkAfter ''
     # atuin
