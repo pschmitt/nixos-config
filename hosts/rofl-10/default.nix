060e9a3eb347cbc925e3a05d8fd8feab2f3035b2
Philipp Schmitt <philipp@schmitt.co>
2026-01-18 18:36:00 +0100

    remove open-webui

--- hosts/rofl-10/default.nix
+++ hosts/rofl-10/default.nix
@@ -26,7 +26,6 @@
     ../../services/luks-ssh-unlock-homelab.nix
     ../../services/mealie.nix
     ../../services/n8n.nix
-    ../../services/open-webui.nix
     ../../services/paperless-ngx.nix
     ../../services/pinchflat.nix
     ../../services/podsync.nix
