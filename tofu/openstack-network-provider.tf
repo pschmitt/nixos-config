a4279bcc4cc35ec6e260835d27a81187a506f68e
Philipp Schmitt <philipp@schmitt.co>
2026-01-19 14:22:17 +0100

    Remove legacy openstack provider (optimist)

--- tofu/openstack-network-provider.tf
+++ tofu/openstack-network-provider.tf
@@ -1,19 +1,3 @@
-resource "openstack_networking_addressscope_v2" "bgpv6_legacy" {
-  provider   = openstack.optimist-legacy
-  name       = "bgpv6"
-  ip_version = 6
-  shared     = true
-}
-
-resource "openstack_networking_subnetpool_v2" "customer_ipv6_legacy" {
-  provider         = openstack.optimist-legacy
-  name             = "customer-ipv6"
-  prefixes         = ["2a00:c320:1000::/48"]
-  address_scope_id = openstack_networking_addressscope_v2.bgpv6_legacy.id
-  is_default       = true
-  shared           = true
-}
-
 # resource "openstack_networking_addressscope_v2" "bgpv6" {
 #   provider   = openstack.openstack-wiit
 #   name       = "bgpv6"
