1eb9e0655189274a595cbb6040416129df0dde0b
Philipp Schmitt <philipp@schmitt.co>
2026-01-24 13:56:13 +0100

    psql 17

--- services/postgresql.nix
+++ services/postgresql.nix
@@ -19,7 +19,7 @@ in
 
   services = {
     postgresql = {
-      package = pkgs.postgresql_16;
+      package = pkgs.postgresql_17;
       dataDir = "${psqlDir}/${config.services.postgresql.package.psqlSchema}";
     };
 
@@ -39,10 +39,13 @@ in
       let
         # XXX specify the postgresql package you'd like to upgrade to.
         # Do not forget to list the extensions you need.
-        newPostgres = pkgs.postgresql_16.withPackages (pp: [
-          # immich uses (used?) pgvecto-rs
-          pp.pgvecto-rs
-        ]);
+        # newPostgres = pkgs.postgresql_17.withPackages (pp: [
+        #   # immich used to use pgvecto-rs
+        #   # pp.pgvecto-rs
+        #   pp.pgvector
+        #   pp.vectorchord
+        # ]);
+        newPostgres = pkgs.postgresql_17.withPackages config.services.postgresql.extensions;
         cfg = config.services.postgresql;
       in
       pkgs.writeScriptBin "upgrade-pg-cluster" ''
@@ -67,7 +70,7 @@ in
         sudo -u postgres "$NEWBIN/pg_upgrade" \
           --old-datadir "$OLDDATA" --new-datadir "$NEWDATA" \
           --old-bindir "$OLDBIN" --new-bindir "$NEWBIN" \
-          --new-options "-c shared_preload_libraries='vectors.so'" \
+          --new-options "-c shared_preload_libraries='vchord.so'" \
           "$@"
       ''
     )
