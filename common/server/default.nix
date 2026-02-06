caa1d409730600ffce50fa216684a99746e68e34
Philipp Schmitt <philipp@schmitt.co>
2026-02-06 15:17:27 +0100

    explictily set global dns resolver for servers

--- common/server/default.nix
+++ common/server/default.nix
@@ -54,6 +54,20 @@
 
   networking.useNetworkd = lib.mkDefault true;
 
+  # Prefer Cloudflare DNS on servers (also used by some container modules that
+  # read `config.networking.nameservers` to populate container resolv.conf).
+  networking.nameservers = lib.mkDefault [
+    "1.1.1.1"
+    "1.0.0.1"
+  ];
+
+  # Make systemd-resolved use Cloudflare as its primary resolvers (not only as
+  # fallback).
+  services.resolved.settings.Resolve.DNS = [
+    "1.1.1.1#one.one.one.one"
+    "1.0.0.1#one.one.one.one"
+  ];
+
   programs.nix-index-database.comma.enable = true;
 
   environment.systemPackages = with pkgs; [
