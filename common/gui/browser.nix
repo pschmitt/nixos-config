d6a6aab26f98f666c9544516b96183e251b30444
Philipp Schmitt <philipp@schmitt.co>
2026-02-06 14:08:41 +0100

    set about:config vals using hm

--- common/gui/browser.nix
+++ common/gui/browser.nix
@@ -1,27 +1,28 @@
 { pkgs, ... }:
 {
 
+  # NOTE see also home-manager/gui/browser.nix
   programs.firefox = {
     enable = true;
-    # FIXME This does not seem to work.
-    # See home-manager/home.nix for the dirty but working solution.
-    nativeMessagingHosts.packages = with pkgs; [
-      brotab
-      tridactyl-native
-    ];
-    preferences = {
-      # Enable custom css (userChrome.css)
-      "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
-      # Hide share indicator
-      "privacy.webrtc.legacyGlobalIndicator" = false;
-      "privacy.webrtc.hideGlobalIndicator" = true;
-      # Prevent Firefox from Googling .lan addresses and opening them directly
-      "browser.fixup.domainsuffixwhitelist.lan" = true;
-      # Hide the "Summarize Page" button in the AI sidebar
-      # https://www.reddit.com/r/firefox/comments/1o8zt65/how_do_i_remove_the_summarize_page_button_below/
-      "browser.ml.chat.page" = false;
-    };
-    preferencesStatus = "user";
+    # nativeMessagingHosts.packages = with pkgs; [
+    #   brotab
+    #   tridactyl-native
+    # ];
+    #
+    # preferences = {
+    #   # Enable custom css (userChrome.css)
+    #   "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
+    #   # Hide share indicator
+    #   "privacy.webrtc.legacyGlobalIndicator" = false;
+    #   "privacy.webrtc.hideGlobalIndicator" = true;
+    #   # Prevent Firefox from Googling .lan addresses and opening them directly
+    #   "browser.fixup.domainsuffixwhitelist.lan" = true;
+    #   # Hide the "Summarize Page" button in the AI sidebar
+    #   # https://www.reddit.com/r/firefox/comments/1o8zt65/how_do_i_remove_the_summarize_page_button_below/
+    #   "browser.ml.chat.page" = false;
+    # };
+    #
+    # preferencesStatus = "locked";
   };
 
   environment.systemPackages = with pkgs; [
