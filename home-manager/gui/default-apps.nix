ca5f1cba5f59decfe7450e6c902fb2ccf6b6c4e9
Philipp Schmitt <philipp@schmitt.co>
2026-02-08 09:33:02 +0100

    open images in eog by default

--- home-manager/gui/default-apps.nix
+++ home-manager/gui/default-apps.nix
@@ -10,6 +10,16 @@
 
       # FIXME This does not seem to work?
       "application/pdf" = "org.gnome.Evince.desktop";
+
+      # Image viewers
+      "image/jpeg" = "org.gnome.eog.desktop";
+      "image/png" = "org.gnome.eog.desktop";
+      "image/gif" = "org.gnome.eog.desktop";
+      "image/bmp" = "org.gnome.eog.desktop";
+      "image/webp" = "org.gnome.eog.desktop";
+      "image/tiff" = "org.gnome.eog.desktop";
+      "image/x-portable-pixmap" = "org.gnome.eog.desktop";
+      "image/x-xbitmap" = "org.gnome.eog.desktop";
     };
   };
 }
