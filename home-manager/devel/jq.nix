2bb5de3c4ad0b867e4d512b148680d4f38424fdb
Philipp Schmitt <philipp@schmitt.co>
2026-02-10 16:12:33 +0100

    add mq

--- home-manager/devel/jq.nix
+++ home-manager/devel/jq.nix
@@ -1,9 +1,10 @@
-{ pkgs, ... }:
+{ inputs, pkgs, ... }:
 {
   home.packages = with pkgs; [
     jc
     jq
     jsonrepair
+    inputs.mq.packages.${pkgs.stdenv.hostPlatform.system}.mq
     yq-go
   ];
 }
