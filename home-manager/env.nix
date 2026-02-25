c6e63bf3829f0005f288ddd6136947f7f679649c
Philipp Schmitt <philipp@schmitt.co>
2026-02-25 12:37:56 +0100

    add xdg dirs

--- home-manager/env.nix
+++ home-manager/env.nix
@@ -2,6 +2,18 @@
 
 {
   xdg.enable = true;
+  xdg.userDirs = {
+    enable = true;
+    createDirectories = true;
+    desktop = "${config.home.homeDirectory}/Desktop";
+    documents = "${config.home.homeDirectory}/Documents";
+    download = "${config.home.homeDirectory}/Downloads";
+    music = "${config.home.homeDirectory}/Music";
+    pictures = "${config.home.homeDirectory}/Pictures";
+    publicShare = "${config.home.homeDirectory}/Public";
+    templates = "${config.home.homeDirectory}/Templates";
+    videos = "${config.home.homeDirectory}/Videos";
+  };
 
   home = {
     sessionPath = [
