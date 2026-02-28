47f2f5144848d215a4d4325716c29c6cf5893547
Philipp Schmitt <philipp@schmitt.co>
2026-02-28 10:41:59 +0100

    update hyprdynamicmonitor config

--- home-manager/gui/hyprland/services/hyprdynamicmonitors.nix
+++ home-manager/gui/hyprland/services/hyprdynamicmonitors.nix
@@ -83,7 +83,7 @@ let
 
   tmpl = name: text: pkgs.writeText name text;
 
-  inherit (osConfig.networking) hostName;
+  hostName = if osConfig != null then osConfig.networking.hostName or "" else "";
   isGk4 = hostName == "gk4";
 
   laptopMonitorAutoSettings =
@@ -91,76 +91,6 @@ let
   laptopMonitorOriginSettings =
     if isGk4 then "preferred,0x0,1.666,transform,3" else "preferred,0x0,1";
   lenovoRightOfLaptopPosition = if isGk4 then "1600x0" else "1920x0";
-
-  laptopTemplate = tmpl "laptop.go.tmpl" ''
-    {{- $laptop := index .MonitorsByTag "laptop" -}}
-    monitor={{$laptop.Name}},${laptopMonitorAutoSettings}
-    {{- range .ExtraMonitors }}
-    monitor={{.Name}},disable
-    {{- end }}
-  '';
-
-  laptopEdpM14Template = tmpl "laptop-edp-m14.go.tmpl" ''
-    {{- $laptop := index .MonitorsByTag "laptop" -}}
-    {{- $lenovo := index .MonitorsByTag "lenovo_m14" -}}
-    monitor={{$laptop.Name}},${laptopMonitorOriginSettings}
-    monitor={{$lenovo.Name}},preferred,${lenovoRightOfLaptopPosition},1
-    {{- range .ExtraMonitors }}
-    monitor={{.Name}},disable
-    {{- end }}
-  '';
-
-  dualDisplayTemplate = tmpl "docked-dual.go.tmpl" ''
-    {{- $lenovo := index .MonitorsByTag "lenovo_m14" -}}
-    {{- $lg := index .MonitorsByTag "lg_wqhd" -}}
-    monitor={{$lg.Name}},3440x1440@60,0x0,1
-    monitor={{$lenovo.Name}},1920x1080@60,-1920x0,1
-    {{- range .ExtraMonitors }}
-    monitor={{.Name}},disable
-    {{- end }}
-  '';
-
-  dualDisplayNoInternalTemplate = tmpl "dual-display-no-internal.go.tmpl" ''
-    {{- $lenovo := index .MonitorsByTag "lenovo_m14" -}}
-    {{- $lg := index .MonitorsByTag "lg_wqhd" -}}
-    {{- $laptop := index .MonitorsByTag "laptop" -}}
-    monitor={{$lg.Name}},3440x1440@60,0x0,1
-    monitor={{$lenovo.Name}},1920x1080@60,-1920x0,1
-    monitor={{$laptop.Name}},disable
-    {{- range .ExtraMonitors }}
-    monitor={{.Name}},disable
-    {{- end }}
-  '';
-
-  dualDisplayPiKvmTemplate = tmpl "dual-display-pikvm.go.tmpl" ''
-    {{- $lenovo := index .MonitorsByTag "lenovo_m14" -}}
-    {{- $lg := index .MonitorsByTag "lg_wqhd" -}}
-    {{- $pikvm := index .MonitorsByTag "pikvm" -}}
-    monitor={{$lg.Name}},3440x1440@60,0x0,1
-    monitor={{$lenovo.Name}},1920x1080@60,-1920x0,1
-    monitor={{$pikvm.Name}},disable
-    {{- range .ExtraMonitors }}
-    monitor={{.Name}},disable
-    {{- end }}
-  '';
-
-  dualDisplayPiKvmLaptopTemplate = tmpl "dual-display-pikvm-with-laptop.go.tmpl" ''
-    {{- $lenovo := index .MonitorsByTag "lenovo_m14" -}}
-    {{- $lg := index .MonitorsByTag "lg_wqhd" -}}
-    {{- $pikvm := index .MonitorsByTag "pikvm" -}}
-    {{- $laptop := index .MonitorsByTag "laptop" -}}
-    monitor={{$lg.Name}},3440x1440@60,0x0,1
-    monitor={{$lenovo.Name}},1920x1080@60,-1920x0,1
-    monitor={{$pikvm.Name}},disable
-    monitor={{$laptop.Name}},disable
-    {{- range .ExtraMonitors }}
-    monitor={{.Name}},disable
-    {{- end }}
-  '';
-
-  fallbackConfig = tmpl "hyprdynamicmonitors-fallback.conf" ''
-    monitor=,preferred,auto,1
-  '';
 
   callbackCommand = profile: "${callbackScriptEscaped} set ${escapeShellArg profile}";
 
@@ -171,166 +101,184 @@ let
       monitor_tag = tag;
       match_description_using_regex = true;
     };
+
+  monitorTags = {
+    laptop = {
+      name = "eDP-1";
+      monitor_tag = "laptop";
+    };
+    lenovo_m14 = regexMonitor {
+      pattern = "Lenovo.* M14.*";
+      tag = "lenovo_m14";
+    };
+    lg_wqhd = regexMonitor {
+      pattern = "LG Electronics .* WQHD .*";
+      tag = "lg_wqhd";
+    };
+    pikvm = regexMonitor {
+      pattern = ".*(PiKVM|Synaptics Inc).*";
+      tag = "pikvm";
+    };
+  };
+
+  # Helper to create templates with common suffix to disable extra monitors
+  mkTmpl =
+    name: content:
+    tmpl "${name}.go.tmpl" ''
+      ${content}
+      {{- range .ExtraMonitors }}
+      monitor={{.Name}},disable
+      {{- end }}
+    '';
+
+  fallbackConfig = tmpl "hyprdynamicmonitors-fallback.conf" ''
+    monitor=,preferred,auto,1
+  '';
+
+  # Profile configurations: name -> { tags, content }
+  profileConfigs = {
+    "laptop" = {
+      tags = [ "laptop" ];
+      content = ''
+        {{- $laptop := index .MonitorsByTag "laptop" -}}
+        monitor={{$laptop.Name}},${laptopMonitorAutoSettings}
+      '';
+    };
+    "laptop-edp-m14" = {
+      tags = [
+        "laptop"
+        "lenovo_m14"
+      ];
+      content = ''
+        {{- $laptop := index .MonitorsByTag "laptop" -}}
+        {{- $lenovo := index .MonitorsByTag "lenovo_m14" -}}
+        monitor={{$laptop.Name}},${laptopMonitorOriginSettings}
+        monitor={{$lenovo.Name}},preferred,${lenovoRightOfLaptopPosition},1
+      '';
+    };
+    "laptop-lg-wqhd" = {
+      tags = [
+        "laptop"
+        "lg_wqhd"
+      ];
+      content = ''
+        {{- $laptop := index .MonitorsByTag "laptop" -}}
+        {{- $lg := index .MonitorsByTag "lg_wqhd" -}}
+        monitor={{$lg.Name}},3440x1440@60,0x0,1
+        monitor={{$laptop.Name}},${laptopMonitorAutoSettings}
+      '';
+    };
+    "dual-display" = {
+      tags = [
+        "lenovo_m14"
+        "lg_wqhd"
+      ];
+      content = ''
+        {{- $lenovo := index .MonitorsByTag "lenovo_m14" -}}
+        {{- $lg := index .MonitorsByTag "lg_wqhd" -}}
+        monitor={{$lg.Name}},3440x1440@60,0x0,1
+        monitor={{$lenovo.Name}},1920x1080@60,-1920x0,1
+      '';
+    };
+    "dual-display-pikvm" = {
+      tags = [
+        "lenovo_m14"
+        "lg_wqhd"
+        "pikvm"
+      ];
+      content = ''
+        {{- $lenovo := index .MonitorsByTag "lenovo_m14" -}}
+        {{- $lg := index .MonitorsByTag "lg_wqhd" -}}
+        {{- $pikvm := index .MonitorsByTag "pikvm" -}}
+        monitor={{$lg.Name}},3440x1440@60,0x0,1
+        monitor={{$lenovo.Name}},1920x1080@60,-1920x0,1
+        monitor={{$pikvm.Name}},disable
+      '';
+    };
+    "dual-display-pikvm-with-internal" = {
+      tags = [
+        "lenovo_m14"
+        "lg_wqhd"
+        "pikvm"
+        "laptop"
+      ];
+      content = ''
+        {{- $lenovo := index .MonitorsByTag "lenovo_m14" -}}
+        {{- $lg := index .MonitorsByTag "lg_wqhd" -}}
+        {{- $pikvm := index .MonitorsByTag "pikvm" -}}
+        {{- $laptop := index .MonitorsByTag "laptop" -}}
+        monitor={{$lg.Name}},3440x1440@60,0x0,1
+        monitor={{$lenovo.Name}},1920x1080@60,-1920x0,1
+        monitor={{$pikvm.Name}},disable
+        monitor={{$laptop.Name}},disable
+      '';
+    };
+    "dual-display-no-internal" = {
+      tags = [
+        "lenovo_m14"
+        "lg_wqhd"
+        "laptop"
+      ];
+      content = ''
+        {{- $lenovo := index .MonitorsByTag "lenovo_m14" -}}
+        {{- $lg := index .MonitorsByTag "lg_wqhd" -}}
+        {{- $laptop := index .MonitorsByTag "laptop" -}}
+        monitor={{$lg.Name}},3440x1440@60,0x0,1
+        monitor={{$lenovo.Name}},1920x1080@60,-1920x0,1
+        monitor={{$laptop.Name}},disable
+      '';
+    };
+  };
+
+  profiles = lib.mapAttrs (name: cfg: {
+    config_file = "hyprconfigs/${name}.go.tmpl";
+    config_file_type = "template";
+    post_apply_exec = callbackCommand name;
+    conditions.required_monitors = map (tag: monitorTags.${tag}) cfg.tags;
+  }) profileConfigs;
+
+  extraFiles =
+    (lib.mapAttrs' (
+      name: cfg:
+      lib.nameValuePair "hyprdynamicmonitors/hyprconfigs/${name}.go.tmpl" (mkTmpl name cfg.content)
+    ) profileConfigs)
+    // {
+      "hyprdynamicmonitors/hyprconfigs/default.conf" = fallbackConfig;
+    };
 in
 {
   imports = [ inputs.hyprdynamicmonitors.homeManagerModules.hyprdynamicmonitors ];
 
-  home =
-    let
-      profilesCommon = {
-        "laptop" = {
-          config_file = "hyprconfigs/laptop.go.tmpl";
-          config_file_type = "template";
-          post_apply_exec = callbackCommand "laptop";
-          conditions.required_monitors = [
-            {
-              name = "eDP-1";
-              monitor_tag = "laptop";
-            }
-          ];
-        };
-        "laptop-edp-m14" = {
-          config_file = "hyprconfigs/laptop-edp-m14.go.tmpl";
-          config_file_type = "template";
-          post_apply_exec = callbackCommand "laptop-edp-m14";
-          conditions.required_monitors = [
-            {
-              name = "eDP-1";
-              monitor_tag = "laptop";
-            }
-            (regexMonitor {
-              pattern = "Lenovo.* M14.*";
-              tag = "lenovo_m14";
-            })
-          ];
-        };
-
-        "dual-display" = {
-          config_file = "hyprconfigs/docked-dual.go.tmpl";
-          config_file_type = "template";
-          post_apply_exec = callbackCommand "dual-display";
-          conditions.required_monitors = [
-            (regexMonitor {
-              pattern = "Lenovo.* M14.*";
-              tag = "lenovo_m14";
-            })
-            (regexMonitor {
-              pattern = "LG Electronics .* WQHD .*";
-              tag = "lg_wqhd";
-            })
-          ];
-        };
-
-        "dual-display-pikvm" = {
-          config_file = "hyprconfigs/dual-display-pikvm.go.tmpl";
-          config_file_type = "template";
-          post_apply_exec = callbackCommand "dual-display-pikvm";
-          conditions.required_monitors = [
-            (regexMonitor {
-              pattern = "Lenovo.* M14.*";
-              tag = "lenovo_m14";
-            })
-            (regexMonitor {
-              pattern = "LG Electronics .* WQHD .*";
-              tag = "lg_wqhd";
-            })
-            (regexMonitor {
-              pattern = ".*(PiKVM|Synaptics Inc).*";
-              tag = "pikvm";
-            })
-          ];
-        };
-
-        "dual-display-pikvm-with-internal" = {
-          config_file = "hyprconfigs/dual-display-pikvm-with-laptop.go.tmpl";
-          config_file_type = "template";
-          post_apply_exec = callbackCommand "dual-display-pikvm-with-internal";
-          conditions.required_monitors = [
-            (regexMonitor {
-              pattern = "Lenovo.* M14.*";
-              tag = "lenovo_m14";
-            })
-            (regexMonitor {
-              pattern = "LG Electronics .* WQHD .*";
-              tag = "lg_wqhd";
-            })
-            (regexMonitor {
-              pattern = ".*(PiKVM|Synaptics Inc).*";
-              tag = "pikvm";
-            })
-            {
-              name = "eDP-1";
-              monitor_tag = "laptop";
-            }
-          ];
-        };
-
-        "dual-display-no-internal" = {
-          config_file = "hyprconfigs/dual-display-no-internal.go.tmpl";
-          config_file_type = "template";
-          post_apply_exec = callbackCommand "dual-display-no-internal";
-          conditions.required_monitors = [
-            (regexMonitor {
-              pattern = "Lenovo.* M14.*";
-              tag = "lenovo_m14";
-            })
-            (regexMonitor {
-              pattern = "LG Electronics .* WQHD .*";
-              tag = "lg_wqhd";
-            })
-            {
-              name = "eDP-1";
-              monitor_tag = "laptop";
-            }
-          ];
-        };
-      };
-
-      profiles = profilesCommon;
-
-      extraFilesCommon = {
-        "hyprdynamicmonitors/hyprconfigs/laptop.go.tmpl" = laptopTemplate;
-        "hyprdynamicmonitors/hyprconfigs/laptop-edp-m14.go.tmpl" = laptopEdpM14Template;
-        "hyprdynamicmonitors/hyprconfigs/docked-dual.go.tmpl" = dualDisplayTemplate;
-        "hyprdynamicmonitors/hyprconfigs/dual-display-pikvm.go.tmpl" = dualDisplayPiKvmTemplate;
-        "hyprdynamicmonitors/hyprconfigs/dual-display-pikvm-with-laptop.go.tmpl" =
-          dualDisplayPiKvmLaptopTemplate;
-        "hyprdynamicmonitors/hyprconfigs/dual-display-no-internal.go.tmpl" = dualDisplayNoInternalTemplate;
-        "hyprdynamicmonitors/hyprconfigs/default.conf" = fallbackConfig;
-      };
-
-      extraFiles = extraFilesCommon;
-    in
-    {
-      file."${callbackScriptPath}" = {
-        source = callbackScript;
-        executable = true;
-      };
-
-      packages = [
-        hyprdynamicmonitorsPkg
-
-        # Alternative monitor config tools
-        pkgs.kanshi
-        pkgs.shikane # kanshi alternative, rust
-      ];
-
-      hyprdynamicmonitors = {
-        enable = true;
-        package = hyprdynamicmonitorsPkg;
-        configFile = (pkgs.formats.toml { }).generate "hyprdynamicmonitors-config.toml" {
-          general.destination = "${config.xdg.configHome}/hypr/config.d/monitors.conf";
-          inherit profiles;
-          fallback_profile = {
-            config_file = "hyprconfigs/default.conf";
-            config_file_type = "static";
-            post_apply_exec = callbackCommand "default";
-          };
-        };
-
-        inherit extraFiles;
-      };
+  home = {
+    file."${callbackScriptPath}" = {
+      source = callbackScript;
+      executable = true;
     };
+
+    packages = [
+      hyprdynamicmonitorsPkg
+
+      # Alternative monitor config tools
+      pkgs.kanshi
+      pkgs.shikane # kanshi alternative, rust
+    ];
+
+    hyprdynamicmonitors = {
+      enable = true;
+      package = hyprdynamicmonitorsPkg;
+      configFile = (pkgs.formats.toml { }).generate "hyprdynamicmonitors-config.toml" {
+        general.destination = "${config.xdg.configHome}/hypr/config.d/monitors.conf";
+        inherit profiles;
+        fallback_profile = {
+          config_file = "hyprconfigs/default.conf";
+          config_file_type = "static";
+          post_apply_exec = callbackCommand "default";
+        };
+      };
+
+      inherit extraFiles;
+    };
+  };
 
   wayland.windowManager.hyprland.settings.source = [
     # Include HyprDynamicMonitors output so Hyprland uses the generated layout.
