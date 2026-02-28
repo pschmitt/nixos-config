d350f95191c6628a532f88aea2d7f9fe59d7e234
Philipp Schmitt <philipp@schmitt.co>
2026-02-28 10:02:12 +0100

    slightly less hacky nvim-nightly setup

--- home-manager/cli/nvim.nix
+++ home-manager/cli/nvim.nix
@@ -1,41 +1,14 @@
 {
-  config,
   inputs,
   pkgs,
   ...
 }:
 let
-  # The stable (fully wrapped) nvim from Home Manager.
-  nvimStable = config.programs.neovim.finalPackage;
-
   # The unwrapped nightly binary from your flake input.
   nvimNightly = inputs.neovim-nightly.packages.${pkgs.stdenv.hostPlatform.system}.neovim;
 
-  # A second script that copies stable's wrapper environment,
-  # but calls the nightly binary.
-  nvimNightlyWrapped =
-    pkgs.runCommand "nvim-nightly-from-stable"
-      {
-        nativeBuildInputs = [
-          pkgs.coreutils
-          pkgs.gnused
-          pkgs.makeWrapper
-        ];
-      }
-      ''
-        mkdir -p $out/bin
-        cp ${nvimStable}/bin/nvim $out/bin/nvim-nightly
-
-        sed -i "s|${config.programs.neovim.package}|${nvimNightly}|" \
-          $out/bin/nvim-nightly
-      '';
-
-in
-{
-  programs.neovim = {
-    enable = true;
-    # package = inputs.neovim-nightly.packages.${pkgs.stdenv.hostPlatform.system}.neovim;
-
+  # Define common configuration to avoid duplication.
+  commonConfig = {
     withRuby = false;
     withPython3 = false;
 
@@ -101,10 +74,35 @@ in
       taplo
     ];
 
+    extraLuaPackages = ps: [ ps.magick ];
+  };
+
+  # Prepare the configuration for the wrapper.
+  neovimConfig = pkgs.neovimUtils.makeNeovimConfig {
+    inherit (commonConfig) withPython3 withRuby extraLuaPackages;
+    customLuaRC = ''
+      dofile(vim.fn.stdpath("config") .. "/init.lua##class.notnixos")
+    '';
+  };
+
+  # Wrap the nightly binary with the same environment as stable.
+  nvimNightlyWrapped = pkgs.wrapNeovimUnstable nvimNightly (
+    neovimConfig
+    // {
+      wrapperArgs = neovimConfig.wrapperArgs ++ [
+        "--prefix"
+        "PATH"
+        ":"
+        (pkgs.lib.makeBinPath commonConfig.extraPackages)
+      ];
+    }
+  );
+in
+{
+  programs.neovim = commonConfig // {
+    enable = true;
     viAlias = false;
     vimAlias = true;
-
-    extraLuaPackages = ps: [ ps.magick ];
 
     # DIRTYFIX load our regular init.lua (yadm-managed)
     initLua = ''
@@ -112,5 +110,11 @@ in
     '';
   };
 
-  home.packages = [ nvimNightlyWrapped ];
+  # Provide the nvim-nightly command.
+  home.packages = [
+    (pkgs.runCommand "nvim-nightly" { } ''
+      mkdir -p $out/bin
+      ln -s ${nvimNightlyWrapped}/bin/nvim $out/bin/nvim-nightly
+    '')
+  ];
 }
