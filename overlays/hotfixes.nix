ea5dc6b58f4792ec8f6ebd7875e9ab1f974a54d7
Philipp Schmitt <philipp@schmitt.co>
2026-03-05 09:45:36 +0100

    remove old overlays

--- overlays/hotfixes.nix
+++ overlays/hotfixes.nix
@@ -6,24 +6,6 @@
 }:
 
 {
-  # droidcam-obs-patched =
-  #   inputs.droidcam-obs.legacyPackages.${final.system}.obs-studio-plugins.droidcam-obs;
-
-  # TODO Remove once https://github.com/NixOS/nixpkgs/pull/419713 reaches
-  # nixos-unstable
-  # -> https://nixpk.gs/pr-tracker.html?pr=419713
-  # python313 = prev.python313.override {
-  #   packageOverrides = python-final: python-prev: {
-  #     # FIX FAIL: test_host_whitelist_invalid (tests.test_clean.CleanerTest.test_host_whitelist_invalid)
-  #     # See: https://github.com/NixOS/nixpkgs/issues/418689
-  #     # Related PR: https://github.com/NixOS/nixpkgs/pull/419520
-  #     lxml-html-clean = python-prev.lxml-html-clean.overridePythonAttrs (oldAttrs: {
-  #       # disable running tests entirely (simplest fix)
-  #       doCheck = false;
-  #     });
-  #   };
-  # };
-
   # FIX For google-chrome crashing on Hyprland when moving the window from
   # one monitor to another.
   # https://github.com/hyprwm/Hyprland/discussions/11843
