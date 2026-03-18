d14d10cee535dc138494cc4cce4671d7f6f948a6
Philipp Schmitt <philipp@schmitt.co>
2026-03-18 13:50:50 +0100

    remove bw-desktop

--- services/bitwarden.nix
+++ services/bitwarden.nix
@@ -8,7 +8,8 @@
   # NOTE For the biometrics to work, the bitwarden-deskop pkg must be installed
   # as a system package. See:
   # https://github.com/NixOS/nixpkgs/pull/339384
-  environment.systemPackages = with pkgs; [
-    bitwarden-desktop
-  ];
+  # FIXME this fails to build as of Wed Mar 18 01:48:35 PM CET 2026
+  # environment.systemPackages = with pkgs; [
+  #   bitwarden-desktop
+  # ];
 }
