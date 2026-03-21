fd70303343f7ef7e6304f3ed445c306c892b00fb
pschmitt <37886+pschmitt@users.noreply.github.com>
2026-03-21 04:59:11 +0000

    chore: update go-hass-agent from 14.10.xx to 14.10.xx

--- pkgs/go-hass-agent/default.nix
+++ pkgs/go-hass-agent/default.nix
@@ -10,16 +10,16 @@
 
 buildGoModule rec {
   pname = "go-hass-agent";
-  version = "14.10.2";
+  version = "14.10.3";
 
   src = pkgs.fetchFromGitHub {
     owner = "joshuar";
     repo = "go-hass-agent";
     rev = "v${version}";
-    hash = "sha256-PsR3UISsxKZ+Pn3beFrUpTBYy9uppOZS8c2MlV26PJg=";
+    hash = "sha256-7F4zxxMKNrUiKonfO7dQQuODEnFgaFRM7Rzb7n1Erys=";
   };
 
-  vendorHash = "sha256-Wlk/vAy31xvvAB+Q9UUFrXbkwL0CadAm+KzZC20TBXM=";
+  vendorHash = "sha256-WPglpc8xqCW51LmdhGLAuB4jg96T72eRuaS61zagoNw=";
 
   ldflags = [
     "-s"
