c4bd1a2f955187b7e22563adb6f8171880b2913a
Philipp Schmitt <philipp@schmitt.co>
2026-04-11 16:42:46 +0200

    more unfuckering attempts

--- services/netbox.nix
+++ services/netbox.nix
@@ -6,6 +6,25 @@
 }:
 let
   netboxHost = "netbox.${config.domains.main}";
+  pythonFmt = pkgs.formats.pythonVars { };
+  settingsFile = pythonFmt.generate "netbox-settings.py" config.services.netbox.settings;
+  extraConfigFile = pkgs.writeTextFile {
+    name = "netbox-extraConfig.py";
+    text = config.services.netbox.extraConfig;
+  };
+  configFile = pkgs.concatText "configuration.py" [
+    settingsFile
+    extraConfigFile
+  ];
+  netboxPkg =
+    (config.services.netbox.package.overrideAttrs (old: {
+      installPhase = old.installPhase + ''
+        ln -s ${configFile} $out/opt/netbox/netbox/netbox/configuration.py
+      '';
+    })).override
+      {
+        inherit (config.services.netbox) plugins;
+      };
 in
 {
   sops.secrets = {
@@ -125,18 +144,18 @@ in
     # object change rows from plugins can make it fail and block startup.
     versionFile="${config.services.netbox.dataDir}/version"
 
-    if [[ -h "$versionFile" && "$(readlink -- "$versionFile")" == "${config.services.netbox.package.outPath}" ]]
+    if [[ -h "$versionFile" && "$(readlink -- "$versionFile")" == "${netboxPkg}" ]]
     then
       exit 0
     fi
 
-    ${config.services.netbox.package}/bin/netbox migrate
-    ${config.services.netbox.package}/bin/netbox trace_paths --no-input
-    ${config.services.netbox.package}/bin/netbox collectstatic --clear --no-input
-    ${config.services.netbox.package}/bin/netbox reindex --lazy
-    ${config.services.netbox.package}/bin/netbox clearsessions
+    ${netboxPkg}/bin/netbox migrate
+    ${netboxPkg}/bin/netbox trace_paths --no-input
+    ${netboxPkg}/bin/netbox collectstatic --clear --no-input
+    ${netboxPkg}/bin/netbox reindex --lazy
+    ${netboxPkg}/bin/netbox clearsessions
 
-    ln -sfn "${config.services.netbox.package.outPath}" "$versionFile"
+    ln -sfn "${netboxPkg}" "$versionFile"
   '';
 
   # Fix permissions after UID changes (e.g., after reinstall)
