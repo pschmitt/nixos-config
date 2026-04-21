37ae9b40b6dbdba6947cc3fbe4b4dcf720d2e8af
Philipp Schmitt <philipp@schmitt.co>
2026-04-21 15:51:38 +0200

    fix lang

--- home-manager/cli/zsh/default.nix
+++ home-manager/cli/zsh/default.nix
@@ -1,5 +1,6 @@
 {
   config,
+  lib,
   pkgs,
   ...
 }:
@@ -28,6 +29,20 @@
 
   xdg.configFile = {
     "zsh/custom/os/nixos/system.zsh".text = ''
+      if [[ -f "$HOME/.local/state/nix/profiles/home-manager/home-path/etc/profile.d/hm-session-vars.sh" ]]; then
+        source "$HOME/.local/state/nix/profiles/home-manager/home-path/etc/profile.d/hm-session-vars.sh"
+      fi
+
+      # On non-NixOS hosts, prefer the system locale data.
+      if [[ -f /usr/lib/locale/locale-archive ]]; then
+        export LOCALE_ARCHIVE=/usr/lib/locale/locale-archive
+        export NIX_LOCALE_ARCHIVE=/usr/lib/locale/locale-archive
+        unset LOCPATH
+      elif [[ -d /usr/lib/locale ]]; then
+        export LOCPATH=/usr/lib/locale
+        unset LOCALE_ARCHIVE LOCALE_ARCHIVE_2_27 NIX_LOCALE_ARCHIVE
+      fi
+
       [[ -o interactive ]] || return
 
       # DEPRECATED: Use wezterm.sh instead
@@ -36,6 +51,25 @@
       # FIXME the osc7 shell func produces output which p10k complains about
       # on startup (hence the WEZTERM_SHELL_SKIP_CWD)
       # WEZTERM_SHELL_SKIP_CWD=1 source ${pkgs.wezterm}/etc/profile.d/wezterm.sh
+    '';
+
+    "zsh/custom/os/not-nixos/nix.zsh".text = lib.mkAfter ''
+      [[ -r /etc/profile.d/nix.sh ]] || return
+      source /etc/profile.d/nix.sh &>/dev/null
+      (( $+commands[nix] )) || return
+
+      if [[ -f "$HOME/.local/state/nix/profiles/home-manager/home-path/etc/profile.d/hm-session-vars.sh" ]]; then
+        source "$HOME/.local/state/nix/profiles/home-manager/home-path/etc/profile.d/hm-session-vars.sh"
+      fi
+
+      if [[ -f /usr/lib/locale/locale-archive ]]; then
+        export LOCALE_ARCHIVE=/usr/lib/locale/locale-archive
+        export NIX_LOCALE_ARCHIVE=/usr/lib/locale/locale-archive
+        unset LOCPATH
+      elif [[ -d /usr/lib/locale ]]; then
+        export LOCPATH=/usr/lib/locale
+        unset LOCALE_ARCHIVE LOCALE_ARCHIVE_2_27 NIX_LOCALE_ARCHIVE
+      fi
     '';
 
     # completions
