2cbb6b16a4bcba3c7eac4eceb746073d9558284f
Philipp Schmitt <philipp@schmitt.co>
2026-04-21 14:07:47 +0200

    move fnuc nix file

---
+++ hosts/fnuc/default.nix
@@ -0,0 +1,17 @@
+{ config, ... }:
+{
+  imports = [
+    ../../modules/main-user.nix
+    ../../home-manager/server-base.nix
+    ../../home-manager/sops-standalone.nix
+  ];
+
+  sops.defaultSopsFile = ./secrets.sops.yaml;
+
+  targets.genericLinux.enable = true;
+
+  home = {
+    inherit (config.mainUser) username homeDirectory;
+    stateVersion = "25.11";
+  };
+}
