9608b78a3c889fe46c852499fce4a783b5a9f3ab
Philipp Schmitt <philipp@schmitt.co>
2026-04-21 14:32:52 +0200

    full hm on fnuc

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -1,40 +1,27 @@
 {
   lib,
-  pkgs,
   osConfig,
   ...
 }:
 {
   imports = lib.concatLists [
-    [
-      ./banking.nix
-      ./bitwarden.nix
-      ./cli
-      ./crypto.nix
-      ./devel
-      ./env.nix
-      ./flatpak.nix
-      ./mail.nix
-      ./network.nix
-      ./nix-index-database.nix
-      ./nrf.nix
-      # ./openclaw.nix
-      ./gpg.nix
-      ./sops.nix
-      ./ssh.nix
-      ./work
-      ./yadm.nix
-    ]
+    (
+      [
+        ./common.nix
+      ]
+      ++ import ./main-modules.nix { develModule = ./devel; }
+      ++ [
+        # ./openclaw.nix
+        ./gpg.nix
+        ./sops.nix
+        ./ssh.nix
+        ./work
+        ./yadm.nix
+      ]
+    )
     (lib.optional osConfig.hardware.bluetooth.enable ./bluetooth.nix)
     (lib.optional osConfig.services.xserver.enable ./gui)
   ];
-
-  programs.home-manager.enable = true;
-
-  systemd.user.startServices = "sd-switch";
-
-  home.preferXdgDirectories = true;
-  xdg.userDirs.setSessionVariables = true;
 
   home = {
     # The home.stateVersion option does not have a default and must be set
