19dfbcf20d0d041f1dd25a42c792b18e8a6d279f
Philipp Schmitt <philipp@schmitt.co>
2026-04-23 14:13:56 +0200

    Switch browser config from Chrome to Chromium

--- common/gui/browser.nix
+++ common/gui/browser.nix
@@ -1,22 +1,8 @@
 {
   inputs,
   pkgs,
-  lib,
   ...
 }:
-let
-  bruvtabPkg = inputs.bruvtab.packages.${pkgs.stdenv.hostPlatform.system}.default;
-  bruvtabChromeCrx = inputs.bruvtab.packages.${pkgs.stdenv.hostPlatform.system}.chromeCrx;
-  bruvtabChromeExtensionId = lib.removeSuffix "\n" (
-    builtins.readFile "${bruvtabChromeCrx}/extension-id"
-  );
-  bruvtabChromeExtensionJson = pkgs.writeText "${bruvtabChromeExtensionId}.json" (
-    builtins.toJSON {
-      external_crx = "${bruvtabChromeCrx}/bruvtab.crx";
-      external_version = bruvtabPkg.version;
-    }
-  );
-in
 {
 
   # NOTE see also home-manager/gui/browser.nix
@@ -45,30 +31,8 @@ in
 
   environment.systemPackages = with pkgs; [
     firefox
-    google-chrome
+    chromium
   ];
-
-  systemd.tmpfiles.rules = [
-    "d /usr/share/google-chrome/extensions 0755 root root - -"
-  ];
-
-  system.activationScripts.bruvtabChromeExtension = {
-    text = ''
-      install -d -m0755 /usr/share/google-chrome/extensions
-      for json in /usr/share/google-chrome/extensions/*.json
-      do
-        if [[ -f "$json" ]] && grep -Fq '/bruvtab.crx' "$json"
-        then
-          rm -f "$json"
-        fi
-      done
-      install -m0444 ${bruvtabChromeExtensionJson} \
-        /usr/share/google-chrome/extensions/${bruvtabChromeExtensionId}.json
-      rm -f /usr/share/google-chrome/extensions/external_extensions.json
-      rm -f /usr/share/google-chrome/extensions/${bruvtabChromeExtensionId}.crx
-      rm -f /opt/google/chrome/extensions/*.json
-    '';
-  };
 }
 
 # vim: set ft=nix et ts=2 sw=2 :
