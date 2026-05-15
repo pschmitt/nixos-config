f9f4dca4b9bbfcb1a0cf64ebef72b61a0342314f
Philipp Schmitt <philipp@schmitt.co>
2026-05-15 21:57:38 +0200

    explicitly use hyprlang config

--- home-manager/gui/hyprland/default.nix
+++ home-manager/gui/hyprland/default.nix
@@ -1,6 +1,12 @@
 {
-  # use the version provided by the nixpkgs module
-  wayland.windowManager.hyprland.package = null;
+
+  wayland.windowManager.hyprland = {
+    # use the version provided by the nixpkgs module
+    package = null;
+    # TODO switch to lua! see the hyprland-lua branch.
+    # current blockers: hypr-dynamic-cursors and hyprgrass
+    configType = "hyprlang";
+  };
 
   imports = [
     ./bin
