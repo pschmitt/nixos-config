cac83ef1a8979414a185574fb21e67344e46701b
Philipp Schmitt <philipp@schmitt.co>
2026-05-18 12:09:25 +0200

    add workstuff

--- home-manager/work/gui.nix
+++ home-manager/work/gui.nix
@@ -1,11 +1,18 @@
 {
   lib,
-  osConfig,
+  osConfig ? null,
   pkgs,
   ...
 }:
+let
+  hasXserver =
+    osConfig != null
+    && osConfig ? services
+    && osConfig.services ? xserver
+    && osConfig.services.xserver.enable;
+in
 {
-  home.packages = lib.optionals osConfig.services.xserver.enable [
+  home.packages = lib.optionals hasXserver [
     pkgs.onlyoffice-desktopeditors
     pkgs.thunderbird
   ];
