fa13f8d5bc5c20ea00a1183766a1a479f5d0aaf2
gh-brkn-lol <136078287+gh-brkn-lol@users.noreply.github.com>
2026-07-22 09:11:27 +0200

    chore: update native-client from 1.0.xx to 1.1.xx (#673)
    Co-authored-by: pschmitt <37886+pschmitt@users.noreply.github.com>

--- pkgs/native-client/default.nix
+++ pkgs/native-client/default.nix
@@ -9,13 +9,13 @@
 
 stdenv.mkDerivation rec {
   pname = "native-client";
-  version = "1.0.8";
+  version = "1.1.0";
 
   src = fetchFromGitHub {
     owner = "andy-portmen";
     repo = "native-client";
     rev = "v${version}";
-    hash = "sha256-/Zr5FSfZ5Sh1kE/x0wF0Uljg0mnE0QkO6etgopaIXmo=";
+    hash = "sha256-zqjorORcbLH5DGoalFM31DAbDqhZw/C/oRFHA0zDKMc=";
   };
 
   nativeBuildInputs = [
