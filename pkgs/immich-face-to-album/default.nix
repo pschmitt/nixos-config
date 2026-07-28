708202d9abf89b55e1bd9f7613938d2549105b62
pschmitt <37886+pschmitt@users.noreply.github.com>
2026-07-28 07:10:29 +0000

    chore: update immich-face-to-album from 1.0.xx to 1.0.xx

--- pkgs/immich-face-to-album/default.nix
+++ pkgs/immich-face-to-album/default.nix
@@ -6,13 +6,13 @@
 
 python3.pkgs.buildPythonApplication rec {
   pname = "immich-face-to-album";
-  version = "1.0.14";
+  version = "1.0.16";
   pyproject = true;
 
   src = fetchPypi {
     pname = "immich_face_to_album";
     inherit version;
-    hash = "sha256-gxXFqPgXD0CYiqx3di5MCxguYTZVEEih+BM5cIBZwNc=";
+    hash = "sha256-D6Fbll/hrSDJq/YmR1zEYtkDLHgS2JgSIPm91qaYRGs=";
   };
 
   build-system = [
