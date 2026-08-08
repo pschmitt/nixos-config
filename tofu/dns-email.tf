65dc0e8101c369d3d3317d23c436b40012aca33a
Philipp Schmitt <philipp@schmitt.co>
2026-08-08 10:01:25 +0200

    Hand Stalwart DNS records to provider

--- tofu/dns-email.tf
+++ tofu/dns-email.tf
@@ -1,36 +1,23 @@
 variable "domains" {
   type = map(object({
-    dkim_public_key  = optional(string, null)
-    dkim_public_keys = optional(map(string), {})
-    mx_provider      = string
-    dmarc_policy     = optional(string, null)
+    dkim_public_key = optional(string, null)
+    mx_provider     = string
+    dmarc_policy    = optional(string, null)
   }))
 
   validation {
-    condition     = alltrue([for domain in var.domains : contains(["custom", "google", "cloudflare"], domain.mx_provider)])
-    error_message = "The mx_provider must be one of 'custom', 'google', or 'cloudflare'."
+    condition     = alltrue([for domain in var.domains : contains(["custom", "google", "cloudflare", "stalwart"], domain.mx_provider)])
+    error_message = "The mx_provider must be one of 'custom', 'google', 'cloudflare', or 'stalwart'."
   }
 
   default = {
     "pschmitt.dev" = {
       dkim_public_key = "v=DKIM1; h=sha256; k=rsa; p=MIICIjANBgkqhkiG9w0BAQEFAAOCAg8AMIICCgKCAgEAz2Yz+Sxm+fvWjnQyTpMRJFUUBBjBYENNWB/+rlQ25J9RJxWwSyIhRJQdcGquXARoKf9WT31KgbxglL4eUcRjWvGRP6Kz9e7bvseT1y4HR4lx0/yCzt4BmzsdX0BBGUL/PvWLFr0p/8hC6KNGzDpHgSAWXS8hF1yIyAe5yMnVzSogJo+cJTc+nmcH8g+j86naXmtRKXxuL8GfM97dpVwflulfUPWMhAoeTUzpUU90t45B0tz7GOCRXM4unIY0ZJDnboXlSX92vFTiVRvzgm5eBE+qUkKBZKHTcNhxZizQGZAULVhGNs3fsP8jl7ni2Pt9fEoGIzmAfQc/FYmVQtYQ5JZhGgk8SjDP8D7KzjR4Eg0kQN+oRuhDLnGrXwyJFc5bWiGt8+a5Fiy2sNAzEn8iIeucan9HFrB1oeLnvIwQMQXXhNsqPQjwmC7/2CNbaENQFFXTmZSsdY0UPkc7jBdgvO3mmokehVtAm3rGJJi/DjNDMp9a42tvvHQHHNcbdmEO3xEUFxfLSGY0dMGlWm0wGzhw6Uu6DjG3Sc5QO3AHY3Q4L4BnEIZZABw+aC1yFKDpTUbeUAhsJirH3NUFOAK4OB4CNiV4RisTAkyt7xN2P4VuAivSUY5tW88Dp76BDuwAJNa5e6O8Gjr0Qi8KeKSr9/v6ee/M5tXq59nfs/nUSgMCAwEAAQ=="
-      dkim_public_keys = {
-        "v1-ed25519-20260808" = "v=DKIM1; k=ed25519; h=sha256; p=/mE3fic3nlRnBvdSSOZM+8pT7mYkbAZE28FtWO8nVK4="
-        "v1-rsa-20260808"     = "v=DKIM1; k=rsa; h=sha256; p=MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAxocq+NddgtZlE2ohH9yWK7d9+WMnFVsPQokACfWUTNAMClrBCVsEauDfOyEfc1SsYzIs2cTEVh522EsjWcgncEw4KR+bpHrRy+NdByFe4ghw0Nwl4nCAvgrMTfzsO3zgRaPTohq0RFYcLkwzKVdsfWGJ41zKGOyElVxwDxvqpO8fwjphe4SHQ09qrs51wYPQSayNpSuv8AyLOotkh7OeMftqing55GqzY2CKr2NoGZqKDxT9Tf+72uG1oxwxRcYNpyZKOqS3lGbNLNgj9q4OomOo4O/9Xxhpay4TLND7zWwy5l35uMs8GiWNffiWYbpLjbuWfwCIt/lb18pFhbvMrwIDAQAB"
-        "v2-ed25519-20260808" = "v=DKIM1; k=ed25519; h=sha256; p=0xZypMbHvRyU3x6AjquYIemqUO3kd4N7sKMYEJ/Ubsg="
-        "v2-rsa-20260808"     = "v=DKIM1; k=rsa; h=sha256; p=MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAvzh48hlws4BA3kc0l+ZTQoZIj9V5kWlBP3qd6P07uE0p27BkdohIIzx7C2MPiS9YQ5+n/3mvBZfUvWb5j42zyH4RhQs/yoCYazNFUI/KmtQAN/VMPzm+4WbPspPAzAd86xoVguZ/HvTD18wTArV5WcE8pi+0bQcddAk2KAAEWNqoCRJ0aKgSaGFWSc8ivIyS4J1FkHVg7ooETnPf9YLivZafM9mQJ4zKs1bpmY4/JGVwYSkUrR7+0b4J6nfLZqDkW/Gb27Qr3x/QjigZngNEofIaLnWpR138V0Hm9acV+bYj0Y3tKu2AyX6vTCkb4Ql8FsGzgdqTHoMRLRjxeU20GQIDAQAB"
-      }
-      mx_provider = "custom"
+      mx_provider     = "stalwart"
     },
     "brkn.lol" = {
       dkim_public_key = "v=DKIM1; h=sha256; k=rsa; p=MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAzYK3JqwjHHIhk9OxYBE/FHIZxb7SkzZQLMx8XboOtoEAMsNJ99iKJ0mvywtadIt1giywb/q5xaFFqjsByWxA3vBIdqxTXSjNtTqV93qfq8Lw5YTwk7BGXZoVmMblg9aYOJRkF4LhH0uxOURnQz+Lhoj/cz4bnehfHEE/qii1cKXUhW4wTN4HypC22A3IVcNf3z+eTOcu8KPljpKARka7r0gmgPiunLpppafgbSZKtnycIvLdyTu45AD3+aTi836aZsX0vTty9wTl2JyB1buSpiq54IJyma8YSiwphCzBo4PI9OCXlIHTcuRLcU0id99BN6q3A4kE/Q85ks6DPv0dUQIDAQAB"
-      dkim_public_keys = {
-        "v1-ed25519-20260807" = "v=DKIM1; k=ed25519; h=sha256; p=9NVXkXEX/LfQKcRQPBKjXnJTcrQLeRgzgOUlwFA5slo="
-        "v1-rsa-20260807"     = "v=DKIM1; k=rsa; h=sha256; p=MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAzg2iS29qi0ase2Qe3mV6lnYGTRS2PBMj4EOqrHcqyXG/R8LRHkrqDKiB1P3A4jp6m4s4AYGwloa+3sMFMwwpJoEB9DTHadHWUewd2K4bq1P0+ccL/T63WPLAH63qG8q7rgutxyAoFEp8n6Hh86ExYEmlu6w6VUMr34niHHzlJesJ/HDv1czWSmRf2qRSVlUuliS+noWENGxwPmRxXy7W23jLE6sjzhzJuJNSVtXm5SpFCFxHGtI29uuJFJeZ3DWQEaetVrVG+5wnvSlqZ5bXF5WiwpYy/86WMX3J9zCNHu1wautRIF1/dGhKZYjZ7uO+BotzQ/nNQ8ScAx5XAVYDwwIDAQAB"
-        "v2-ed25519-20260808" = "v=DKIM1; k=ed25519; h=sha256; p=28FNw8V0ONAveiTWIswyILrgDCTExvKc566MqHHLArE="
-        "v2-rsa-20260808"     = "v=DKIM1; k=rsa; h=sha256; p=MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA20xcjKfcc/s19Ku2GV/NtuXEIq2leqmpaqxVYbajBXX8OhjQlTJrJaiLYh/sXGs32KcjTaMAy8u1mz3/nzmH+ZH+S6b/FCEae9VK2KgtTTUgu5Jolffpov+MSdX4VaDMLQKEZjwZhl14wNS3s2d2pSGpcP/DJwz0mJRViU9ljTK/0nKebV/ZCsUOsVqKmPsjQFlm8bM4rvbt0d5zrTWcbGlARf1fqptGuAvbtB1N7/LVhp1WGABB83KMrJ444D56FpxB1rBCQ0lCxSRJlYcMEfTzBL0flt+1RFH/+mfN5mjZA+eYlJZtXdpPASI84Oa6ZtkaQkB/pdfU1uKULiGyBwIDAQAB"
-      }
-      mx_provider = "custom"
+      mx_provider     = "stalwart"
     },
     "schmi.tt" = {
       mx_provider = "cloudflare"
@@ -96,7 +83,7 @@ resource "cloudflare_email_routing_settings" "cf_mail_routing" {
 
 resource "cloudflare_dns_record" "mx" {
   for_each = {
-    for domain, config in var.domains : domain => config if config.mx_provider != "cloudflare"
+    for domain, config in var.domains : domain => config if !contains(["cloudflare", "stalwart"], config.mx_provider)
   }
 
   zone_id = data.cloudflare_zone.zones[each.key].id
@@ -114,7 +101,7 @@ resource "cloudflare_dns_record" "mx" {
 
 resource "cloudflare_dns_record" "mail" {
   for_each = {
-    for domain, config in var.domains : domain => config if config.mx_provider == "custom"
+    for domain, config in var.domains : domain => config if contains(["custom", "stalwart"], config.mx_provider)
   }
 
   zone_id = data.cloudflare_zone.zones[each.key].id
@@ -140,7 +127,7 @@ resource "cloudflare_dns_record" "mail_google" {
 
 resource "cloudflare_dns_record" "spf" {
   for_each = {
-    for domain, config in var.domains : domain => config if config.mx_provider != "cloudflare"
+    for domain, config in var.domains : domain => config if !contains(["cloudflare", "stalwart"], config.mx_provider)
   }
 
   zone_id = data.cloudflare_zone.zones[each.key].id
@@ -158,7 +145,7 @@ resource "cloudflare_dns_record" "spf" {
 resource "cloudflare_dns_record" "dmarc" {
   # for_each = data.cloudflare_zone.zones
   for_each = {
-    for domain, config in var.domains : domain => config if config.mx_provider != "cloudflare"
+    for domain, config in var.domains : domain => config if !contains(["cloudflare", "stalwart"], config.mx_provider)
   }
 
   zone_id = data.cloudflare_zone.zones[each.key].id
@@ -195,32 +182,6 @@ resource "cloudflare_dns_record" "dkim" {
   comment = var.dns_email_comment
 }
 
-locals {
-  stalwart_dkim_records = flatten([
-    for domain, config in var.domains : [
-      for selector, public_key in config.dkim_public_keys : {
-        key     = "${domain}/${selector}"
-        zone_id = data.cloudflare_zone.zones[domain].id
-        name    = "${selector}._domainkey"
-        content = public_key
-      }
-    ]
-  ])
-}
-
-resource "cloudflare_dns_record" "dkim_stalwart" {
-  for_each = {
-    for record in local.stalwart_dkim_records : record.key => record
-  }
-
-  zone_id = each.value.zone_id
-  type    = "TXT"
-  name    = each.value.name
-  content = each.value.content
-  ttl     = 3600
-  comment = var.dns_email_comment
-}
-
 resource "cloudflare_dns_record" "dkim_google" {
   for_each = {
     for domain, config in var.domains : domain => config if config.mx_provider == "google" && config.dkim_public_key != null
@@ -249,7 +210,7 @@ resource "cloudflare_dns_record" "mailconf" {
 
 resource "cloudflare_dns_record" "autoconfig" {
   for_each = {
-    for domain, config in var.domains : domain => config if config.mx_provider != "cloudflare"
+    for domain, config in var.domains : domain => config if !contains(["cloudflare", "stalwart"], config.mx_provider)
   }
 
   zone_id = data.cloudflare_zone.zones[each.key].id
@@ -324,7 +285,7 @@ resource "cloudflare_dns_record" "srv-imap" { # starttls
 
 resource "cloudflare_dns_record" "srv-imaps" {
   for_each = {
-    for domain, config in var.domains : domain => config if config.mx_provider != "cloudflare"
+    for domain, config in var.domains : domain => config if !contains(["cloudflare", "stalwart"], config.mx_provider)
   }
 
   zone_id = data.cloudflare_zone.zones[each.key].id
@@ -391,7 +352,7 @@ resource "cloudflare_dns_record" "srv-submission" { # starttls
 
 resource "cloudflare_dns_record" "srv-submissions" {
   for_each = {
-    for domain, config in var.domains : domain => config if config.mx_provider != "cloudflare"
+    for domain, config in var.domains : domain => config if !contains(["cloudflare", "stalwart"], config.mx_provider)
   }
 
   zone_id = data.cloudflare_zone.zones[each.key].id
