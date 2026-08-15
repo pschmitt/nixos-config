aa12a8ba98a71012363540efb6ac87e08e6a02cd
Philipp Schmitt <philipp@schmitt.co>
2026-08-15 17:36:43 +0200

    home-manager: install Shelly BLE RPC CLI

--- home-manager/devel/iot.nix
+++ home-manager/devel/iot.nix
@@ -1,8 +1,10 @@
-{ pkgs, ... }:
+{ inputs, pkgs, ... }:
 {
   home.packages = with pkgs; [
     home-assistant-cli
     mosquitto
     shellyctl
+
+    inputs.shelly-ble-rpc.packages.${pkgs.stdenv.hostPlatform.system}.default
   ];
 }
