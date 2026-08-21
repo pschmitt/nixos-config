59a43c168a9dff7ef4f39c73b46fe6295176e181
gh-brkn-lol <136078287+gh-brkn-lol@users.noreply.github.com>
2026-08-21 17:34:37 +0200

    chore: update todoist-cli from 3.2.xx to 3.2.xx (#758)
    Co-authored-by: pschmitt <37886+pschmitt@users.noreply.github.com>

--- pkgs/todoist-cli/default.nix
+++ pkgs/todoist-cli/default.nix
@@ -10,16 +10,16 @@
 
 buildNpmPackage (finalAttrs: {
   pname = "todoist-cli";
-  version = "3.2.1";
+  version = "3.2.3";
 
   src = fetchFromGitHub {
     owner = "Doist";
     repo = finalAttrs.pname;
     rev = "v${finalAttrs.version}";
-    hash = "sha256-tVgeKHWakydkflgttV/faLCL9S05HQ6Pc7E79qYVtAY=";
+    hash = "sha256-7c1gsvtkYLBzgutE2QPGFs31rl9p5/AVZwf45tP0yZ0=";
   };
 
-  npmDepsHash = "sha256-R3BZSJ/tKpyObpI8x9oYjgecgF9n5m0aUWSLa1IVGxk=";
+  npmDepsHash = "sha256-Ecg65k289KE2X6R4iXmkmdLEW4vl8h+DRTeisbzpLh0=";
 
   doCheck = false;
 
