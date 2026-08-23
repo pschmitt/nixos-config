93b3d47cc3ca7c47d270ef9e931b2cc48e880f15
Philipp Schmitt <philipp@schmitt.co>
2026-08-23 14:37:46 +0200

    netbox: try 4.6 nixpkgs package

--- services/netbox.nix
+++ services/netbox.nix
@@ -53,7 +53,7 @@ in
   services = {
     netbox = {
       enable = true;
-      package = pkgs.master.netbox;
+      package = pkgs.netbox.netbox_4_6;
       bind = netboxBind;
       dataDir = "/mnt/data/srv/netbox";
       secretKeyFile = config.sops.secrets."netbox/secretKey".path;
@@ -66,17 +66,7 @@ in
           '';
         }))
         ps.netbox-interface-synchronization
-        # Pinned to 0.0.20: 0.0.21 requires NetBox >= 4.6, we're still on 4.5.9
-        # https://github.com/NixOS/nixpkgs/pull/530025
-        (ps.netbox-qrcode.overrideAttrs (old: {
-          version = "0.0.20";
-          src = pkgs.fetchFromGitHub {
-            owner = "netbox-community";
-            repo = "netbox-qrcode";
-            tag = "v0.0.20";
-            hash = "sha256-7dPMpuJ2nuj9rRmVrfthD+xrEHoUaLFqDJWC6cGGCwY=";
-          };
-        }))
+        ps.netbox-qrcode
         ps.netbox-topology-views
       ];
       settings = {
