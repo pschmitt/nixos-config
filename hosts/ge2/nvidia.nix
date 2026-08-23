e1000e78c7090534e25260a058bd70b80a5886ae
Philipp Schmitt <philipp@schmitt.co>
2026-08-23 12:10:53 +0200

    linux 7.2

--- hosts/ge2/nvidia.nix
+++ hosts/ge2/nvidia.nix
@@ -12,7 +12,7 @@
   ];
 
   # Newer kernels might not be compatible with the Nvidia crap.
-  boot.kernelPackages = lib.mkForce pkgs.linuxPackages_7_1;
+  boot.kernelPackages = lib.mkForce pkgs.linuxPackages_7_2;
 
   # boot.kernelPackages = lib.mkForce pkgs.linuxPackages_zen;
 
