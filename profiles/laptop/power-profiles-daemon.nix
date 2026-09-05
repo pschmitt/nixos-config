f9e46b9d5a56b008c90d68dd23875fd95f80ef3a
Philipp Schmitt <philipp@schmitt.co>
2026-09-05 23:43:56 +0200

    laptop: silence power profile watcher notifications

--- profiles/laptop/power-profiles-daemon.nix
+++ profiles/laptop/power-profiles-daemon.nix
@@ -32,8 +32,6 @@ in
       "power-profiles-daemon.service"
     ];
     wants = [ "power-profiles-daemon.service" ];
-    environment.NOTIFY = "1";
-
     serviceConfig = {
       ExecStart = "${ppdReact}/bin/ppd-react";
       Restart = "always";
