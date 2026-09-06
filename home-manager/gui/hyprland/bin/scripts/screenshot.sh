f83e964c0e6150f170248051ab7fbcfe7edcdc0f
Philipp Schmitt <philipp@schmitt.co>
2026-09-06 14:07:38 +0200

    screenshot: copy raw capture to clipboard and update after editing

--- home-manager/gui/hyprland/bin/scripts/screenshot.sh
+++ home-manager/gui/hyprland/bin/scripts/screenshot.sh
@@ -4,5 +4,10 @@ if [[ "${BASH_SOURCE[0]}" == "${0}" ]]
 then
   killall slurp still # swappy
 
-  still -c 'slurp | grim -g- -' | swappy -f -
+  tmp=$(mktemp --suffix=.png)
+  trap 'rm -f "${tmp}"' EXIT
+
+  still -c 'slurp | grim -g- -' | tee >(wl-copy) > "${tmp}"
+  swappy -f "${tmp}" -o "${tmp}"
+  wl-copy < "${tmp}"
 fi
