49c2b419f56c069eac1354b7b9083f35a84a5577
Philipp Schmitt <philipp@schmitt.co>
2026-09-22 14:51:59 +0200

    enable ssh-clipboard service on lrz

--- hosts/lrz/user-services.nix
+++ hosts/lrz/user-services.nix
@@ -3,8 +3,13 @@
   # FNUC-013: keep fnuc's automation authoritative during staging. In particular,
   # do not import hosts/fnuc/default.nix or copy its home/service identities.
   home-manager.users.${config.mainUser.username} = {
+    imports = [
+      ../../home-manager/ssh-clipboard-peers.nix
+    ];
+
     services = {
       jcalapi.enable = lib.mkForce false;
+      ssh-clipboard.headlessX11 = true;
       home-manager.autoUpgrade.enable = lib.mkForce false;
     };
   };
